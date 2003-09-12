3ae57203fe88f497feed259e9ff068ae9d49fe26
James Strachan <jastrachan@mac.com>
2003-09-12 10:42:44 +0000

    added more details to failing field test
    git-svn-id: http://svn.codehaus.org/groovy/trunk@74 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/notworking/LocalFieldWithNoFieldDefinitionTest.groovy
+++ groovy/groovy-core/src/test/groovy/notworking/LocalFieldWithNoFieldDefinitionTest.groovy
@@ -4,12 +4,6 @@ import org.codehaus.groovy.GroovyTestCase;
 
 class LocalFieldTest extends GroovyTestCase {
 
-	// lets define some fields - no necessary for instance variables but supported
-    private x;
-    private String y;
-    private static z;
-    private static Integer iz;
-	
 	void testAssert() {
         this.x = "abc";
 	    
