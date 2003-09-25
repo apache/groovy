be392dec8f159266dca561f28a18bdf6f1c01194
James Strachan <jastrachan@mac.com>
2003-09-25 18:14:44 +0000

    added an initial implementation of GroovyLog for logging in Groovy applications
    git-svn-id: http://svn.codehaus.org/groovy/trunk@148 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/test/groovy/LogTest.groovy
@@ -0,0 +1,29 @@
+package groovy;
+
+import org.codehaus.groovy.GroovyTestCase;
+import org.codehaus.groovy.lang.GroovyLog;
+
+/** 
+ * Tests the use of GroovyLog
+ * 
+ * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
+ * @version $Revision$
+ */
+class LogTest extends GroovyTestCase {
+
+	void testUseLog() {
+	    file = "something.txt"
+	    
+        log = GroovyLog.newInstance(getClass())
+        
+        log.starting("Hey I'm starting up...");
+        
+	    log.openFile("Am about to open file ${file}")
+	    
+	    // ...
+	    
+	    log.closeFile("Have closed the file ${file}")
+
+        log.stopping("..Finished");
+	}
+}
