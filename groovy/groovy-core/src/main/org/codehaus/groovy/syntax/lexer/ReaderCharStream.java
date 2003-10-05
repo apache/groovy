2e33db3943dc5ef9edd55020da0e6d50231d4067
Boc McWhirter <bob@codehaus.org>
2003-10-05 23:53:50 +0000

    Added ReaderCharStream for aslak.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@174 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/main/org/codehaus/groovy/syntax/lexer/ReaderCharStream.java
@@ -0,0 +1,39 @@
+package org.codehaus.groovy.syntax.lexer;
+
+import java.io.Reader;
+import java.io.IOException;
+
+public class ReaderCharStream
+    extends AbstractCharStream
+{
+    private Reader in;
+
+    public ReaderCharStream(Reader in)
+    {
+        this.in = in;
+    }
+
+    public ReaderCharStream(Reader in,
+                            String description)
+    {
+        super( description );
+        this.in = in;
+    }
+
+    public Reader getReader()
+    {
+        return in;
+    }
+
+    protected char nextChar()
+        throws IOException
+    {
+        return (char) getReader().read();
+    }
+
+    public void close()
+        throws IOException
+    {
+        getReader().close();
+    }
+}
