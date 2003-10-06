62b45cc40e7662b76107f68fd650db3b503ed415
James Strachan <jastrachan@mac.com>
2003-10-06 04:52:23 +0000

    Deleted some old cruft
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@177 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/LogMixin.groovy
+++
@@ -1,9 +0,0 @@
-package groovy; 
- 
-import org.apache.commons.logging.LogFactory; 
- 
-/** A simple mixin to add logging to a class using an instance variable */
-class StaticLogMixin { 
-	// lets make a logger available 
-	static log = LogFactory.getLog(thisClass); 
-} 
