3a0cf2ac8b033e59d6fd1973b2d0f480c1dfb233
James Strachan <jastrachan@mac.com>
2003-10-07 15:47:35 +0000

    Added an AntBuilder which scripts Ant tasks from inside a Groovy script.
    Pretty neat - though I'm sure there must be a simpler way of invoking Ant tasks
    
    
    git-svn-id: http://svn.codehaus.org/groovy/trunk@190 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/test/groovy/ant/AntTest.groovy
@@ -0,0 +1,31 @@
+package groovy.ant
+
+import java.io.File
+
+class AntTest extends GroovyTestCase {
+    
+    void testAnt() {
+        ant = new AntBuilder()
+        
+        ant.echo('hello')
+        
+        /** @todo change to concise syntax when its available */
+        ant.sequential() {|
+			echo('inside sequential')
+			
+			myDir = 'src/target/dummy'
+			
+			mkdir(['dir':myDir])
+			copy(['todir':myDir]) {|
+			    fileset(['dir':'src/test']) {|
+			        include(['name':'**/*.groovy'])
+			    }
+			}
+
+            echo('done')
+        }
+        
+        file = new File('src/target/dummy/groovy/ant/AntTest.groovy')
+        assert file.exists()
+    }
+}
