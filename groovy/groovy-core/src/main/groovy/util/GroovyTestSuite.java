ddc280b486f299455f56b551d6cfc135bc7cc493
James Strachan <jastrachan@mac.com>
2003-10-30 14:57:40 +0000

    Added a GroovyTestSuite which is a helper method for running Groovy unit test cases from inside an IDE which doesn't support Groovy. It just looks like a normal Java test suite which compiles a given Groovy script and uses that as the test suite
    git-svn-id: http://svn.codehaus.org/groovy/trunk@234 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/groovy/util/GroovyTestSuite.java
+++ groovy/groovy-core/src/main/groovy/util/GroovyTestSuite.java
@@ -1,51 +1,44 @@
 /*
- $Id$
-
- Copyright 2003 (C) James Strachan and Bob Mcwhirter. All Rights Reserved.
-
- Redistribution and use of this software and associated documentation
- ("Software"), with or without modification, are permitted provided
- that the following conditions are met:
-
- 1. Redistributions of source code must retain copyright
-    statements and notices.  Redistributions must also contain a
-    copy of this document.
-
- 2. Redistributions in binary form must reproduce the
-    above copyright notice, this list of conditions and the
-    following disclaimer in the documentation and/or other
-    materials provided with the distribution.
-
- 3. The name "groovy" must not be used to endorse or promote
-    products derived from this Software without prior written
-    permission of The Codehaus.  For written permission,
-    please contact info@codehaus.org.
-
- 4. Products derived from this Software may not be called "groovy"
-    nor may "groovy" appear in their names without prior written
-    permission of The Codehaus. "groovy" is a registered
-    trademark of The Codehaus.
-
- 5. Due credit should be given to The Codehaus -
-    http://groovy.codehaus.org/
-
- THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS
- ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
- NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- OF THE POSSIBILITY OF SUCH DAMAGE.
-
+ * $Id$
+ * 
+ * Copyright 2003 (C) James Strachan and Bob Mcwhirter. All Rights Reserved.
+ * 
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ *  1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *  3. The name "groovy" must not be used to endorse or promote products
+ * derived from this Software without prior written permission of The Codehaus.
+ * For written permission, please contact info@codehaus.org.
+ *  4. Products derived from this Software may not be called "groovy" nor may
+ * "groovy" appear in their names without prior written permission of The
+ * Codehaus. "groovy" is a registered trademark of The Codehaus.
+ *  5. Due credit should be given to The Codehaus - http://groovy.codehaus.org/
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *  
  */
 package groovy.util;
 
+import junit.framework.Test;
 import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import org.codehaus.groovy.classgen.GroovyClassLoader;
 
 /**
  * A TestSuite which will run a Groovy unit test case inside any Java IDE.
@@ -54,6 +47,40 @@ import junit.framework.TestSuite;
  * @version $Revision$
  */
 public class GroovyTestSuite extends TestSuite {
+    
+    protected static String file = "src/test/groovy/PrintTest.groovy";
+    
+    protected GroovyClassLoader loader = new GroovyClassLoader(GroovyTestSuite.class.getClassLoader());
 
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            file = args[0];
+        }
+        TestRunner.run( suite() );
+    }
+    
+    public static Test suite() {
+        GroovyTestSuite suite = new GroovyTestSuite();
+        try {
+            suite.loadTestSuite();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Could not create the test suite: " + e, e);
+        }
+        return suite;
+    }
 
+    public void loadTestSuite() throws Exception {
+        String fileName = System.getProperty("testScript", file);
+        if (fileName == null) {
+            throw new RuntimeException("No filename given so cannot run a Groovy unit test");
+        }
+        System.out.println("Compiling: " + fileName);
+        Class type = compile(fileName);
+        addTestSuite(type);
+    }
+    
+    public Class compile(String fileName) throws Exception {
+        return loader.parseClass(fileName);
+    }
 }
