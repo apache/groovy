68643220d3b5479efcd286d6374f3b62e43cf4cf
James Strachan <jastrachan@mac.com>
2003-11-03 08:49:52 +0000

    Added a unit test which runs all the sample scripts as a test.
    Also added experimental shell script in src/script which should work once the parser handles #
    
    
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@253 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/script/ScriptTest.groovy
@@ -0,0 +1,36 @@
+import java.io.File
+
+class ScriptTest extends GroovyTestCase {
+    void testScripts() {
+        file = new File("src/test/groovy/script")
+        file.eachFile {
+            name = it.name
+            if (name.endsWith('.groovy')) {
+                if (name.startsWith('ScriptTest')) {
+                    //
+                }
+				else {                        
+                	runScript(it) 
+				}
+            } 
+        }
+    }
+    
+    protected runScript(file) {
+        println("Running script: " + file)
+        
+        shell = new GroovyShell()
+        args = ['a', 'b', 'c']
+        
+        shell.run(file, args)
+
+        /** @todo this doesn't work when ran in an IDE?
+        try {
+	        shell.run(file, args)
+        } 
+        catch (Exception e) {
+            println("Caught: " + e)
+        }
+        */
+    }
+}
