d72c04f0274470752a8ce6ec44cf6f41c71faa41
Sam Pullara <sam@sampullara.com>
2003-11-22 23:42:20 +0000

    Tidying up the imports, should have gotten this the first time.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@314 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/org/codehaus/groovy/classgen/ClassGenerator.java
+++ groovy/groovy-core/src/main/org/codehaus/groovy/classgen/ClassGenerator.java
@@ -44,8 +44,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.ConstructorNode;
