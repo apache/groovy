dc086938040a4414d62676b50635861fa39bb4e8
Sam Pullara <sam@sampullara.com>
2003-11-23 23:21:19 +0000

    Groovy-59 NEW FEATURE: Heredocs/verbatim/reports whatever you want to call them are implemented.  The syntax is <<<TOKEN .....\nTOKEN  They act just like double quoted GStrings so you can use them for large template text quite easily:
    println(<<<EOS
    Hello there!
    EOS)
    
    is exactly the same as
    println("Hello there!")
    
    Notice there are no trailing or prefixed new lines.  You can see other examples in VerbatimGStringTest.groovy.
    
    
    git-svn-id: http://svn.codehaus.org/groovy/trunk@319 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/test/groovy/VerbatimGStringTest.groovy
@@ -0,0 +1,78 @@
+class VerbatimGStringTest extends GroovyTestCase {
+
+    void testWithOneVariable() {
+        
+        name = <<<EOF
+Bob
+EOF
+        
+        template = <<<EOF
+hello ${name} how are you?
+EOF
+				
+		assert template instanceof GString
+											 
+	 	count = template.getValueCount()
+		assert count == 1
+		assert template.getValue(0) == "Bob"
+											 
+		string = template.toString()
+		assert string == "hello Bob how are you?"
+	}
+    
+    void testWithVariableAtEnd() {
+        name = <<<EOS
+Bob
+EOS
+        template = <<<EOS
+hello ${name}
+EOS
+
+        string = template.toString()
+        
+        assert string == "hello Bob"
+    }
+    
+    void testWithVariableAtBeginning() {
+        name = <<<EOS
+Bob
+EOS
+
+        template = <<<EOS
+${name} hey,
+hello
+EOS
+        string = template.toString()
+        
+        assert string == "Bob hey,\nhello"
+    }
+
+    void testWithJustVariable() {
+        name = <<<EOS
+Bob
+EOS
+
+        template = <<<EOS
+${name}
+EOS
+        string = template.toString()
+        
+        assert string == "Bob"
+    }
+
+	void testInterestingCases() {
+		name = <<<EOSEOSEOS
+Bob
+EOS
+EOSEOSEO
+EOSEOSEOS
+
+		assert name == "Bob\nEOS\nEOSEOSEO"
+		
+		perl = <<<__END__
+Sam
+__END__
+
+		assert perl == "Sam"
+	}
+}
