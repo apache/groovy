8725e3e7ddc59020d26741095c32e6a67ebcf49c
James Strachan <jastrachan@mac.com>
2003-12-01 19:35:14 +0000

    Early cut of trying to add 'switch' support to the parser. Its not quite there but its close.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@334 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/ToArrayBug.groovy
@@ -0,0 +1,15 @@
+import java.util.Arrays
+
+class ToArrayBug extends GroovyTestCase {
+    
+    void testToArrayBug() {
+        list = [1, 2, 3, 4]
+        array = list.toArray()
+
+        assert array != null
+        list2 = Arrays.asList(array)
+        
+        assert list2.size() == 4
+        assert list2 == list
+    }
+}
