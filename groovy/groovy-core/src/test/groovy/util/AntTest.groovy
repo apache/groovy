f116e3940cebb8a00fdaf65852a7650d2e361cb8
James Strachan <jastrachan@mac.com>
2003-12-10 06:17:10 +0000

    used double quotes in test case as it looks nicer in viewcvs :)
    git-svn-id: http://svn.codehaus.org/groovy/trunk@382 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/util/AntTest.groovy
+++ groovy/groovy-core/src/test/groovy/util/AntTest.groovy
@@ -8,26 +8,26 @@ class AntTest extends GroovyTestCase {
         ant = new AntBuilder()
 
         // lets just call one task
-        ant.echo('hello')
+        ant.echo("hello")
         
-        // here's an example of a block of Ant inside GroovyMarkup
+        // here"s an example of a block of Ant inside GroovyMarkup
         ant.sequential {
-			echo('inside sequential')
-			
-			myDir = 'target/AntTest/'
-			
-			mkdir(dir:myDir) 
-			copy(todir:myDir) {
-			    fileset(dir:'src/test') {
-			        include(name:'**/*.groovy')
-			    }
-			}
-			
-            echo('done')
+            echo("inside sequential")
+            
+            myDir = "target/AntTest/"
+            
+            mkdir(dir:myDir) 
+            copy(todir:myDir) {
+                fileset(dir:"src/test") {
+                    include(name:"**/*.groovy")
+                }
+            }
+            
+            echo("done")
         }
         
         // now lets do some normal Groovy again
-        file = new File('target/AntTest/groovy/util/AntTest.groovy')
+        file = new File("target/AntTest/groovy/util/AntTest.groovy")
         assert file.exists()
     }
 }
