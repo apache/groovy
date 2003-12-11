2e6b3c54e1be2eadc55706b56f99c7a1012010cb
James Strachan <jastrachan@mac.com>
2003-12-11 15:42:09 +0000

    Added Jez's test case for GROOVY-67 which is now fixed
    git-svn-id: http://svn.codehaus.org/groovy/trunk@391 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/test/groovy/NullPropertyTest.groovy
@@ -0,0 +1,12 @@
+/**
+ * @author Jeremy Rayner 
+ */
+class NullPropertyTest extends GroovyTestCase { 
+    wensleydale = null 
+               
+    void testNullProperty() { 
+        assert wensleydale == null 
+    } 
+} 
+
+
