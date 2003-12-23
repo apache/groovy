19f478e11a1ae0cb849f3e3db3b31db15b2e5116
James Strachan <jastrachan@mac.com>
2003-12-23 14:09:06 +0000

    added a test case to represent Guillames bug which generates a parser error for me
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@457 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/GuillamesBug.groovy
@@ -0,0 +1,12 @@
+/**
+ * @author Guillaume Laforge
+ * @version $Revision$
+ */
+class GuillamesBug extends GroovyTestCase {
+    
+    void testBug() {
+        /* @todo creates a parser exception
+        if (true) println("true")
+        */
+    }
+}
