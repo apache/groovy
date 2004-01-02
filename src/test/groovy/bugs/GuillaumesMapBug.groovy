0f5c73d85d5f6a7126b5083dcc4a78f5b219c44a
James Strachan <jastrachan@mac.com>
2004-01-02 16:19:00 +0000

    added new broken tests (commented out for now) which need to be fixed ASAP
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@491 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/GuillaumesMapBug.groovy
+++ src/test/groovy/bugs/GuillaumesMapBug.groovy
@@ -8,27 +8,30 @@ class GuillaumesMapBug extends GroovyTestCase {
         list = [1, 2, 3]
         map = [:]
         
-        /*
         doLoop(list, map)
     
         assert map[0] == 1 
         assert map[1] == 2 
         assert map[2] == 3 
-        */
     }
     
     void doLoop(list, map) {
         i = 0
-        /*
         for (it in list) {
-            map[i] = it 
+            set(map, i++, it)
+            
+            /** @todo fix this bug
+            //map[i] = it
             //i++
+             */
         }
-        */
-        
     }
     
-    /*
+    void set(map, i, it) {
+        map[i] = it
+    }
+    
+    /** @todo fix this bug
     void testBug2() {
         i = 0
         list = [1, 2, 3]
