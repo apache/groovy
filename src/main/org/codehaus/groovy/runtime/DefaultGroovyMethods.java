500c0a0a31d99091676e50b37c4c1e34d7082f5a
James Strachan <jastrachan@mac.com>
2004-01-08 11:40:37 +0000

    whoops, fixed typeo
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@557 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -572,7 +572,7 @@ public class DefaultGroovyMethods {
      * @param text
      * @return the Character object at the given index
      */
-    public static Object get(CharSequence text, int index) {
+    public static CharSequence get(CharSequence text, int index) {
         index = normaliseIndex(index, text.length());
         return text.subSequence(index, index + 1);
     }
@@ -580,7 +580,7 @@ public class DefaultGroovyMethods {
     /**
      * Support the range subscript operator for String
      */
-    public static Object get(CharSequence text, Range range) {
+    public static CharSequence get(CharSequence text, Range range) {
         int from = normaliseIndex(InvokerHelper.asInt(range.getFrom()), text.length());
         int to = normaliseIndex(InvokerHelper.asInt(range.getTo()), text.length());
         int length = text.length();
@@ -677,11 +677,11 @@ public class DefaultGroovyMethods {
     }
 
     /**
-     * Allows a List to be used as the indices to be used on a String
+     * Allows a List to be used as the indices to be used on a CharSequence
      * 
      * @returns a String of the values at the given indices
      */
-    public static String get(String self, Collection indices) {
+    public static CharSequence get(CharSequence self, Collection indices) {
         StringBuffer answer = new StringBuffer();
         for (Iterator iter = indices.iterator(); iter.hasNext();) {
             Object value = iter.next();
