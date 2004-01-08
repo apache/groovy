9bc67bcd6ab394c12e96fd7e05cfa2dc8c96ec4c
James Strachan <jastrachan@mac.com>
2004-01-08 14:18:52 +0000

    added test case which didn't reproduce Rods bug
    git-svn-id: http://svn.codehaus.org/groovy/trunk@561 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/test/groovy/bugs/UseStaticInClosureBug.groovy
@@ -0,0 +1,14 @@
+/**
+ * @version $Revision$
+ */
+class UseStaticInClosureBug extends GroovyTestCase {
+
+    static stuff = [:]
+    
+    void testBug() {
+        [1,2,3].each { stuff[it] = "dog" }
+        
+        assert stuff.size() == 3
+        assert stuff[2] == "dog"
+    }
+}
