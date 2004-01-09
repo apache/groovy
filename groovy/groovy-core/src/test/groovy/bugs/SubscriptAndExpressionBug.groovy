c49a4cf6ee3bdde993a8239f78dca1a26c342f05
James Strachan <jastrachan@mac.com>
2004-01-09 13:33:01 +0000

    Fixed a few bugs related to using subscript operators on the LHS
    git-svn-id: http://svn.codehaus.org/groovy/trunk@577 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/bugs/SubscriptAndExpressionBug.groovy
+++ groovy/groovy-core/src/test/groovy/bugs/SubscriptAndExpressionBug.groovy
@@ -9,21 +9,36 @@ class SubscriptAndExpressionBug extends GroovyTestCase {
     }
 
     void testSubscriptIncrement() {
-        foo = [5]
-
-        foo[0] = foo[0] + 5
-        
         /** @todo bug
-        foo[0] += 5
+        foo = [5]
         */
+        foo = [5, 6, 7]
+        foo[0] += 5
         
         assert foo[0] == 10
+        
+        i = 0
+        foo[i++] = 1
+        assert foo[0] == 1
+        assert i == 1
+        
+        foo[i++] += 5
+        assert i == 2
+        assert foo[1] == 11
     }
     
     void testDoubleSubscript() {
         foo = ["nice cheese grommit"]
         
         cheese = foo[0][5..10]
+        
+        assert cheese == "cheese"
+    }
+    
+    void testSubscriptAndProperty() {
+        foo = [['gromit':'cheese']]
+        
+        cheese = foo[0].gromit
         
         assert cheese == "cheese"
     }
