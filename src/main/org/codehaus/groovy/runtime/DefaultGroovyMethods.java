645d48cce2da3ee743a23f0539702f976c1bfc5c
James Strachan <jastrachan@mac.com>
2004-01-13 19:24:53 +0000

    fix for GROOVY-125
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@599 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -1496,7 +1496,7 @@ public class DefaultGroovyMethods {
      * Iterates from this number up to the given number
      */
     public static void upto(Number self, Number to, Closure closure) {
-        for (int i = self.intValue(), size = to.intValue(); i < size; i++) {
+        for (int i = self.intValue(), size = to.intValue(); i <= size; i++) {
             closure.call(new Integer(i));
         }
     }
