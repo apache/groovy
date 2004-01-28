c33dab3d8e3bf1982e52c761b14fcfd71095694d
James Strachan <jastrachan@mac.com>
2004-01-28 08:06:58 +0000

    test case and fix for GROOVY-168
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@700 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/ForAndSqlBug.groovy
@@ -0,0 +1,38 @@
+import groovy.sql.TestHelper
+
+/**
+ * @author Jonathan Carlson
+ * @version $Revision$
+ */
+class ForAndSqlBug extends GroovyTestCase {
+    
+    void testBugInNormalMethod() {
+        sql = TestHelper.makeSql()
+        
+        li = ["a", "b"]
+        for (x in li) {
+            sql.queryEach("SELECT count(*) FROM FOOD") { e |
+            println " ${x}"
+            
+            assert x != null
+            }
+        }
+    }
+    
+    void testBugInsideScript() {
+        assertScript( <<<EOF
+import groovy.sql.TestHelper
+sql = TestHelper.makeSql()
+
+li = ["a", "b"]
+for (x in li) {
+    sql.queryEach("SELECT count(*) FROM FOOD") { e |
+    	println " $${x}"
+    	
+    	assert x != null
+    }
+}
+EOF)        
+	}
+
+}
