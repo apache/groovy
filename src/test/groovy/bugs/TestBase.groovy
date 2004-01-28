ae3abc8a35496376d5a18a667762a59e295bf1ac
James Strachan <jastrachan@mac.com>
2004-01-28 18:25:36 +0000

    added test case for GROOVY-175
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@706 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/TestBase.groovy
@@ -0,0 +1,18 @@
+/**
+ * A base class for testing constructors
+ * 
+ * @version $Revision$
+ */
+class TestBase {
+
+    String foo
+    
+    TestBase() {
+    }
+    
+    /** @todo fix bug
+    TestBase(String aFoo) {
+        this.foo = aFoo
+    }
+    */
+}
