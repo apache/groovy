5a92d94e7a59713b298a234488152e591cbd595d
James Strachan <jastrachan@mac.com>
2004-01-30 12:05:18 +0000

    Applied John's fix for GROOVY-179
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@719 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/MinusEqualsTest.groovy
+++ src/test/groovy/MinusEqualsTest.groovy
@@ -11,6 +11,18 @@ class MinusEqualsTest extends GroovyTestCase {
         
         assert y == 1
     }
+
+    void testCharacterMinusEquals() {
+        Character x = 4
+        Character y = 2
+        x -= y
+        
+        assert x == 2
+        
+        y -= 1
+        
+        assert y == 1
+    }
     
     void testNumberMinusEquals() {
         x = 4.2
