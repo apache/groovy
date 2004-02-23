283f3280f6f96acbbc24a23debc8825bb4a14cb0
James Strachan <jastrachan@mac.com>
2004-02-23 13:10:54 +0000

    * refactor so that Closure invocation uses the MetaClass mecahnism now to minimise the reliance on reflection * refactoring to minimise the synchronization inside MetaClass method dispatching.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@787 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/groovy/util/GroovyTestCase.java
+++ groovy/groovy-core/src/main/groovy/util/GroovyTestCase.java
@@ -45,12 +45,13 @@
  */
 package groovy.util;
 
+import groovy.lang.Closure;
+import groovy.lang.GroovyShell;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.logging.Logger;
 
-import groovy.lang.Closure;
-import groovy.lang.GroovyShell;
 import junit.framework.TestCase;
 
 import org.codehaus.groovy.runtime.DefaultGroovyMethods;
@@ -227,7 +228,8 @@ public class GroovyTestCase extends TestCase {
         }
         catch (Exception e) {
             failed = true;
-            System.out.println("Worked: caught expected exception: " + e);
+            log.info("Worked: caught expected exception: " + e);
+            //log.log(Level.INFO, "Worked: caught expected exception: " + e, e);
         }
         assertTrue("Closure " + code + " should have failed", failed);
     }
