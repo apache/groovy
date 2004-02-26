d9e87f9e6ccd257d758b1e0a7ac98c0d81afd6b0
James Strachan <jastrachan@mac.com>
2004-02-26 16:04:41 +0000

    added a test case of GROOVY-211
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@807 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/ClosureParameterPassingBug.groovy
+++ src/test/groovy/bugs/ClosureParameterPassingBug.groovy
@@ -15,4 +15,23 @@ class ClosureParameterPassingBug extends GroovyTestCase {
 
 		c([1]) 
     }
+    
+/** @todo this reproduces John's bug...
+    
+    void testBugInScript() {
+    	assertScript <<<EOF
+c = { x | 
+	c1 = { 
+		assert x != null : "Could not find a value for x"
+		println x[0] 
+	} 
+	
+	c1() 
+} 
+
+c([1]) 
+EOF    	
+	}
+*/
+   
 }
