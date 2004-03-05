591c9cbd1c43dafe564f465febd412b4e5793b7c
James Strachan <jastrachan@mac.com>
2004-03-05 17:56:27 +0000

    added typed parameter test case
    git-svn-id: http://svn.codehaus.org/groovy/trunk@840 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/DefaultParamTest.groovy
+++ groovy/groovy-core/src/test/groovy/DefaultParamTest.groovy
@@ -13,9 +13,29 @@ class DefaultParamTest extends GroovyTestCase {
     	shouldFail { doSomething() }
     }
 
+    void testDefaultTypedParameters() {
+    	value = doTypedSomething("X", "Y", "Z")
+    	assert value == "X-Y-Z"
+    	
+    	value = doTypedSomething("X", "Y")
+    	assert value == "X-Y-defC"
+    	
+    	value = doTypedSomething("X")
+    	assert value == "X-defB-defC"
+    	
+    	value = doTypedSomething()
+    	assert value == "defA-defB-defC"
+    }
+
 
 	doSomething(a, b = 'defB', c = 'defC') {
 		println "Called with a: ${a}, b ${b}, c ${c}"
+		
+		return a + "-" + b + "-" + c
+	}
+
+	String doTypedSomething(String a = 'defA', String b = 'defB', String c = 'defC') {
+		println "Called typed method with a: ${a}, b ${b}, c ${c}"
 		
 		return a + "-" + b + "-" + c
 	}
