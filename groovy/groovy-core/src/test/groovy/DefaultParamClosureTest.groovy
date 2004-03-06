5afd08d65bbed80e13344d4d55d439538cd7a712
James Strachan <jastrachan@mac.com>
2004-03-06 13:27:43 +0000

    added new test cases we need to fix
    git-svn-id: http://svn.codehaus.org/groovy/trunk@842 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/DefaultParamClosureTest.groovy
+++ groovy/groovy-core/src/test/groovy/DefaultParamClosureTest.groovy
@@ -1,7 +1,13 @@
 class DefaultParamClosureTest extends GroovyTestCase {
 
     void testDefaultParameters() {
-    
+    /** @todo get default parameters working for closures 
+		doSomething = { | a, b = 'defB', c = 'defC' |
+			println "Called with a: ${a}, b ${b}, c ${c}"
+			
+			return a + "-" + b + "-" + c
+		}
+	
     	value = doSomething("X", "Y", "Z")
     	assert value == "X-Y-Z"
     	
@@ -12,9 +18,18 @@ class DefaultParamClosureTest extends GroovyTestCase {
     	assert value == "X-defB-defC"
     	
     	shouldFail { doSomething() }
+     */
     }
 
     void testDefaultTypedParameters() {
+		/** @todo parser to handle typed parameters 
+		
+		doTypedSomething = { | String a = 'defA', String b = 'defB', String c = 'defC' |
+			println "Called typed method with a: ${a}, b ${b}, c ${c}"
+			
+			return a + "-" + b + "-" + c
+		}
+	
     	value = doTypedSomething("X", "Y", "Z")
     	assert value == "X-Y-Z"
     	
@@ -26,18 +41,7 @@ class DefaultParamClosureTest extends GroovyTestCase {
     	
     	value = doTypedSomething()
     	assert value == "defA-defB-defC"
+	*/
     }
 
-
-	doSomething(a, b = 'defB', c = 'defC') {
-		println "Called with a: ${a}, b ${b}, c ${c}"
-		
-		return a + "-" + b + "-" + c
-	}
-
-	String doTypedSomething(String a = 'defA', String b = 'defB', String c = 'defC') {
-		println "Called typed method with a: ${a}, b ${b}, c ${c}"
-		
-		return a + "-" + b + "-" + c
-	}
 }
