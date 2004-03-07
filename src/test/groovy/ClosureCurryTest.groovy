2b2bbe46954f0f32d7165555683375fb3135ca62
John Wilson <tug@wilson.co.uk>
2004-03-07 16:16:38 +0000

    Fix Groovy-243
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@851 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/ClosureCurryTest.groovy
+++ src/test/groovy/ClosureCurryTest.groovy
@@ -14,11 +14,42 @@ class ClosureCurryTest extends GroovyTestCase {
 		clos4 = clos3.curry('a')
 		clos5 = clos4.curry('b')
 		clos6 = clos4.curry('x')
+		clos7 = clos4.curry('f', 'g')
 		value = clos5('c')
 		assert value == "abc"
 		value = clos6('c')
 		assert value == "axc"
 		value = clos4('y', 'z')
 		assert value == "ayz"
+		value = clos7()
+		assert value == "afg"
+		
+		clos3 = {s1, s2, s3 | s1 + s2 + s3}.asWritable()
+		clos4 = clos3.curry('a')
+		clos5 = clos4.curry('b')
+		clos6 = clos4.curry('x')
+		clos7 = clos4.curry('f', 'g')
+		value = clos5('c')
+		assert value == "abc"
+		value = clos6('c')
+		assert value == "axc"
+		value = clos4('y', 'z')
+		assert value == "ayz"
+		value = clos7()
+		assert value == "afg"
+		
+		clos3 = {s1, s2, s3 | s1 + s2 + s3}
+		clos4 = clos3.curry('a').asWritable()
+		clos5 = clos4.curry('b').asWritable()
+		clos6 = clos4.curry('x').asWritable()
+		clos7 = clos4.curry('f', 'g').asWritable()
+		value = clos5('c')
+		assert value == "abc"
+		value = clos6('c')
+		assert value == "axc"
+		value = clos4('y', 'z')
+		assert value == "ayz"
+		value = clos7()
+		assert value == "afg"
     }  
 }
