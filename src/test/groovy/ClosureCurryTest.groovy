3278cf3afab63fc7bc80ed0394422cc4ed29e8b0
John Wilson <tug@wilson.co.uk>
2004-03-07 14:15:43 +0000

    Fix Groovy-241
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@849 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/ClosureCurryTest.groovy
+++ src/test/groovy/ClosureCurryTest.groovy
@@ -9,5 +9,16 @@ class ClosureCurryTest extends GroovyTestCase {
 		clos2 = clos1.curry("hi")
 		value = clos2("there") 
 		assert value == "hithere"
+		
+		clos3 = {s1, s2, s3 | s1 + s2 + s3}
+		clos4 = clos3.curry('a')
+		clos5 = clos4.curry('b')
+		clos6 = clos4.curry('x')
+		value = clos5('c')
+		assert value == "abc"
+		value = clos6('c')
+		assert value == "axc"
+		value = clos4('y', 'z')
+		assert value == "ayz"
     }  
 }
