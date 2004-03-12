1b6089b695db8976b52f7200d8ab32632eb9557f
James Strachan <jastrachan@mac.com>
2004-03-12 12:16:49 +0000

    added an example test case trying to reproduce Rod's issue
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@918 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/ClosureWithStaticVariablesBug.groovy
@@ -0,0 +1,34 @@
+package groovy.bugs
+
+import org.codehaus.groovy.classgen.TestSupport
+
+/**
+ * @version $Revision: 1.6 $
+ */
+class ClosureWithStaticVariablesBug extends TestSupport {
+    
+    static y = [:]
+    
+    void testBug() {
+		c = { x |
+			return {
+			    foo = Cheese.z
+			    println foo
+			    assert foo.size() == 0
+			    
+    		    println y
+    		    assert y.size() == 0
+    		    
+    		    return 6
+			}
+		}
+		c2 = c(5)
+		answer = c2()
+		assert answer == 6
+    }
+}
+
+class Cheese {
+    static z = [:]
+	
+}
