d6defd6a9b62fd3407a70bf930f91382cdfe210c
John Wilson <tug@wilson.co.uk>
2004-03-14 12:03:59 +0000

    Fix Groovy 268
    git-svn-id: http://svn.codehaus.org/groovy/trunk@935 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/org/codehaus/groovy/syntax/lexer/Lexer.java
+++ groovy/groovy-core/src/main/org/codehaus/groovy/syntax/lexer/Lexer.java
@@ -850,6 +850,45 @@ public class Lexer {
                         StringBuffer numericLiteral = new StringBuffer();
 
                         boolean isFloat = false;
+                        
+                        if (c == '0') {
+                            numericLiteral.append(consume());
+                            c = la();
+                            
+                            if (c == 'x' || c =='X') {
+                                numericLiteral.append(consume());
+                                c = la();
+
+                                while (c == '0'
+                                    || c == '1'
+                                    || c == '2'
+                                    || c == '3'
+                                    || c == '4'
+                                    || c == '5'
+                                    || c == '6'
+                                    || c == '7'
+                                    || c == '8'
+                                    || c == '9'
+                                    || c == 'a'
+                                    || c == 'b'
+                                    || c == 'c'
+                                    || c == 'd'
+                                    || c == 'e'
+                                    || c == 'f'
+                                    || c == 'A'
+                                    || c == 'B'
+                                    || c == 'C'
+                                    || c == 'D'
+                                    || c == 'E'
+                                    || c == 'F') {
+	                                    numericLiteral.append(consume());
+	                                    c = la();
+	                                }
+                                
+                                	token = Token.integerNumber(getStartLine(), getStartColumn(), numericLiteral.toString());
+                                	break ROOT_SWITCH;
+                            }
+                        }
 
                         while (c == '0'
                             || c == '1'
