c785d8266e6c426cfbc61257eb43f8c081a090a5
Guillaume Laforge <glaforge@gmail.com>
2004-03-25 01:00:56 +0000

    Added Thread.start{closure} and Thread.startDaemon{closure} See GROOVY-260
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@985 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyStaticMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyStaticMethods.java
@@ -1,4 +1,40 @@
+/*
+ * $Id$
+ *
+ * Copyright 2003 (C) James Strachan and Bob Mcwhirter. All Rights Reserved.
+ *
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ *  1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *  3. The name "groovy" must not be used to endorse or promote products
+ * derived from this Software without prior written permission of The Codehaus.
+ * For written permission, please contact info@codehaus.org.
+ *  4. Products derived from this Software may not be called "groovy" nor may
+ * "groovy" appear in their names without prior written permission of The
+ * Codehaus. "groovy" is a registered trademark of The Codehaus.
+ *  5. Due credit should be given to The Codehaus - http://groovy.codehaus.org/
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ */
 package org.codehaus.groovy.runtime;
+
+import groovy.lang.Closure;
 
 /**
  * This class defines all the new static groovy methods which appear on normal JDK
@@ -10,8 +46,33 @@ package org.codehaus.groovy.runtime;
  */
 public class DefaultGroovyStaticMethods {
 
-    public static void hello(String stringClass, String msg)
-    {
+    // @todo should be removed, was there for testing purpose
+    public static void hello(String stringClass, String msg) {
         System.out.println("Hello " + msg);
+    }
+
+    /**
+     * Start a Thread with the given closure as a Runnable instance.
+     *
+     * @param closure the Runnable closure
+     * @return the started thread
+     */
+    public static Thread start(Thread self, Closure closure) {
+        Thread thread = new Thread(closure);
+        thread.start();
+        return thread;
+    }
+
+    /**
+     * Start a daemon Thread with the given closure as a Runnable instance.
+     *
+     * @param closure the Runnable closure
+     * @return the started thread
+     */
+    public static Thread startDaemon(Thread self, Closure closure) {
+        Thread thread = new Thread(closure);
+        thread.setDaemon(true);
+        thread.start();
+        return thread;
     }
 }
