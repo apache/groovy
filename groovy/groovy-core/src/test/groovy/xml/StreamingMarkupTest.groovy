97516e0be79b0a4653242b86e1fb1523fcb4b861
John Wilson <tug@wilson.co.uk>
2004-03-31 15:00:43 +0000

    Change the name of the class to match the name of the file
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1018 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/xml/StreamingMarkupTest.groovy
+++ groovy/groovy-core/src/test/groovy/xml/StreamingMarkupTest.groovy
@@ -4,7 +4,7 @@ package org.codehaus.groovy.sandbox.markup
  * This test uses the concise syntax to test the building of 
  * textual markup (XML or HTML) using GroovyMarkup
  */
-class MarkupTest extends groovy.xml.TestXmlSupport {
+class StreamingMarkupTest extends groovy.xml.TestXmlSupport {
     
     void testSmallTree() {
         b = new StreamingMarkupBuilder()
