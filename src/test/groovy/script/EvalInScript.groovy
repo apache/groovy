44fd5c6b4353fd6ba5946acca589b5881dd81f60
James Strachan <jastrachan@mac.com>
2004-04-16 17:17:37 +0000

    added a helper method 'evaluate' for use inside scripts to evaluate groovy expressions
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1070 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/script/EvalInScript.groovy
@@ -0,0 +1,14 @@
+import java.io.File
+
+a = 1
+evaluate("a = 3")
+assert a == 3
+
+println("Done and now set a to ${a}")
+
+
+println("About to call another script")
+
+evaluate(new File("src/test/groovy/script/HelloWorld.groovy"))
+
+println("Done")
