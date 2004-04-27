570ef451fc9e21c051f0ffcd339287002b3f2c50
Sam Pullara <sam@sampullara.com>
2004-04-27 08:13:47 +0000

    Updated test with StreamingDOMBuilder and added a microbenchmark for creating DOM trees 3 different ways.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1109 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/DOMTest.groovy
+++ src/test/groovy/DOMTest.groovy
@@ -1,11 +1,11 @@
 import java.io.*;
 import groovy.xml.*;
+import org.codehaus.groovy.sandbox.markup.*;
+import org.codehaus.groovy.tools.xml.*;
 
 class DOMTest extends GroovyTestCase {
 
   void testDOMParser() {
-    use groovy.xml.dom.DOMCategory;
-
     xml = new StringReader("<html><head><title class='mytitle'>Test</title></head><body><p class='mystyle'>This is a test.</p></body></html>");
     doc = DOMBuilder.parse(xml);
     html = doc.documentElement;
@@ -14,34 +14,72 @@ class DOMTest extends GroovyTestCase {
   }
 
   void testDOMBuilder() {
-    use groovy.xml.dom.DOMCategory;
-
     html = DOMBuilder.newInstance().
         
     html {
       head {
-		title (class:"mytitle", 'Test');
+		title (class:"mytitle", "Test")
       }
       body {
-		p (class:"mystyle", 'This is a test.')
+		p (class:"mystyle", "This is a test.")
       }
     }
 
     assertCorrect html;
   }
 
+  void testStreamingDOMBuilder() {
+
+    doc = new StreamingDOMBuilder().bind {
+      html {
+        head {
+		  title (class:"mytitle") {
+		    mkp.yield "Test";
+		  }
+        }
+        body {
+		  p (class:"mystyle") {
+		    mkp.yield "This is a test.";
+		  }
+        }
+      }
+	}
+
+    assertCorrect doc().documentElement;
+  }
+
   assertCorrect(html) {
+    use groovy.xml.dom.DOMCategory;
+
     assert html.head.title.textContent == "Test";
     assert html.body.p.textContent == "This is a test.";
     assert html.find { it.tagName == "body" }.tagName == "body";
     assert html.getElementsByTagName("*").findAll { if (it["@class"] != "") { return it } }.size() == 2;
   }
   
-  /*
   static void main(args) {
+  	// Relative results:
+  	// Test       04/27/2004
+  	// Parser:    1.0
+  	// Builder:   1.28
+  	// Streaming: 0.69
+  	
   	test = new DOMTest();
-  	test.testDOMParser();
-  	test.testDOMBuilder();
+  	standard = 0;
+  	[test.testDOMParser, test.testDOMBuilder, test.testStreamingDOMBuilder].each {
+  		// Run the method once to fill any caches and to load classes
+  		it();
+	  	start = System.currentTimeMillis();
+  		for (i in 1..2000) {
+  			it();
+	  	}
+  		elapsed = System.currentTimeMillis() - start;
+  		result = i * 1000 / elapsed;
+
+		standard = (standard == 0 ? result : standard);  		
+		factor = result/standard;
+  		
+	  	println "${it.method}: ${factor}x (${result} trees/s)";
+	}
   }
-  */
 }
