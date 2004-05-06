074f59e21783371cef3e59e1d0c2c6dc215e08a2
John Wilson <tug@wilson.co.uk>
2004-05-06 17:01:49 +0000

    Add base64 encoding and decoding methods
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1140 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -33,7 +33,6 @@
  *
  */
 package org.codehaus.groovy.runtime;
-
 import groovy.lang.Closure;
 import groovy.lang.GroovyObject;
 import groovy.lang.MetaClass;
@@ -1947,7 +1946,149 @@ public class DefaultGroovyMethods {
     public static List toList(double[] array) {
         return InvokerHelper.primitiveArrayToList(array);
     }
+    
+	private static final char[] tTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".toCharArray();
+    
+    /**
+     * Produce a Writable object which writes the base64 encoding of the byte array
+     * Calling toString() on the result rerurns the encoding as a String
+     * 
+     * @param byte array to be encoded
+     * @return object which will write the base64 encoding of the byte array
+     */
+    public static Writable encodeBase64(final byte[] data) {
+		return new Writable() {
+			public Writer writeTo(final Writer writer) throws IOException {
+				int charCount = 0;
+				final int dLimit = (data.length / 3) * 3;
 
+				for (int dIndex = 0; dIndex != dLimit; dIndex += 3) {
+					int d = ((data[dIndex] & 0XFF) << 16) |  ((data[dIndex + 1] & 0XFF) << 8) | (data[dIndex + 2] & 0XFF);
+
+					writer.write(tTable[d >> 18]);
+					writer.write(tTable[(d >> 12) & 0X3F]);
+					writer.write(tTable[(d >> 6) & 0X3F]);
+					writer.write(tTable[d & 0X3F]);
+
+					if (++charCount == 18) {
+						writer.write('\n');
+						charCount = 0;
+					}
+				}
+
+				if (dLimit != data.length) {
+					int d = (data[dLimit] & 0XFF) << 16;
+
+					if (dLimit + 1 != data.length) {
+						d |= (data[dLimit + 1] & 0XFF) << 8;
+					}
+
+					writer.write(tTable[d >> 18]);
+					writer.write(tTable[(d >> 12) & 0X3F]);
+					writer.write((dLimit + 1 < data.length) ? tTable[(d >> 6) & 0X3F] : '=');
+					writer.write('=');
+				}
+				
+				return writer;
+			}
+			
+			public String toString() {
+		        StringWriter buffer = new StringWriter();
+		        
+		        try {
+		            writeTo(buffer);
+		        }
+		        catch (IOException e) {
+		            throw new RuntimeException(e); // TODO: change this exception type
+		        }
+		        
+		        return buffer.toString();
+			}
+		};
+    }
+    
+	private static final byte[] translateTable = (
+			//
+			"\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+			//                    \t    \n                \r
+			+ "\u0042\u0042\u0041\u0041\u0042\u0042\u0041\u0042"
+			//
+			+ "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+			//
+			+ "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+			//        sp    !     "     #     $     %     &     '
+			+ "\u0041\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+			//         (    )     *     +     ,     -     .     /
+			+ "\u0042\u0042\u0042\u003E\u0042\u0042\u0042\u003F"
+			//         0    1     2     3     4     5     6     7
+			+ "\u0034\u0035\u0036\u0037\u0038\u0039\u003A\u003B"
+			//         8    9     :     ;     <     =     >     ?
+			+ "\u003C\u003D\u0042\u0042\u0042\u0040\u0042\u0042"
+			//         @    A     B     C     D     E     F     G
+			+ "\u0042\u0000\u0001\u0002\u0003\u0004\u0005\u0006"
+			//         H    I   J K   L     M   N   O
+			+ "\u0007\u0008\t\n\u000B\u000C\r\u000E"
+			//         P    Q     R     S     T     U     V    W
+			+ "\u000F\u0010\u0011\u0012\u0013\u0014\u0015\u0016"
+			//         X    Y     Z     [     \     ]     ^    _
+			+ "\u0017\u0018\u0019\u0042\u0042\u0042\u0042\u0042"
+			//         '    a     b     c     d     e     f     g
+			+ "\u0042\u001A\u001B\u001C\u001D\u001E\u001F\u0020"
+			//        h   i   j     k     l     m     n     o    p
+			+ "\u0021\"\u0023\u0024\u0025\u0026\u0027\u0028"
+			//        p     q     r     s     t     u     v     w
+			+ "\u0029\u002A\u002B\u002C\u002D\u002E\u002F\u0030"
+			//        x     y     z
+			+ "\u0031\u0032\u0033").getBytes();
+   
+    /**
+     * 
+     * Decode the Sting from base64 into a byte array
+     * 
+     * @param the string to be decoded
+     * @return the decoded bytes as an array
+     */
+    public static byte[] decodeBase64(final String value) {
+		int byteShift = 4;
+		int tmp = 0;
+		boolean done = false;
+		final StringBuffer buffer = new StringBuffer();
+		
+		for (int i = 0; i != value.length(); i++) {
+		final char c = value.charAt(i);
+		final int sixBit = (c < 123) ? translateTable[c] : 66;
+
+			if (sixBit < 64) {
+				if (done) throw new RuntimeException("= character not at end of base64 value"); // TODO: change this exception type
+
+				tmp = (tmp << 6) | sixBit;
+
+				if (byteShift-- != 4) {
+					buffer.append((char)((tmp >> (byteShift * 2)) & 0XFF));
+				}
+
+			} else if (sixBit == 64) {
+
+				byteShift--;
+				done = true;
+
+			} else if (sixBit == 66) {
+				// RFC 2045 says that I'm allowed to take the presence of 
+				// these characters as evedence of data corruption
+				// So I will
+				throw new RuntimeException("bad character in base64 value"); // TODO: change this exception type
+			}
+
+			if (byteShift == 0) byteShift = 4;
+		}
+
+		try {
+			return buffer.toString().getBytes("ISO-8859-1");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Base 64 decode produced byte values > 255"); // TODO: change this exception type
+		}
+    }
+    
     /**
      * Implements the getAt(int) method for primitve type arrays
      */
