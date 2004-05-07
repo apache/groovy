be1334c7f4eb42e832cd6bf97496a32fc193370b
Christiaan ten Klooster <ckl@dacelo.nl>
2004-05-07 10:30:31 +0000

    refactor widget factory
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1142 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/groovy-swt/src/main/groovy/swt/factory/AbstractSwtFactory.java
+++ groovy/modules/groovy-swt/src/main/groovy/swt/factory/AbstractSwtFactory.java
@@ -1,26 +1,34 @@
 /*
  * Created on Feb 15, 2004
- *
+ *  
  */
 package groovy.swt.factory;
 
 import groovy.swt.SwtUtils;
+import groovy.swt.convertor.ColorConverter;
+import groovy.swt.convertor.PointConverter;
 
 import java.lang.reflect.Field;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.codehaus.groovy.GroovyException;
 import org.codehaus.groovy.runtime.InvokerHelper;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Control;
 
 /**
  * @author <a href="mailto:ckl@dacelo.nl">Christiaan ten Klooster </a>
  * @version $Revision$
  */
 public abstract class AbstractSwtFactory {
+
     public abstract Object newInstance(Map properties, Object parent)
-    throws GroovyException;
+            throws GroovyException;
 
     /**
      * set the properties
@@ -29,6 +37,30 @@ public abstract class AbstractSwtFactory {
      * @param properties
      */
     protected void setBeanProperties(Object bean, Map properties) {
+        if (bean instanceof Control) {
+            Control control = (Control) bean;
+
+            // set size of widget
+            Object size = properties.remove("size");
+            if (size != null) {
+                setSize(control, size);
+            }
+
+            // set background of widget
+            Object colorValue = properties.remove("background");
+            if (colorValue != null) {
+                Color background = getColor(control, colorValue);
+                control.setBackground(background);
+            }
+
+            // set foreground of widget
+            colorValue = properties.remove("foreground");
+            if (colorValue != null) {
+                Color foreground = getColor(control, colorValue);
+                control.setForeground(foreground);
+            }
+        }
+
         for (Iterator iter = properties.entrySet().iterator(); iter.hasNext();) {
             Map.Entry entry = (Map.Entry) iter.next();
             String property = entry.getKey().toString();
@@ -39,29 +71,52 @@ public abstract class AbstractSwtFactory {
                 if (value instanceof String) {
                     int style = SwtUtils.parseStyle(SWT.class, (String) value);
                     field.setInt(bean, style);
-                }
-                else if (value instanceof Boolean) {
+                } else if (value instanceof Boolean) {
                     field.setBoolean(bean, ((Boolean) value).booleanValue());
-                }
-                else if (value instanceof Integer) {
+                } else if (value instanceof Integer) {
                     field.setInt(bean, ((Integer) value).intValue());
-                }
-                else if (value instanceof Double) {
+                } else if (value instanceof Double) {
                     field.setDouble(bean, ((Double) value).doubleValue());
-                }
-                else if (value instanceof Float) {
+                } else if (value instanceof Float) {
                     field.setFloat(bean, ((Float) value).floatValue());
-                }
-                else {
+                } else {
                     InvokerHelper.setProperty(bean, property, value);
                 }
-                
-            }
-            catch (Exception e) {
+
+            } catch (Exception e) {
             }
             if (field == null) {
                 InvokerHelper.setProperty(bean, property, value);
             }
+        }
+    }
+
+    protected Color getColor(Control control, Object colorValue) {
+        Color color = null;
+        if (colorValue != null) {
+            RGB rgb = null;
+            if (colorValue instanceof Color) {
+                color = (Color) colorValue;
+            } else if (colorValue instanceof List) {
+                rgb = ColorConverter.getInstance().parse((List) colorValue);
+                color = new Color(control.getDisplay(), rgb);
+            } else {
+                rgb = ColorConverter.getInstance().parse(colorValue.toString());
+                color = new Color(control.getDisplay(), rgb);
+            }
+        }
+        return color;
+    }
+
+    protected void setSize(Control control, Object size) {
+        Point point = null;
+        if (size != null) {
+            if (size instanceof Point) {
+                point = (Point) size;
+            } else if (size instanceof List) {
+                point = PointConverter.getInstance().parse((List) size);
+            }
+            control.setSize(point);
         }
     }
 }
--- groovy/modules/groovy-swt/src/main/groovy/swt/factory/WidgetFactory.java
+++ groovy/modules/groovy-swt/src/main/groovy/swt/factory/WidgetFactory.java
@@ -1,24 +1,18 @@
 /*
  * Created on Feb 15, 2004
- *
+ *  
  */
 package groovy.swt.factory;
 
 import groovy.swt.SwtUtils;
-import groovy.swt.convertor.ColorConverter;
-import groovy.swt.convertor.PointConverter;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 import java.util.Map;
 
 import org.codehaus.groovy.GroovyException;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.TabItem;
 
@@ -27,7 +21,9 @@ import org.eclipse.swt.widgets.TabItem;
  * @version $Revision$
  */
 public class WidgetFactory extends AbstractSwtFactory implements SwtFactory {
+
     protected Class beanClass;
+
     protected int style = SWT.NONE;
 
     public WidgetFactory(Class beanClass) {
@@ -44,24 +40,23 @@ public class WidgetFactory extends AbstractSwtFactory implements SwtFactory {
     }
 
     /*
-     * @see groovy.swt.impl.Factory#newInstance(java.util.Map,
-     *      java.lang.Object)
+     * @see groovy.swt.impl.Factory#newInstance(java.util.Map, java.lang.Object)
      */
     public Object newInstance(Map properties, Object parent)
-    throws GroovyException {
+            throws GroovyException {
         String styleProperty = (String) properties.remove("style");
         if (styleProperty != null) {
             style = SwtUtils.parseStyle(SWT.class, styleProperty);
         }
-        
-        Object parentWidget = SwtUtils.getParentWidget(parent);        
+
+        Object parentWidget = SwtUtils.getParentWidget(parent);
         Object bean = createWidget(parentWidget);
         if (bean != null) {
             setBeanProperties(bean, properties);
         }
-        
+
         setControl(bean, parent);
-        
+
         return bean;
     }
 
@@ -72,25 +67,18 @@ public class WidgetFactory extends AbstractSwtFactory implements SwtFactory {
      *         GroovyException
      */
     protected Object createWidget(Object parent) throws GroovyException {
-        if (beanClass == null) {
-            throw new GroovyException(
-            "No Class available to create the new widget");
-        }
+        if (beanClass == null) { throw new GroovyException(
+                "No Class available to create the new widget"); }
         try {
             if (parent == null) {
                 // lets try call a constructor with a single style
-                Class[] types = {
-                    int.class
-                };
+                Class[] types = { int.class};
                 Constructor constructor = beanClass.getConstructor(types);
                 if (constructor != null) {
-                    Object[] arguments = {
-                        new Integer(style)
-                    };
+                    Object[] arguments = { new Integer(style)};
                     return constructor.newInstance(arguments);
                 }
-            }
-            else {
+            } else {
                 // lets try to find the constructor with 2 arguments with the
                 // 2nd argument being an int
                 Constructor[] constructors = beanClass.getConstructors();
@@ -99,65 +87,33 @@ public class WidgetFactory extends AbstractSwtFactory implements SwtFactory {
                         Constructor constructor = constructors[i];
                         Class[] types = constructor.getParameterTypes();
                         if (types.length == 2
-                        && types[1].isAssignableFrom(int.class)) {
+                                && types[1].isAssignableFrom(int.class)) {
                             if (types[0].isAssignableFrom(parent.getClass())) {
-                                Object[] arguments = {
-                                        parent, 
-                                        new Integer(style)
-                                };
+                                Object[] arguments = { parent,
+                                        new Integer(style)};
                                 return constructor.newInstance(arguments);
                             }
                             // lets try to find the constructor with 1
                             // arguments
-                        }
-                        else if (types.length == 1
-                        && types[0].isAssignableFrom(parent.getClass())) {
-                            Object[] arguments = {
-                                parent
-                            };
+                        } else if (types.length == 1
+                                && types[0].isAssignableFrom(parent.getClass())) {
+                            Object[] arguments = { parent};
                             return constructor.newInstance(arguments);
                         }
                     }
                 }
             }
             return beanClass.newInstance();
-        }
-        catch (NoSuchMethodException e) {
+        } catch (NoSuchMethodException e) {
             throw new GroovyException(e.getMessage());
-        }
-        catch (InstantiationException e) {
+        } catch (InstantiationException e) {
             throw new GroovyException(e.getMessage());
-        }
-        catch (IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             throw new GroovyException(e.getMessage());
-        }
-        catch (InvocationTargetException e) {
+        } catch (InvocationTargetException e) {
             throw new GroovyException(e.getTargetException()
-            .getLocalizedMessage());
+                    .getLocalizedMessage());
         }
-    }
-
-    protected void setBeanProperties(Object bean, Map properties) {
-        if (bean instanceof Control) {
-            Control control = (Control) bean;
-
-            // set size of widget
-            Object size = properties.remove("size");
-            setSize(control, size);
-
-            // set background of widget
-            Object colorValue = properties.remove("background");
-            Color background = getColor(control, colorValue);
-            control.setBackground(background);
-
-            // set foreground of widget
-            colorValue = properties.remove("foreground");
-            Color foreground = getColor(control, colorValue);
-            control.setForeground(foreground);
-        }
-
-        // set the properties
-        super.setBeanProperties(bean, properties);
     }
 
     protected void setControl(Object bean, Object parent) {
@@ -169,36 +125,7 @@ public class WidgetFactory extends AbstractSwtFactory implements SwtFactory {
             tabItem.setControl((Control) bean);
         }
     }
-    
-    protected void setSize(Control control, Object size) {
-        Point point = null;
-        if (size != null) {
-            if (size instanceof Point) {
-                point = (Point) size;
-            }
-            else if (size instanceof List) {
-                point = PointConverter.getInstance().parse((List) size);
-            }
-            control.setSize(point);
-        }
-    }
 
-    protected Color getColor(Control control, Object colorValue) {
-        Color color = null;
-        if (colorValue != null) {
-            RGB rgb = null;
-            if (colorValue instanceof Color) {
-                color = (Color) colorValue;
-            }
-            else if (colorValue instanceof List) {
-                rgb = ColorConverter.getInstance().parse((List) colorValue);
-                color = new Color(control.getDisplay(), rgb);
-            }
-            else {
-                rgb = ColorConverter.getInstance().parse(colorValue.toString());
-                color = new Color(control.getDisplay(), rgb);
-            }
-        }
-        return color;
-    }
+  
+
 }
