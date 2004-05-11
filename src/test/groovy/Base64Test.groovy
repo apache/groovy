9b9972fb6a6919b1e9e810885bacda5255d81c89
John Wilson <tug@wilson.co.uk>
2004-05-11 09:04:14 +0000

    Try to fix DC failure by ensuring that the strings being compared produced with identical encodings
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1169 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/Base64Test.groovy
+++ src/test/groovy/Base64Test.groovy
@@ -1,14 +1,28 @@
 class Base64Test extends GroovyTestCase {
 
     void testCodec() {
-    		// testString ="§1234567890-=±!@£$%^&*()_+qwertyuiop[]QWERTYUIOP{}asdfghjkl;'\\ASDFGHJKL:\"|`zxcvbnm,./~ZXCVBNM<>?\u0000\u00ff\u00f0\u000f"
-    		testString ="1234567890qwertyuiopQWERTYUIOPasdfghjklASDFGHJKL"
-    		// testBytes = testString.getBytes("ISO-8859-1")
-    		testBytes = testString.getBytes()
-    		encodedBtyes = testBytes.encodeBase64().toString()
-    		decodedBytes = encodedBtyes.decodeBase64()
-    		// decodedString = new String(decodedBytes, "ISO-8859-1")
-    		decodedString = new String(decodedBytes)
+   		testString ="§1234567890-=±!@£$%^&*()_+qwertyuiop[]QWERTYUIOP{}asdfghjkl;'\\ASDFGHJKL:\"|`zxcvbnm,./~ZXCVBNM<>?\u0000\u00ff\u00f0\u000f"
+
+		// get a byte array using the least significant eigth bits of each caharacter
+   		testBytes = testString.getBytes("ISO-8859-1")
+   		
+   		// turn the bytes back into a string for later comparison
+    		testString = new String(testBytes, "ISO-8859-1")
+    		
+    		// encode the bytes as base64. This produces a Writable object convert it to a String
+    		encodedBytes = testBytes.encodeBase64().toString()
+    		
+    		// decode the base64 back to a byte array
+    		decodedBytes = encodedBytes.decodeBase64()
+    		
+    		// turn the byte array back to a String for caomparison
+    		decodedString = new String(decodedBytes, "ISO-8859-1")
+    		
+    		println "'${decodedString}'"
+    		println "'${testString}'"
+    		println decodedString.size()
+    		println testString.size()
+    		
     		assert decodedString.equals(testString)
     }
 
