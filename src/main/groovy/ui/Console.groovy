a2d1ea4d60ba8a0169730377fd91f29dbcf89d58
Guillaume Laforge <glaforge@gmail.com>
2004-07-14 20:56:10 +0000

    Applied patch from Danno
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1310 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/ui/Console.groovy
+++ src/main/groovy/ui/Console.groovy
@@ -4,6 +4,7 @@ import groovy.swing.SwingBuilder
 
 import java.awt.Toolkit
 import java.awt.event.KeyEvent
+import java.util.EventObject
 
 import javax.swing.KeyStroke
 import javax.swing.JSplitPane
@@ -29,11 +30,6 @@ class Console extends ConsoleSupport {
     scriptFile
     private boolean dirty
 
-    void setDirty(boolean newDirty) {
-        dirty = newDirty
-        updateTitle()
-    }
-
     static void main(args) {
         console = new Console()
         console.run()
@@ -41,57 +37,59 @@ class Console extends ConsoleSupport {
 
     void run() {
         scriptList = []
-        swing = new SwingBuilder()
+        // if menu modifier is two keys we are out of luck as the javadocs
+        // incicates it returns "Control+Shift" instead of "Control Shift"
+        menuModifier = KeyEvent.getKeyModifiersText( 
+            Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())
+            .toLowerCase() + ' '
 
-        menuModifier = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()
-        frame = swing.frame(title:'GroovyConsole', location:[100,100], size:[800,400], defaultCloseOperation:javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE) {
+        swing = new SwingBuilder()
+        frame = swing.frame(
+		title:'GroovyConsole', 
+		location:[100,100], 
+		size:[800,400], 
+		defaultCloseOperation:javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE) {
+            newAction = action(
+                name:'New',
+                closure: fileNew,
+                mnemonic: 'N',
+                accelerator: menuModifier + 'N'
+            )
+            openAction = action(
+                name:'Open',
+                closure: fileOpen,
+                mnemonic: 'O',
+                accelerator: menuModifier + 'O'
+            )
+            saveAction = action(
+                name:'Save',
+                closure: fileSave,
+                mnemonic: 'S',
+                accelerator: menuModifier + 'S'
+            )
+            exitAction = action(name:'Exit', closure: exit, mnemonic: 'x')
+            runAction = action(
+                name:'Run',
+                closure: runScript,
+                mnemonic: 'R',
+                keyStroke: 'ctrl ENTER',
+                accelerator: 'ctrl R'
+            )
+            aboutAction = action(name:'About', closure: showAbout, mnemonic: 'A')
             menuBar {
                 menu(text:'File', mnemonic:0x46) {
-                    menuItem() {
-                        action(
-                            name:'New',
-                            closure:{ fileNew() },
-                            mnemonicKey:0x4e,
-                            acceleratorKey:KeyStroke.getKeyStroke(KeyEvent.VK_N, menuModifier)
-                        )
-                    }
-                    menuItem() {
-                        action(
-                            name:'Open',
-                            closure:{ fileOpen() },
-                            mnemonicKey:0x4f,
-                            acceleratorKey:KeyStroke.getKeyStroke(KeyEvent.VK_O, menuModifier)
-                        )
-                    }
+                    menuItem() { action(newAction) }
+                    menuItem() { action(openAction) }
                     separator()
-                    menuItem() {
-                        action(
-                            name:'Save',
-                            closure:{ fileSave() },
-                            mnemonicKey:0x53,
-                            acceleratorKey:KeyStroke.getKeyStroke(KeyEvent.VK_S, menuModifier)
-                        )
-                    }
+                    menuItem() { action(saveAction) }
                     separator()
-                    menuItem() {
-                        action(name:'Exit', closure:{ exit() }, mnemonicKey:0x58)
-                    }
+                    menuItem() { action(exitAction) }
                 }
-                menu(text:'Actions', mnemonic:0x41) {
-                    menuItem() {
-                        action(
-                            name:'Run',
-                            closure:{ runScript() },
-                            mnemonicKey:0x52,
-                            keyStroke:'ctrl enter',
-                            acceleratorKey:KeyStroke.getKeyStroke(KeyEvent.VK_R, menuModifier)
-                        )
-                    }
+                menu(text:'Actions', mnemonic: 'A') {
+                    menuItem() { action(runAction) }
                 }
-                menu(text:'Help', mnemonic:0x48) {
-                    menuItem() {
-                        action(name:'About', closure:{ showAbout() }, mnemonic:0x41)
-                    }
+                menu(text:'Help', mnemonic: 'H') {
+                    menuItem() { action(aboutAction) }
                 }
             }
             splitPane(orientation:JSplitPane.VERTICAL_SPLIT, resizeWeight:0.50F) {
@@ -100,18 +98,25 @@ class Console extends ConsoleSupport {
                     addStylesToDocument(outputArea)
                 }
                 scrollPane {
-                    textArea = textArea()
+                    textArea = textArea() { action(runAction) }
+                    
                 }
             }
         }
+        
         frame.setSize(500,400)
 
         // add listeners
-        frame.windowClosing = { exit() }
+        frame.windowClosing = exit
         textArea.document.undoableEditHappened = { setDirty(true) }
 
         frame.show()
         SwingUtilities.invokeLater({textArea.requestFocus()});
+    }
+
+    void setDirty(boolean newDirty) {
+        dirty = newDirty
+        updateTitle()
     }
 
     updateTitle() {
@@ -122,7 +127,43 @@ class Console extends ConsoleSupport {
         }
     }
 
-    runScript() {
+    selectFilename(name = "Open") {
+        fc = new JFileChooser()
+        fc.fileSelectionMode = JFileChooser.FILES_ONLY
+        if (fc.showDialog(frame, name) == JFileChooser.APPROVE_OPTION) {
+            return fc.selectedFile
+        } else {
+            return null
+        }
+    }
+
+    fileNew(EventObject evt = null) {
+      (new Console()).run()
+    }
+
+    fileOpen(EventObject evt = null) {
+        scriptFile = selectFilename();
+        if (scriptFile != null) {
+            textArea.text = scriptFile.readLines().join('\n');
+            setDirty(false)
+            textArea.caretPosition = 0
+        }
+    }
+
+    boolean fileSave(EventObject evt = null) {
+        if (scriptFile == null) {
+            scriptFile = selectFilename("Save");
+        }
+        if (scriptFile != null) {
+            scriptFile.write(textArea.text)
+            setDirty(false);
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    runScript(EventObject evt = null) {
         text = textArea.getText()
         scriptList.add(text)
 
@@ -150,23 +191,23 @@ class Console extends ConsoleSupport {
         }
     }
 
-    showAbout() {
-        version = InvokerHelper.getVersion()
-        pane = swing.optionPane(message:'Welcome to the Groovy Console for evaluating Groovy scripts\nVersion ' + version)
-        dialog = pane.createDialog(frame, 'About GroovyConsole')
-        dialog.show()
-    }
-
     protected void handleException(String text, Exception e) {
         pane = swing.optionPane(message:'Error: ' + e.getMessage() + '\nafter compiling: ' + text)
         dialog = pane.createDialog(frame, 'Compile error')
         dialog.show()
     }
 
-    exit() {
-   if (scriptFile != null && dirty) {
-       switch (JOptionPane.showConfirmDialog(frame,
-       "Save changes to " + scriptFile.name + "?",
+    showAbout(EventObject evt = null) {
+        version = InvokerHelper.getVersion()
+        pane = swing.optionPane(message:'Welcome to the Groovy Console for evaluating Groovy scripts\nVersion ' + version)
+        dialog = pane.createDialog(frame, 'About GroovyConsole')
+        dialog.show()
+    }
+
+    exit(EventObject evt = null) {
+        if (scriptFile != null && dirty) {
+            switch (JOptionPane.showConfirmDialog(frame,
+                "Save changes to " + scriptFile.name + "?",
                 "GroovyConsole", JOptionPane.YES_NO_CANCEL_OPTION))
             {
                 case JOptionPane.YES_OPTION:
@@ -182,40 +223,4 @@ class Console extends ConsoleSupport {
         }
     }
 
-    fileNew() {
-      (new Console()).run()
-    }
-
-    fileOpen() {
-        scriptFile = selectFilename();
-        if (scriptFile != null) {
-            textArea.text = scriptFile.readLines().join('\n');
-            setDirty(false)
-            textArea.caretPosition = 0
-        }
-    }
-
-    boolean fileSave() {
-        if (scriptFile == null) {
-            scriptFile = selectFilename("Save");
-        }
-        if (scriptFile != null) {
-            scriptFile.write(textArea.text)
-            setDirty(false);
-            return true
-        } else {
-            return false
-        }
-    }
-
-
-    selectFilename(name = "Open") {
-        fc = new JFileChooser()
-        fc.fileSelectionMode = JFileChooser.FILES_ONLY
-        if (fc.showDialog(frame, name) == JFileChooser.APPROVE_OPTION) {
-            return fc.selectedFile
-        } else {
-            return null
-        }
-    }
 }
