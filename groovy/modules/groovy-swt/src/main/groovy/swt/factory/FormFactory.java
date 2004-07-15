e722a125d388ceec97b01a0c97721b6d6c27fbe7
Christiaan ten Klooster <ckl@dacelo.nl>
2004-07-15 13:46:00 +0000

    provide access to formtoolkit
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1321 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/groovy-swt/src/main/groovy/swt/factory/FormFactory.java
+++ groovy/modules/groovy-swt/src/main/groovy/swt/factory/FormFactory.java
@@ -33,6 +33,13 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
     /** static is evil, too many toolkits is evil */
     protected static FormToolkit toolkit;
 
+    /**
+     * @return Returns the toolkit.
+     */
+    public static FormToolkit getToolkit() {
+        return toolkit;
+    }
+
     /** type of */
     private String type;
 
@@ -47,10 +54,9 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
      * @see groovy.swt.factory.AbstractSwtFactory#newInstance(java.util.Map,
      *      java.lang.Object)
      */
-    public Object newInstance(Map properties, Object parent)
-            throws GroovyException {
-        Composite parentComposite = (Composite) SwtUtils
-                .getParentWidget(parent);
+    public Object newInstance(Map properties, Object parent) throws GroovyException {
+        Composite parentComposite = (Composite) SwtUtils.getParentWidget(parent);
+
         if (toolkit == null) {
             FormColors formColors = new FormColors(Display.getCurrent());
             toolkit = new FormToolkit(formColors);
@@ -67,13 +73,13 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
             }
         }
         if (parentComposite != null) {
-            Object formWidget = getFormWidget(parentComposite, properties,
-                    style, text);
+            Object formWidget = getFormWidget(parentComposite, properties, style, text);
             setBeanProperties(formWidget, properties);
 
             // if (formWidget instanceof Control) {
             //  	Control control = (Control) parent;
-            //      control.setData(FormToolkit.KEY_DRAW_BORDER, FormToolkit.TEXT_BORDER);
+            //      control.setData(FormToolkit.KEY_DRAW_BORDER,
+            // FormToolkit.TEXT_BORDER);
             // }
 
             // if (parent instanceof Composite) {
@@ -93,41 +99,50 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
      * @param text
      * @return
      */
-    private Object getFormWidget(final Composite parentComposite,
-            Map properties, int style, String text) throws GroovyException {
+    private Object getFormWidget(final Composite parentComposite, Map properties, int style,
+            String text) throws GroovyException {
         if ("form".equals(type)) {
             Form form = toolkit.createForm(parentComposite);
             form.setText(text);
             return form;
         }
         if ("scrolledForm".equals(type)) {
-            ScrolledForm scrolledForm = toolkit
-                    .createScrolledForm(parentComposite);
+            ScrolledForm scrolledForm = toolkit.createScrolledForm(parentComposite);
             scrolledForm.setText(text);
             return scrolledForm;
         }
-        if ("formButton".equals(type)) { return toolkit.createButton(
-                parentComposite, text, style); }
-        if ("formColors".equals(type)) { return toolkit.getColors(); }
-        if ("formComposite".equals(type)) { return toolkit.createComposite(
-                parentComposite, style); }
-        if ("formCompositeSeparator".equals(type)) { return toolkit
-                .createCompositeSeparator(parentComposite); }
-        if ("formExpandableComposite".equals(type)) { return toolkit
-                .createExpandableComposite(parentComposite, style); }
+        if ("formButton".equals(type)) {
+            return toolkit.createButton(parentComposite, text, style);
+        }
+        if ("formColors".equals(type)) {
+            return toolkit.getColors();
+        }
+        if ("formComposite".equals(type)) {
+            return toolkit.createComposite(parentComposite, style);
+        }
+        if ("formCompositeSeparator".equals(type)) {
+            return toolkit.createCompositeSeparator(parentComposite);
+        }
+        if ("formExpandableComposite".equals(type)) {
+            return toolkit.createExpandableComposite(parentComposite, style);
+        }
         if ("formText".equals(type)) {
             Text text2 = toolkit.createText(parentComposite, text, style);
             toolkit.paintBordersFor(parentComposite);
             return text2;
         }
-        if ("formHyperlink".equals(type)) { return toolkit.createHyperlink(
-                parentComposite, text, style); }
-        if ("formImageHyperlink".equals(type)) { return toolkit
-                .createImageHyperlink(parentComposite, style); }
-        if ("formLabel".equals(type)) { return toolkit.createLabel(
-                parentComposite, text, style); }
-        if ("formPageBook".equals(type)) { return toolkit.createPageBook(
-                parentComposite, style); }
+        if ("formHyperlink".equals(type)) {
+            return toolkit.createHyperlink(parentComposite, text, style);
+        }
+        if ("formImageHyperlink".equals(type)) {
+            return toolkit.createImageHyperlink(parentComposite, style);
+        }
+        if ("formLabel".equals(type)) {
+            return toolkit.createLabel(parentComposite, text, style);
+        }
+        if ("formPageBook".equals(type)) {
+            return toolkit.createPageBook(parentComposite, style);
+        }
         if ("formPageBookPage".equals(type)) {
             if (parentComposite instanceof ScrolledPageBook) {
                 ScrolledPageBook pageBook = (ScrolledPageBook) parentComposite;
@@ -146,8 +161,7 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
             if (text != null) {
                 section.setText(text);
             }
-            section.setSeparatorControl(toolkit
-                    .createCompositeSeparator(section));
+            section.setSeparatorControl(toolkit.createCompositeSeparator(section));
             String description = (String) properties.remove("description");
             if (description != null) {
                 section.setDescription(description);
@@ -157,23 +171,25 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
             section.setClient(client);
             return section;
         }
-        if ("formSeparator".equals(type)) { return toolkit.createSeparator(
-                parentComposite, style); }
-        if ("formTable".equals(type)) { return toolkit.createTable(
-                parentComposite, style); }
-        if ("formToolkit".equals(type)) { return toolkit; }
+        if ("formSeparator".equals(type)) {
+            return toolkit.createSeparator(parentComposite, style);
+        }
+        if ("formTable".equals(type)) {
+            return toolkit.createTable(parentComposite, style);
+        }
+        if ("formToolkit".equals(type)) {
+            return toolkit;
+        }
         if ("formFormattedText".equals(type)) {
             boolean parseTags = false;
             boolean expandURLs = false;
             if (properties.get("parseTags") != null) {
-                parseTags = ((Boolean) properties.remove("parseTags"))
-                        .booleanValue();
+                parseTags = ((Boolean) properties.remove("parseTags")).booleanValue();
             }
             if (properties.get("expandURLs") != null) {
-                expandURLs = ((Boolean) properties.remove("expandURLs"))
-                        .booleanValue();
+                expandURLs = ((Boolean) properties.remove("expandURLs")).booleanValue();
             }
-            
+
             FormText formText = toolkit.createFormText(parentComposite, true);
             HyperlinkSettings hyperlinkSettings = new HyperlinkSettings(Display.getCurrent());
             hyperlinkSettings.setBackground(toolkit.getColors().getBackground());
@@ -181,13 +197,14 @@ public class FormFactory extends AbstractSwtFactory implements SwtFactory {
             hyperlinkSettings.setForeground(toolkit.getColors().getForeground());
             hyperlinkSettings.setActiveForeground(toolkit.getColors().getBackground());
             formText.setHyperlinkSettings(hyperlinkSettings);
-            
+
             formText.setText(text, parseTags, expandURLs);
 
             return formText;
         }
-        if ("formTree".equals(type)) { return toolkit.createTree(
-                parentComposite, style); }
+        if ("formTree".equals(type)) {
+            return toolkit.createTree(parentComposite, style);
+        }
         return null;
     }
 }
