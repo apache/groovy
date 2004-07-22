526d620975d22666b649c5212a6b6c812f5a5adf
Jeremy Rayner <jeremy.rayner@gmail.com>
2004-07-22 06:45:00 +0000

    Added test case to prove GROOVY-467 is no longer an issue
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1362 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/AmbiguousInvocationTest.groovy
+++ groovy/groovy-core/src/test/groovy/AmbiguousInvocationTest.groovy
@@ -1,20 +1,21 @@
+import groovy.DummyMethods
+
 class AmbiguousInvocationTest extends GroovyTestCase { 
     // to prove GROOVY-467 is no longer an issue    
-    property mock
+    property dummy
     
     void setUp() {
-    // @todo as DummyMethods.java not being compiled prior to test run in maven build
-    //	dummy = new DummyMethods()
+    	dummy = new DummyMethods()
     }
     
     void testAmbiguousInvocationWithFloats() {
-   	//assert "float args" == dummy.foo("bar",1.0f,2.0f)
-   	//assert "float args" == dummy.foo("bar",(float)1,(float)2)
-   	//assert "float args" == dummy.foo("bar",(Float)1,(Float)2)
+   	assert "float args" == dummy.foo("bar",1.0f,2.0f)
+   	assert "float args" == dummy.foo("bar",(float)1,(float)2)
+   	assert "float args" == dummy.foo("bar",(Float)1,(Float)2)
     }
     void testAmbiguousInvocationWithInts() {
-   	//assert "int args" == dummy.foo("bar",1,2)
-   	//assert "int args" == dummy.foo("bar",(int)1,(int)2)
-   	//assert "int args" == dummy.foo("bar",(Integer)1,(Integer)2)
+   	assert "int args" == dummy.foo("bar",1,2)
+   	assert "int args" == dummy.foo("bar",(int)1,(int)2)
+   	assert "int args" == dummy.foo("bar",(Integer)1,(Integer)2)
     }
 } 
