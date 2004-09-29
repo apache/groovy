263d38f4fb7b17a5193f26162722462f5461f58d
Codehaus infra <support@codehaus.org>
2004-09-29 15:31:50 +0000

    This commit was manufactured by cvs2svn to create tag 'GROOVY_1_0_BETA_7'.
    git-svn-id: http://svn.codehaus.org/groovy/tags/GROOVY_1_0_BETA_7@1435 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/process/src/test_scripts/cat_to_file.groovy
+++
@@ -1,5 +0,0 @@
-gsh = new com.baulsupp.groovy.groosh.Groosh();
-
-gsh.cat('test_scripts/blah.txt').toFile(new java.io.File('blah.out'));
-
-
--- groovy/modules/process/src/test_scripts/stdin_cat.groovy
+++
@@ -1,6 +0,0 @@
-gsh = new com.baulsupp.groovy.groosh.Groosh();
-
-// does this fail if you change c to cat?
-gsh.cat().fromStdIn().toStdOut();
-
-
