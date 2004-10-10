a5d4e758317a687006e6057effc9880521e3c9e0
Guillaume Laforge <glaforge@gmail.com>
2004-10-10 14:27:17 +0000

    - GROOVY-646: applied patch from Brian to allow to use the InteractiveShell (groovysh) as an embedded remote shell inside another running application. - GROOVY-647: added a "binding" command which lists all the available variables/values bound in the current context/binding
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1448 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/org/codehaus/groovy/sandbox/ui/PromptFactory.java
+++ groovy/groovy-core/src/main/org/codehaus/groovy/sandbox/ui/PromptFactory.java
@@ -1,24 +1,31 @@
 package org.codehaus.groovy.sandbox.ui;
 
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 /**
  * Factory to build a command line prompt.  Should build the most featureful
  * prompt available.
- * 
- * Currently readline prompt will be looked up dynamically, and defaults to 
+ * <p/>
+ * Currently readline prompt will be looked up dynamically, and defaults to
  * normal System.in prompt.
  */
-public class PromptFactory {
-  public static Prompt buildPrompt() throws IOException {
-    try {
-      return (Prompt) Class.forName("org.codehaus.groovy.sandbox.ui.ReadlinePrompt").newInstance();
-    } catch (ClassNotFoundException e) {
-      // nothing
-      return new JavaPrompt();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return new JavaPrompt();
+public class PromptFactory
+{
+    public static Prompt buildPrompt(InputStream in, PrintStream out, PrintStream err)
+    {
+        try
+        {
+            return (Prompt) Class.forName("org.codehaus.groovy.sandbox.ui.ReadlinePrompt").newInstance();
+        }
+        catch (ClassNotFoundException e)
+        {
+            return new JavaPrompt(in, out, err);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return new JavaPrompt(in, out, err);
+        }
     }
-  }
 }
