94f9ad56dbae86f7fad8ef12d3a0f123dea19e38
James Strachan <jastrachan@mac.com>
2004-12-02 23:55:49 +0000

    added a better script example
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1465 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/gram/src/script/Dump.groovy
+++ groovy/modules/gram/src/script/Dump.groovy
@@ -1,8 +1,11 @@
-println "Hello"
-println classes.length
-
 persistentClasses = classes.findAll { it.getAnnotation("hibernate.class") != null }
 
 println "Found ${persistentClasses.size()} instances out of ${classes.size()}"
 
-persistentClasses.each { println it.simpleName }
+persistentClasses.each { c |
+    println c.simpleName
+
+    for (p in c.properties) {
+        println "  property: ${p.simpleName}"
+    }
+}
