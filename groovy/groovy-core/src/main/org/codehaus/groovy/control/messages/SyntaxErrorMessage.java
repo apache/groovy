2c408cfbfe067d14efb59612c7443b66053b76e2
Sam Pullara <sam@sampullara.com>
2004-12-07 21:53:00 +0000

    Fix the error reporting system to simplify the output of scripts so that people don't see huge unrelated stacktraces.  This new system only prints out errors from non-java files, i.e. groovy files.  If you are a groovy developer and need to see the compelte stack trace, use -d/--debug on the command-line.  Also added System.exit() to the command line so that you can use it in the shell and have it fail properly when it fails.  Before it was returning success no matter what.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1475 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/org/codehaus/groovy/control/messages/SyntaxErrorMessage.java
+++ groovy/groovy-core/src/main/org/codehaus/groovy/control/messages/SyntaxErrorMessage.java
@@ -1,66 +1,57 @@
 package org.codehaus.groovy.control.messages;
 
-import java.io.PrintWriter;
-
 import org.codehaus.groovy.control.Janitor;
 import org.codehaus.groovy.control.ProcessingUnit;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.syntax.SyntaxException;
 
+import java.io.PrintWriter;
 
 
 /**
- *  A class for error messages produced by the parser system.
+ * A class for error messages produced by the parser system.
  *
- *  @author <a href="mailto:cpoirier@dreaming.org">Chris Poirier</a>
- *
- *  @version $Id$
+ * @author <a href="mailto:cpoirier@dreaming.org">Chris Poirier</a>
+ * @version $Id$
  */
 
-public class SyntaxErrorMessage extends Message
-{
+public class SyntaxErrorMessage extends Message {
     protected SyntaxException cause = null;
-    
-    public SyntaxErrorMessage( SyntaxException cause )
-    {
+
+    public SyntaxErrorMessage(SyntaxException cause) {
         this.cause = cause;
     }
-    
-    
-   
-   /**
-    *  Returns the underlying SyntaxException.
-    */
-    
-    public SyntaxException getCause()
-    {
+
+
+    /**
+     * Returns the underlying SyntaxException.
+     */
+
+    public SyntaxException getCause() {
         return this.cause;
     }
-    
 
 
-   /**
-    *  Writes out a nicely formatted summary of the syntax error. 
-    */
-    
-    public void write( PrintWriter output, ProcessingUnit context, Janitor janitor )
-    {
-        SourceUnit source = (SourceUnit)context;   // This is reliably true
+    /**
+     * Writes out a nicely formatted summary of the syntax error.
+     */
 
-        String name   = source.getName();
-        int    line   = getCause().getStartLine();
-        int    column = getCause().getStartColumn();
-        String sample = source.getSample( line, column, janitor );
-        
-        output.println( name + ": " + line + ": " + getCause().getMessage() );
-        if( sample != null )
-        {
-            output.println( source.getSample(line, column, janitor) );
+    public void write(PrintWriter output, ProcessingUnit context, Janitor janitor) {
+        SourceUnit source = (SourceUnit) context;   // This is reliably true
+
+        String name = source.getName();
+        int line = getCause().getStartLine();
+        int column = getCause().getStartColumn();
+        String sample = source.getSample(line, column, janitor);
+
+        output.print(name + ": " + line + ": " + getCause().getMessage());
+        if (sample != null) {
+            output.println();
+            output.print(source.getSample(line, column, janitor));
         }
-        output.println("");
-    }    
-    
-    
+    }
+
+
 }
 
 
