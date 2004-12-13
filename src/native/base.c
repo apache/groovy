8054a5ed8bdc33f6eb056339547b75e9798d942e
Sam Pullara <sam@sampullara.com>
2004-12-13 22:26:09 +0000

    Add a native executable, ideal for scripts.  Currently only works on Mac.  Would love to see versions for other platforms.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1509 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/native/base.c
@@ -0,0 +1,41 @@
+#include <errno.h>
+#include <sys/syslimits.h>
+#include <mach-o/dyld.h>
+
+int main(int argc, char* argv[]) {
+
+  // Get the location of the executable -- platform specific
+  char jarexe[PATH_MAX];
+  unsigned long length;
+  _NSGetExecutablePath(jarexe, &length);
+
+  // Setup the command line.
+  // TODO: Add -J support for passing Java options
+  char* argv2[argc+2];
+  argv2[0] = "java";
+  argv2[1] = "groovy.lang.GroovyShell";
+
+  // Setup the CLASSPATH environment
+  char *CLASSPATH = (char*) getenv("CLASSPATH");
+  if (!CLASSPATH) CLASSPATH="";
+  char *NEWCLASSPATH = (char*) calloc(strlen(CLASSPATH)+1+strlen(jarexe)+1); // +1 for :, +1 for null terminator
+  sprintf(NEWCLASSPATH, "%s:%s", jarexe, CLASSPATH);
+  setenv("CLASSPATH", NEWCLASSPATH, 1);
+
+  // Setup the rest of the command line that was passed in.
+  // TODO: This will also be affected by -J options
+  int i;
+  for (i = 1; i < argc; i++) {
+    argv2[i+1] = argv[i];
+  }
+  argv2[argc+1] = 0;
+
+  // Execute java
+  execvp("java", argv2);
+
+  // Report if the exec fails
+  printf("Cannot execute '");
+  for (i = 0; i < argc+1; i ++) { printf("%s ", argv2[i]); }
+  printf("', caused by error: %d\n", errno);
+
+}
