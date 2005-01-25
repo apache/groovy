03df9738223b59b6a8615380bd991dd4f9c43d47
Jeremy Rayner <jeremy.rayner@gmail.com>
2005-01-25 20:47:10 +0000

    * added a proposal for possible behaviour of closure parameters in the jsr grammar
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1649 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/jsr/tck/test/proposals/Closures.groovy
@@ -0,0 +1,15 @@
+/**
+ * Closure Example test cases.
+ */
+class Closures {
+    def someClosures() {
+        // c1 = {print "hello"}   //@pass
+        // c2 = {foo| print foo}  //@fail:parse
+        // c3 = {|bar| print bar} //@pass
+        // c4 = {|String telson| println telson} //@pass
+        // c5 = {|Cool car, funky, Moped yeah| println yeah + funky + car} //@pass
+        // c6 = {|String name, age| println "${name) is ${age} years old"} //@pass
+        // myList.each{print it}   //@pass
+        // myList.each{foo|print foo} //@pass
+  }
+}
