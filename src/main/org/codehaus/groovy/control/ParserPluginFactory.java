c8452ef576eeffa7c00cee19e39b9095d43665ff
James Strachan <jastrachan@mac.com>
2005-01-26 12:41:37 +0000

    Added a factory class so that we can resolve the parser plugin just once, then cache it around and be able to make new instances quickly without looking in class loaders etc
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1653 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/main/org/codehaus/groovy/control/ParserPluginFactory.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * Copyright 2004 James Strachan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+package org.codehaus.groovy.control;
+
+import org.codehaus.groovy.runtime.InvokerHelper;
+
+/**
+ * A factory of parser plugin instances
+ *
+ * @version $Revision$
+ */
+public abstract class ParserPluginFactory {
+
+    public static ParserPluginFactory newInstance() {
+        try {
+            Class type = Class.forName("org.codehaus.groovy.antlr.AntlrParserPluginFactory");
+            try {
+                return (ParserPluginFactory) type.newInstance();
+            }
+            catch (Exception e) {
+                throw new RuntimeException("Could not create AntlrParserPluginFactory: " + e, e);
+            }
+        }
+        catch (ClassNotFoundException e) {
+            // ignore
+        }
+        // can't find Antlr parser, so lets use the Classic one
+        return new ClassicParserPluginFactory();
+    }
+
+    public abstract ParserPlugin createParserPlugin();
+}
