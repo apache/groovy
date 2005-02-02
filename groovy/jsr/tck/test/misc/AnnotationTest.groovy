31329a261514572fcf87b2b7207008cd4b02d117
Jeremy Rayner <jeremy.rayner@gmail.com>
2005-02-02 00:02:12 +0000

    * added test case demonstrating various annotations associated with variable declarations
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1706 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/jsr/tck/test/misc/AnnotationTest.groovy
@@ -0,0 +1,15 @@
+class AnotationTest {
+    @property String foo
+    @property protected def bar
+    protected @property def mooky
+    
+    @property
+    String wibble = "wobble"
+
+    @property Cheese gouda, edam, wensleydale, gorgonzola, parmesan,
+    mozarella, 
+    cheddar
+
+    def bazouki = "off" 
+}
+
