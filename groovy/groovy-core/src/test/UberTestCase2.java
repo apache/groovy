bd42d65fa0a549aa1532c05097188813deed289a
Pilho Kim <phkim@cluecom.co.kr>
2005-02-03 15:28:24 +0000

    Add to test Groovy675_Bug.groovy
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1734 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/UberTestCase2.java
+++ groovy/groovy-core/src/test/UberTestCase2.java
@@ -21,6 +21,7 @@ public class UberTestCase2 extends TestCase {
         suite.addTestSuite(groovy.bugs.Groovy308_Bug.class);
         suite.addTestSuite(groovy.bugs.Groovy558_616_Bug.class);
         suite.addTestSuite(groovy.bugs.Groovy666_Bug.class);
+        suite.addTestSuite(groovy.bugs.Groovy675_Bug.class);
         suite.addTestSuite(groovy.bugs.IanMaceysBug.class);
         suite.addTestSuite(groovy.bugs.InterfaceImplBug.class);
         suite.addTestSuite(groovy.bugs.MarkupInScriptBug.class);
