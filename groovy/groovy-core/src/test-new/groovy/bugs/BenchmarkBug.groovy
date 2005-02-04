075182a8f002599a46a15fd9a379f5bf8a35307a
James Strachan <jastrachan@mac.com>
2005-02-04 19:12:31 +0000

    A bunch more working tests
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1758 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test-new/groovy/bugs/BenchmarkBug.groovy
+++ groovy/groovy-core/src/test-new/groovy/bugs/BenchmarkBug.groovy
@@ -5,24 +5,26 @@
 class BenchmarkBug extends GroovyTestCase {
     
     void testPerformance() {
-		start = System.currentTimeMillis()
-		
-		total = 0
-		size = 10000
-		for (i in 0..size) {
-		    total = total + callSomeMethod("hello", total)
-		} 
-		
-		end = System.currentTimeMillis()
-		
-		time = end - start
-		
-		println "Performed ${size} iterations in ${time / 1000} seconds which is ${time / size} ms per iteration"
-		
-		assert total == size * 10 + 10
+        start = System.currentTimeMillis()
+
+        total = 0
+        size = 10000
+        for (i in 0..size) {
+            total = total + callSomeMethod("hello", total)
+        }
+
+        end = System.currentTimeMillis()
+
+        time = end - start
+
+        println "Performed ${size} iterations in ${time / 1000} seconds which is ${time / size} ms per iteration"
+
+        // TODO: parser bug
+        // assert total == size * 10 + 10
+        assert total == 100010
     }
     
-    callSomeMethod(text, total) {
+    def callSomeMethod(text, total) {
         return 10
     }
 }
