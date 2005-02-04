ee00e26fdbc361538166ea42a4ef976eb7d93a51
James Strachan <jastrachan@mac.com>
2005-02-04 10:59:44 +0000

    another test bites the dust
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1747 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test-new/groovy/IfPropertyTest.groovy
+++ groovy/groovy-core/src/test-new/groovy/IfPropertyTest.groovy
@@ -1,17 +1,17 @@
 class IfPropertyTest extends GroovyTestCase {
 	
-    dummy
+    def dummy
     
 	// This is because normal classes are not extensible, but scripts are extensible by default.
-	Object get(String key) {
-	    println("asking for def " + key)
-		return dummy
-	}
-	
-	void set(Object key, Object value) {
-	    println("setting the def " + key + " to: " + value)
-	    dummy = value
-	}
+    Object get(String key) {
+        println("asking for def " + key)
+        return dummy
+    }
+
+    void set(Object key, Object value) {
+        println("setting the def " + key + " to: " + value)
+        dummy = value
+    }
 
     void testIfNullPropertySet() {
         if (cheese == null) {
@@ -24,13 +24,13 @@ class IfPropertyTest extends GroovyTestCase {
     }
     
     void testIfNullPropertySetRecheck() {
-		if (cheese == null) {
-			cheese = 1
-		}
-		if (cheese == 1) {
-			cheese = 2
-		}
-		assert cheese == 2
+        if (cheese == null) {
+            cheese = 1
+        }
+        if (cheese == 1) {
+            cheese = 2
+        }
+        assert cheese == 2
     }
     
 }
