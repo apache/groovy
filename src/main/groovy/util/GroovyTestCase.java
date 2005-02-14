d0af94fe7b555f839941b323936badd46500186b
Jochen Theodorou <blackdrag@gmx.org>
2005-02-14 18:48:04 +0000

    corrected a little cut&paste bug
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1797 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/util/GroovyTestCase.java
+++ src/main/groovy/util/GroovyTestCase.java
@@ -102,7 +102,7 @@ public class GroovyTestCase extends TestCase {
     protected void assertArrayEquals(Object[] expected, Object[] value) {
         String message =
             "expected array: " + InvokerHelper.toString(expected) + " value array: " + InvokerHelper.toString(value);
-        assertNotNull(message + ": expected should not be null", value);
+        assertNotNull(message + ": expected should not be null", expected);
         assertNotNull(message + ": value should not be null", value);
         assertEquals(message, expected.length, value.length);
         for (int i = 0, size = expected.length; i < size; i++) {
