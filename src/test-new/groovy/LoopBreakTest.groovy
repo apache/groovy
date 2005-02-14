e459494ad08085e0bf829a5de3f1b4d1818deda4
James Strachan <jastrachan@mac.com>
2005-02-14 15:54:35 +0000

    Reordered the parser-bugs in the AllTests.java test case into priority order, so we can focus on fixing the high priority stuff first. Got a few more tests working too
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1794 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test-new/groovy/LoopBreakTest.groovy
+++ src/test-new/groovy/LoopBreakTest.groovy
@@ -1,40 +1,38 @@
 class LoopBreakTest extends GroovyTestCase {
 
     void testWhileWithBreak() {
-        x = 0
+        def x = 0
         while (true) {
             if (x == 5) {
                 break
             }
-            x++
-            
+            ++x
+
             assert x < 10 , "Should never get here"
         }
         
         println "worked: while completed with value ${x}"
-	}
+    }
     
     
     void testDoWhileWithBreak() {
-        x = 0
+        def x = 0
         do {
             //println "in do-while loop and x = ${x}"
             
             if (x == 5) {
                 break
             }
-            x++
+            ++x
             
             assert x < 10 , "Should never get here"
         }
-    	while (true)
+        while (true)
         
         println "worked: do-while completed with value ${x}"
     }
 
     void testForWithBreak() {
-        /** @todo commenting out the x = 0 causes bytecode gen error */
-        x = 0
         for (x in 0..20) {
             if (x == 5) {
                 break
