8a91bbdec6553a97a4d0b69a425903845a0aba67
James Strachan <jastrachan@mac.com>
2005-02-14 13:26:26 +0000

    More working tests and fixed bug in AST generation for named  parameters, with closure arguments
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1791 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test-new/groovy/gravy/Build.groovy
+++ src/test-new/groovy/gravy/Build.groovy
@@ -5,60 +5,60 @@ package groovy.gravy
  * Represents a build process
  */
 class Build implements Runnable {
-	ant = new AntBuilder()
-	args
-	pom
-	defaultTargets = ['clean', 'compile']
-	
-	static void main(args) {
-		// autogenerate this
-		b = new Build(args)
-		//b.args = args
-		b.run()
-	}
-	
-	Build(someArgs) {
-		args = someArgs;
-		if (args instanceof String) {
-			args = [args]
-		}
-		/*
-		if (args == null || args.size() == 0) {
-			args = defaultTargets
-		}
-		*/
-	}
-	
-	void run() {
-		for (a in args) {
-			println "Target: ${a}"
-			invokeMethod(a.toString(), null)
-		}
-	}
-	
-	getPom() {
-		if (pom == null) {
-			pom = parsePOM()
-		}
-		return pom
-	}
+    def ant = new AntBuilder()
+    def args
+    def pom
+    def defaultTargets = ['clean', 'compile']
+
+    static void main(args) {
+        // autogenerate this
+        b = new Build(args)
+        //b.args = args
+        b.run()
+    }
+
+    Build(someArgs) {
+        args = someArgs;
+        if (args instanceof String) {
+            args = [args]
+        }
+        /*
+        if (args == null || args.size() == 0) {
+            args = defaultTargets
+        }
+        */
+    }
+
+    void run() {
+        for (a in args) {
+            println "Target: ${a}"
+            invokeMethod(a.toString(), null)
+        }
+    }
+
+    def getPom() {
+        if (pom == null) {
+            pom = parsePOM()
+        }
+        return pom
+    }
 
 
-	// Default goals
-	clean() {
-		ant.rmdir(dir:'gravy')
-	}
-	
-	compile() {
-		ant.mkdir(dir:'gravy/classes')
-		ant.compile(srdir:'src/main/java', destdir:'gravy/classes') {
-			fileset {
-				includes(name:'**/*.java')
-			}
-		}
-	}
-		
-	protected parsePOM() {
-		return new XmlParser().parse("project.xml")
-	}
+    // Default goals
+    def clean() {
+        ant.rmdir(dir:'gravy')
+    }
+
+    def compile() {
+        ant.mkdir(dir:'gravy/classes')
+        ant.compile(srdir:'src/main/java', destdir:'gravy/classes') {
+            fileset {
+                includes(name:'**/*.java')
+            }
+        }
+    }
+
+    protected def parsePOM() {
+        return new XmlParser().parse("project.xml")
+    }
 }
