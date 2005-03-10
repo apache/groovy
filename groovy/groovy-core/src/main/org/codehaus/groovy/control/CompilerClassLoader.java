d380467f3d0e9e5127c124d4780f2d0d80f31831
Jochen Theodorou <blackdrag@gmx.org>
2005-03-10 19:20:58 +0000

    allows the compiler to load classes but ignore the normal class loading mechanisms and use the classes given on the classpath in the first place
    git-svn-id: http://svn.codehaus.org/groovy/trunk@1959 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/org/codehaus/groovy/control/CompilerClassLoader.java
+++ groovy/groovy-core/src/main/org/codehaus/groovy/control/CompilerClassLoader.java
@@ -4,20 +4,76 @@ import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CompilerClassLoader
-    extends URLClassLoader
-{
+public class CompilerClassLoader extends ClassLoader {
+
     private static final URL[] EMPTY_URL_ARRAY = new URL[0];
+    private ClassLoader parent;
+    private InnerLoader inner;
+    private Map map;
+    
+    private class InnerLoader extends URLClassLoader{
+        public InnerLoader() {
+            super(EMPTY_URL_ARRAY);
+        }
 
+        public void addPath(String path) throws MalformedURLException{
+            addURL( new File( path ).toURL() );
+        }        
+        
+        protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            Class c = (Class) map.get(name);
+            if (c!=null) return c;
+            try {
+                // we prefer to search on the given paths first
+                c = super.findClass(name);
+                map.put(name,c);
+                return c;
+            } catch (ClassNotFoundException cnfe) {
+                c = parent.loadClass(name);
+                map.put(name,c);
+                return c;
+            } 
+        }   
+        
+        public Class loadClass(String name) throws ClassNotFoundException {
+            return loadClass(name,false);
+        }
+        
+        protected Class findClass(String name) throws ClassNotFoundException {
+            return loadClass(name,false);
+        }
+        
+    }
+    
+    
+    
     public CompilerClassLoader()
     {
-        super( EMPTY_URL_ARRAY );
+        super(Thread.currentThread().getContextClassLoader());
+        parent = Thread.currentThread().getContextClassLoader();
+        inner = new InnerLoader();
+        map = new HashMap();
+        //super(EMPTY_URL_ARRAY);
     }
 
     public void addPath(String path)
         throws MalformedURLException
     {
-        addURL( new File( path ).toURL() );
+        inner.addPath(path);
+    }
+    
+    protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        return inner.loadClass(name);
+    }
+    
+    public Class loadClass(String name) throws ClassNotFoundException {
+        return loadClass(name,false);
+    }
+    
+    protected Class findClass(String name) throws ClassNotFoundException {
+        return loadClass(name,false);
     }
 }
