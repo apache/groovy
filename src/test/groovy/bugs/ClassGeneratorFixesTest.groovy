f88a55105526e3278ee519cfbc0dff06b3468a98
Guillaume Laforge <glaforge@gmail.com>
2005-03-11 13:36:14 +0000

    foo = {i :: {j :: i += j; i} } is ambiguous, in that case, the return keyword is needed, like in foo = {i :: return {j :: i += j; i} }
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1965 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/ClassGeneratorFixesTest.groovy
+++ src/test/groovy/bugs/ClassGeneratorFixesTest.groovy
@@ -19,7 +19,7 @@ class ClassGeneratorFixesTest extends GroovyTestCase {
 
         foo =
             {i::
-                {j::
+                return {j::
                     i += j
                     i
                 }
