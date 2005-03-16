21434767b3271ae5e976bccb869feaa61389ae3f
Jeremy Rayner <jeremy.rayner@gmail.com>
2005-03-16 08:23:24 +0000

    * Commented out tests in UberTestCase* , this is not because it is the easy way out, but people are confused that CVSHEAD doesn't have 100% pass rate. These have been commented out on the premise that we can get useful feedback from Continuous Integration testing as we gradually fix each of the commented out tests. * For JSR-01 we would like 100% of these tests working again
    * Also modified maven.xml so that you don't need to supply 'generate' to each invocation
    * i.e. you can now do the old style 'maven clean'  and 'maven' to clean and build with tests respectively.
    
    * included a small ant script that has a default target to run junitreport over results of groovy-core tests.
    
    
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@1988 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/antlr/parser/GroovyTokenTypes.java
+++ src/main/org/codehaus/groovy/antlr/parser/GroovyTokenTypes.java
@@ -1,4 +1,4 @@
-// $ANTLR 2.7.5 (20050128): "groovy.g" -> "GroovyRecognizer.java"$
+// $ANTLR 2.7.2: "groovy.g" -> "GroovyRecognizer.java"$
 
 package org.codehaus.groovy.antlr.parser;
 import org.codehaus.groovy.antlr.*;
