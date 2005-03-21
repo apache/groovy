c6354a7d992a053c309bb38c1aa71d94103b49ea
Jochen Theodorou <blackdrag@gmx.org>
2005-03-21 22:42:29 +0000

    added a save option for a more easy start
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2005 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/org/codehaus/groovy/tools/FailsGenerator.groovy
+++ groovy/groovy-core/src/main/org/codehaus/groovy/tools/FailsGenerator.groovy
@@ -7,13 +7,56 @@ class FailsGenerator {
 
     static def conf;
     static def map=new HashMap();
+    static def save=false
+    static def hasChanged=false
     
     public FailsGenerator(){}    
 
     static void main(args) {
-        conf = new File(args[0])
+        if (args.length<2) {
+            println "usage: FailsGenerator [--save] <conf-file> <reports-dir>"
+            println "         save         is optional argument and let the script store the results"
+            println "                      found in the xml files in the conf file"
+            println "         conf-file    is the configuration file"
+            println "         reports-dir  is the directory containing the xml reports from JUnit"
+            System.exit(1)
+        }
+        int argIndex=0
+        if (args[0]=="--save") {
+            save=true
+            argIndex=1
+        }
+        conf = new File(args[argIndex])
         readConf()
-        compareFiles(args[1])
+        compareFiles(args[argIndex+1])
+        if (save) saveConf()
+        println "DONE"
+    }
+    
+    static void saveConf() {
+        if (!save) return
+        if (!hasChanged) {
+            println "no changes to configuration"
+            return
+        }
+        println "saving conf in ${conf}, old configuration is saved in ${conf}.old"
+        println "WARNING: comments are not in the new file"
+        oldConf = new File(conf.absolutePath+".old")
+        bytes = conf.readBytes()
+        out = oldConf.newOutputStream()
+        out << bytes
+        out.close()
+        out = conf.newWriter()
+        try {
+            map.each {
+                 out.writeLine("[${it.key}]")
+                 it.value.each {
+                     out.writeLine("${it.key}=${it.value}")
+                 }
+            }
+        } finally {
+            out.close();
+        }
     }
     
     static void compareFiles(dir) {
@@ -28,16 +71,17 @@ class FailsGenerator {
             } else if (newVal>oldVal) {
                 println("${name}: more ${attName} (from ${oldVal} to ${newVal}");
             }
+            hasChanged = hasChanged || newVal!=oldVal
             return success 
         }
 
     
         dir = new File(dir)
-        if (!dir.isDirectory() throw new RuntimeException("${dir} has to be a directory containg the xml tests reports")
+        if (!dir.isDirectory()) throw new RuntimeException("${dir} has to be a directory containg the xml tests reports")
         dir.eachFileRecurse {file::
             if (!file.getName().endsWith(".xml")) return
-            if (!file.getName().indexOf("\$")>-1) {
-              println("${file.name) is ignored because it's output for a subclass")
+            if (file.getName().indexOf("\$")>-1) {
+              println("${file.name} is ignored because it's output for a subclass")
               return
             }
             node = new XmlSlurper().parse(file);
@@ -46,7 +90,15 @@ class FailsGenerator {
             failureVal = Integer.parseInt(node['@failures'])
             
             el = map.get(name)
-            if (el==null) throw new RuntimeException("unknown test ${name}, please add it to conf file ${conf.name}")
+            if (el==null && !save) throw new RuntimeException("unknown test ${name}, please add it to conf file ${conf.name}")
+            if (el==null && save) {
+                el = new HashMap()
+                el.put("errors","0");
+                el.put("failures","0");
+                println "added configuration for test ${name}"
+                addToMap(name,el)
+                hasChanged=true
+            }
             
             err  = attReader(name,"errors",el.errors,errorVal)
             fail = attReader(name,"failures",el.failures,failureVal)
@@ -113,7 +165,11 @@ class FailsGenerator {
             }
         }
         
-        fileStart()
+        try {
+            fileStart()
+        } finally {
+            reader.close();
+        }
         if (map.size==0) throw new RuntimeException("${conf.name} was empty");
     }
     
