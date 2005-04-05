a60d58b4c6d0ed7498b6154d900e09a9d6c45679
Jeremy Rayner <jeremy.rayner@gmail.com>
2005-04-05 07:19:10 +0000

    * integer divide operator "\" is no longer in spec.  instead use x.intdiv(y)
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2087 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/lang/ScriptIntegerDivideTest.java
+++ groovy/groovy-core/src/test/groovy/lang/ScriptIntegerDivideTest.java
@@ -6,31 +6,14 @@ import org.codehaus.groovy.control.CompilationFailedException;
 
 /**
  * @author Steve Goetze
+ * @author Jeremy Rayner
  */
 public class ScriptIntegerDivideTest extends TestSupport {
 
 	/**
-	 * If a script that invokes integer division is specified as a literal string, the
-	 * division token "\" needs to be escaped. 
+	 * Check integer division which is now a method call rather than the symbol "\".
 	 */
 	public void testIntegerDivide() throws Exception {
-   		assertScript( "assert 4\\3 == 1" );
-    }
-
-	/**
-	 * If a groovy script is spec'd in a literal string, any intended Integer
-	 * divisions need to be escaped.  In cases where the integer division token "\"
-	 * is surrounded by whitespace the literal cannot be constructed, which is OK.
-	 * In the case where the second operand immediately follows the token, the literal
-	 * can be specified, but will result in a compilation error as the token immediately
-	 * following the backslash will be taken as the escape sequence.
-	 */
-	public void testIntegerDivideWithBadEscape() throws Exception {
-    	try {
-    		assertScript( "assert 4\3 == 1" );
-    	} catch (CompilationFailedException e) {
-    		return;
-    	}
-    	fail("Should catch a CompilationFailedException");
+   		assertScript( "assert 4.intdiv(3) == 1" );
     }
 }
