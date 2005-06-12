330885310b0c908935bcf26aea6fe083aa631d19
Guillaume Laforge <glaforge@gmail.com>
2005-06-12 20:23:15 +0000

    Not a gstring but a bad property name
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2298 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/CompilerErrorTest.groovy
+++ groovy/groovy-core/src/test/groovy/CompilerErrorTest.groovy
@@ -19,7 +19,7 @@ class CompilerErrorTest extends GroovyTestCase {
             def shell = new GroovyShell()
             shell.evaluate """
                 def x = [:]
-                x.$foo = 123
+                x.\$foo = 123
             """
         }
     }
@@ -29,7 +29,7 @@ class CompilerErrorTest extends GroovyTestCase {
         shouldFail {
             def shell = new GroovyShell()
             shell.evaluate """
-                def $x = 123
+                def \$x = 123
             """
         }
     }
