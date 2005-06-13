95dddc3cb2a658f556bbdf32e480d2fb14bf1185
Dierk Koenig <dierk.koenig@canoo.com>
2005-06-13 19:27:49 +0000

    clarify the use of multi-dim arrays and provide a workaround
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2320 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/SomeClass.java
+++ groovy/groovy-core/src/test/groovy/SomeClass.java
@@ -1,7 +1,16 @@
 package groovy;
 
+/**
+ * Arbitrary holder for Java Methods to be called by Groovy TestCases.
+ * @author Dierk Koenig
+ */
+
 public class SomeClass {
+    // currently not supported to be called from Groovy
     public String[][] anArrayOfStringArrays(){
         return new String[][]{{"whatever"}};
+    }
+    public Object[] anArrayOfStringArraysWorkaround(){
+        return new Object[]{ new String[]{"whatever"}};
     }
 }
