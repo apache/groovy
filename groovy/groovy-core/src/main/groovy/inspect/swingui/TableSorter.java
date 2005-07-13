38459a3f7c706741faa734a9b871201f124dcbf1
Christian Stein <sormuras@gmx.de>
2005-07-13 19:28:07 +0000

    Organized imports to remove compiler warnings.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2544 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/main/groovy/inspect/swingui/TableSorter.java
+++ groovy/groovy-core/src/main/groovy/inspect/swingui/TableSorter.java
@@ -58,19 +58,16 @@ package groovy.inspect.swingui;
  * @author Minimal adjustments by Dierk Koenig, June 2005
  */
 
-import java.util.*;
-
-import javax.swing.table.TableModel;
-import javax.swing.event.TableModelEvent;
-
-// Imports for picking up mouse events from the JTable.
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.InputEvent;
+import java.util.Date;
+import java.util.Vector;
+
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 
 public class TableSorter extends TableMap
 {
