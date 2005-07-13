d5a6b797f2e568a1cb784ff17b6e724afdfdcd47
Dierk Koenig <dierk.koenig@canoo.com>
2005-07-13 09:28:14 +0000

    applied patch for basedir handling on behalf of Steven
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2540 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/bin/grails.bat
+++ groovy/modules/grails/bin/grails.bat
@@ -32,7 +32,7 @@ goto loopArguments
 
 :startGrails
 
-call %ANT_HOME%\bin\ant.bat -f %GRAILS_HOME%\src\grails\build.xml %GRAILS_ARGUMENTS%
+call %ANT_HOME%\bin\ant.bat -f %GRAILS_HOME%\src\grails\build.xml -Dbasedir=%CD% %GRAILS_ARGUMENTS%
 
 :errorExit
 
