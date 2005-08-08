02045786328befbbf9d96e781760d51388e63537
Graeme Rocher <grocher@vmware.com>
2005-08-08 10:02:48 +0000

    test relationship domain classes
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2679 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/test/persistence/org/codehaus/groovy/grails/domain/OneToOneTest.groovy
+++ groovy/modules/grails/test/persistence/org/codehaus/groovy/grails/domain/OneToOneTest.groovy
@@ -1,9 +1,7 @@
 package org.codehaus.groovy.grails.domain;
 
 class OneToOneTest {
-
-   @Property int id;
-   @Property int version;
-   @Property RelationshipsTest other; // one-to-one relationship
-
+	
+   @Property Long id;
+   @Property Long version;
 }
--- groovy/modules/grails/test/persistence/org/codehaus/groovy/grails/domain/RelationshipsTest.groovy
+++ groovy/modules/grails/test/persistence/org/codehaus/groovy/grails/domain/RelationshipsTest.groovy
@@ -1,16 +1,14 @@
 package org.codehaus.groovy.grails.domain;
 
 class RelationshipsTest {
-   @Property Map relationships = [ 	"ones" : OneToManyTest2.class,
-   									"manys" : ManyToManyTest.class,
-   									"uniones" :  UniOneToManyTest.class ];
-   									
-   @Property int id;
-   @Property int version;
-   @Property Set ones; // bi-directional one-to-many relationship 
-   @Property Set uniones; // uni-directional one-to-many
-   @Property Set manys; // many-to-many relationship
-   @Property OneToOneTest one; // bi-directional one-to-one
+  @Property Map relationships = [ "ones" : OneToManyTest2.class ];
+   
+   @Property Long id;
+   @Property Long version;
+
+   //@Property Set manys; // many-to-many relationship
+   @Property OneToOneTest one; // uni-directional one-to-one
+   @Property Set ones; // uni-directional one-to-many relationship    
 }
 
 
