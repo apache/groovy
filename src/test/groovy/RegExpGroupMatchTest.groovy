6abb38b8d98f10ff64fa67e36d4857072995ccc7
Pilho Kim <phkim@cluecom.co.kr>
2005-08-16 07:55:42 +0000

    The previous patch, DefaultGroovyMethods.java 1.173, has been failed to stop infinite loops. So now fix again the issue GROOVY-1000.
    Now mather[inex] is either a matched String or a List of matched group Strings
    at the position of the given index.
    For an example,
    
        def matcher = "\$abc." =~ /\$(.*)\./
        assert "\\\$(.*)\\." == /\$(.*)\./
        // assert matcher[1] == "abc"            // This has worked before jsr-03-release
        assert matcher[0] == ["\$abc.", "abc"]   // But this should work since jsr-03-release
        assert matcher[0][1] == "abc"            // This should work since jsr-03-release
    
    
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@2711 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/RegExpGroupMatchTest.groovy
@@ -0,0 +1,100 @@
+/**
+ * Test for fixing the Jira issue GROOVY-1000
+ *
+ *    Fix an infinite loop when getting after group matching in regular expression.
+ *    Graham Miller has given this idea.
+ *
+ * @author Pilho Kim
+ * @version $Revision$
+ */
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class RegExpGroupMatchTest extends GroovyTestCase {
+
+    void testFirst() {
+        assert "cheesecheese" =~ "cheese"
+        assert "cheesecheese" =~ /cheese/
+        assert "cheese" == /cheese/   /*they are both string syntaxes*/
+    }
+
+    void testSecond() {
+        // Lets create a regex Pattern
+        def pattern = ~/foo/
+        assert pattern instanceof Pattern
+        assert pattern.matcher("foo").matches()
+    }
+
+    void testThird() {
+        // Lets create a Matcher
+        def matcher = "cheesecheese" =~ /cheese/
+        assert matcher instanceof Matcher
+
+        def answer = matcher.replaceAll("edam")
+        println answer
+    }
+
+    void testFourth() {
+        // Lets do some replacement
+        def cheese = ("cheesecheese" =~ /cheese/).replaceFirst("nice")
+        assert cheese == "nicecheese"
+    }
+
+    void testFifth() {
+        // Group demo
+        def matcher = "\$abc." =~ "\\\$(.*)\\."
+        matcher.matches();                   // must be invoked
+        assert matcher.group(1) == "abc"     // is one, not zero
+        // assert matcher[1] == "abc"     // This has worked only before jsr-03-release
+        println (matcher[0])
+        assert matcher[0] == ["\$abc.", "abc"]
+        assert matcher[0][1] == "abc"
+    }
+
+    void testSixth() {
+        // Group demo
+        // Avoid having to double all the backslash escaping characters.
+        def matcher = "\$abc." =~ /\$(.*)\./    // no need to double-escape!
+        assert "\\\$(.*)\\." == /\$(.*)\./
+        matcher.matches();                      // must be invoked
+        assert matcher.group(1) == "abc"        // is one, not zero
+        // assert matcher[1] == "abc"     // This has worked only before jsr-03-release
+        println (matcher[0])
+        assert matcher[0] == ["\$abc.", "abc"]
+        assert matcher[0][1] == "abc"
+    }
+
+    // Test no group match.
+    void testNoGroupMatcherAndGet() {
+        def p = /ab[d|f]/
+        def m = "abcabdabeabf" =~ p 
+
+        for (i in 0..<m.count) { 
+            println( "m.groupCount() = " + m.groupCount())
+            println( "  " + i + ": " + m[i] )   // m[i] is a String
+        }
+    }
+
+    // Test group matches.
+    void testGroupMatcherAndGet() {
+        def p = /(?:ab([c|d|e|f]))/
+        def m = "abcabdabeabf" =~ p 
+
+        for (i in 0..<m.count) { 
+            println( "m.groupCount() = " + m.groupCount())
+            println( "  " + i + ": " + m[i] )   // m[i] is a String
+        }
+    }
+
+    // Test group matches.
+    void testAnotherGroupMatcherAndGet() {
+        def m = "abcabdabeabfabxyzabx" =~ /(?:ab([d|x-z]+))/
+
+        m.count.times { 
+            println( "m.groupCount() = " + m.groupCount())
+            println( "  " + it + ": " + m[it] )   // m[it] is a String
+        }
+    }
+}
+
