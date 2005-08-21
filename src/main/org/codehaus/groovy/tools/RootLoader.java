e476911f7736e8b86ba1495eaae1850fb05972c2
Jochen Theodorou <blackdrag@gmx.org>
2005-08-21 11:28:15 +0000

    documentation added
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@2729 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/tools/RootLoader.java
+++ src/main/org/codehaus/groovy/tools/RootLoader.java
@@ -1,7 +1,48 @@
 /*
- * RootLoader.java created on 10.08.2005
- *
- */
+$Id$
+
+Copyright 2003 (C) James Strachan and Bob Mcwhirter. All Rights Reserved.
+
+Redistribution and use of this software and associated documentation
+("Software"), with or without modification, are permitted provided
+that the following conditions are met:
+
+1. Redistributions of source code must retain copyright
+   statements and notices.  Redistributions must also contain a
+   copy of this document.
+
+2. Redistributions in binary form must reproduce the
+   above copyright notice, this list of conditions and the
+   following disclaimer in the documentation and/or other
+   materials provided with the distribution.
+
+3. The name "groovy" must not be used to endorse or promote
+   products derived from this Software without prior written
+   permission of The Codehaus.  For written permission,
+   please contact info@codehaus.org.
+
+4. Products derived from this Software may not be called "groovy"
+   nor may "groovy" appear in their names without prior written
+   permission of The Codehaus. "groovy" is a registered
+   trademark of The Codehaus.
+
+5. Due credit should be given to The Codehaus -
+   http://groovy.codehaus.org/
+
+THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 package org.codehaus.groovy.tools;
 
 import java.io.IOException;
@@ -10,7 +51,44 @@ import java.net.URLClassLoader;
 import java.util.Enumeration;
 
 /**
- * This ClassLoader should be used as root of class loaders.  
+ * This ClassLoader should be used as root of class loaders. Any
+ * RootLoader does have it's own classpath. when searching for a 
+ * class or resource, this classpath will be used for a search.
+ * Parent Classloaders are ignored first. If a class or ressource 
+ * can't be find in the classpath of the RooLoader, then parent is
+ * checked.
+ * 
+ * <b>Note:</b> this is very against the normal behavior of 
+ * classloaders. Normal is to frist check parent and then look in
+ * the ressources you gave this classloader.
+ * 
+ * <b>Why using RootLoader?</b>
+ * If you have to load classes with multiple classloaders and a
+ * classloader does know a class which depends on a class only 
+ * a child of this loader does know, then you won't be able to 
+ * load the class. To load the class the child is not allowed 
+ * to redirect it's search for the class to the parent first. 
+ * For example:
+ *  
+ *  <pre>
+ *  parentLoader   (has classpath: a.jar;c.jar)
+ *      |
+ *      |
+ *  childLoader    (has classpath: a.jar;b.jar;c.jar)
+ *  </pre>
+ *  
+ *  class C (from c.jar) extends B (from b.jar)
+ *  
+ *  childLoader.find("C")
+ *  --> parentLoader does know C.class, try to load it
+ *  --> to load the class c it has to load B.class
+ *  --> parentLoader is unable to find B.class in a.jar or c.jar
+ *  --> NoClassDefFoundException!
+ *  
+ *  if childLoader had tried to load the class by itself, there
+ *  would be no problem. Changing childLoader to be a RootLoader 
+ *  instance will solve that problem.
+ *   
  * @author Jochen Theodorou
  */
 public class RootLoader extends ClassLoader {
@@ -18,20 +96,35 @@ public class RootLoader extends ClassLoader {
     private ClassLoader parent; 
     private URLClassLoader inner;
     
+    /**
+     * constructs a new Rooloader without classpath
+     * @param parent the parent Loader
+     */   
     private RootLoader(ClassLoader parent) {
         super(parent);
     }
     
+    /**
+     * constructs a new Rooloader with a parent loader and an
+     * array of URLs as classpath
+     */
     public RootLoader(URL[] urls, ClassLoader parent) {
         this(parent);
         inner = new URLClassLoader(urls,null);
     }
     
+    /**
+     * constructs a new RootLoader with a @see LoaderConfiguration
+     * object which holds the classpath
+     */
     public RootLoader(LoaderConfiguration lc) {
         this(RootLoader.class.getClassLoader());
         inner = new URLClassLoader(lc.getClassPathUrls(),null);
     }
 
+    /**
+     * loads a class using the name of the class
+     */
     protected synchronized Class loadClass(final String name, boolean resolve) throws ClassNotFoundException {
         // only check the parent if the searched class can't be found
         try {
@@ -42,13 +135,20 @@ public class RootLoader extends ClassLoader {
         return super.loadClass(name,resolve);
     }
     
+    /**
+     * returns the URL of a resource, or null if it is not found
+     */
     public URL getResource(String name) {
         URL url = inner.getResource(name);
         url = super.getResource(name);
         return url;
-    }
+    }    
     
-    
+    /**
+     * returns an Enumeration of all found ressources. Resources found
+     * in the classpath of this loader are at the beginning of the
+     * returned enumeration
+     */
     protected Enumeration findResources(String name) throws IOException {
         final Enumeration enum1 = inner.findResources(name);
         final Enumeration enum2 = super.findResources(name);
