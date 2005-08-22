993fc120632793ab27c35f080fce8c0d7c8c64f6
Pilho Kim <phkim@cluecom.co.kr>
2005-08-22 03:20:59 +0000

    GROOVY-1023: Test the feature that the length of arguments can be variable when invoking methods with or without parameters.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@2731 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/VarargsMethodTest.groovy
+++ src/test/groovy/VarargsMethodTest.groovy
@@ -1,28 +1,47 @@
-class VarargsMethodTest extends GroovyTestCase {
+/** 
+ * VarargsMethodTest.groovy
+ *
+ *     Test the feature that the length of arguments can be variable
+ *     when invoking methods with or without parameters.
+ *
+ * @author Dierk Koenig
+ * @author Pilho Kim
+ * @version $Revision$
+ */
 
-    void testVarargsOnly() {
-        // assertEquals 0, varargsOnlyMethod() // todo: fix. throws NPE
-        assertEquals 1, varargsOnlyMethod('')
-        assertEquals 1, varargsOnlyMethod(1)
-        assertEquals 2, varargsOnlyMethod('','')
-        assertEquals 1, varargsOnlyMethod( ['',''] )
-        assertEquals 2, varargsOnlyMethod( *['',''] )
-    }
+class VarargsMethodTest extends GroovyTestCase {  
 
-    Integer varargsOnlyMethod(Object[] args) {
-        return args.size()
-    }
+    void testVarargsOnly() {  
+        assertEquals 0, varargsOnlyMethod() // todo: GROOVY-1023
+        assertEquals 1, varargsOnlyMethod('')  
+        assertEquals 1, varargsOnlyMethod(1)  
+        assertEquals 2, varargsOnlyMethod('','')  
+        assertEquals 1, varargsOnlyMethod( ['',''] )  
+        assertEquals 2, varargsOnlyMethod( ['',''] as Object[])  
+        assertEquals 2, varargsOnlyMethod( *['',''] )  
+     }  
 
-    void testVarargsLast() {
-        assertEquals 0, varargsLastMethod('')
-        assertEquals 0, varargsLastMethod(1)
-        assertEquals 1, varargsLastMethod('','')
-        assertEquals 2, varargsLastMethod('','','')
-        assertEquals 1, varargsLastMethod('', ['',''] )
-        assertEquals 2, varargsLastMethod('', *['',''] )
-    }
-
-    Integer varargsLastMethod(Object first, Object[] args) {
-        return args.size()
-    }
-}
+     Integer varargsOnlyMethod(Object[] args) {  
+         println("args = " + args)
+         // todo: GROOVY-1023
+         //     If this method is invoked with no parameter,
+         //     then args is null.
+         if (args == null)
+             return 0
+         return args.size()  
+     }  
+  
+     void testVarargsLast() {  
+         assertEquals 0, varargsLastMethod('')  
+         assertEquals 0, varargsLastMethod(1)  
+         assertEquals 1, varargsLastMethod('','')  
+         assertEquals 2, varargsLastMethod('','','')  
+         assertEquals 1, varargsLastMethod('', ['',''] )  
+         assertEquals 2, varargsLastMethod('', ['',''] as Object[])  
+         assertEquals 2, varargsLastMethod('', *['',''] )  
+     }  
+  
+     Integer varargsLastMethod(Object first, Object[] args) {  
+         return args.size()  
+     }  
+}  
