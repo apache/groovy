73bbdf9486230c2b45d48ea8936e03a79bb36367
Pilho Kim <phkim@cluecom.co.kr>
2005-08-23 04:10:12 +0000

    GROOVY-1026: Invoking methods having varargs with last parameters nulls. (Similar to the varargs feature of Java 5)
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2740 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/VarargsMethodTest.groovy
+++ groovy/groovy-core/src/test/groovy/VarargsMethodTest.groovy
@@ -1,34 +1,44 @@
 /** 
  * VarargsMethodTest.groovy
  *
- *     Test the feature that the length of arguments can be variable
- *     when invoking methods with or without parameters.
+ *   1) Test to fix the Jira issues GROOVY-1023 and GROOVY-1026.
+ *   2) Test the feature that the length of arguments can be variable
+ *      when invoking methods with or without parameters.
  *
  * @author Dierk Koenig
  * @author Pilho Kim
+ * @author Hein Meling
  * @version $Revision$
  */
 
 class VarargsMethodTest extends GroovyTestCase {  
 
     void testVarargsOnly() {  
-        assertEquals 0, varargsOnlyMethod() // todo: GROOVY-1023
         assertEquals 1, varargsOnlyMethod('')  
         assertEquals 1, varargsOnlyMethod(1)  
         assertEquals 2, varargsOnlyMethod('','')  
         assertEquals 1, varargsOnlyMethod( ['',''] )  
         assertEquals 2, varargsOnlyMethod( ['',''] as Object[])  
         assertEquals 2, varargsOnlyMethod( *['',''] )  
+
+        // todo: GROOVY-1023
+        assertEquals 0, varargsOnlyMethod()
+
+        // todo: GROOVY-1026
+        assertEquals(-1, varargsOnlyMethod(null))
+        assertEquals(2, varargsOnlyMethod(null, null))
      }  
 
      Integer varargsOnlyMethod(Object[] args) {  
          println("args = " + args)
-         // todo: GROOVY-1023
-         //     If this method is invoked with no parameter,
+         // (1) todo: GROOVY-1023 (Java 5 feature)
+         //     If this method having varargs is invoked with no parameter,
          //     then args is not null, but an array of length 0.
-         /* if (args == null)
-               return 0
-         */
+         // (2) todo: GROOVY-1026 (Java 5 feature)
+         //     If this method having varargs is invoked with one parameter
+         //     null, then args is null, and so -1 is returned here.
+         if (args == null)
+               return -1
          return args.size()  
      }  
   
@@ -40,9 +50,18 @@ class VarargsMethodTest extends GroovyTestCase {
          assertEquals 1, varargsLastMethod('', ['',''] )  
          assertEquals 2, varargsLastMethod('', ['',''] as Object[])  
          assertEquals 2, varargsLastMethod('', *['',''] )  
+
+         // todo: GROOVY-1026
+         assertEquals(-1, varargsLastMethod('',null))
+         assertEquals(2, varargsLastMethod('',null, null))
      }  
   
      Integer varargsLastMethod(Object first, Object[] args) {  
+         // (1) todo: GROOVY-1026 (Java 5 feature)
+         //     If this method having varargs is invoked with two parameters
+         //     1 and null, then args is null, and so -1 is returned here.
+         if (args == null)
+               return -1
          return args.size()  
      }  
 }  
