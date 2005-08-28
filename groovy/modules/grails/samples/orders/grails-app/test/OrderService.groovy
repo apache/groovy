a217f882c5fb184dff7b42013c8bcbce1fe81003
Steven Devijver <support@codehaus.org>
2005-08-28 08:46:06 +0000

    Load test.OrderEntry from class classloader.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2768 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/samples/orders/grails-app/test/OrderService.groovy
+++ groovy/modules/grails/samples/orders/grails-app/test/OrderService.groovy
@@ -4,6 +4,7 @@ class OrderService {
 	@Property transactional = true
 		
 	def findAllOrders() {
+		println(getClass().getClassLoader().loadClass("test.OrderEntry"))
 		return OrderEntry.findAll()
 	}
 	
