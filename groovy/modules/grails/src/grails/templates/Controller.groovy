30a051a1bd3cb4e613e45b75ffc041871d440837
Graeme Rocher <grocher@vmware.com>
2005-10-07 19:57:13 +0000

    added template classes for build
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2933 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/modules/grails/src/grails/templates/Controller.groovy
@@ -0,0 +1,10 @@
+class @controller.name@Controller {
+	
+	@Property String defaultClosure = "list"
+	
+	@Property Closure list = {
+		request,response ->
+		return null;
+	}
+}
+
---
+++ groovy/modules/grails/src/grails/templates/DataSource.groovy
@@ -0,0 +1,6 @@
+class @data.source.name@DataSource {
+   @Property String url = "jdbc:hsqldb:hsql://localhost:9101/"
+   @Property String driverClassName = "org.hsqldb.jdbcDriver"
+   @Property String username = "sa"
+   @Property String password = ""
+}
