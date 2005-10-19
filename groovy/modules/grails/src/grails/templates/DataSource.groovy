54fdf49afe2bdfd6506c54279e992b31bce7c985
Graeme Rocher <grocher@vmware.com>
2005-10-19 16:16:38 +0000

    Changed default to in-memory database
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2963 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/src/grails/templates/DataSource.groovy
+++ groovy/modules/grails/src/grails/templates/DataSource.groovy
@@ -1,5 +1,5 @@
 class @data.source.name@DataSource {
-   @Property String url = "jdbc:hsqldb:hsql://localhost:9101/"
+   @Property String url = "jdbc:hsqldb:mem:testDB"
    @Property String driverClassName = "org.hsqldb.jdbcDriver"
    @Property String username = "sa"
    @Property String password = ""
