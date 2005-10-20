1c2d1681230ede7775aad408201094be3e573af8
Graeme Rocher <grocher@vmware.com>
2005-10-20 12:58:57 +0000

    updated scripts to have more specific classpath for Jetty to avoid classloading issues
    git-svn-id: http://svn.codehaus.org/groovy/trunk@2982 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/bin/grails.bat
+++ groovy/modules/grails/bin/grails.bat
@@ -22,17 +22,20 @@ goto errorExit
 
 :getArguments
 set GRAILS_ARGUMENTS=%1
-if ""%1""=="""" goto startGrails
+if ""%1""=="""" goto getClasspath
 shift
 :loopArguments
-if ""%1""=="""" goto startGrails
+if ""%1""=="""" goto getClasspath
 set GRAILS_ARGUMENTS=%GRAILS_ARGUMENTS% %1
 shift
 goto loopArguments
 
-:startGrails
+:getClasspath
+set GRAILS_ANT_CLASSPATH="%GRAILS_HOME%\lib\bsf.jar;%GRAILS_HOME%\lib\groovy-all-1.0-jsr-04-SNAPSHOT.jar;%GRAILS_HOME%\lib\org.mortbay.jetty.jar;%GRAILS_HOME%\lib\commons-logging.jar;%GRAILS_HOME%\lib\commons-el.jar;%GRAILS_HOME%\lib\javax.servlet.jar;%GRAILS_HOME%\lib\log4j-1.2.8.jar;%GRAILS_HOME%\lib\jasper-compiler.jar;%GRAILS_HOME%\lib\jasper-runtime.jar"
+goto startGrails
 
-call %ANT_HOME%\bin\ant.bat -lib  %GRAILS_HOME%\lib -f %GRAILS_HOME%\src\grails\build.xml -Dbasedir=%CD% %GRAILS_ARGUMENTS%
+:startGrails
+call %ANT_HOME%\bin\ant.bat -lib %GRAILS_ANT_CLASSPATH% -f %GRAILS_HOME%\src\grails\build.xml -Dbasedir=%CD% %GRAILS_ARGUMENTS%
 
 :errorExit
 
