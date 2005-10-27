58578f2c5856168834e666ea39d373f3657bdf35
Graeme Rocher <grocher@vmware.com>
2005-10-27 12:12:54 +0000

    new controller exception type
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3037 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/servlet/mvc/exceptions/ControllerExecutionException.java
@@ -0,0 +1,25 @@
+package org.codehaus.groovy.grails.web.servlet.mvc.exceptions;
+
+public class ControllerExecutionException extends GrailsMVCException {
+
+	public ControllerExecutionException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerExecutionException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerExecutionException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerExecutionException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
