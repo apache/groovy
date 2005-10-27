a1139c59489a9cbee7d5223da2292ef044410bbc
Graeme Rocher <grocher@vmware.com>
2005-10-27 12:12:38 +0000

    Moved the guts of the controller into a helper class that can be more easily passed around to dynamic methods
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3036 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/servlet/mvc/GrailsControllerHelper.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.grails.web.servlet.mvc;
+
+import groovy.lang.GroovyObject;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.groovy.grails.commons.GrailsControllerClass;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * An interface for a helper class that processes Grails controller requests and responses 
+ * 
+ * @author Graeme Rocher
+ * @since Oct 27, 2005
+ */
+public interface GrailsControllerHelper {
+
+	/**
+	 * Retrieves a controller class for the specified class name
+	 * @param name
+	 * @return
+	 */
+	public abstract GrailsControllerClass getControllerClassByName(String className);
+
+	public abstract GrailsControllerClass getControllerClassByURI(String uri);
+
+	public abstract GroovyObject getControllerInstance(
+			GrailsControllerClass controllerClass);
+	/**
+	 * Handles a Grails URI
+	 * @param uri The URI to processs
+	 * @param request The request instance
+	 * @param response The response instance
+	 * @return A ModelAndView instance
+	 */
+	public abstract ModelAndView handleURI(String uri,
+			HttpServletRequest request, HttpServletResponse response);
+
+	/**
+	 * Processes an action response for the specified arguments
+	 * 
+	 * @param controllerClass The controller class of the closure
+	 * @param returnValue The response from the closure
+	 * @param closurePropertyName The property name of the closure
+	 * @param viewName The name of the view
+	 * 
+	 * @return A ModelAndView object
+	 */
+	public abstract ModelAndView handleActionResponse(
+			GrailsControllerClass controllerClass, Object returnValue,
+			String closurePropertyName, String viewName);
+
+	/**
+	 * Handles a Grails URI
+	 * @param uri The URI to processs
+	 * @param request The request instance
+	 * @param response The response instance
+	 * @param params A map of controller parameters
+	 * @return A ModelAndView instance
+	 */
+	public abstract ModelAndView handleURI(String uri,
+			HttpServletRequest request, HttpServletResponse response, Map params);
+
+}
---
+++ groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/servlet/mvc/SimpleGrailsControllerHelper.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.grails.web.servlet.mvc;
+
+import groovy.lang.Closure;
+import groovy.lang.GroovyObject;
+import groovy.lang.ProxyMetaClass;
+import groovy.util.Proxy;
+
+import java.beans.IntrospectionException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.WordUtils;
+import org.codehaus.groovy.grails.commons.GrailsApplication;
+import org.codehaus.groovy.grails.commons.GrailsControllerClass;
+import org.codehaus.groovy.grails.metaclass.PropertyAccessProxyMetaClass;
+import org.codehaus.groovy.grails.web.metaclass.ControllerDynamicMethodsInterceptor;
+import org.codehaus.groovy.grails.web.metaclass.GetParamsDynamicProperty;
+import org.codehaus.groovy.grails.web.servlet.GrailsHttpServletRequest;
+import org.codehaus.groovy.grails.web.servlet.GrailsHttpServletResponse;
+import org.codehaus.groovy.grails.web.servlet.mvc.exceptions.ControllerExecutionException;
+import org.codehaus.groovy.grails.web.servlet.mvc.exceptions.IncompatibleParameterCountException;
+import org.codehaus.groovy.grails.web.servlet.mvc.exceptions.NoClosurePropertyForURIException;
+import org.codehaus.groovy.grails.web.servlet.mvc.exceptions.NoViewNameDefinedException;
+import org.codehaus.groovy.grails.web.servlet.mvc.exceptions.UnknownControllerException;
+import org.codehaus.groovy.grails.web.servlet.mvc.exceptions.UnsupportedReturnValueException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SimpleGrailsControllerHelper implements GrailsControllerHelper {
+
+	private GrailsApplication application;
+	private ApplicationContext applicationContext;
+
+	public SimpleGrailsControllerHelper(GrailsApplication application, ApplicationContext context) {
+		super();
+		this.application = application;
+		this.applicationContext = context;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper#getControllerClassByName(java.lang.String)
+	 */
+	public GrailsControllerClass getControllerClassByName(String name) {
+		return this.application.getController(name);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper#getControllerClassByURI(java.lang.String)
+	 */
+	public GrailsControllerClass getControllerClassByURI(String uri) {
+		return this.application.getControllerByURI(uri);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper#getControllerInstance(org.codehaus.groovy.grails.commons.GrailsControllerClass)
+	 */
+	public GroovyObject getControllerInstance(GrailsControllerClass controllerClass) {
+		return (GroovyObject)this.applicationContext.getBean(controllerClass.getFullName());
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper#handleURI(java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	public ModelAndView handleURI(String uri, HttpServletRequest request, HttpServletResponse response) {
+		return handleURI(uri,request,response,Collections.EMPTY_MAP);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper#handleActionResponse(org.codehaus.groovy.grails.commons.GrailsControllerClass, java.lang.Object, java.lang.String, java.lang.String)
+	 */
+	public ModelAndView handleActionResponse( GrailsControllerClass controllerClass,Object returnValue,String closurePropertyName, String viewName) {
+		boolean viewNameBlank = (viewName == null || viewName.length() == 0);
+		
+		if (returnValue == null) {
+			if (viewNameBlank) {
+				return null;
+			} else {
+				return new ModelAndView(viewName);
+			}
+		} else if (returnValue instanceof Map) {
+			// remove any Proxy wrappers and set the adaptee as the value
+			removeProxiesFromModelObjects((Map)returnValue);
+			if (viewNameBlank) {
+				throw new NoViewNameDefinedException("Map instance returned by and no view name specified for closure on property [" + closurePropertyName + "] in controller [" + controllerClass.getFullName() + "]!");
+			} else {
+				return new ModelAndView(viewName, (Map)returnValue);
+			}
+		} else if (returnValue instanceof ModelAndView) {
+			ModelAndView modelAndView = (ModelAndView)returnValue;
+			
+			// remove any Proxy wrappers and set the adaptee as the value
+			removeProxiesFromModelObjects(modelAndView.getModel());
+			
+			if (modelAndView.getView() == null && modelAndView.getViewName() == null) {
+				if (viewNameBlank) {
+					throw new NoViewNameDefinedException("ModelAndView instance returned by and no view name defined by nor for closure on property [" + closurePropertyName + "] in controller [" + controllerClass.getFullName() + "]!");
+				} else {
+					modelAndView.setViewName(viewName);
+				}
+			}
+			return modelAndView;
+		}
+		
+		throw new UnsupportedReturnValueException("Return value [" + returnValue + "] is not supported for closure property [" + closurePropertyName + "] in controller [" + controllerClass.getFullName() + "]!");
+	}
+
+	/**
+	 * If in Proxy's are used in the Groovy context, unproxy (is that a word?) them by setting
+	 * the adaptee as the value in the map so that they can be used in non-groovy view technologies
+	 *   
+	 * @param model The model as a map
+	 */
+	private void removeProxiesFromModelObjects(Map model) {
+		
+		for (Iterator keyIter = model.keySet().iterator(); keyIter.hasNext();) {
+			Object current = keyIter.next();
+			Object modelObject = model.get(current);
+			if(modelObject instanceof Proxy) {
+				model.put( current, ((Proxy)modelObject).getAdaptee() );
+			}			
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper#handleURI(java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.util.Map)
+	 */
+	public ModelAndView handleURI(String uri, HttpServletRequest request, HttpServletResponse response, Map params) {
+		if(uri == null)
+			throw new IllegalArgumentException("Controller URI [" + uri + "] cannot be null!");
+		
+		if (uri.indexOf("?") > -1) {
+			uri = uri.substring(0, uri.indexOf("?"));
+		}		
+		if(!uri.startsWith("/"))
+			uri = '/' + uri;
+		if(uri.endsWith("/"))
+			uri = uri.substring(0,uri.length() - 2);
+		
+		// Step 2: lookup the controller in the application.
+		GrailsControllerClass controllerClass = getControllerClassByURI(uri);
+		if (controllerClass == null) {
+			throw new UnknownControllerException("No controller found for URI [" + uri + "]!");
+		}
+		String controllerName = WordUtils.uncapitalize(controllerClass.getName());
+		
+		// Step 3: load controller from application context.
+		GroovyObject controller = getControllerInstance(controllerClass);
+		
+		// Step 3a: Configure a proxy interceptor for controller dynamic methods for this request
+		try {
+			ProxyMetaClass pmc = PropertyAccessProxyMetaClass.getInstance(controller.getClass());
+			ControllerDynamicMethodsInterceptor interceptor = new ControllerDynamicMethodsInterceptor(controller.getClass(),this,request,response);
+			// if there are additional params add them to the params dynamic property
+			if(params != null && !params.isEmpty()) {
+				GetParamsDynamicProperty paramsProp = (GetParamsDynamicProperty)interceptor.getDynamicProperty( GetParamsDynamicProperty.PROPERTY_NAME );
+				paramsProp.addParams( params );
+			}			
+			pmc.setInterceptor( interceptor );
+			controller.setMetaClass(pmc);
+		}
+		catch(IntrospectionException ie) {
+			throw new ControllerExecutionException("Error creating dynamic controller methods for controller ["+controller.getClass()+"]: " + ie.getMessage(), ie);
+		}
+		
+		
+		// Step 4: get closure property name for URI.
+		String closurePropertyName = controllerClass.getClosurePropertyName(uri);
+		if (closurePropertyName == null) {
+			throw new NoClosurePropertyForURIException("Could not find closure property for URI [" + uri + "] for controller [" + controllerClass.getFullName() + "]!");
+		}
+		
+		// Step 5: get the view name for this URI.
+		String viewName = controllerClass.getViewByURI(uri);
+		
+		
+		// Step 6: get closure from closure property
+		Closure closure = (Closure)controller.getProperty(closurePropertyName);
+		
+		// Step 7: determine argument count and execute.
+		Object returnValue = null;
+		if (closure.getParameterTypes().length == 1) {
+			// closure may have zero or one parameter, we cannot be sure.
+			returnValue = closure.call(new GrailsHttpServletRequest(request));
+		} else if (closure.getParameterTypes().length == 2) {
+			returnValue = closure.call(new Object[] { new  GrailsHttpServletRequest(request), new GrailsHttpServletResponse(response) });
+		} else {
+			throw new IncompatibleParameterCountException("Closure on property [" + closurePropertyName + "] in [" + controllerClass.getFullName() + "] has an incompatible parameter count [" + closure.getParameterTypes().length + "]! Supported values are 0 and 2.");			
+		}
+		
+		// Step 8: determine return value type and handle accordingly
+		return handleActionResponse(controllerClass,returnValue,closurePropertyName,viewName);
+	}
+	
+}
