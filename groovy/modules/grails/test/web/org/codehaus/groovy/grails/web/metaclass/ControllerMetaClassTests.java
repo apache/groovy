ccb8452a344c43c4c189f6a59fa1452c23aad58d
Graeme Rocher <grocher@vmware.com>
2005-10-27 12:19:20 +0000

    added tests for action redirection
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3043 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/test/web/org/codehaus/groovy/grails/web/metaclass/ControllerMetaClassTests.java
+++ groovy/modules/grails/test/web/org/codehaus/groovy/grails/web/metaclass/ControllerMetaClassTests.java
@@ -2,10 +2,24 @@ package org.codehaus.groovy.grails.web.metaclass;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.groovy.grails.commons.DefaultGrailsApplication;
+import org.codehaus.groovy.grails.commons.GrailsApplication;
+import org.codehaus.groovy.grails.commons.spring.SpringConfigUtils;
 import org.codehaus.groovy.grails.metaclass.PropertyAccessProxyMetaClass;
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsControllerHelper;
+import org.codehaus.groovy.grails.web.servlet.mvc.SimpleGrailsControllerHelper;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
 
+import groovy.lang.Closure;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
 import groovy.lang.MissingMethodException;
@@ -16,9 +30,8 @@ import junit.framework.TestCase;
 
 public class ControllerMetaClassTests extends TestCase {
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
+	
+	private GenericApplicationContext context;
 
 	public void testMetaClassProxy()
 		throws Exception {
@@ -65,13 +78,9 @@ public class ControllerMetaClassTests extends TestCase {
 						"}\n" +
 						"}" );
 		
-		ProxyMetaClass pmc = PropertyAccessProxyMetaClass.getInstance(groovyClass);
-		// proof of concept to try out proxy meta class
-		pmc.setInterceptor( new ControllerDynamicMethodsInterceptor(groovyClass,request,response) );
-		
-		GroovyObject go = (GroovyObject)groovyClass.newInstance();
-		
-		go.setMetaClass( pmc );
+
+		GrailsApplication application = new DefaultGrailsApplication(new Class[] { groovyClass });		
+		GroovyObject go = configureDynamicGO(groovyClass,application, request,response);		
 		try {
 			Object params = go.getProperty( "params" );			
 			assertNotNull(params);
@@ -87,5 +96,84 @@ public class ControllerMetaClassTests extends TestCase {
 		catch(MissingPropertyException mpex) {
 			fail("Missing property exception should not have been thrown!");
 		}
+	}
+	
+	public void testRedirectDynamicMethod() throws Exception {
+
+		GroovyClassLoader gcl = new GroovyClassLoader();
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		
+		request.addParameter("testParam", "testValue");
+		
+		Class groovyClass = gcl.parseClass( "class TestController {\n" +
+						"@Property next = {\n" +
+							"return ['success':this.params['testParam2']]" +
+						"}\n" +
+						"@Property list = {\n" +
+							"return redirect(action:this.next,params:['testParam2':'testValue2'])\n" +
+						"}\n" +
+						"}" );
+		Class secondController = gcl.parseClass( "class SecondController {\n" +
+				"@Property list = {\n" +
+					"return redirect(action:'test/list',params:['testParam2':'testValue2'])\n" +
+				"}\n" +
+				"}" );		
+		
+		GrailsApplication application = new DefaultGrailsApplication(new Class[] { groovyClass,secondController });		
+		GroovyObject go = configureDynamicGO(groovyClass, application,request,response);
+		GroovyObject go2 = configureDynamicGO(secondController, application,request,response);
+		
+		// first test redirection within the same controller		
+		try {
+			Closure closure = (Closure)go.getProperty("list");
+			Object returnValue = closure.call();
+			assertNotNull(returnValue);
+			assertTrue(returnValue instanceof ModelAndView);
+			Map model = ((ModelAndView)returnValue).getModel();
+			
+			assertEquals("testValue2", model.get("success"));
+		}
+		catch(MissingMethodException mme) {
+			fail("Missing method exception should not have been thrown!");
+		}	
+		catch(MissingPropertyException mpex) {
+			fail("Missing property exception should not have been thrown!");
+		}
+		// now redirection to another controller
+		try {
+			Closure closure = (Closure)go2.getProperty("list");
+			Object returnValue = closure.call();
+			assertNotNull(returnValue);
+			assertTrue(returnValue instanceof ModelAndView);
+			Map model = ((ModelAndView)returnValue).getModel();
+			
+			assertEquals("testValue2", model.get("success"));
+		}
+		catch(MissingMethodException mme) {
+			fail("Missing method exception should not have been thrown!");
+		}	
+		catch(MissingPropertyException mpex) {
+			fail("Missing property exception should not have been thrown!");
+		}		
+	}
+	
+	private GroovyObject configureDynamicGO(Class groovyClass,GrailsApplication application, HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+		ProxyMetaClass pmc = PropertyAccessProxyMetaClass.getInstance(groovyClass);
+		// proof of concept to try out proxy meta class
+		if(context == null)
+			this.context = new GenericApplicationContext();
+		
+		BeanDefinition bd = new RootBeanDefinition(groovyClass,false);
+		context.registerBeanDefinition( groovyClass.getName(), bd );
+				
+		
+		GrailsControllerHelper helper = new SimpleGrailsControllerHelper(application,context);
+		pmc.setInterceptor( new ControllerDynamicMethodsInterceptor(groovyClass,helper,request,response) );
+		
+		GroovyObject go = (GroovyObject)groovyClass.newInstance();
+		go.setMetaClass( pmc );
+		return go;
 	}
 }
