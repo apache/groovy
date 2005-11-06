a97aec20279cab407d1b32eb4dba3eb30a5c3561
Guillaume Laforge <glaforge@gmail.com>
2005-11-06 09:45:38 +0000

    The sample script using Windows Media Player now leverages the support of synchronized statements
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3069 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/scriptom/src/script/wmplayer.groovy
+++ groovy/modules/scriptom/src/script/wmplayer.groovy
@@ -7,25 +7,21 @@ println "Playing files from: ${folderName}"
 // create a Windows Media Player (from its Class ID)
 def player = new ActiveXProxy("clsid:{6BF52A52-394A-11D3-B153-00C04F79FAA6}")
 
-// a flag indicating the player is ready to play
-def ready = true
-
 // react upon a state change
 player.events.PlayStateChange = { 
     // if the media ended change the status flag
     // so that the main thread notices the change
-    if (it[0].toInt() == 1) ready = true
+    if (it[0].toInt() == 1) synchronized(player) { player.notify() }
 }
 // start listening to the events we're subsribed to
 player.events.listen()
 
 // loop over the available files and play them
-new File(folderName).eachFileMatch(~/.*(wav|mp3)/) { song ->
-    while (!ready) sleep(1000)
-    ready = false
+new File(folderName).eachFileMatch(~/.*(wav|au|wma|mp3)/) { song ->
     println "Listening to: $song"
     player.URL = song.absolutePath
     control = player.controls.play()
+    synchronized(player) { player.wait() }
 }
 
 // close the player
