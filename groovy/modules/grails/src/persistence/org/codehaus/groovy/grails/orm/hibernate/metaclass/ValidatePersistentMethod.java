efc2a0479be226b8d50b6e14deb5fa44b2fd41d8
Graeme Rocher <grocher@vmware.com>
2005-11-22 13:47:12 +0000

    Changed to use the metaclass to set the errors and hasErrors properties
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3185 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/src/persistence/org/codehaus/groovy/grails/orm/hibernate/metaclass/ValidatePersistentMethod.java
+++ groovy/modules/grails/src/persistence/org/codehaus/groovy/grails/orm/hibernate/metaclass/ValidatePersistentMethod.java
@@ -16,16 +16,10 @@ package org.codehaus.groovy.grails.orm.hibernate.metaclass;
 
 
 
-import groovy.lang.GroovyObject;
-
-import java.beans.IntrospectionException;
-import java.util.List;
-
 import org.codehaus.groovy.grails.commons.GrailsApplication;
-import org.codehaus.groovy.grails.commons.metaclass.DefaultGroovyDynamicMethodsInterceptor;
-import org.codehaus.groovy.grails.commons.metaclass.DynamicMethods;
-import org.codehaus.groovy.grails.commons.metaclass.GenericDynamicProperty;
-import org.codehaus.groovy.grails.exceptions.GrailsDomainException;
+import org.codehaus.groovy.grails.commons.metaclass.DelegatingMetaClass;
+import org.codehaus.groovy.grails.metaclass.DomainClassMethods;
+import org.codehaus.groovy.runtime.InvokerHelper;
 import org.hibernate.SessionFactory;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
@@ -39,8 +33,6 @@ import org.springframework.validation.Validator;
 public class ValidatePersistentMethod extends AbstractDynamicPersistentMethod {
 
 	private static final String METHOD_NAME = "validate";
-	private static final String HAS_ERRORS_PROPERTY = "hasErrors";
-	private static final String ERRORS_PROPERTY = "errors";
 	private GrailsApplication application;
 	
 
@@ -62,18 +54,10 @@ public class ValidatePersistentMethod extends AbstractDynamicPersistentMethod {
 			validator.validate(target,errors);
 			
 			if(errors.hasErrors()) {
-				valid = new Boolean(!errors.hasErrors());			
-				GenericDynamicProperty hasErrorsProperty = new GenericDynamicProperty(HAS_ERRORS_PROPERTY,Boolean.class,valid,true);
-				GenericDynamicProperty errorsProperty = new GenericDynamicProperty(ERRORS_PROPERTY,List.class,errors.getAllErrors(),true);
-				
-				DynamicMethods interceptor;
-				try {
-					interceptor = new DefaultGroovyDynamicMethodsInterceptor((GroovyObject)target);
-					interceptor.addDynamicProperty(hasErrorsProperty);
-					interceptor.addDynamicProperty( errorsProperty );			
-				} catch (IntrospectionException e) {
-					throw new GrailsDomainException("Introspection exception validating object ["+target+"] or class ["+target.getClass()+"]: " + e.getMessage(),e);
-				}
+				valid = new Boolean(!errors.hasErrors());	
+				DelegatingMetaClass metaClass = (DelegatingMetaClass)InvokerHelper.getInstance().getMetaRegistry().getMetaClass(target.getClass());
+				metaClass.setProperty(target,DomainClassMethods.HAS_ERRORS_PROPERTY,valid);
+				metaClass.setProperty(target,DomainClassMethods.ERRORS_PROPERTY,errors.getAllErrors());											
 			}
 		}
 		return valid;
