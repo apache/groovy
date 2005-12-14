34fe2384d4844889a5ab4ee620b9aaa041ec5147
Graeme Rocher <grocher@vmware.com>
2005-12-14 17:15:08 +0000

    removed, moved to artifact sub-directory
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3267 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/src/grails/templates/DataSource.groovy
+++
@@ -1,6 +0,0 @@
-class @data.source.name@DataSource {
-   @Property String url = "jdbc:hsqldb:mem:testDB"
-   @Property String driverClassName = "org.hsqldb.jdbcDriver"
-   @Property String username = "sa"
-   @Property String password = ""
-}
