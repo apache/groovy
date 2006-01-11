58959635c993eea315ebcd7559171d7b4f44e670
Graeme Rocher <grocher@vmware.com>
2006-01-11 09:51:09 +0000

    fixed compile error referencing incorrect constant
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3374 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/metaclass/ControllerDynamicMethods.java
+++ groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/metaclass/ControllerDynamicMethods.java
@@ -62,7 +62,7 @@ public class ControllerDynamicMethods extends
         addDynamicProperty(new GenericDynamicProperty(REQUEST_PROPERTY, HttpServletRequest.class,new GrailsHttpServletRequest( request,controller),true) );
         addDynamicProperty(new GenericDynamicProperty(RESPONSE_PROPERTY, HttpServletResponse.class,response,true) );
         addDynamicProperty(new GenericDynamicProperty(ERRORS_PROPERTY, Errors.class, null, false));
-        addDynamicProperty(new GenericDynamicProperty(MODEL_AND_VIEW, ModelAndView.class,null,false));
+        addDynamicProperty(new GenericDynamicProperty(MODEL_AND_VIEW_PROPERTY, ModelAndView.class,null,false));
 
         // add dynamic methods
         addDynamicMethodInvocation( new RedirectDynamicMethod(helper,request,response) );
