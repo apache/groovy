5514f00542e2e0a79201da38cc781f794c032961
Graeme Rocher <grocher@vmware.com>
2006-01-12 01:12:20 +0000

    Implemented filter that copies across resources so that views, resources etc can at least be reloaded
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3384 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/servlet/filter/GrailsReloadServletFilter.java
+++ groovy/modules/grails/src/web/org/codehaus/groovy/grails/web/servlet/filter/GrailsReloadServletFilter.java
@@ -41,36 +41,25 @@ public class GrailsReloadServletFilter extends OncePerRequestFilter {
 
     public static final Log LOG = LogFactory.getLog(GrailsReloadServletFilter.class);
 
-    private Resource basedir;
-    private Resource destdir;
     private GroovyObject copyScript;
 
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        if(LOG.isDebugEnabled()) {
-            LOG.debug("Appling filter for basedir ["+basedir+"]");
-            LOG.debug("Appling filter for destdir ["+destdir+"]");
-        }
-        if(basedir != null && destdir != null) {
-              if(copyScript == null) {
-                  GroovyClassLoader gcl = new GroovyClassLoader(Thread.currentThread().getContextClassLoader());
+      if(copyScript == null) {
+          GroovyClassLoader gcl = new GroovyClassLoader(Thread.currentThread().getContextClassLoader());
 
-                  Class groovyClass = null;
-                  try {
-                      groovyClass = gcl.parseClass(gcl.getResource("org/codehaus/groovy/grails/web/servlet/filter/GrailsResourceCopier.groovy").openStream());
-                      copyScript = (GroovyObject)groovyClass.newInstance();
-                  } catch (IllegalAccessException e) {
-                      LOG.error("Illegal access creating resource copier. Save/reload disabled: " + e.getMessage(), e);
-                  } catch (InstantiationException e) {
-                      LOG.error("Error instantiating resource copier. Save/reload disabled: " + e.getMessage(), e);
-                  } catch (CompilationFailedException e) {
-                       LOG.error("Error compiling resource copier. Save/reload disabled: " + e.getMessage(), e);
-                  } catch(Exception e) {
-                     LOG.error("Error loading resource copier. Save/reload disabled: " + e.getMessage(), e);
-                  }
-
-
-              }
-
+          Class groovyClass = null;
+          try {
+              groovyClass = gcl.parseClass(gcl.getResource("org/codehaus/groovy/grails/web/servlet/filter/GrailsResourceCopier.groovy").openStream());
+              copyScript = (GroovyObject)groovyClass.newInstance();
+          } catch (IllegalAccessException e) {
+              LOG.error("Illegal access creating resource copier. Save/reload disabled: " + e.getMessage(), e);
+          } catch (InstantiationException e) {
+              LOG.error("Error instantiating resource copier. Save/reload disabled: " + e.getMessage(), e);
+          } catch (CompilationFailedException e) {
+               LOG.error("Error compiling resource copier. Save/reload disabled: " + e.getMessage(), e);
+          } catch(Exception e) {
+             LOG.error("Error loading resource copier. Save/reload disabled: " + e.getMessage(), e);
+          }
             if(copyScript != null) {
                 copyScript.invokeMethod("run", new Object[0]);
             }
@@ -78,19 +67,4 @@ public class GrailsReloadServletFilter extends OncePerRequestFilter {
         filterChain.doFilter(httpServletRequest,httpServletResponse);
     }
 
-    /**
-     * Sets the base directory of the source project
-     * @param basedir
-     */
-    public void setBasedir(Resource basedir) {
-        this.basedir = basedir;
-    }
-
-    /**
-     * Sets the destination deployment directory
-     * @param destdir
-     */
-    public void setDestdir(Resource destdir) {
-        this.destdir = destdir;
-    }
 }
