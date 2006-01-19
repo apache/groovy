61cd5d8134d48f032a9eedda8c583645f6316f98
Jochen Theodorou <blackdrag@gmx.org>
2006-01-19 17:52:14 +0000

    first tests for the scoping
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3431 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/groovy-core/src/test/gls/scope/MultipleDefinitionOfSameVariableTest.groovy
@@ -0,0 +1,59 @@
+
+import gls.scope.CompilableTestSupport;
+
+public class MultipleDefinitionOfSameVariableTest extends CompilableTestSupport {
+
+   public void testInSameBlock() {
+     shouldNotCompile("""
+       def foo = 1
+       def foo = 2
+     """)
+      
+     shouldNotCompile("""
+       class Foo {
+         def foo() {
+           def bar=1
+           def bar=2
+         }
+       }
+     """)
+   }
+   
+   public void testInSubblocks() {
+     shouldNotCompile("""
+       def foo = 1
+       5.times { def foo=2 }
+     """)
+     
+     shouldNotCompile("""
+       def foo = 1
+       label1: { def foo=2 }
+     """)
+     
+     shouldNotCompile("""
+       def foo = 1
+       for (i in []) { def foo=2 }
+     """)
+     
+     shouldNotCompile("""
+       def foo = 1
+       while (true) { def foo=2 }
+     """)     
+   } 
+   
+   public void testInNestedClosure() {
+     shouldNotCompile("""
+       def foo = 1
+       5.times { 6.times {def foo=2 }
+     """)
+   }
+   
+   public void testBindingHiding() {
+     assertScript("""
+       foo = 1
+       def foo = 3
+       assert foo==3
+     """)
+   }
+}
+ 
