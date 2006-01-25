508e983e4373e262a6b028805d9ab5486c32a20b
Jochen Theodorou <blackdrag@gmx.org>
2006-01-25 22:00:30 +0000

    same test as before but now with correct spelling
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@3444 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/lang/ClassReloadingTest.groovy
@@ -0,0 +1,35 @@
+package groovy.lang
+
+class ClassReloadingTest extends GroovyTestCase {
+
+	public void testRealoding() {
+		def cl = new GroovyClassLoader(this.class.classLoader);
+		def currentDir = new File(".").getAbsolutePath() 
+		cl.addClasspath(currentDir)
+		
+		def file = new File("TestReload.groovy")
+		assert !file.exists()
+		
+		file.write """
+		  class TestReload {
+		    @Property hello = "hello"
+		  }
+		  """
+		def groovyClass = cl.loadClass("TestReload",true,false)
+		def object = groovyClass.newInstance()
+		assert "hello"== object.hello
+					
+		// change class
+		file.write """
+		  class TestReload {
+		    @Property hello = "goodbye"
+		  }
+		  """
+		
+		// reload		
+		groovyClass = cl.loadClass("TestReload",true,false)
+		object  = groovyClass.newInstance()
+		assert "goodbye" == object.hello
+		file.delete()
+	}
+}
