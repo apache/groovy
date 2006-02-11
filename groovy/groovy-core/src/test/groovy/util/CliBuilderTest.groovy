4b930672351f4d41b0275d8428626f2ca1551af5
Dierk Koenig <dierk.koenig@canoo.com>
2006-02-11 17:56:06 +0000

    make the tests less sensitive to accidental reorderings in toString()
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3538 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/groovy-core/src/test/groovy/util/CliBuilderTest.groovy
+++ groovy/groovy-core/src/test/groovy/util/CliBuilderTest.groovy
@@ -8,7 +8,12 @@ class CliBuilderTest extends GroovyTestCase {
         cli.i(argName: 'extension', optionalArg: true,
              "modify files in place, create backup if extension is given (e.g. \'.bak\')")
 
-        assert cli.options.toString() == "[ Options: [ short {i=[ option: i  :: modify files in place, create backup if extension is given (e.g. '.bak') ], c=[ option: c encoding  :: character encoding ], h=[ option: h help  :: usage information ]} ] [ long {encoding=[ option: c encoding  :: character encoding ], help=[ option: h help  :: usage information ]} ]"
+        def stringified = cli.options.toString()
+        assert stringified =~ /i=. option: i  :: modify files in place, create backup if extension is given/
+        assert stringified =~ /c=. option: c encoding  :: character encoding/
+        assert stringified =~ /h=. option: h help  :: usage information/
+        assert stringified =~ /encoding=. option: c encoding  :: character encoding/
+        assert stringified =~ /help=. option: h help  :: usage information/
 
         def options = cli.parse(['-h','-c','ASCII'])
 
