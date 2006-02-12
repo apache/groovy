9a4a256c9c8c932067f62fec9fd2f7f2f9a60b81
Dierk Koenig <dierk.koenig@canoo.com>
2006-02-12 19:09:29 +0000

    avoid doubled collection of tests in the suites. small brush-up
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@3541 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/UberTestCase.java
+++ src/test/UberTestCase.java
@@ -18,7 +18,7 @@ public class UberTestCase extends TestCase {
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        TestSuite suite = (TestSuite) AllTestSuite.suite("./src/test/groovy","Test.groovy");
+        TestSuite suite = (TestSuite) AllTestSuite.suite("./src/test/groovy","src.test.groovy.[^/\\\\]*Test.groovy");
 
         String osName = System.getProperty ( "os.name" ) ;
         if ( osName.equals ( "Linux" ) || osName.equals ( "SunOS" ) ) {
@@ -33,46 +33,46 @@ public class UberTestCase extends TestCase {
 
 //  The following classes appear in target/test-classes but do not extend junit.framework.TestCase
 //
-//        suite.addTestSuite(AnotherMockInputStream.class);
-//        suite.addTestSuite(Bean.class);
-//        suite.addTestSuite(Bean249.class);
-//        suite.addTestSuite(BooleanBean.class);
-//        suite.addTestSuite(CallAnotherScript.class);
-//        suite.addTestSuite(ClassWithScript.class);
-//        suite.addTestSuite(ComparableFoo.class);
-//        suite.addTestSuite(CreateData.class);
-//        suite.addTestSuite(Entry.class);
-//        suite.addTestSuite(EvalInScript.class);
-//        suite.addTestSuite(Feed.class);
-//        suite.addTestSuite(Foo.class);
-//        suite.addTestSuite(HelloWorld.class);
-//        suite.addTestSuite(HelloWorld2.class);
-//        suite.addTestSuite(Html2Wiki.class);
-//        suite.addTestSuite(IntegerCategory.class);
-//        suite.addTestSuite(Loop.class);
-//        suite.addTestSuite(Loop2.class);
-//        suite.addTestSuite(MapFromList.class);
-//        suite.addTestSuite(MarkupTestScript.class);
-//        suite.addTestSuite(MethodTestScript.class);
-//        suite.addTestSuite(MockInputStream.class);
-//        suite.addTestSuite(MockProcess.class);
-//        suite.addTestSuite(MockSocket.class);
-//        suite.addTestSuite(OverloadA.class);
-//        suite.addTestSuite(OverloadB.class);
-//        suite.addTestSuite(NavToWiki.class);
-//        suite.addTestSuite(Person.class);
-//        suite.addTestSuite(SampleMain.class);
-//        suite.addTestSuite(ScriptWithFunctions.class);
-//        suite.addTestSuite(ShowArgs.class);
-//        suite.addTestSuite(StringCategory.class);
-//        suite.addTestSuite(SuperBase.class);
-//        suite.addTestSuite(SuperDerived.class);
-//        suite.addTestSuite(TestBase.class);
-//        suite.addTestSuite(TestCaseBug.class);
-//        suite.addTestSuite(TestDerived.class);
-//        suite.addTestSuite(TinyAgent.class);
-//        suite.addTestSuite(UnitTestAsScript.class);
-//        suite.addTestSuite(UseClosureInScript.class);
-//        suite.addTestSuite(X.class);
-//        suite.addTestSuite(createLoop.class);
+//        AnotherMockInputStream.class
+//        Bean.class
+//        Bean249.class
+//        BooleanBean.class
+//        CallAnotherScript.class
+//        ClassWithScript.class
+//        ComparableFoo.class
+//        CreateData.class
+//        Entry.class
+//        EvalInScript.class
+//        Feed.class
+//        Foo.class
+//        HelloWorld.class
+//        HelloWorld2.class
+//        Html2Wiki.class
+//        IntegerCategory.class
+//        Loop.class
+//        Loop2.class
+//        MapFromList.class
+//        MarkupTestScript.class
+//        MethodTestScript.class
+//        MockInputStream.class
+//        MockProcess.class
+//        MockSocket.class
+//        OverloadA.class
+//        OverloadB.class
+//        NavToWiki.class
+//        Person.class
+//        SampleMain.class
+//        ScriptWithFunctions.class
+//        ShowArgs.class
+//        StringCategory.class
+//        SuperBase.class
+//        SuperDerived.class
+//        TestBase.class
+//        TestCaseBug.class
+//        TestDerived.class
+//        TinyAgent.class
+//        UnitTestAsScript.class
+//        UseClosureInScript.class
+//        X.class
+//        createLoop.class
 }
--- src/test/UberTestCase2.java
+++ src/test/UberTestCase2.java
@@ -13,45 +13,45 @@ public class UberTestCase2 extends TestCase {
     }
 
 // no tests inside (should we have an AbstractGroovyTestCase???)
-//        suite.addTestSuite(groovy.bugs.TestSupport.class);
-//        suite.addTestSuite(groovy.sql.TestHelper.class);
-//        suite.addTestSuite(groovy.swing.Demo.class);
+//        groovy.bugs.TestSupport.class
+//        groovy.sql.TestHelper.class
+//        groovy.swing.Demo.class
 
 //  The following classes appear in target/test-classes but do not extend junit.framework.TestCase
 //
-//        suite.addTestSuite(cheese.Cheddar.class);
-//        suite.addTestSuite(cheese.Provolone.class);
-//        suite.addTestSuite(groovy.bugs.Cheese.class);
-//        suite.addTestSuite(groovy.bugs.MyRange.class);
-//        suite.addTestSuite(groovy.bugs.Scholastic.class);
-//        suite.addTestSuite(groovy.bugs.SimpleModel.class);
-//        suite.addTestSuite(groovy.DummyInterface.class);
-//        suite.addTestSuite(groovy.DummyMethods.class);
-//        suite.addTestSuite(groovy.gravy.Build.class);
-//        suite.addTestSuite(groovy.j2ee.J2eeConsole.class);
-//        suite.addTestSuite(groovy.lang.DerivedScript.class);
-//        suite.addTestSuite(groovy.lang.DummyGString.class);
-//        suite.addTestSuite(groovy.lang.MockWriter.class);
-//        suite.addTestSuite(groovy.mock.example.CheeseSlicer.class);
-//        suite.addTestSuite(groovy.mock.example.SandwichMaker.class);
-//        suite.addTestSuite(groovy.model.MvcDemo.class);
-//        suite.addTestSuite(groovy.OuterUser.class);
-//        suite.addTestSuite(groovy.script.AtomTestScript.class);
-//        suite.addTestSuite(groovy.script.Entry.class);
-//        suite.addTestSuite(groovy.script.Feed.class);
-//        suite.addTestSuite(groovy.script.PackageScript.class);
-//        suite.addTestSuite(groovy.script.Person.class);
-//        suite.addTestSuite(groovy.sql.Person.class);
-//        suite.addTestSuite(groovy.swing.MyTableModel.class);
-//        suite.addTestSuite(groovy.swing.SwingDemo.class);
-//        suite.addTestSuite(groovy.swing.TableDemo.class);
-//        suite.addTestSuite(groovy.swing.TableLayoutDemo.class);
-//        suite.addTestSuite(groovy.txn.TransactionBean.class);
-//        suite.addTestSuite(groovy.txn.TransactionBuilder.class);
-//        suite.addTestSuite(groovy.util.Dummy.class);
-//        suite.addTestSuite(groovy.util.DummyMBean.class);
-//        suite.addTestSuite(groovy.util.SpoofTask.class);
-//        suite.addTestSuite(groovy.util.SpoofTaskContainer.class);
-//        suite.addTestSuite(groovy.xml.TestXmlSupport.class);
+//        cheese.Cheddar.class
+//        cheese.Provolone.class
+//        groovy.bugs.Cheese.class
+//        groovy.bugs.MyRange.class
+//        groovy.bugs.Scholastic.class
+//        groovy.bugs.SimpleModel.class
+//        groovy.DummyInterface.class
+//        groovy.DummyMethods.class
+//        groovy.gravy.Build.class
+//        groovy.j2ee.J2eeConsole.class
+//        groovy.lang.DerivedScript.class
+//        groovy.lang.DummyGString.class
+//        groovy.lang.MockWriter.class
+//        groovy.mock.example.CheeseSlicer.class
+//        groovy.mock.example.SandwichMaker.class
+//        groovy.model.MvcDemo.class
+//        groovy.OuterUser.class
+//        groovy.script.AtomTestScript.class
+//        groovy.script.Entry.class
+//        groovy.script.Feed.class
+//        groovy.script.PackageScript.class
+//        groovy.script.Person.class
+//        groovy.sql.Person.class
+//        groovy.swing.MyTableModel.class
+//        groovy.swing.SwingDemo.class
+//        groovy.swing.TableDemo.class
+//        groovy.swing.TableLayoutDemo.class
+//        groovy.txn.TransactionBean.class
+//        groovy.txn.TransactionBuilder.class
+//        groovy.util.Dummy.class
+//        groovy.util.DummyMBean.class
+//        groovy.util.SpoofTask.class
+//        groovy.util.SpoofTaskContainer.class
+//        groovy.xml.TestXmlSupport.class);
 
 }
