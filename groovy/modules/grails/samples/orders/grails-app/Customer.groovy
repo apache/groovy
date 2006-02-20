429a3afb5461a0ae87550052c86c902600bc8817
Graeme Rocher <grocher@vmware.com>
2006-02-20 14:02:02 +0000

    removed old redundant sample app
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3620 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/samples/orders/grails-app/Customer.groovy
+++
@@ -1,5 +0,0 @@
-class Customer {
-   @Property int id
-   @Property int version
-   @Property String name
-}
--- groovy/modules/grails/samples/orders/grails-app/test/Customer.groovy
+++
@@ -1,7 +0,0 @@
-package test
-
-class Customer {
-   @Property int id
-   @Property int version
-   @Property String name
-}
