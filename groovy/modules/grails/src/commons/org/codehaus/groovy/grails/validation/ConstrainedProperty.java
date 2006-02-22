8f57589036d5f3794435b9ace34c07b159da8531
Graeme Rocher <grocher@vmware.com>
2006-02-22 18:23:22 +0000

    fixed various problems with the validation logic related to size ranges etc.
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3650 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/grails/src/commons/org/codehaus/groovy/grails/validation/ConstrainedProperty.java
+++ groovy/modules/grails/src/commons/org/codehaus/groovy/grails/validation/ConstrainedProperty.java
@@ -505,10 +505,10 @@ public class ConstrainedProperty   {
                 if(propertyValue == null) {
                     super.rejectValue(errors,RANGE_CONSTRAINT + INVALID_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_RANGE_MESSAGE, args ));
                 }
-                else if(range.getFrom().compareTo( propertyValue ) == -1) {
+                else if(range.getFrom().compareTo( propertyValue ) == 1) {
                     super.rejectValue(errors,SIZE_CONSTRAINT + TOOSMALL_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_SIZE_MESSAGE, args ));
                 }
-                else if(range.getFrom().compareTo(propertyValue) == 1) {
+                else if(range.getTo().compareTo(propertyValue) == -1) {
                     super.rejectValue(errors,SIZE_CONSTRAINT + TOOBIG_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_SIZE_MESSAGE, args ));
                 }
 
@@ -775,10 +775,10 @@ public class ConstrainedProperty   {
                 Integer length = new Integer(Array.getLength( propertyValue ));
                 if(!range.contains(length)) {
 
-                    if(range.getFrom().compareTo( length ) == -1) {
+                    if(range.getFrom().compareTo( length ) == 1) {
                         super.rejectValue(errors,LENGTH_CONSTRAINT + TOOSHORT_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_LENGTH_MESSAGE, args ));
                     }
-                    else if(range.getFrom().compareTo(length) == 1) {
+                    else if(range.getTo().compareTo(length) == -1) {
                         super.rejectValue(errors,LENGTH_CONSTRAINT + TOOLONG_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_LENGTH_MESSAGE, args ));
                     }
                     return;
@@ -787,29 +787,29 @@ public class ConstrainedProperty   {
             if(propertyValue instanceof Collection) {
                 Integer collectionSize = new Integer(((Collection)propertyValue).size());
                 if(!range.contains( collectionSize )) {
-                    if(range.getFrom().compareTo( collectionSize ) == -1) {
+                    if(range.getFrom().compareTo( collectionSize ) == 1) {
                         super.rejectValue(errors,SIZE_CONSTRAINT + TOOSMALL_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_SIZE_MESSAGE, args ));
                     }
-                    else if(range.getFrom().compareTo(collectionSize) == 1) {
+                    else if(range.getTo().compareTo(collectionSize) == -1) {
                         super.rejectValue(errors,SIZE_CONSTRAINT + TOOBIG_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_SIZE_MESSAGE, args ));
                     }
                 }
             }
             else if(propertyValue instanceof Number) {
-                if(range.getFrom().compareTo( propertyValue ) == -1) {
+                if(range.getFrom().compareTo( propertyValue ) == 1) {
                     super.rejectValue(errors,SIZE_CONSTRAINT + TOOSMALL_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_SIZE_MESSAGE, args ));
                 }
-                else if(range.getFrom().compareTo(propertyValue) == 1) {
+                else if(range.getTo().compareTo(propertyValue) == -1) {
                     super.rejectValue(errors,SIZE_CONSTRAINT + TOOBIG_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_SIZE_MESSAGE, args ));
                 }
             }
             else if(propertyValue instanceof String) {
                 Integer stringLength =  new Integer(((String)propertyValue ).length());
                 if(!range.contains(stringLength)) {
-                    if(range.getFrom().compareTo( stringLength ) == -1) {
+                    if(range.getFrom().compareTo( stringLength ) == 1) {
                         super.rejectValue(errors,LENGTH_CONSTRAINT + TOOSHORT_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_LENGTH_MESSAGE, args ));
                     }
-                    else if(range.getFrom().compareTo(stringLength) == 1) {
+                    else if(range.getTo().compareTo(stringLength) == -1) {
                         super.rejectValue(errors,LENGTH_CONSTRAINT + TOOLONG_SUFFIX,args,MessageFormat.format( DEFAULT_INVALID_LENGTH_MESSAGE, args ));
                     }
                 }
