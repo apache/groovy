a4ada6f3afca0d96345d019a0d56f062ccce952d
Guillaume Alleon <guillaume.alleon@gmail.com>
2006-04-23 14:40:14 +0000

    new package hierarchy and new server side for Groovy objects
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3772 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ groovy/modules/gsoap/src/main/groovy/net/groovysoap/PublicMethodNotFoundException.java
@@ -0,0 +1,38 @@
+package groovy.net.groovysoap;
+/*
+ * PublicMethodNotFoundException.java
+ *
+ * Created on 23 avril 2006, 08:29
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SU16766
+ */
+public class PublicMethodNotFoundException extends RuntimeException {
+    
+    /** Creates a new instance of PublicMethodNotFoundException */
+    public PublicMethodNotFoundException(String message) {
+        super(message);
+    }
+    
+    public PublicMethodNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public String getMessage() {
+        return super.getMessage() + getLocationText();
+    }
+    
+    protected String getLocationText() {
+        String answer = ". ";
+        
+        if (answer.equals(". ")) {
+            return "";
+        }
+        return answer;
+    }
+}
