5e85e747c465b24d74a7db4a44bba325e8cefd79
Guillaume Alleon <guillaume.alleon@gmail.com>
2006-04-23 14:03:17 +0000

    patched following XmlShema.jar corrections
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3767 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/gsoap/src/test/Test1.groovy
+++ groovy/modules/gsoap/src/test/Test1.groovy
@@ -1,14 +1,14 @@
-import org.codehaus.groovy.gsoap.SoapClient
+import groovy.net.groovysoap.SoapClient
 
-import org.codehaus.xfire.XFire;
-import org.codehaus.xfire.XFireFactory;
-import org.codehaus.xfire.server.http.XFireHttpServer;
-import org.codehaus.xfire.service.Service;
-import org.codehaus.xfire.service.binding.ObjectInvoker;
-import org.codehaus.xfire.service.binding.ObjectServiceFactory;
+import org.codehaus.xfire.XFire
+import org.codehaus.xfire.XFireFactory
+import org.codehaus.xfire.server.http.XFireHttpServer
+import org.codehaus.xfire.service.Service
+import org.codehaus.xfire.service.invoker.ObjectInvoker
+import org.codehaus.xfire.service.binding.ObjectServiceFactory
 
-import test.PersonServiceImpl;
-import test.PersonService;
+import test.PersonServiceImpl
+import test.PersonService
 
 //
 // Create a SoapClient using http://localhost:8090/PersonService?wsdl as WSDL
@@ -17,20 +17,20 @@ import test.PersonService;
 class Test1 extends GroovyTestCase {
 
     void testSoapClient() {
-        def serviceFactory = new ObjectServiceFactory();
-        def service = serviceFactory.create(PersonService.class);
+        def serviceFactory = new ObjectServiceFactory()
+        def service = serviceFactory.create(PersonService.class)
         // Set the implementation class
-        service.setProperty(ObjectInvoker.SERVICE_IMPL_CLASS, PersonServiceImpl.class);
+        service.setProperty(ObjectInvoker.SERVICE_IMPL_CLASS, PersonServiceImpl.class)
         
         // Register the service in the ServiceRegistry
-        def xfire = XFireFactory.newInstance().getXFire();
-        xfire.getServiceRegistry().register(service);
+        def xfire = XFireFactory.newInstance().getXFire()
+        xfire.getServiceRegistry().register(service)
         
         // Start the HTTP server
-        def server = new XFireHttpServer();
-        server.setPort(6969);
-        System.out.println("start Server & run tests");
-        server.start();
+        def server = new XFireHttpServer()
+        server.setPort(6969)
+        System.out.println("start Server & run tests")
+        server.start()
 
         try {
             def proxy = new SoapClient("http://localhost:6969/PersonService?wsdl")
@@ -38,9 +38,9 @@ class Test1 extends GroovyTestCase {
             assert (result.firstName == "Guillaume")
 
             result = proxy.getPersons()
-            assert (result.ns1Person.size() == 3)
-            assert (result.ns1Person[0].firstName == "Guillaume")
-            assert (result.ns1Person[2].firstName == "Jochen")
+            assert (result.Person.size() == 3)
+            assert (result.Person[0].firstName == "Guillaume")
+            assert (result.Person[2].firstName == "Jochen")
         } finally {
             server.stop()
         }
