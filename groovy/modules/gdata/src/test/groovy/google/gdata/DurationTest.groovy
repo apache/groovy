ae7a9224b4a6407a4a7cd27f1180304d9da40222
John Wilson <tug@wilson.co.uk>
2006-04-24 17:44:03 +0000

    fix the test
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3775 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/gdata/src/test/groovy/google/gdata/DurationTest.groovy
+++ groovy/modules/gdata/src/test/groovy/google/gdata/DurationTest.groovy
@@ -1,9 +1,13 @@
+package groovy.google.gdata
+
 class DurationTest extends GroovyTestCase {
     void testFixedDurationArithmetic() {
-        def oneDay = 2.days - 1.day
-        assert oneDay.getMillis() == (24 * 60 *60 * 1000)
-        
-        oneDay = 1.day - 2.days + 24.hours
-        assert oneDay.getMillis() == (24 * 60 *60 * 1000)
+        use(TimeCategory) {
+            def oneDay = 2.days - 1.day
+            assert oneDay.getMillis() == (24 * 60 *60 * 1000)
+            
+            oneDay = 1.day - 2.days + 24.hours
+            assert oneDay.getMillis() == (24 * 60 *60 * 1000)
+        }
    }
 }
