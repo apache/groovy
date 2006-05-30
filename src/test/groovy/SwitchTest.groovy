4c6c59a1087dd38365a8f53bac6680df2aa57821
Dierk Koenig <dierk.koenig@canoo.com>
2006-05-30 13:15:26 +0000

    GROOVY-1329: handle null more gracefully in switch/case
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@3786 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/SwitchTest.groovy
+++ src/test/groovy/SwitchTest.groovy
@@ -1,8 +1,10 @@
 class SwitchTest extends GroovyTestCase {
 
+
     void testSwitch() {
         callSwitch("foo", "foo")
         callSwitch("bar", "barfoo")
+        callSwitch("dummy", "d*")
         callSwitch("xyz", "xyzDefault")
         callSwitch("zzz", "Default")
         callSwitch(4, "List")
@@ -11,17 +13,26 @@ class SwitchTest extends GroovyTestCase {
         callSwitch("inList", "List")
         callSwitch(1, "Integer")
         callSwitch(1.2, "Number")
+        callSwitch(null, "null")
     }
-    
+
     def callSwitch(x, expected) {
         println("Calling switch with ${x}")
 
         def result = ""
 
         switch (x) {
+        	case null:
+        		result = "null"
+                break
+
+        	case ~/d.*/:
+        		result = "d*"
+                break
+
             case "bar":
                 result = result + "bar"
-                
+
             case "foo":
                 result = result + "foo"
                 break
@@ -29,29 +40,29 @@ class SwitchTest extends GroovyTestCase {
             case [4, 5, 6, 'inList']:
                 result = "List"
                 break
-                
+
             case Integer:
                 result = "Integer"
                 break
-                
+
             case Number:
                 result = "Number"
                 break
-                
+
             case "xyz":
                 result = result + "xyz"
-                
+
             default:
                 result = result + "Default"
-                
+
                 // unnecessary just testing compiler
                 break;
         }
         println("Found result ${result}")
-        
+
         assert result == expected , "when calling switch with ${x}"
     }
-
+    
     // test the continue in switch, which should jump to the the while start
     void testSwitchScope() {
         def i = 0
