61186f27c15b725606735a5013b15e33cad7f9b8
John Wilson <tug@wilson.co.uk>
2006-06-09 10:40:21 +0000

    Fix bug in toMilliseconds and update unit test to chack for correct operation
    git-svn-id: http://svn.codehaus.org/groovy/trunk@3824 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/modules/gdata/src/test/groovy/google/gdata/DurationTest.groovy
+++ groovy/modules/gdata/src/test/groovy/google/gdata/DurationTest.groovy
@@ -6,10 +6,10 @@ class DurationTest extends GroovyTestCase {
     void testFixedDurationArithmetic() {
         use(TimeCategory) {
             def oneDay = 2.days - 1.day
-            assert oneDay.getMillis() == (24 * 60 *60 * 1000)
+            assert oneDay.toMilliseconds() == (24 * 60 * 60 * 1000)
             
             oneDay = 2.days - 1.day + 24.hours - 1440.minutes
-            assert oneDay.getMillis() == (24 * 60 *60 * 1000)
+            assert oneDay.toMilliseconds() == (24 * 60 * 60 * 1000)
         }
    }
 }
