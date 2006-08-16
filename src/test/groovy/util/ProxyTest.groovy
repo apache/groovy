c63e8b844116f4153daaf02d681b4f5cede0e9c5
Russel Winder <russel@russel.org.uk>
2006-08-16 16:44:36 +0000

    Added some unit tests for perceived problems cf GROOVY-1461.  They all pass!
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@3983 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/util/ProxyTest.groovy
+++ src/test/groovy/util/ProxyTest.groovy
@@ -20,6 +20,41 @@ class ProxyTest extends GroovyTestCase {
         // method from decorator, that is not in adaptee
         assertEquals 'new Method reached', proxy.someNewMethod()
     }
+
+  /*
+   *  Some test cases to probe perceived problems with each and collect on Proxy objects.
+   *  cf. GROOVY-1461.  Jonathan Carlson <Jonathan.Carlson@katun.com> made a proposal for a test
+   *  as a single method, Russel Winder <russel@russel.org.uk> split things up when entering
+   *  them so that there is only a single assert per method to try and maximize the benefit of
+   *  the tests.
+   */
+
+  void testProxyCollect ( ) {
+    def collection = [ 1 , 2 , 3 ]
+    def proxy = ( new Proxy ( ) ).wrap ( collection ) 
+    assertEquals ( [ 2 , 3 , 4 ] , proxy.collect { it + 1 } )
+  }
+
+  void testProxyAny ( ) {
+    def collection = [ 1 , 2 , 3 ]
+    def proxy = ( new Proxy ( ) ).wrap ( collection ) 
+    assertEquals ( true , proxy.any { it == 2 } )
+  }
+
+  void testProxyFind ( ) {
+    def collection = [ 1 , 2 , 3 ]
+    def proxy = ( new Proxy ( ) ).wrap ( collection ) 
+    assertEquals ( 2 , proxy.find { it == 2 } )
+  }
+
+  void testProxyEach ( ) {
+    def collection = [ 1 , 2 , 3 ]
+    def proxy = ( new Proxy ( ) ).wrap ( collection ) 
+    def testString = ''
+    proxy.each { testString += it }
+    assertEquals ( '123' , testString )
+  }
+
 }
 
 class StringDecorator extends Proxy{
