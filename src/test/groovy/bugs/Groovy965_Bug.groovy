5a74ac360c4820d09268215fb624a9954b2b17da
Russel Winder <russel@russel.org.uk>
2006-08-18 08:10:08 +0000

    Removing all the content as we cannot guarantee UTF-8 encoding works on any given platform.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@3987 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/Groovy965_Bug.groovy
+++ src/test/groovy/bugs/Groovy965_Bug.groovy
@@ -4,10 +4,20 @@ package groovy.bug
  *  A test case to ensure that Groovy can compile class names and variable names with non-ASCII
  *  characters and that non-ASCII characters in Strings do the right thing.
  *
+ *  <p>Unfortunately, we cannot actually have this test in the Subversion store since it
+ *  requires having an encoding.  Java internally uses UTF-16.  Most Linux/UNIX/Mac OS X users
+ *  use UTF-8 (or if they haven't caught up yet ISO-8859-{1..15], in Europe anyway).  Windows
+ *  internally is UTF-16 LE but it appears that the Europe region defaults to ISO-8859-1 which
+ *  is very silly.  All in all we cannot gurantee an ecoding so we cannot have the tests.</p>
+ *
+ *  <p>If anyone spots any errors in the rationale or finds a way to fix things please update at
+ *  will.</p>
+ *
  *  @suthor Russel Winder
  *  @version $LastChangedRevision$ $LastChangedDate$
  */
 class Groovy965_Bug extends GroovyTestCase {
+  /*
   void testUnicodeVariableNamesAndStrings ( ) {
     def âøñè = 'âøñè'
     assertEquals ( 'âøñè' , âøñè )
@@ -17,6 +27,9 @@ class Groovy965_Bug extends GroovyTestCase {
     def object = new Bläh ( ) 
     assert true
   }
+  */
 }
 
+/*
 class Bläh { }
+*/
