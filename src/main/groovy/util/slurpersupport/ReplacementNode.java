dae2d1f1ff6f4fe76c437d0aea413b041af0cba9
John Wilson <tug@wilson.co.uk>
2006-11-29 11:40:19 +0000

    add class missing from previous commit
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@4276 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/main/groovy/util/slurpersupport/ReplacementNode.java
@@ -0,0 +1,35 @@
+/*
+ * Created on Nov 27, 2006
+ *
+ * Copyright 2006 John G. Wilson
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package groovy.util.slurpersupport;
+
+import groovy.lang.GroovyObject;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class ReplacementNode {
+    public void build(final GroovyObject builder, final Map namespaceMap, final Map namespaceTagHints) {
+        
+    }
+    
+    public Writer writeTo(final Writer out) throws IOException {
+        return out;
+    }
+}
