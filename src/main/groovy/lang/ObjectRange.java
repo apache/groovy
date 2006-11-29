bc3758378ac48fdb684c9be64c238eb290798b3b
Jochen Theodorou <blackdrag@gmx.org>
2006-11-29 10:23:37 +0000

    adds a contains(Object) bridge method to delegate to contains(Comparable)
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@4274 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/lang/ObjectRange.java
+++ src/main/groovy/lang/ObjectRange.java
@@ -276,22 +276,21 @@ public class ObjectRange extends AbstractList implements Range {
         String fromText = InvokerHelper.inspect(from);
         return (reverse) ? "" + toText + ".." + fromText : "" + fromText + ".." + toText;
     }
+    
+    public boolean contains(Object value) {
+        if (value instanceof Comparable) {
+            return contains((Comparable) value);
+        } else {
+            return super.contains(value);
+        }
+    }
 
     public boolean contains(Comparable value) {
-        if (from instanceof BigDecimal || to instanceof BigDecimal) {
-            int result = (new BigDecimal("" + from)).compareTo(new BigDecimal("" + value));
-            if (result == 0) {
-                return true;
-            }
-            return result < 0 && (new BigDecimal("" + to)).compareTo(new BigDecimal("" + value)) >= 0;
+        int result = from.compareTo(value);
+        if (result == 0) {
+            return true;
         }
-        else {
-            int result = from.compareTo(value);
-            if (result == 0) {
-                return true;
-            }
-            return result < 0 && to.compareTo(value) >= 0;
-        }
+        return result < 0 && to.compareTo(value) >= 0;
     }
 
     public void step(int step, Closure closure) {
