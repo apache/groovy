28851b4c6196304728eebd62e152f252a9b90214
John Wilson <tug@wilson.co.uk>
2007-02-09 17:28:41 +0000

    Last part of commit to change MetaClassregistry to an interface
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@5042 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/main/groovy/lang/MetaClassRegistry.java
@@ -0,0 +1,56 @@
+/*
+ * Created on Feb 9, 2007
+ *
+ * Copyright 2007 John G. Wilson
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package groovy.lang;
+
+import java.util.List;
+
+import org.codehaus.groovy.runtime.Reflector;
+
+public interface MetaClassRegistry {
+    /*
+     * The intialisation part of this is temporary
+     * eventually we will support plug in implementations
+     */
+    final MetaClassRegistry registry  = new MetaClassRegistryImpl();
+    
+    /*
+     * The main function of the Registry
+     * If a Metaclass exists then return it
+     * otherwise create one, put it in the Registry and return it
+     */
+    MetaClass getMetaClass(Class theClass);
+    
+    /*
+     * Do we really want these two?
+     */
+    void setMetaClass(Class theClass, MetaClass theMetaClass);
+    void removeMetaClass(Class theClass);
+    
+    /*
+     * What's this for?
+     */
+    Reflector loadReflector(Class theClass, List methods);
+    
+    /*
+     * These seem MetaClassImpl specific should they be on MetaClassRegistry?
+     */
+    boolean useAccessible();
+    List getInstanceMethods();
+    List getStaticMethods();
+}
