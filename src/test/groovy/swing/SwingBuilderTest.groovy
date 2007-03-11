64d210049ac6a17cd06f0bbe44b5e9633b627c6c
Paul King <paulk@asert.com.au>
2007-03-11 09:48:43 +0000

    increase coverage
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@5523 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/swing/SwingBuilderTest.groovy
+++ src/test/groovy/swing/SwingBuilderTest.groovy
@@ -1,16 +1,21 @@
 package groovy.swing
 
 import javax.swing.JFrame
+import javax.swing.JDialog
 
 class SwingBuilderTest extends GroovyTestCase {
     void testNamedWidgetCreation() {
         def swing = new SwingBuilder()
         // invoke builder
         swing.frame(id:'myFrameId', title:'This is my Frame')
+        swing.dialog(id:'myDialogId', title:'This is my Dialog')
         // extract named widget
         def myFrame = swing.myFrameId
+        def myDialog = swing.myDialogId
         // test widget properties
         assert myFrame instanceof JFrame
         assert myFrame.title == 'This is my Frame'
+        assert myDialog instanceof JDialog
+        assert myDialog.title == 'This is my Dialog'
     }
 }
