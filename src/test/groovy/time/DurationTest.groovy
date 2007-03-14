e648f958ae1dc1159e25d5f1f54fefffa0ff7074
Paul King <paulk@asert.com.au>
2007-03-14 10:27:23 +0000

    better error messages, trying to debug different build server timezones
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@5553 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/time/DurationTest.groovy
+++ src/test/groovy/time/DurationTest.groovy
@@ -7,10 +7,12 @@ class DurationTest extends GroovyTestCase {
     void testFixedDurationArithmetic() {
         use(TimeCategory) {
             def oneDay = 2.days - 1.day
-            assert oneDay.toMilliseconds() == (24 * 60 * 60 * 1000)
+            assert oneDay.toMilliseconds() == (24 * 60 * 60 * 1000): \
+                "Expected ${24 * 60 * 60 * 1000} but was ${oneDay.toMilliseconds()}"
             
             oneDay = 2.days - 1.day + 24.hours - 1440.minutes
-            assert oneDay.toMilliseconds() == (24 * 60 * 60 * 1000)
+            assert oneDay.toMilliseconds() == (24 * 60 * 60 * 1000): \
+                "Expected ${24 * 60 * 60 * 1000} but was ${oneDay.toMilliseconds()}"
         }
    }
 
@@ -20,13 +22,14 @@ class DurationTest extends GroovyTestCase {
             def twoMonthsA = 1.month + 1.month
             // subtract two absolute dates to get a duration
             def twoMonthsB = 2.months.from.now - 0.months.from.now
-            assert twoMonthsA.toMilliseconds() == twoMonthsB.toMilliseconds()
-
+            assert twoMonthsA.toMilliseconds() == twoMonthsB.toMilliseconds(): \
+                "Expected ${twoMonthsA.toMilliseconds()} but was ${twoMonthsB.toMilliseconds()}"
             // add two durations
             def monthAndWeekA = 1.month + 1.week
             // subtract two absolute dates to get a duration
             def monthAndWeekB = 1.month.from.now - 1.week.ago
-            assert monthAndWeekA.toMilliseconds() == monthAndWeekB.toMilliseconds()
+            assert monthAndWeekA.toMilliseconds() == monthAndWeekB.toMilliseconds(): \
+                "Expected ${monthAndWeekA.toMilliseconds()} but was ${monthAndWeekB.toMilliseconds()}"
         }
     }
 
@@ -39,10 +42,12 @@ class DurationTest extends GroovyTestCase {
             def dstAdjustment = nowOffset - thenOffset
 
             def week = then - 1.month - now - dstAdjustment
-            assert week.toMilliseconds() == (7 * 24 * 60 * 60 * 1000)
+            assert week.toMilliseconds() == (7 * 24 * 60 * 60 * 1000): \
+                "Expected ${7 * 24 * 60 * 60 * 1000} but was ${week.toMilliseconds()} with an adjustment of $dstAdjustment"
 
             week = then - (now + 1.month) - dstAdjustment
-            assert week.toMilliseconds() == (7 * 24 * 60 * 60 * 1000)
+            assert week.toMilliseconds() == (7 * 24 * 60 * 60 * 1000): \
+                "Expected ${7 * 24 * 60 * 60 * 1000} but was ${week.toMilliseconds()} with an adjustment of $dstAdjustment"
         }
     }
 }
