6a5eda43e537077c910d3eb4fbeba0136d600ba7
Paul King <paulk@asert.com.au>
2007-03-24 07:01:55 +0000

    fix build
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@5756 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/swing/SwingBuilder.java
+++ src/main/groovy/swing/SwingBuilder.java
@@ -724,7 +724,7 @@ public class SwingBuilder extends BuilderSupport {
             BoxLayout answer = new BoxLayout(target, axis);
 
             // now let's try to set the layout property
-            InvokerHelper.setProperty(parent, "layout", answer);
+            InvokerHelper.setProperty(target, "layout", answer);
             return answer;
         } else {
             throw new RuntimeException("Must be nested inside a Container");
