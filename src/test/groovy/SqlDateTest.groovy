58bf9b93da5d7b0cd1fc34ce9ab10a7e38f968e8
Paul King <paulk@asert.com.au>
2007-03-25 07:48:42 +0000

    timezone formula AGAIN!!
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@5774 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/SqlDateTest.groovy
+++ src/test/groovy/SqlDateTest.groovy
@@ -5,62 +5,57 @@ import org.codehaus.groovy.runtime.TimeCategory
 class SqlDateTest extends GroovyTestCase {
 
     void testIncrement() {
-        def rightNowMillis = System.currentTimeMillis()
-        def sqlDate = new java.sql.Date(rightNowMillis)
-        def oldOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def nowMillis = System.currentTimeMillis()
+        def sqlDate = new java.sql.Date(nowMillis)
+        def nowOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
         sqlDate++
-        def newOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def incOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
+        assertTrue "incrementing a java.sql.Date returned an incorrect type: ${sqlDate.class}", sqlDate instanceof java.sql.Date
 
-        assertTrue  "incrementing a java.sql.Date returned an incorrect type: ${sqlDate.class}", sqlDate instanceof java.sql.Date
-
-        def diff = sqlDate.getTime() - rightNowMillis
-        // adjust for daylight savings
-        diff += (newOffset - oldOffset).toMilliseconds()
+        // difference adjusted for daylight savings
+        def diff = (sqlDate.getTime() + incOffset) - (nowMillis + nowOffset)
         assertEquals "incrementing a java.sql.Date did not work properly", 1000 * 60 * 60 * 24, diff
     }
 
       void testDecrement() {
-        def rightNowMillis = System.currentTimeMillis()
-        def sqlDate = new java.sql.Date(rightNowMillis)
-        def oldOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def nowMillis = System.currentTimeMillis()
+        def sqlDate = new java.sql.Date(nowMillis)
+        def nowOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
         sqlDate--
-        def newOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
-
-        assertTrue  "decrementing a java.sql.Date returned an incorrect type: ${sqlDate.class}", sqlDate instanceof java.sql.Date
+        def decOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
+        assertTrue "decrementing a java.sql.Date returned an incorrect type: ${sqlDate.class}", sqlDate instanceof java.sql.Date
         
-        def diff = rightNowMillis - sqlDate.getTime()
-        // adjust for daylight savings
-        diff += (newOffset - oldOffset).toMilliseconds()
+        // difference adjusted for daylight savings
+        def diff = (nowMillis + nowOffset) - (sqlDate.getTime() + decOffset)
+
         assertEquals "decrementing a java.sql.Date did not work properly", 1000 * 60 * 60 * 24, diff
     }
       
     void testPlusOperator() {
-        def rightNowMillis = System.currentTimeMillis()
-        def sqlDate = new java.sql.Date(rightNowMillis)
-        def oldOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def nowMillis = System.currentTimeMillis()
+        def sqlDate = new java.sql.Date(nowMillis)
+        def nowOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
         sqlDate += 1
-        def newOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def incOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
 
         assertTrue  "the plus operator applied to a java.sql.Date returned an incorrect type: ${sqlDate.class}", sqlDate instanceof java.sql.Date
         
-        def diff = sqlDate.getTime() - rightNowMillis
-        // adjust for daylight savings
-        diff += (newOffset - oldOffset).toMilliseconds()
+        // difference adjusted for daylight savings
+        def diff = (sqlDate.getTime() + incOffset) - (nowMillis + nowOffset)
         assertEquals "decrementing a java.sql.Date did not work properly", 1000 * 60 * 60 * 24, diff
     }
     
     void testMinusOperator() {
-        def rightNowMillis = System.currentTimeMillis()
-        def sqlDate = new java.sql.Date(rightNowMillis)
-        def oldOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def nowMillis = System.currentTimeMillis()
+        def sqlDate = new java.sql.Date(nowMillis)
+        def nowOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
         sqlDate -= 1
-        def newOffset = TimeCategory.getDaylightSavingsOffset(sqlDate)
+        def decOffset = TimeCategory.getDaylightSavingsOffset(sqlDate).toMilliseconds()
 
         assertTrue  "the minus operator applied to a java.sql.Date returned an incorrect type: ${sqlDate.class}", sqlDate instanceof java.sql.Date
         
-        def diff = rightNowMillis - sqlDate.getTime()
-        // adjust for daylight savings
-        diff += (newOffset - oldOffset).toMilliseconds()
+        // difference adjusted for daylight savings
+        def diff = (nowMillis + nowOffset) - (sqlDate.getTime() + decOffset)
         assertEquals "decrementing a java.sql.Date did not work properly", 1000 * 60 * 60 * 24, diff
     }
 }
