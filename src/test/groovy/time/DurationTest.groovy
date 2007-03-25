ca9d0d7e4822c36d1d26474e4ae07bea001863ee
Paul King <paulk@asert.com.au>
2007-03-25 11:50:10 +0000

    timezone formula AGAIN!!
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@5775 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/time/DurationTest.groovy
+++ src/test/groovy/time/DurationTest.groovy
@@ -18,17 +18,23 @@ class DurationTest extends GroovyTestCase {
 
     void testDurationArithmetic() {
         use(TimeCategory) {
+            def nowOffset = (new Date()).daylightSavingsOffset
+
             // add two durations
             def twoMonthsA = 1.month + 1.month
-            // subtract two absolute dates to get a duration
-            def twoMonthsB = 2.months.from.now - 0.months.from.now
-            assert twoMonthsA.toMilliseconds() == twoMonthsB.toMilliseconds()
+            // subtract dates which are two months apart
+            def offset = 2.months.from.now.daylightSavingsOffset - 0.months.from.now.daylightSavingsOffset
+            def twoMonthsB = 2.months.from.now + offset - 0.months.from.now
+            assertEquals "Two months should equal difference between two fates two months apart",
+                (twoMonthsA - nowOffset).toMilliseconds(), twoMonthsB.toMilliseconds()
 
             // add two durations
             def monthAndWeekA = 1.month + 1.week
-            // add absolute date and a duration
-            def monthAndWeekB = 1.month.from.now + 1.week - 0.months.from.now
-            assert monthAndWeekA.toMilliseconds() == monthAndWeekB.toMilliseconds()
+            // subtract absolute date and a duration from another absolute date
+            offset = (1.month.from.now + 1.week).daylightSavingsOffset - 0.months.from.now.daylightSavingsOffset
+            def monthAndWeekB = 1.month.from.now + 1.week + offset - 0.months.from.now
+            assertEquals "A week and a month absolute duration should be the same as the difference between two dates that far apart",
+                (monthAndWeekA - nowOffset).toMilliseconds(), monthAndWeekB.toMilliseconds()
         }
     }
 
