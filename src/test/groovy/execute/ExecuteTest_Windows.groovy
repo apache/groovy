09c9dc22cdaae3e3423e7dc29d4cefeefa7922a0
Paul King <paulk@asert.com.au>
2007-04-30 13:50:36 +0000

    tweaked execute tests
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@6215 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/execute/ExecuteTest_Windows.groovy
+++ src/test/groovy/execute/ExecuteTest_Windows.groovy
@@ -9,22 +9,25 @@ package groovy.execute
  *  @version $Revision: 6165 $
  */
 class ExecuteTest_Windows extends GroovyTestCase {
-  void testShellEchoOneArray() {
+  void testCmdEchoOneArray() {
     def process = ( [ "cmd.exe" , "/c" , "echo 1" ] as String[] ).execute()
     process.waitFor()
     assert process.in.text.trim() == "1"
   }
-  void testShellEchoOneList() {
+
+  void testCmdEchoOneList() {
     def process = [ "cmd.exe" , "/c" , "echo 1" ].execute ( )
     process.waitFor()
     assert process.in.text.trim() == "1"
   }
-  void testShellDate() {
+
+  void testCmdDate() {
     def process = "cmd.exe /c date.exe /t".execute()
     process.waitFor()
     def theDate = process.in.text.trim()
     assert theDate.size() > 9, "Expected '$theDate' to be at least 9 chars long" 
   }
+
   void testEchoOneArray() {
     try {
       def process = ( [ "echo 1" ] as String[] ).execute()
@@ -33,6 +36,7 @@ class ExecuteTest_Windows extends GroovyTestCase {
     }
     catch ( IOException ioe ) { }
   }
+
   void testEchoOneList() {
     try {
       def process = [ "echo 1" ].execute()
