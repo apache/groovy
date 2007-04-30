124384c585757707d037c15ff87c5440808297e9
Paul King <paulk@asert.com.au>
2007-04-30 08:37:09 +0000

    skip test relying on Java 1.5 for earlier JVMs
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@6189 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/lang/InnerClassResolvingTest.groovy
+++ src/test/groovy/lang/InnerClassResolvingTest.groovy
@@ -2,13 +2,19 @@ package groovy.lang
 
 class InnerClassResolvingTest extends GroovyTestCase {
     public void testInnerClass() {
-        def caught = false
-        def t = Thread.start {
-	        Thread.setDefaultUncaughtExceptionHandler(
-                {thread,ex -> caught=true} as Thread.UncaughtExceptionHandler)
-            throw new Exception("huhu")
+        // Thread.UncaughtExceptionHandler was added in Java 1.5
+        if (System.properties.'java.version'[2] >= '5') {
+            def script = '''
+                def caught = false
+                def t = Thread.start {
+                    Thread.setDefaultUncaughtExceptionHandler(
+                        {thread,ex -> caught=true} as Thread.UncaughtExceptionHandler)
+                    throw new Exception("huhu")
+                }
+                t.join()
+                assert caught==true
+            '''
+            new GroovyShell().evaluate(script)
         }
-        t.join()
-        assert caught==true
     }
 }
