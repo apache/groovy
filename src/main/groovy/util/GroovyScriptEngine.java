06a919c4df4e283e12ffae1c0ae4ccfd74360cdf
Paul King <paulk@asert.com.au>
2007-06-16 23:31:52 +0000

    GROOVY-1912: GroovyScriptEngine.loadScriptByName is the cause of Stack Overflow Error
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@6587 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/util/GroovyScriptEngine.java
+++ src/main/groovy/util/GroovyScriptEngine.java
@@ -273,14 +273,15 @@ public class GroovyScriptEngine implements ResourceConnector {
 
     /**
      * Get the class of the scriptName in question, so that you can instantiate Groovy objects with caching and reloading.
-     * Note: This class is deprecated because we should not use a different parentClassLoader
+     * Note: This method is deprecated because we should not use a different parentClassLoader
      * @param scriptName
      * @return the loaded scriptName as a compiled class
      * @throws ResourceException
      * @throws ScriptException
+     * @deprecated
      */
     public Class loadScriptByName(String scriptName) throws ResourceException, ScriptException {
-        return loadScriptByName( scriptName);
+        return loadScriptByName( scriptName, groovyLoader);
     }
 
 
