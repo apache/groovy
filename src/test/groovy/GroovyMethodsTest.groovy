c90bca302da270f7bd432c3e75d4dfa00b1ee573
Paul King <paulk@asert.com.au>
2007-06-20 06:30:33 +0000

    remove duplicated test
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@6598 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/GroovyMethodsTest.groovy
+++ src/test/groovy/GroovyMethodsTest.groovy
@@ -374,19 +374,6 @@ class GroovyMethodsTest extends GroovyTestCase {
 		assert 3 == result.size()
     }
     
-    void testGroupByMapEntry() {
-		def expectedKeys = [Integer: [1, 3], String: [2, 4], BigDecimal: [5, 6]]
-		def expectedVals = [Integer: [1, 2], String: ["a", "b"], BigDecimal: [3.5, 4.6]]
-		def map = [1:1, 2:"a", 3:2, 4:"b", 5:3.5, 6:4.6]
-		def result = map.groupBy{ key, value -> value.class }
-		assert expectedKeys.Integer == result[Integer].collect{it.key}
-		assert expectedVals.Integer == result[Integer].collect{it.value}
-		assert expectedKeys.String == result[String].collect{it.key}
-		assert expectedVals.String == result[String].collect{it.value}
-		assert expectedKeys.BigDecimal == result[BigDecimal].collect{it.key}
-		assert expectedVals.BigDecimal == result[BigDecimal].collect{it.value}
-		assert 3 == result.size()
-	}
     def leftCol  = ["2"]
     def rightCol = ["1","2","3"]
 
