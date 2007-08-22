73b8f6fea9ad4802c32c4f394db85e73bac78f0d
Jason Dillon <jason.dillon@gmail.com>
2007-08-22 12:06:00 +0000

    Use the new message source for the current (soon to be legacy shell)
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@7529 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/ui/InteractiveShell.java
+++ src/main/groovy/ui/InteractiveShell.java
@@ -19,7 +19,7 @@ package groovy.ui;
 import groovy.lang.Binding;
 import groovy.lang.Closure;
 import groovy.lang.GroovyShell;
-import groovy.util.MessageSource;
+import groovy.text.MessageSource;
 
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.control.SourceUnit;
