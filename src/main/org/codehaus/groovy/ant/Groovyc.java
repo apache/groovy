6e114625c0ee3ef4d1c71b3e02d484d7e84c58a5
Jochen Theodorou <blackdrag@gmx.org>
2007-08-28 16:08:37 +0000

    missing import added (the conflict editor swallowed it)
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@7800 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/ant/Groovyc.java
+++ src/main/org/codehaus/groovy/ant/Groovyc.java
@@ -36,6 +36,7 @@ import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
