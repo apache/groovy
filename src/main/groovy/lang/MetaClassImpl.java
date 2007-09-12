08150a8b832496655874a80562fcd707dee98dfd
Joachim Baumann <joachim.baumann@xinaris.de>
2007-09-12 22:41:10 +0000

    fixed cast exception in retrieveConstructor()
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@8053 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/lang/MetaClassImpl.java
+++ src/main/groovy/lang/MetaClassImpl.java
@@ -854,13 +854,13 @@ public class MetaClassImpl implements MetaClass, MutableMetaClass {
 
 
    public Constructor retrieveConstructor(Class[] arguments) {
-       Constructor constructor = (Constructor) chooseMethod("<init>", constructors, arguments, false);
+       CachedConstructor constructor = (CachedConstructor) chooseMethod("<init>", constructors, arguments, false);
        if (constructor != null) {
-           return constructor;
+           return constructor.cachedConstructor;
        }
-       constructor = (Constructor) chooseMethod("<init>", constructors, arguments, true);
+       constructor = (CachedConstructor) chooseMethod("<init>", constructors, arguments, true);
        if (constructor != null) {
-           return constructor;
+           return constructor.cachedConstructor;
        }
        return null;
    }
