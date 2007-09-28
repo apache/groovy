e8f166a5333a4d3adf14adbe98ee3538c5b0aa57
Danno Ferrin <danno.ferrin@shemnon.com>
2007-09-28 16:26:36 +0000

    GROOVY-2132 - reference groovy instead of grails, tweak default imports
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@8293 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/StackTraceUtils.java
+++ src/main/org/codehaus/groovy/runtime/StackTraceUtils.java
@@ -1,4 +1,4 @@
-/* Copyright 2004-2005 the original author or authors.
+/* Copyright 2004-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,8 @@ import java.util.logging.Logger;
 
 /**
  *
- * Originally was grails.utils.GrailsUtils
- * Grails utility methods for command line and GUI applications
+ * Originally was grails.utils.GrailsUtils, removed some grails specific stuff.
+ * Utility methods removing internal lines from stack traces
  *
  * @author Graeme Rocher
  * @since 0.2
@@ -39,48 +39,16 @@ public class StackTraceUtils {
     private static final Logger STACK_LOG  = Logger.getLogger("StackTrace"); //TODO use shell logger?  commons logger?
     private static final String[] GROOVY_PACKAGES =
             System.getProperty("groovy.sanitized.stacktraces",
-                "org.codehaus.groovy.runtime.," +
-                "org.codehaus.groovy.reflection.," +
-                "groovy.lang.," +
-                "gjdk.groovy.lang.," +
-                "sun." +
-                "java.lang.reflect."
+                "groovy.," +
+                "org.codehaus.groovy.," +
+                "java.," +
+                "javax.," +
+                "sun.," +
+                "gjdk.groovy.,"
             ).split("(\\s|,)+");
 
-//    static {
-//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-//        String version = null;
-//        try {
-//            Resource[] manifests = resolver.getResources("classpath*:META-INF/GRAILS-MANIFEST.MF");
-//            Manifest grailsManifest = null;
-//            for (int i = 0; i < manifests.length; i++) {
-//                Resource r = manifests[i];
-//                Manifest mf = new Manifest(r.getInputStream());
-//                String implTitle = mf.getMainAttributes().getValue(Attributes.Name.IMPLEMENTATION_TITLE);
-//                if(!StringUtils.isBlank(implTitle) && implTitle.equals(GRAILS_IMPLEMENTATION_TITLE))   {
-//                    grailsManifest = mf;
-//                    break;
-//                }
-//            }
-//
-//            if(grailsManifest != null) {
-//                version = grailsManifest.getMainAttributes().getValue(Attributes.Name.IMPLEMENTATION_VERSION);
-//            }
-//
-//            if(StringUtils.isBlank(version)) {
-//                LOG.error("Unable to read Grails version from MANIFEST.MF. Are you sure it the grails-core jar is on the classpath? " );
-//                version = "Unknown";
-//            }
-//        } catch (IOException e) {
-//            version = "Unknown";
-//            LOG.error("Unable to read Grails version from MANIFEST.MF. Are you sure it the grails-core jar is on the classpath? " + e.getMessage(), e);
-//        }
-//
-//        GRAILS_VERSION = version;
-//    }
-
     /**
-     * <p>Remove all apparently Grails-internal trace entries from the exception instance<p>
+     * <p>Remove all apparently groovy-internal trace entries from the exception instance<p>
      * <p>This modifies the original instance and returns it, it does not clone</p>
      * @param t
      * @return The exception passed in, after cleaning the stack trace
@@ -123,8 +91,8 @@ public class StackTraceUtils {
 
     public static boolean isApplicationClass(String className) {
         for (int i = 0; i < GROOVY_PACKAGES.length; i++) {
-            String grailsPackage = GROOVY_PACKAGES[i];
-            if (className.startsWith(grailsPackage)) {
+            String groovyPackage = GROOVY_PACKAGES[i];
+            if (className.startsWith(groovyPackage)) {
                 return false;
             }
         }
@@ -148,7 +116,7 @@ public class StackTraceUtils {
      * <p>Get the root cause of an exception and sanitize it for display to the user</p>
      * <p>This will MODIFY the stacktrace of the root cause exception object and return it</p>
      * @param t
-     * @return The root cause exception instance, with its stace trace modified to filter out grails runtime classes
+     * @return The root cause exception instance, with its stace trace modified to filter out groovy runtime classes
      */
     public static Throwable sanitizeRootCause(Throwable t) {
         return StackTraceUtils.sanitize(StackTraceUtils.extractRootCause(t));
@@ -158,7 +126,7 @@ public class StackTraceUtils {
      * <p>Sanitize the exception and ALL nested causes</p>
      * <p>This will MODIFY the stacktrace of the exception instance and all its causes irreversibly</p>
      * @param t
-     * @return The root cause exception instances, with stack trace modified to filter out grails runtime classes
+     * @return The root cause exception instances, with stack trace modified to filter out groovy runtime classes
      */
     public static Throwable deepSanitize(Throwable t) {
         Throwable current = t;
