2cdb6de0d96f4b7ca0b905537c6a6f5580423163
Paul King <paulk@asert.com.au>
2007-10-26 07:45:11 +0000

    javadoc typo only
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@8818 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/metaclass/ThreadManagedMetaBeanProperty.java
+++ src/main/org/codehaus/groovy/runtime/metaclass/ThreadManagedMetaBeanProperty.java
@@ -29,39 +29,38 @@ import org.codehaus.groovy.reflection.ReflectionCache;
 import org.codehaus.groovy.reflection.ParameterTypes;
 
 /**
- * This MetaBeanProperty will create a psuedo property whoes value is bound to the current
+ * This MetaBeanProperty will create a pseudo property whose value is bound to the current
  * Thread using soft references. The values will go out of scope and be garabage collected when
  * the Thread dies or when memory is required by the JVM
- *
- * The property uses a InheritableThreadLocal instance internally so child threads will still be able
+ * <p/>
+ * The property uses an InheritableThreadLocal instance internally so child threads will still be able
  * to see the property
  *
  * @author Graeme Rocher
  * @since 1.1
- *
  */
 public class ThreadManagedMetaBeanProperty extends MetaBeanProperty {
-	private static final CachedClass[] ZERO_ARGUMENT_LIST = new CachedClass[0];
-	private static final ThreadLocal PROPERTY_INSTANCE_HOLDER = new InheritableThreadLocal();
+    private static final CachedClass[] ZERO_ARGUMENT_LIST = new CachedClass[0];
+    private static final ThreadLocal PROPERTY_INSTANCE_HOLDER = new InheritableThreadLocal();
 
-	private Class declaringClass;
-	private ThreadBoundGetter getter;
-	private ThreadBoundSetter setter;
-	private Object initialValue;
+    private Class declaringClass;
+    private ThreadBoundGetter getter;
+    private ThreadBoundSetter setter;
+    private Object initialValue;
     private static final String PROPERTY_SET_PREFIX = "set";
     private Closure initialValueCreator;
 
     /**
-	 * Retrieves the initial value of the ThreadBound property
-	 *
-	 * @return The initial value
-	 */
-	public synchronized Object getInitialValue() {
+     * Retrieves the initial value of the ThreadBound property
+     *
+     * @return The initial value
+     */
+    public synchronized Object getInitialValue() {
         return getInitialValue(null);
     }
 
     public synchronized Object getInitialValue(Object object) {
-        if(initialValueCreator != null) {
+        if (initialValueCreator != null) {
             return initialValueCreator.call(object);
         }
         return initialValue;
@@ -69,112 +68,109 @@ public class ThreadManagedMetaBeanProperty extends MetaBeanProperty {
     }
 
     /**
-     * Closure responsible for creating the initial value of thread managed bean properties
+     * Closure responsible for creating the initial value of thread-managed bean properties
      *
      * @param callable The closure responsible for creating the initial value
      */
     public void setInitialValueCreator(Closure callable) {
-       this.initialValueCreator = callable;
+        this.initialValueCreator = callable;
     }
 
     /**
-	 * Constructs a new ThreadManagedBeanProperty for the given arguments
-	 *
-	 * @param declaringClass The class that declares the property
-	 * @param name The name of the property
-	 * @param type The type of the property
-	 * @param iv The properties initial value
-	 */
-	public ThreadManagedMetaBeanProperty(Class declaringClass, String name, Class type, Object iv) {
-		super(name, type, null,null);
-		this.type = type;
-		this.declaringClass = declaringClass;
+     * Constructs a new ThreadManagedBeanProperty for the given arguments
+     *
+     * @param declaringClass The class that declares the property
+     * @param name           The name of the property
+     * @param type           The type of the property
+     * @param iv             The properties initial value
+     */
+    public ThreadManagedMetaBeanProperty(Class declaringClass, String name, Class type, Object iv) {
+        super(name, type, null, null);
+        this.type = type;
+        this.declaringClass = declaringClass;
 
-		this.getter = new ThreadBoundGetter(name);
-		this.setter = new ThreadBoundSetter(name);
-		initialValue = iv;
+        this.getter = new ThreadBoundGetter(name);
+        this.setter = new ThreadBoundSetter(name);
+        initialValue = iv;
 
-	}
+    }
 
     /**
-	 * Constructs a new ThreadManagedBeanProperty for the given arguments
-	 *
-	 * @param declaringClass The class that declares the property
-	 * @param name The name of the property
-	 * @param type The type of the property
-	 * @param initialValueCreator The closure responsible for creating the initial value
-	 */
-	public ThreadManagedMetaBeanProperty(Class declaringClass, String name, Class type, Closure initialValueCreator) {
-		super(name, type, null,null);
-		this.type = type;
-		this.declaringClass = declaringClass;
+     * Constructs a new ThreadManagedBeanProperty for the given arguments
+     *
+     * @param declaringClass      The class that declares the property
+     * @param name                The name of the property
+     * @param type                The type of the property
+     * @param initialValueCreator The closure responsible for creating the initial value
+     */
+    public ThreadManagedMetaBeanProperty(Class declaringClass, String name, Class type, Closure initialValueCreator) {
+        super(name, type, null, null);
+        this.type = type;
+        this.declaringClass = declaringClass;
 
-		this.getter = new ThreadBoundGetter(name);
-		this.setter = new ThreadBoundSetter(name);
-		this.initialValueCreator = initialValueCreator;
+        this.getter = new ThreadBoundGetter(name);
+        this.setter = new ThreadBoundSetter(name);
+        this.initialValueCreator = initialValueCreator;
 
-	}
+    }
 
     private static Object getThreadBoundPropertyValue(Object obj, String name, Object initialValue) {
-		Map propertyMap = getThreadBoundPropertMap();
-		String key = System.identityHashCode(obj) + name;
-		if(propertyMap.containsKey(key)) {
-			return propertyMap.get(key);
-		}
-		else {
-			propertyMap.put(key, initialValue);
-			return initialValue;
-		}
-	}
+        Map propertyMap = getThreadBoundPropertMap();
+        String key = System.identityHashCode(obj) + name;
+        if (propertyMap.containsKey(key)) {
+            return propertyMap.get(key);
+        } else {
+            propertyMap.put(key, initialValue);
+            return initialValue;
+        }
+    }
 
-	private static Map getThreadBoundPropertMap() {
-		Map propertyMap = (Map) PROPERTY_INSTANCE_HOLDER.get();
-		if(propertyMap == null) {
-			propertyMap = new WeakHashMap();
-			PROPERTY_INSTANCE_HOLDER.set(propertyMap);
-		}
-		return propertyMap;
-	}
+    private static Map getThreadBoundPropertMap() {
+        Map propertyMap = (Map) PROPERTY_INSTANCE_HOLDER.get();
+        if (propertyMap == null) {
+            propertyMap = new WeakHashMap();
+            PROPERTY_INSTANCE_HOLDER.set(propertyMap);
+        }
+        return propertyMap;
+    }
 
-	private static Object setThreadBoundPropertyValue(Object obj, String name, Object value) {
-		Map propertyMap = getThreadBoundPropertMap();
-		String key = System.identityHashCode(obj) + name;
-		return propertyMap.put(key,value);
-	}
+    private static Object setThreadBoundPropertyValue(Object obj, String name, Object value) {
+        Map propertyMap = getThreadBoundPropertMap();
+        String key = System.identityHashCode(obj) + name;
+        return propertyMap.put(key, value);
+    }
 
-	/* (non-Javadoc)
-	 * @see groovy.lang.MetaBeanProperty#getGetter()
-	 */
-	public MetaMethod getGetter() {
-		return this.getter;
-	}
+    /* (non-Javadoc)
+      * @see groovy.lang.MetaBeanProperty#getGetter()
+      */
+    public MetaMethod getGetter() {
+        return this.getter;
+    }
 
-	/* (non-Javadoc)
-	 * @see groovy.lang.MetaBeanProperty#getSetter()
-	 */
-	public MetaMethod getSetter() {
-		return this.setter;
-	}
-
+    /* (non-Javadoc)
+      * @see groovy.lang.MetaBeanProperty#getSetter()
+      */
+    public MetaMethod getSetter() {
+        return this.setter;
+    }
 
 
-	/**
-	 * Accesses the ThreadBound state of the property as a getter
-	 *
-	 * @author Graeme Rocher
-	 *
-	 */
-	class ThreadBoundGetter extends MetaMethod {
+    /**
+     * Accesses the ThreadBound state of the property as a getter
+     *
+     * @author Graeme Rocher
+     */
+    class ThreadBoundGetter extends MetaMethod {
 
 
-		private final String name, name0;
+        private final String name, name0;
         private final ParameterTypes pt = new ParameterTypes(new CachedClass[0]);
 
 
         public ThreadBoundGetter(String name) {
-			this.name = getGetterName(name, type);
+            this.name = getGetterName(name, type);
             this.name0 = name;
-		}
+        }
 
 
         public int getModifiers() {
@@ -182,8 +178,8 @@ public class ThreadManagedMetaBeanProperty extends MetaBeanProperty {
         }
 
         public String getName() {
-			return name;
-		}
+            return name;
+        }
 
         public Class getReturnType() {
             return type;
@@ -201,25 +197,24 @@ public class ThreadManagedMetaBeanProperty extends MetaBeanProperty {
         /* (non-Javadoc)
            * @see groovy.lang.MetaMethod#invoke(java.lang.Object, java.lang.Object[])
            */
-		public Object invoke(Object object, Object[] arguments) {
-			return getThreadBoundPropertyValue(object, name0, getInitialValue());
-		}
-	}
+        public Object invoke(Object object, Object[] arguments) {
+            return getThreadBoundPropertyValue(object, name0, getInitialValue());
+        }
+    }
 
-	/**
-	 * Sets the ThreadBound state of the property like a setter
-	 *
-	 * @author Graeme Rocher
-	 *
-	 */
-	private class ThreadBoundSetter extends MetaMethod {
+    /**
+     * Sets the ThreadBound state of the property like a setter
+     *
+     * @author Graeme Rocher
+     */
+    private class ThreadBoundSetter extends MetaMethod {
 
 
-		private final String name, name0;
+        private final String name, name0;
         private ParameterTypes pt = new ParameterTypes(new CachedClass[]{ReflectionCache.getCachedClass(type)});
 
         public ThreadBoundSetter(String name) {
-			this.name = getSetterName(name);
+            this.name = getSetterName(name);
             this.name0 = name;
         }
 
@@ -229,9 +224,10 @@ public class ThreadManagedMetaBeanProperty extends MetaBeanProperty {
         }/* (non-Javadoc)
 		 * @see groovy.lang.MetaMethod#getName()
 		 */
-		public String getName() {
-			return name;
-		}
+
+        public String getName() {
+            return name;
+        }
 
         public Class getReturnType() {
             return type;
@@ -248,20 +244,19 @@ public class ThreadManagedMetaBeanProperty extends MetaBeanProperty {
         /* (non-Javadoc)
            * @see groovy.lang.MetaMethod#invoke(java.lang.Object, java.lang.Object[])
            */
-		public Object invoke(Object object, Object[] arguments) {
-			return setThreadBoundPropertyValue(object, name0, arguments[0]);
-		}
-	}
+        public Object invoke(Object object, Object[] arguments) {
+            return setThreadBoundPropertyValue(object, name0, arguments[0]);
+        }
+    }
 
-    private String getGetterName(String propertyName, Class type)
-    {
+    private String getGetterName(String propertyName, Class type) {
         String prefix = type == boolean.class || type == Boolean.class ? "is" : "get";
         return prefix + Character.toUpperCase(propertyName.charAt(0))
-            + propertyName.substring(1);
+                + propertyName.substring(1);
     }
 
     private String getSetterName(String propertyName) {
-        return PROPERTY_SET_PREFIX+propertyName.substring(0,1).toUpperCase()+ propertyName.substring(1);
+        return PROPERTY_SET_PREFIX + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
     }
 
 }
