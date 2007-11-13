e8a9d851e32ab7452b83fc0af7476f89e42477e7
Alex Tkachman <alex.tkachman@gmail.com>
2007-11-13 06:06:28 +0000

    Test for GROOVY-2263
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@9149 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/CustomMetaClassTest.groovy
+++ src/test/groovy/bugs/CustomMetaClassTest.groovy
@@ -1,6 +1,49 @@
 package groovy.bugs
 
+import org.codehaus.groovy.runtime.InvokerHelper
+
+class MyDelegatingMetaClass extends groovy.lang.DelegatingMetaClass
+{
+    MyDelegatingMetaClass(final Class a_class)
+    {
+        super(a_class);
+        initialize()
+    }
+
+    public Object invokeMethod(Object a_object, String a_methodName, Object[] a_arguments)
+    {
+        return "changed ${super.invokeMethod(a_object, a_methodName, a_arguments)}"
+    }
+}
+
 class CustomMetaClassTest extends GroovyTestCase{
+    void testReplaceMetaClass() {
+        /*
+         * Constructing first instance before meta class replacment
+         * is made.
+         */
+        def firstInstance = "first"
+        assertEquals "first", firstInstance.toString()
+
+        def myMetaClass = new MyDelegatingMetaClass(String.class)
+        def invoker = InvokerHelper.instance
+        invoker.metaRegistry.setMetaClass(String.class, myMetaClass)
+
+        /*
+         * Constructing second instance after meta class replacment
+         * is made.
+         */
+        def secondInstance = "second"
+
+        /*
+         * Since we are replacing a meta class at the class level
+         * we are changing the behavior of the first and second
+         * instance of the string.
+         */
+        assertEquals "changed first", firstInstance.toString()
+        assertEquals "changed second", secondInstance.toString()
+    }
+
   void testNormalCreated () {
       assertTrue metaClass instanceof groovy.runtime.metaclass.groovy.bugs.CustomMetaClassTestMetaClass
       assertEquals  MetaClassImpl, metaClass.delegate.class
