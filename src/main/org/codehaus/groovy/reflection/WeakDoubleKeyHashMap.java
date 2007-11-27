5f7ad0b1bc8b0cc5057c7b85de0ba96bb5ede12e
Alex Tkachman <alex.tkachman@gmail.com>
2007-11-27 09:18:59 +0000

    Expunge stole entries from WeakDoubleKeyhashMap
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@9468 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/reflection/WeakDoubleKeyHashMap.java
+++ src/main/org/codehaus/groovy/reflection/WeakDoubleKeyHashMap.java
@@ -15,15 +15,61 @@
  */
 package org.codehaus.groovy.reflection;
 
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 
 public class WeakDoubleKeyHashMap extends ComplexKeyHashMap
 {
-  public static class Entry extends ComplexKeyHashMap.Entry{
-    public WeakReference key1, key2;
+  private static class Ref extends WeakReference {
+      public Ref(Object referent, ReferenceQueue q, Entry entry) {
+          super(referent, q);
+          this.entry = entry;
+      }
+
+      Entry entry;
   }
 
+  public static class Entry extends ComplexKeyHashMap.Entry{
+    public Ref key1, key2;
+  }
+
+  private final ReferenceQueue queue = new ReferenceQueue();
+
+    private void expungeStaleEntries() {
+	    Ref r;
+        while ( (r = (Ref) queue.poll()) != null) {
+            Entry e = r.entry;
+            if (e == null)
+              continue;
+
+            r.entry = null;
+            e.key1 = e.key2 = null;
+
+            int h = e.hash;
+            int i = h & (table.length-1);
+
+            Entry prev = (Entry) table[i];
+            Entry p = prev;
+            while (p != null) {
+                Entry next = (Entry) p.next;
+                if (p == e) {
+                    if (prev == e)
+                        table[i] = next;
+                    else
+                        prev.next = next;
+                    e.next = null;  // Help GC
+                    e.value = null; //  "   "
+                    size--;
+                    break;
+                }
+                prev = p;
+                p = next;
+            }
+        }
+    }
+
   public final Object get(Object key1, Object key2) {
+    expungeStaleEntries();
     int h = hash (31*key1.hashCode()+key2.hashCode());
     ComplexKeyHashMap.Entry e = table [h & (table.length-1)];
     for (; e != null; e = e.next)
@@ -40,6 +86,7 @@ public class WeakDoubleKeyHashMap extends ComplexKeyHashMap
 
   public Entry getOrPut(Object key1, Object key2)
   {
+    expungeStaleEntries();
     int h = hash (31*key1.hashCode()+key2.hashCode());
     final int index = h & (table.length - 1);
     ComplexKeyHashMap.Entry e = table [index];
@@ -61,8 +108,8 @@ public class WeakDoubleKeyHashMap extends ComplexKeyHashMap
     Entry entry = createEntry ();
     entry.next = table [index];
     entry.hash = h;
-    entry.key1 = new WeakReference(key1);
-    entry.key2 = new WeakReference(key2);
+    entry.key1 = new Ref(key1, queue, entry);
+    entry.key2 = new Ref(key2, queue, entry);
     return entry;
   }
 
@@ -70,7 +117,13 @@ public class WeakDoubleKeyHashMap extends ComplexKeyHashMap
       return new Entry ();
   }
 
-  public final ComplexKeyHashMap.Entry remove(Object key1, Object key2) {
+    public int size() {
+        expungeStaleEntries();
+        return super.size();
+    }
+
+    public final ComplexKeyHashMap.Entry remove(Object key1, Object key2) {
+    expungeStaleEntries();
     int h = hash (31*key1.hashCode()+key2.hashCode());
     int index = h & (table.length -1);
     for (ComplexKeyHashMap.Entry e = table [index], prev = null; e != null; prev = e, e = e.next ) {
