4539f5e22dbb6e2aed87bd324db4650d47fd9d68
Danno Ferrin <danno.ferrin@shemnon.com>
2007-11-30 20:05:41 +0000

    GROOVY-2259 - turn off print on some linux distros
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@9572 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/ui/view/GTKDefaults.groovy
+++ src/main/groovy/ui/view/GTKDefaults.groovy
@@ -27,4 +27,28 @@ if (System.properties['java.version'] =~ /^1\.5/) {
     }
 }
 
+// some current distros (Ubuntu 7.10) have broken printing support :(
+// detect it and disable it
+try {
+    pj = java.awt.print.PrinterJob.getPrinterJob()
+    ps = pj.getPrintService()
+    ps.getAttributes()
+    docFlav  = (ps.getSupportedDocFlavors() as List).find {it.mimeType == 'application/vnd.cups-postscript' }
+    attrset = ps.getAttributes()
+    orient = attrset.get(javax.print.attribute.standard.OrientationRequested) ?: 
+             ps.getDefaultAttributeValue(javax.print.attribute.standard.OrientationRequested)
+    ps.isAttributeValueSupported(orient, docFlav, attrset)
+} catch (NullPointerException npe) {
+    //print will bomb out... replace with disabled print action
+    action(id: 'printAction',
+        name: 'Print...',
+        closure: controller.&print,
+        mnemonic: 'P',
+        accelerator: shortcut('P'),
+        shortDescription: 'Printing does not work in Java with this version of CUPS',
+        enabled: false
+    )
+}
+
+
 
