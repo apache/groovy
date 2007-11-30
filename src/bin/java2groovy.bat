a489937e150daa6d54f0eb3b91a5bbcd227e2a13
Andres Almiray <aalmiray@gmail.com>
2007-11-30 06:30:28 +0000

    GROOVY-1656
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@9551 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/bin/java2groovy.bat
+++ src/bin/java2groovy.bat
@@ -12,7 +12,7 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.\
 
-"%DIRNAME%\startGroovy.bat" "%DIRNAME%" org.codehaus.groovy.antlr.java.Java2Groovy %*
+"%DIRNAME%\startGroovy.bat" "%DIRNAME%" org.codehaus.groovy.antlr.java.Java2GroovyMain %*
 
 @rem End local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" endlocal
