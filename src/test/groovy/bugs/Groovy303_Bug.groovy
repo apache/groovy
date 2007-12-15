e98bd0e95153328084fab88cceadfaefac9be034
Paul King <paulk@asert.com.au>
2007-12-15 01:11:17 +0000

    try to make teamcity Agent 007 happier wrt headless security test
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@9812 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/Groovy303_Bug.groovy
+++ src/test/groovy/bugs/Groovy303_Bug.groovy
@@ -4,44 +4,49 @@ import java.awt.*
 import java.awt.event.*
 import javax.swing.*
 
-
 /**
  * @author Bing Ran
  * @author Andy Dwelly
  * @version $Revision$
  */
 class Groovy303_Bug extends GroovyTestCase {
-    
-    void testBug() {
+
+    private static boolean headless
+
+    static {
         try {
-            def scholastic = new Scholastic()
-               scholastic.createUI()
-           }
-           catch (HeadlessException e) {
-               // called from a non-UI environment
-           }
+            new JFrame("testing")
+            headless = false
+        } catch (HeadlessException he) {
+            headless = true
+        }
+    }
+
+    void testBug() {
+        if (headless) return
+        def scholastic = new Scholastic()
+        scholastic.createUI()
     }
 }
-
 
 class Scholastic implements ActionListener {
 
     void createUI() {
-       println('createUI called')
-       def frame = new JFrame("Hello World")
-       def contents = frame.getContentPane()
-       def pane = new JPanel()
-       pane.setLayout(new BorderLayout())
-       def button = new JButton("A button")
-       button.addActionListener(this)
-       pane.add(button, BorderLayout.CENTER)
-       contents.add(pane)
-       frame.setSize(100, 100)
-       //frame.setVisible(true)
-       button.doClick()
+        println('createUI called')
+        def frame = new JFrame("Hello World")
+        def contents = frame.getContentPane()
+        def pane = new JPanel()
+        pane.setLayout(new BorderLayout())
+        def button = new JButton("A button")
+        button.addActionListener(this)
+        pane.add(button, BorderLayout.CENTER)
+        contents.add(pane)
+        frame.setSize(100, 100)
+        //frame.setVisible(true)
+        button.doClick()
     }
 
     public void actionPerformed(ActionEvent event) {
-       println "hello"
+        println "hello"
     }
 }
