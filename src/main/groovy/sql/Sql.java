4ca00813123f7749513f88bb00470780890345e5
Paul King <paulk@asert.com.au>
2008-01-07 11:38:00 +0000

    GROOVY-2280: ConcurrentModificationException when accessing iterator variable in nested iterator (forgot this last time)
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@10232 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/sql/Sql.java
+++ src/main/groovy/sql/Sql.java
@@ -725,7 +725,7 @@ public class Sql {
             rs = statement.executeQuery();
             while (rs.next()) {
                 ResultSetMetaData metadata = rs.getMetaData();
-                LinkedHashMap lhm = new LinkedHashMap(metadata.getColumnCount(), 1, true);
+                LinkedHashMap lhm = new LinkedHashMap(metadata.getColumnCount(), 1);
                 for (int i = 1; i <= metadata.getColumnCount(); i++) {
                     lhm.put(metadata.getColumnName(i), rs.getObject(i));
                 }
