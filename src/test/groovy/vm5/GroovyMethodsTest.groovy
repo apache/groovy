f856ef138c9e9f2db42e97c5c61c9b79ceb98d72
Paul King <paulk@asert.com.au>
2008-01-09 12:18:18 +0000

    GROOVY-2449: Enum range works only for Groovy enums and not for Java enums (test added)
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@10282 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/vm5/GroovyMethodsTest.groovy
+++ src/test/groovy/vm5/GroovyMethodsTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 the original author or authors.
+ * Copyright 2007-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,16 @@ class GroovyMethodsTest extends GroovyTestCase {
         shouldFail(IllegalArgumentException) {
             "FOO" as Suit
         } 
+    }
+
+    void testJavaEnumType() {
+        def x = Language.English
+        x++
+        assert x == Language.French
+        x = Language.English
+        x--
+        assert x == Language.Spanish
+        assert Language.French in Language.English..Language.Spanish
     }
 
     void testStringBuilderPlusPutAtSizeLeftShift() {
