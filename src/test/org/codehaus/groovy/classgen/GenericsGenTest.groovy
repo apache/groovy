126fa45e18ef66474106fd2ac0fac2a72eb8d3f8
Russel Winder <russel@russel.org.uk>
2008-01-19 13:08:51 +0000

    Prepend groovy- to the temporary directory name.  Removed some more ^M.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@10502 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/classgen/GenericsGenTest.groovy
+++ src/test/org/codehaus/groovy/classgen/GenericsGenTest.groovy
@@ -14,7 +14,7 @@ class GenericsGenTest extends GroovyTestCase{
         }
 
 
-        File dir = createTempDir("src-", "-src")
+        File dir = createTempDir("groovy-src-", "-src")
         assertNotNull dir
 
         def fileList =  [
@@ -53,9 +53,9 @@ class GenericsGenTest extends GroovyTestCase{
         }
 
         CompilerConfiguration config = new CompilerConfiguration()
-         config.targetDirectory = createTempDir("target-", "-target")
+         config.targetDirectory = createTempDir("groovy-target-", "-target")
          config.jointCompilationOptions = [
-			"stubDir" : createTempDir("stub-", "-stub"),
+			"stubDir" : createTempDir("groovy-stub-", "-stub"),
 			"namedValues" : ["target","1.5","source","1.5"] as String[]
 		 ]
          config.classpath = "target/classes"
