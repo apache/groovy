77468ae7a2a2eda787cff316afceb1765c39e996
Jim White <jim@pagesmiths.com>
2008-02-21 18:34:34 +0000

    Fix and a test case for GROOVY-2568
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_5_X/groovy@10889 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy2568Bug.groovy
@@ -0,0 +1,20 @@
+package groovy.bugs
+
+public class Groovy2568Bug extends GroovyTestCase {
+
+    public static void print_byte(String str, byte defaultValue) {
+        System.out.println(str + ", " + defaultValue);
+    }
+
+    public static void printByte(String str, Byte defaultValue) {
+        System.out.println(str + ", " + defaultValue);
+    }
+
+    void testAutoboxBytes() {
+        printByte("1", Byte.valueOf((byte) 1));
+        printByte("1", (byte) 1);
+        print_byte("1", Byte.valueOf((byte) 1));
+        print_byte("1", (byte) 1);
+    }
+
+}
