4d72abd20072db007e5e73db1cdfff1488350c88
Paul King <paulk@asert.com.au>
2008-03-21 22:58:09 +0000

    remove some dup only
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@11182 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/ant/GroovycTest.java
+++ src/test/org/codehaus/groovy/ant/GroovycTest.java
@@ -12,192 +12,180 @@
  *  License.
  */
 
-package org.codehaus.groovy.ant ;
+package org.codehaus.groovy.ant;
 
-import java.io.File ;
-import java.io.FileNotFoundException ;
-import java.io.FileReader ;
-import java.io.IOException ;
-import org.apache.tools.ant.BuildException ;
-import org.apache.tools.ant.Project ;
-import org.apache.tools.ant.ProjectHelper ;
-import groovy.util.GroovyTestCase ;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import groovy.util.GroovyTestCase;
 
 /**
- *  Unit tests for the {@link Groovyc} ant task.
- *
- *  <p>NB the *.groovy files in this directory should not get compiled with the rest of the test classes
- *  since that would ruin the whole point of testing compilation by the Ant tasks.  In fact it doesn't
- *  matter as the tests remove all class files that should not pre-exist from this directory at each
- *  step</p>
+ * Unit tests for the {@link Groovyc} ant task.
+ * <p/>
+ * <p>NB the *.groovy files in this directory should not get compiled with the rest of the test classes
+ * since that would ruin the whole point of testing compilation by the Ant tasks.  In fact it doesn't
+ * matter as the tests remove all class files that should not pre-exist from this directory at each
+ * step</p>
  *
  * @author Russel Winder
  */
 public class GroovycTest extends GroovyTestCase {
-  private final String classDirectory = "target/test-classes/org/codehaus/groovy/ant/" ;
-  private final File antFile = new File ( "src/test/org/codehaus/groovy/ant/GroovycTest.xml" ) ;
-  private Project project ;
-  protected void setUp ( ) throws Exception {
-    super.setUp ( ) ; //  Potentially throws Exception.
-    project = new Project ( ) ;
-    project.init ( ) ;
-    ProjectHelper.getProjectHelper ( ).parse ( project , antFile ) ;
-    project.executeTarget ( "clean" ) ;
-    String altJavaHome = System.getProperty("java.home");
-    if (altJavaHome.lastIndexOf("jre") >= 0) {
-      altJavaHome = altJavaHome.substring(0, altJavaHome.lastIndexOf("jre"));
-    } else {
-      altJavaHome = altJavaHome + File.separator + "jre";
-    }
-    try {
-      File altFile = new File(altJavaHome);
-      if (altFile.exists()) {
-        project.setProperty("alt.java.home", altJavaHome);
-      }
-    } catch (Exception e) {
-      // could be security, io, etc.  Ignore it.  
-      // End result is as if .exists() returned null
-    }
-  }
-  private void ensureNotPresent ( final String classname ) {
-    if ( ! ( new File ( classDirectory + "GroovycTest.class" ) ).exists ( ) ) {
-      fail ( "Class file for GroovycTest does not exist and should." ) ;
-    }
-    if ( ( new File ( classDirectory + classname + ".class" ) ).exists ( ) ) {
-      fail ( "Class file for " + classname + " already exists and shouldn't." ) ;
-    }
-  }
-  private void ensureResultOK ( final String classname ) {
-    if ( ! ( new File ( classDirectory + classname + ".class" ) ).exists ( ) ) {
-      fail ( "Class file for " + classname + " does not exist and should." ) ;
-    }
-    final File result = new File ( classDirectory + classname + "_Result.txt" ) ;
-    final char[] buffer = new char [ 10 ] ;
-    try {
-      ( new FileReader ( result ) ).read ( buffer ) ;
-      assertEquals ( "OK." , new String ( buffer ).trim ( ) ) ;
-    }
-    catch ( final FileNotFoundException fnfe ) { fail ( "File " + result.getName ( ) + " should have been created but wasn't." ) ; }
-    catch ( final IOException ioe ) { fail ( "Error reading file " + result.getName ( ) + "." ) ; }
-  }
+    private final String classDirectory = "target/test-classes/org/codehaus/groovy/ant/";
+    private final File antFile = new File("src/test/org/codehaus/groovy/ant/GroovycTest.xml");
+    private Project project;
+    private static boolean warned = false;
 
-  public void testGroovycTest1_NoFork_NoClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_NoFork_NoClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_NoFork_WithGroovyClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_NoFork_WithGroovyClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_NoFork_WithJavaClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_NoFork_WithJavaClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_NoFork_WithBothClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_NoFork_WithBothClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-
-  public void testGroovycTest1_ForkGroovy_NoClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_ForkGroovy_NoClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_ForkGroovy_WithGroovyClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_ForkGroovy_WithGroovyClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-
-  }
-  public void testGroovycTest1_ForkGroovy_WithJavaClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_ForkGroovy_WithJavaClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_ForkGroovy_WithBothClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_ForkGroovy_WithBothClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-
-  public void testGroovycTest1_Joint_NoFork_NoClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_NoFork_NoClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_Joint_NoFork_WithGroovyClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_NoFork_WithGroovyClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_Joint_NoFork_WithJavaClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_NoFork_WithJavaClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_Joint_NoFork_WithBothClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_NoFork_WithBothClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-
-  public void testGroovycTest1_Joint_ForkGroovy_NoClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_ForkGroovy_NoClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_Joint_ForkGroovy_WithGroovyClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_ForkGroovy_WithGroovyClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-
-  }
-  public void testGroovycTest1_Joint_ForkGroovy_WithJavaClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_ForkGroovy_WithJavaClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-  public void testGroovycTest1_Joint_ForkGroovy_WithBothClasspath ( ) {
-    ensureNotPresent ( "GroovycTest1" ) ;
-    project.executeTarget ( "GroovycTest1_Joint_ForkGroovy_WithBothClasspath" ) ;
-    ensureResultOK ( "GroovycTest1" ) ;
-  }
-
-
-    public void testGroovycTest1_ForkGroovy_NoClasspath_WithJavaHome ( ) {
-      if (project.getProperty("alt.java.home") != null) {
-        ensureNotPresent ( "GroovycTest1" ) ;
-        project.executeTarget ( "GroovycTest1_ForkGroovy_NoClasspath_WithJavaHome" ) ;
-        ensureResultOK ( "GroovycTest1" ) ;
-      } else {
-        System.out.println("Forked Java tests skiped, not a sun JDK layout");
-      }
-    }
-    public void testGroovycTest1_ForkGroovy_WithGroovyClasspath_WithJavaHome ( ) {
-      if (project.getProperty("alt.java.home") != null) {
-        ensureNotPresent ( "GroovycTest1" ) ;
-        project.executeTarget ( "GroovycTest1_ForkGroovy_WithGroovyClasspath_WithJavaHome" ) ;
-        ensureResultOK ( "GroovycTest1" ) ;
-      }
-    }
-    public void testGroovycTest1_ForkGroovy_WithJavaClasspath_WithJavaHome ( ) {
-      if (project.getProperty("alt.java.home") != null) {
-        ensureNotPresent ( "GroovycTest1" ) ;
-        project.executeTarget ( "GroovycTest1_ForkGroovy_WithJavaClasspath_WithJavaHome" ) ;
-        ensureResultOK ( "GroovycTest1" ) ;
-      }
-    }
-    public void testGroovycTest1_ForkGroovy_WithBothClasspath_WithJavaHome ( ) {
-      if (project.getProperty("alt.java.home") != null) {
-        ensureNotPresent ( "GroovycTest1" ) ;
-        project.executeTarget ( "GroovycTest1_ForkGroovy_WithBothClasspath_WithJavaHome" ) ;
-        ensureResultOK ( "GroovycTest1" ) ;
-      }
+    protected void setUp() throws Exception {
+        super.setUp(); //  Potentially throws Exception.
+        project = new Project();
+        project.init();
+        ProjectHelper.getProjectHelper().parse(project, antFile);
+        project.executeTarget("clean");
+        String altJavaHome = System.getProperty("java.home");
+        if (altJavaHome.lastIndexOf("jre") >= 0) {
+            altJavaHome = altJavaHome.substring(0, altJavaHome.lastIndexOf("jre"));
+        } else {
+            altJavaHome = altJavaHome + File.separator + "jre";
+        }
+        try {
+            File altFile = new File(altJavaHome);
+            if (altFile.exists()) {
+                project.setProperty("alt.java.home", altJavaHome);
+            }
+        } catch (Exception e) {
+            // could be security, io, etc.  Ignore it.
+            // End result is as if .exists() returned null
+        }
     }
 
+    private void ensureNotPresent(final String classname) {
+        if (!(new File(classDirectory + "GroovycTest.class")).exists()) {
+            fail("Class file for GroovycTest does not exist and should.");
+        }
+        if ((new File(classDirectory + classname + ".class")).exists()) {
+            fail("Class file for " + classname + " already exists and shouldn't.");
+        }
+    }
+
+    private void ensureResultOK(final String classname) {
+        if (!(new File(classDirectory + classname + ".class")).exists()) {
+            fail("Class file for " + classname + " does not exist and should.");
+        }
+        final File result = new File(classDirectory + classname + "_Result.txt");
+        final char[] buffer = new char[10];
+        try {
+            (new FileReader(result)).read(buffer);
+            assertEquals("OK.", new String(buffer).trim());
+        }
+        catch (final FileNotFoundException fnfe) {
+            fail("File " + result.getName() + " should have been created but wasn't.");
+        }
+        catch (final IOException ioe) {
+            fail("Error reading file " + result.getName() + ".");
+        }
+    }
+
+    public void testGroovycTest1_NoFork_NoClasspath() {
+        ensureExecutes("GroovycTest1_NoFork_NoClasspath");
+    }
+
+    public void testGroovycTest1_NoFork_WithGroovyClasspath() {
+        ensureExecutes("GroovycTest1_NoFork_WithGroovyClasspath");
+    }
+
+    public void testGroovycTest1_NoFork_WithJavaClasspath() {
+        ensureExecutes("GroovycTest1_NoFork_WithJavaClasspath");
+    }
+
+    public void testGroovycTest1_NoFork_WithBothClasspath() {
+        ensureExecutes("GroovycTest1_NoFork_WithBothClasspath");
+    }
+
+    public void testGroovycTest1_ForkGroovy_NoClasspath() {
+        ensureExecutes("GroovycTest1_ForkGroovy_NoClasspath");
+    }
+
+    public void testGroovycTest1_ForkGroovy_WithGroovyClasspath() {
+        ensureExecutes("GroovycTest1_ForkGroovy_WithGroovyClasspath");
+
+    }
+
+    public void testGroovycTest1_ForkGroovy_WithJavaClasspath() {
+        ensureExecutes("GroovycTest1_ForkGroovy_WithJavaClasspath");
+    }
+
+    public void testGroovycTest1_ForkGroovy_WithBothClasspath() {
+        ensureExecutes("GroovycTest1_ForkGroovy_WithBothClasspath");
+    }
+
+    public void testGroovycTest1_Joint_NoFork_NoClasspath() {
+        ensureExecutes("GroovycTest1_Joint_NoFork_NoClasspath");
+    }
+
+    public void testGroovycTest1_Joint_NoFork_WithGroovyClasspath() {
+        ensureExecutes("GroovycTest1_Joint_NoFork_WithGroovyClasspath");
+    }
+
+    public void testGroovycTest1_Joint_NoFork_WithJavaClasspath() {
+        ensureExecutes("GroovycTest1_Joint_NoFork_WithJavaClasspath");
+    }
+
+    public void testGroovycTest1_Joint_NoFork_WithBothClasspath() {
+        ensureExecutes("GroovycTest1_Joint_NoFork_WithBothClasspath");
+    }
+
+    public void testGroovycTest1_Joint_ForkGroovy_NoClasspath() {
+        ensureExecutes("GroovycTest1_Joint_ForkGroovy_NoClasspath");
+    }
+
+    public void testGroovycTest1_Joint_ForkGroovy_WithGroovyClasspath() {
+        ensureExecutes("GroovycTest1_Joint_ForkGroovy_WithGroovyClasspath");
+    }
+
+    public void testGroovycTest1_Joint_ForkGroovy_WithJavaClasspath() {
+        ensureExecutes("GroovycTest1_Joint_ForkGroovy_WithJavaClasspath");
+    }
+
+    public void testGroovycTest1_Joint_ForkGroovy_WithBothClasspath() {
+        ensureExecutes("GroovycTest1_Joint_ForkGroovy_WithBothClasspath");
+    }
+
+    public void testGroovycTest1_ForkGroovy_NoClasspath_WithJavaHome() {
+        ensureExecutesWithJavaHome("GroovycTest1_ForkGroovy_NoClasspath_WithJavaHome");
+    }
+
+    public void testGroovycTest1_ForkGroovy_WithGroovyClasspath_WithJavaHome() {
+        ensureExecutesWithJavaHome("GroovycTest1_ForkGroovy_WithGroovyClasspath_WithJavaHome");
+    }
+
+    public void testGroovycTest1_ForkGroovy_WithJavaClasspath_WithJavaHome() {
+        ensureExecutesWithJavaHome("GroovycTest1_ForkGroovy_WithJavaClasspath_WithJavaHome");
+    }
+
+    public void testGroovycTest1_ForkGroovy_WithBothClasspath_WithJavaHome() {
+        ensureExecutesWithJavaHome("GroovycTest1_ForkGroovy_WithBothClasspath_WithJavaHome");
+    }
+
+    private void ensureExecutes(String target) {
+        ensureNotPresent("GroovycTest1");
+        project.executeTarget(target);
+        ensureResultOK("GroovycTest1");
+    }
+
+    private void ensureExecutesWithJavaHome(String target) {
+        if (project.getProperty("alt.java.home") != null) {
+            ensureExecutes(target);
+        } else {
+            if (!warned) {
+                System.err.println("Forked Java tests skipped, not a sun JDK layout");
+                warned = true;
+            }
+        }
+    }
 
 }
