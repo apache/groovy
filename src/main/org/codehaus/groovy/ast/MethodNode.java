2561fdafbec7c73705386b2c55d677864c83aea2
Martin Kempf <mkempf@hsr.ch>
2008-04-18 07:59:14 +0000

    GROOVY-2615 calling setReturnType(returnType) from c'tor in "MethodNode.java", extended this method
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@11729 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/ast/MethodNode.java
+++ src/main/org/codehaus/groovy/ast/MethodNode.java
@@ -47,8 +47,7 @@ public class MethodNode extends AnnotatedNode implements Opcodes {
         this.name = name;
         this.modifiers = modifiers;
         this.code = code;
-        this.returnType = returnType;
-        if (returnType==null) this.returnType = ClassHelper.OBJECT_TYPE; 
+        setReturnType(returnType); 
         VariableScope scope = new VariableScope();
         setVariableScope(scope);
         setParameters(parameters);
@@ -168,7 +167,9 @@ public class MethodNode extends AnnotatedNode implements Opcodes {
     }
 
     public void setReturnType(ClassNode returnType) {
+    	dynamicReturnType |= ClassHelper.DYNAMIC_TYPE==returnType;
         this.returnType = returnType;
+        if (returnType==null) this.returnType = ClassHelper.OBJECT_TYPE;
     }
 
     public ClassNode[] getExceptions() {
