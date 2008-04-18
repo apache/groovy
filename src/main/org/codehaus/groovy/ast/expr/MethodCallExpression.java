9d86275813aa5f4ed862262225666c3160aee9b4
Martin Kempf <mkempf@hsr.ch>
2008-04-18 09:42:34 +0000

    GROOVY-2646 changed return values of isImplicitThis() to: (was OK after phase CONVERSION, but not after SEMANTIC_ANALYSIS)
        * anObj.methodCall() isImplicitThis() returns false
        * this.println("anything") isImplicitThis() returns false
        * println("anything") isImplicitThis() returns true
    
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@11741 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/ast/expr/MethodCallExpression.java
+++ src/main/org/codehaus/groovy/ast/expr/MethodCallExpression.java
@@ -67,6 +67,7 @@ public class MethodCallExpression extends Expression {
             new MethodCallExpression(transformer.transform(objectExpression), transformer.transform(method), transformer.transform(arguments));
         answer.setSafe(safe);
         answer.setSpreadSafe(spreadSafe);
+        answer.setImplicitThis(implicitThis);
         answer.setSourcePosition(this);
         return answer;
     }
