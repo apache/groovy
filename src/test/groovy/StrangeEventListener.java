b8e21fc35069519980d72b2c0fab91ebc7a0063f
Paul King <paulk@asert.com.au>
2008-04-20 04:42:58 +0000

    bamboo tweaks
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@11864 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/StrangeEventListener.java
+++ src/test/groovy/StrangeEventListener.java
@@ -1,16 +1,16 @@
 package groovy;
 
 import java.util.EventListener;
-import javax.swing.event.ChangeEvent;
+import java.beans.PropertyChangeEvent;
 
 public interface StrangeEventListener extends EventListener {
 
-    /**
+    /*
      * According to section 6.4.1 of the JavaBeans spec this is legal, but not
      * good practice.  We need to test what can be done not what should be done
      */
     void somethingStrangeHappened(String what, String where);
     
-    void somethingChanged(ChangeEvent changeEvent);
+    void somethingChanged(PropertyChangeEvent changeEvent);
     
 }
