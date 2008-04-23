039a6f53084cbca90c7e93b88755fa516aa2b9dc
Paul King <paulk@asert.com.au>
2008-04-23 01:16:09 +0000

    back out bamboo build hack in stages
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@11984 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/UberTestCaseGroovySourceSubPackages.java
+++ src/test/UberTestCaseGroovySourceSubPackages.java
@@ -17,7 +17,7 @@ public class UberTestCaseGroovySourceSubPackages extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite();
 //        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/*Test.groovy", EXCLUDES));
-        // temp hack to track down build issue
+        // temp hack to track down bamboo build issue
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/A*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/B*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/C*Test.groovy", EXCLUDES));
@@ -37,33 +37,7 @@ public class UberTestCaseGroovySourceSubPackages extends TestCase {
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/P*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/Q*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/R*Test.groovy", EXCLUDES));
-        // TODO remove comment once bamboo headless issue is fixed
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SA*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SB*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SC*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SD*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SE*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SF*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SG*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SH*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SI*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SJ*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SK*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SL*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SM*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SN*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SO*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SP*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SQ*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SR*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SS*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/ST*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SU*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SV*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SW*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SX*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SY*Test.groovy", EXCLUDES));
-        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/SZ*Test.groovy", EXCLUDES));
+        suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/S*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/T*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/U*Test.groovy", EXCLUDES));
         suite.addTest(AllTestSuite.suite(BASE, "groovy/*/**/V*Test.groovy", EXCLUDES));
