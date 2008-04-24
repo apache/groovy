224f29f2481a14b8af218ad8bf9131840c3103c5
Paul King <paulk@asert.com.au>
2008-04-24 15:56:04 +0000

    fix 1.4 build plus typo fix
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_5_X@12054 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/src/test/gls/invocaton/CovariantReturnTest.groovy
+++ groovy/src/test/gls/invocaton/CovariantReturnTest.groovy
@@ -56,18 +56,4 @@ public class CovariantReturnTest extends CompilableTestSupport {
     """
   }
 
-  void testCovariantParameterType(){
-    assertScript """
-      class BaseA implements Comparable<BaseA> {
-        int index
-        public int compareTo(BaseA a){
-          return index <=> a.index
-        }
-      }
-
-      def a = new BaseA(index:1)
-      def b = new BaseA(index:2)
-      assert a < b
-    """
-  }
 }
