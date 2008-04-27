a183fb79e056ab6b49dbb22d72cc50b74c486450
Paul King <paulk@asert.com.au>
2008-04-27 11:55:07 +0000

    merge changeset 12135 from trunk
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_5_X@12139 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/src/main/org/codehaus/groovy/tools/shell/Groovysh.groovy
+++ groovy/src/main/org/codehaus/groovy/tools/shell/Groovysh.groovy
@@ -544,7 +544,7 @@ class Groovysh
                         width = 80
                     }
 
-                    io.out.println(messages.format('startup_banner.0', InvokerHelper.version, System.properties['java.vm.version']))
+                    io.out.println(messages.format('startup_banner.0', InvokerHelper.version, System.properties['java.version']))
                     io.out.println(messages['startup_banner.1'])
                     io.out.println('-' * (width - 1))
                 }
