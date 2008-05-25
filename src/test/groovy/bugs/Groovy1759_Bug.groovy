23fe288b339b280d1b1fec0c3c7a74beebfda200
Alex Tkachman <alex.tkachman@gmail.com>
2008-05-25 08:57:20 +0000

    Lost commit for broken test for GROOVY-1759
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@12580 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/Groovy1759_Bug.groovy
+++ src/test/groovy/bugs/Groovy1759_Bug.groovy
@@ -1,11 +1,10 @@
 class Groovy1759_Bug extends GroovyTestCase {
    void testInterception() {
-      /*
       def benchmarkInterceptor = new BenchmarkInterceptor()
       def proxy = ProxyMetaClass.getInstance(A.class)
       proxy.setInterceptor(benchmarkInterceptor)
       proxy.use {
-         def a = new BeanBug1759()
+         def a = new A()
          a.a()
          a.b()
       }
@@ -13,14 +12,10 @@ class Groovy1759_Bug extends GroovyTestCase {
       def actual = benchmarkInterceptor.statistic()
       def expected = [['ctor', 1, 0],['a', 1, 0],['b', 2, 0]]
       assert expected == actual
-      */
-      // this test has been reported to break the build on windows
-      // I'm comenting it out til I find why it breaks the build
-      assert true
    }
 }
 
-class BeanBug1759{
+class A{
    void a(){ b() }
    void b(){}
 }
