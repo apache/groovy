6dbc4072f5d95b98bf456cccc5ea49776e958430
Paul King <paulk@asert.com.au>
2008-07-03 09:10:25 +0000

    javadoc improvementsfix 1.4 build
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@12917 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/swing/vm5/BindPathTest.groovy
+++ src/test/groovy/swing/vm5/BindPathTest.groovy
@@ -1,13 +1,13 @@
-package groovy.swing
+package groovy.swing.vm5
 
 import groovy.beans.Bindable
+import groovy.swing.SwingBuilder
 
 /**
  * Created by IntelliJ IDEA.
  * User: Danno.Ferrin
  * Date: Jun 9, 2008
  * Time: 8:19:38 PM
- * To change this template use File | Settings | File Templates.
  */
 class BindPathTest extends GroovySwingTestCase {
 
