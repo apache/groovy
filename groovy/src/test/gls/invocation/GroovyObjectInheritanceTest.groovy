3dced0175243470bcc4eebeb77c82f5931e1846a
Jochen Theodorou <blackdrag@gmx.org>
2008-09-03 11:19:50 +0000

    additional tests for rev 13427
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_5_X@13428 a5544e8c-8a19-0410-ba12-f9af4593a198

--- groovy/src/test/gls/invocation/GroovyObjectInheritanceTest.groovy
+++ groovy/src/test/gls/invocation/GroovyObjectInheritanceTest.groovy
@@ -12,6 +12,14 @@ class GroovyObjectInheritanceTest extends CompilableTestSupport {
         def bar = new Bar()
         assert bar.foo==1
     """
+    assertScript """
+        class Foo {}
+        class Bar extends Foo{
+          def getProperty(String name) {1}
+        }
+        def bar = new Bar()
+        assert bar.foo==1
+    """
   }
   
   void testInheritanceWithSetProperty() {
@@ -21,6 +29,16 @@ class GroovyObjectInheritanceTest extends CompilableTestSupport {
           void setProperty(String name, x) {this.foo=1}
         }
         class Bar extends Foo{}
+        def bar = new Bar()
+        bar.foo = 2
+        assert bar.foo == 1
+    """
+    assertScript """
+        class Foo {}
+        class Bar extends Foo{
+          def foo
+          void setProperty(String name, x) {this.foo=1}
+        }
         def bar = new Bar()
         bar.foo = 2
         assert bar.foo == 1
@@ -36,6 +54,14 @@ class GroovyObjectInheritanceTest extends CompilableTestSupport {
         def bar = new Bar()
         assert bar.foo() == 1
     """
+    assertScript """
+        class Foo {}
+        class Bar extends Foo{
+          def invokeMethod(String name, args) {1}
+        }
+        def bar = new Bar()
+        assert bar.foo() == 1
+    """
   }
   
   void testMetaClassFieldInheritance() {
@@ -44,6 +70,31 @@ class GroovyObjectInheritanceTest extends CompilableTestSupport {
         class Bar extends Foo{}
         assert Foo.class.declaredFields.find{it.name=="metaClass"}!=null
         assert Bar.class.declaredFields.find{it.name=="metaClass"}==null
+    """
+  }
+  
+  void testStandardInhgeritance() {
+    assertScript """
+        class Foo{}
+        class Bar extends Foo{}
+        
+        assert Foo.class.declaredFields.find{it.name=="metaClass"}!=null
+        assert Bar.class.declaredFields.find{it.name=="metaClass"}==null
+        
+        assert Foo.class.declaredMethods.find{it.name=="getMetaClass"}!=null
+        assert Bar.class.declaredMethods.find{it.name=="getMetaClass"}==null
+        
+        assert Foo.class.declaredMethods.find{it.name=="setMetaClass"}!=null
+        assert Bar.class.declaredMethods.find{it.name=="setMetaClass"}==null
+        
+        assert Foo.class.declaredMethods.find{it.name=="getProperty"}!=null
+        assert Bar.class.declaredMethods.find{it.name=="getProperty"}==null
+        
+        assert Foo.class.declaredMethods.find{it.name=="setProperty"}!=null
+        assert Bar.class.declaredMethods.find{it.name=="setProperty"}==null
+        
+        assert Foo.class.declaredMethods.find{it.name=="invokeMethod"}!=null
+        assert Bar.class.declaredMethods.find{it.name=="invokeMethod"}==null
     """
   }
 }
