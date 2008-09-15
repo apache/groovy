c69eb4a1e7f805cdccfae73501373d5a667a8dbc
Paul King <paulk@asert.com.au>
2008-09-15 07:45:37 +0000

    typo
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@13575 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/transform/GroovyASTTransformationClass.java
+++ src/main/org/codehaus/groovy/transform/GroovyASTTransformationClass.java
@@ -6,8 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation is an annotation on some item that indicated that when it
- * is used the associared transform classes should be executed.  As of
+ * This is an annotation on some item that indicates that
+ * an associated transform classes should be executed.  As of
  * Groovy 1.6 the only valid target is the annotation type.
  *
  * It is a compile time error to specify a {@link #value} that is a class not
@@ -20,7 +20,7 @@ import java.lang.annotation.Target;
  */
 
 @Retention(RetentionPolicy.RUNTIME)
-// in the future the taret will be wider than annotations, but for now it is just on annotations
+// in the future the target will be wider than annotations, but for now it is just on annotations
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface GroovyASTTransformationClass {
     String[] value();
