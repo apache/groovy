5af408fd338f559a9180407bf4f0a04d9aaf9531
Thom Nichols <tmnichols@gmail.com>
2008-10-06 14:05:20 +0000

    GROOVY-3066 added static Date.parse( format, input ) method & tests.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@13699 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/runtime/DateGDKTest.groovy
+++ src/test/org/codehaus/groovy/runtime/DateGDKTest.groovy
@@ -14,9 +14,9 @@ public class DateGDKTest extends GroovyTestCase {
 		Locale.setDefault locale // set this otherwise the test will fail if your locale isn't the same
 		TimeZone.setDefault TimeZone.getTimeZone( 'Etc/GMT' )
 		Date d = new Date(0)
-		println d.dateString
-		println d.timeString
-		println d.dateTimeString
+//		println d.dateString
+//		println d.timeString
+//		println d.dateTimeString
 		assertEquals '1970-01-01', d.format( 'yyyy-MM-dd' )
 		assertEquals DateFormat.getDateInstance(DateFormat.SHORT,locale).format(d), d.dateString
 		assertEquals '01/01/70', d.dateString
@@ -27,5 +27,25 @@ public class DateGDKTest extends GroovyTestCase {
 		
 		Locale.default = defaultLocale
 		TimeZone.setDefault defaultTZ
+	}
+	
+	public void testStaticParse() {
+		TimeZone defaultTZ = TimeZone.default
+		TimeZone.setDefault TimeZone.getTimeZone( 'Etc/GMT' )
+
+		Date d = Date.parse( 'yy/MM/dd hh:mm:ss', '70/01/01 00:00:00' )
+		assertEquals 0, d.time
+
+		TimeZone.setDefault defaultTZ
+	}
+	
+	public void testRoundTrip() {
+		Date d = new Date()
+		String pattern = 'dd MMM yyyy, hh:mm:ss,SSS a z'
+		String out = d.format( pattern )
+		
+		Date d2 = Date.parse( pattern, out )
+		
+		assertEquals d.time, d2.time
 	}
 }
