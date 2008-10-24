6b2613f3c350b7e34ecad1d4eb0d3cee192651b0
Jochen Theodorou <blackdrag@gmx.org>
2008-10-24 12:35:20 +0000

    fix for GROOVY-2760. Disallow overlaying variable scopes for catch blocks
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@13832 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/gls/scope/BlockScopeVisibilityTest.groovy
+++ src/test/gls/scope/BlockScopeVisibilityTest.groovy
@@ -3,12 +3,24 @@ package gls.scope
 class BlockScopeVisibilityTest extends CompilableTestSupport {
 
   public void testForLoopVariableNotVisibleOutside() {
- 	
   	assertScript("""
   	  i=1
   	  for (i in [1,2]) {}
   	  assert i==1
   	""")
+  }
+  
+  public void testCatchParameterNotVisibleInOtherCatch() {
+    shouldFail(MissingPropertyException) {    
+        try {
+            throw new RuntimeException("not important");
+        } catch(AssertionError e) {
+           // here e is defined
+        } catch(Throwable t) {
+           // here e should not be accessible
+           println e 
+        }
+    }
   }
 
 }
