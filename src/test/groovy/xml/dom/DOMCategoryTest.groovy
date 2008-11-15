2000630495917f7de4725a0585e001670d1e7989
Paul King <paulk@asert.com.au>
2008-11-15 22:59:22 +0000

    GROOVY-1193: new convenience methods in DOMCategory: Node.xpath
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14009 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/xml/dom/DOMCategoryTest.groovy
+++ src/test/groovy/xml/dom/DOMCategoryTest.groovy
@@ -73,20 +73,6 @@ class DOMCategoryTest extends GroovyTestCase {
         }
     }
 
-    void testXPathWithDomCategory() {
-        def reader = new StringReader('<a><b>B1</b><b>B2</b><c a1="4" a2="true">C</c></a>')
-        def root = DOMBuilder.parse(reader)
-        use(DOMCategory) {
-            assert root.xpath('a/c/text()') == 'C'
-            def text = { n -> n.xpath('text()') }
-            assert text(root.xpath('a/c', NODE)) == 'C'
-            assert root.xpath('a/c/@a1') == '4'
-            assert root.xpath('a/c/@a1', NUMBER) == 4
-            assert root.xpath('a/c/@a2', BOOLEAN)
-            assert root.xpath('a/b', NODESET).collect(text).join() == 'B1B2'
-        }
-    }
-
     /** Test for GROOVY-3109 */
     void testAccessToUnknownPropertyInAScriptWithDomCategory() {
         assertScript """
