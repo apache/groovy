3618b4b60c7487e7c4953dcc0fb524a8dd473743
Jochen Theodorou <blackdrag@gmx.org>
2008-11-26 17:20:55 +0000

    fix for GROOVY-2933, patch by Roshan Dawrani. Fixes the usage of lists to intialize enums
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14161 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/gls/enums/vm5/EnumTest.groovy
+++ src/test/gls/enums/vm5/EnumTest.groovy
@@ -109,6 +109,34 @@ class EnumTest extends GroovyTestCase {
         assert coins == [UsCoin.nickel]
     }
 
+    void testEnumWithSingleListInConstructor() {
+        def sh = new GroovyShell();
+        def enumStr;
+        
+        enumStr = """
+            enum ListEnum1 {
+                ONE([111, 222])
+                ListEnum1(Object listArg){
+                    assert listArg == [111, 222]
+                    assert listArg instanceof java.util.ArrayList
+                }
+            }
+            println ListEnum1.ONE
+        """
+        sh.evaluate(enumStr);
+            
+        enumStr = """
+            enum ListEnum2 {
+                TWO([234, [567,12]])
+                ListEnum2(Object listArg){
+                    assert listArg == [234, [567, 12]]
+                    assert listArg instanceof java.util.ArrayList
+                }
+            }
+            println ListEnum2.TWO
+        """
+        sh.evaluate(enumStr);
+    }
 }
 
 enum UsCoin {
