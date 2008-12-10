1708a25712852a08370eb51867cbc1bd01eb68c1
Paul King <paulk@asert.com.au>
2008-12-10 13:28:22 +0000

    fix trunk for Java 1.5
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14349 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/tools/FileSystemCompiler.java
+++ src/main/org/codehaus/groovy/tools/FileSystemCompiler.java
@@ -296,7 +296,7 @@ public class FileSystemCompiler {
                             " you are possibly victim to"+
                             " http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6325169. "+
                             " this is no bug in Groovy.";
-                        throw new IOException(msg,ioe);
+                        throw new IOException(msg);
                     } else {
                         throw ioe;
                     }
