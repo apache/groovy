c3cad5627d2c25fb17809430a8aaf02c8468aa69
Jeff Brown <jeff@jeffandbetsy.net>
2008-12-14 15:52:13 +0000

    GROOVY-3200 - UnsupportedOperationException is now thrown instead of NullPointerException when calling unimplemented methods on a proxy created from a map
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14411 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/ConvertedMap.java
+++ src/main/org/codehaus/groovy/runtime/ConvertedMap.java
@@ -40,6 +40,9 @@ public class ConvertedMap extends ConversionHandler {
     throws Throwable {
         Map m = (Map) getDelegate();
         Closure cl = (Closure) m.get(method.getName());
+        if(cl == null) {
+            throw new UnsupportedOperationException();
+        }
         return cl.call(args);
     }
     
