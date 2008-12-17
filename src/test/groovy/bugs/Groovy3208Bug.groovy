cf7d960dd3d49578f4bd9630f0e6d1fd2f97180b
Jim White <jim@pagesmiths.com>
2008-12-17 20:34:55 +0000

    Tweak GROOVY-3208 test case.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14464 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/Groovy3208Bug.groovy
+++ src/test/groovy/bugs/Groovy3208Bug.groovy
@@ -3,14 +3,24 @@ package groovy.bugs
 public class Groovy3208Bug extends GroovyTestCase {
 
    void testBug () {
-      new Sub().each { it.doIt() }
+      new Sub().each { assertEquals("ABC", it.doIt()) }
+      
+      assertEquals("ABC", new Sub().doItAgain())
    }
+   
+//    void testSubclassStaticContextProperty() {
+//       assert "ABC" == Sub.doItStatically()
+//       assert "ABC" == Sub.doItStaticallyAgain()
+//    }
 }
 
 class Super {
    static final String PROP = "ABC"
    def doIt = { PROP }
+//   def static doItStatically = { PROP }
 }
 
 class Sub extends Super {
+   String doItAgain() { PROP }
+//   static String doItStaticallyAgain() { PROP }
 }
