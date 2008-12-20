7e3a29d03b3a6388775f87045a21cf8c9a1cb833
Roshan Dawrani <roshandawrani@gmail.com>
2008-12-20 09:48:39 +0000

    GROOVY-3173 Partial Fix for GROOVY-3173. Added support for assigning single character string/GString to Character[] elements.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14535 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/ArrayTest.groovy
+++ src/test/groovy/ArrayTest.groovy
@@ -222,4 +222,27 @@ class ArrayTest extends GroovyTestCase {
         assertEquals 100, doubles[0]
         assertEquals 50, doubles[1]
     }
+    
+    void testCharacterArrayElementAssignments() {
+        Character[] ca = new Character[1]
+
+        // Assignments statements.
+        ca[0] = 'b' as char
+        assert ca[0] == 'b'
+
+        ca[0] = '\u00A1' as char
+        assert ca[0] == '\u00A1'
+
+        ca[0] = 'a'
+        assert ca[0] == 'a'
+
+        ca[0] = '\u00A0'
+        assert ca[0] == '\u00A0'
+
+        def foo = 'z'
+
+        ca[0] = "$foo"
+        assert ca[0] == 'z'
+    }
+    
 }
