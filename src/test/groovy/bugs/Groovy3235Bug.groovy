e3324bb34b9e1d7cc372789f584509e1bc304583
Jim White <jim@pagesmiths.com>
2008-12-25 07:17:51 +0000

    Test case for GROOVY-3235.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14639 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy3235Bug.groovy
@@ -0,0 +1,20 @@
+package groovy.bugs
+
+public class Groovy3235Bug extends GroovyTestCase {
+
+   void testBug () {
+      def d = """This is one line.
+      This is another.
+      All these lines should be written.
+"""
+      def f = File.createTempFile("groovy.bugs.Groovy3235Bug", ".txt")
+      
+      f.withWriter { w ->
+          d.eachLine { w.println it }
+      }
+
+      def t = f.text
+
+      assert d == t
+   }
+}
