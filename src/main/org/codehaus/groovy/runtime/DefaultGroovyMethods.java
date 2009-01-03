be6cb63067bf567efc3969a9375246020062863a
Jim White <jim@pagesmiths.com>
2009-01-03 08:13:44 +0000

    GROOVY-3248 : Merge in changes 14758, 14762, 14772.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_6_X@14773 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -2726,26 +2726,30 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
      *          y is the first string in the matched group
      *          z is the second string in the matched group
      * </pre>
+     * <p>Note that unlike String.replaceAll(String pattern, String replacement), where the replacement string
+     * treats '$' and '\' specially (for group substitution), the result of the closure is converted to a string
+     * and that value is used literally for the replacement.</p>
      *
      * @param self    a String
      * @param regex   the capturing regex
      * @param closure the closure to apply on each captured group
      * @return a String with replaced content
      * @since 1.0
+     * @see java.util.regex.Matcher.quoteReplacement(String)
      */
-    public static String replaceAll(String self, String regex, Closure closure) {
-        Matcher matcher = Pattern.compile(regex).matcher(self);
+    public static String replaceAll(final String self, final String regex, final Closure closure) {
+        final Matcher matcher = Pattern.compile(regex).matcher(self);
         if (matcher.find()) {
-            matcher.reset();
-            StringBuffer sb = new StringBuffer();
-            while (matcher.find()) {
+            final StringBuffer sb = new StringBuffer(self.length() + 16);
+            do {
                 int count = matcher.groupCount();
                 List groups = new ArrayList();
                 for (int i = 0; i <= count; i++) {
                     groups.add(matcher.group(i));
                 }
-                matcher.appendReplacement(sb, String.valueOf(closure.call(groups.toArray())));
-            }
+                final String replacement = InvokerHelper.toString(closure.call(groups.toArray()));
+                matcher.appendReplacement(sb, Matcher.quoteReplacement(replacement));
+            } while (matcher.find());
             matcher.appendTail(sb);
             return sb.toString();
         } else {
