3f6d4d4fd57468e7499c9aef7daa717194a92458
Paul King <paulk@asert.com.au>
2009-01-05 10:08:37 +0000

    typo
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@14833 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/transform/GroovyASTTransformation.java
+++ src/main/org/codehaus/groovy/transform/GroovyASTTransformation.java
@@ -25,7 +25,7 @@ import java.lang.annotation.Target;
 
 /**
  * This is an annotaton on a class, currently just {@link ASTTransformation}.
- * This provices information about how and when to apply the transformation,
+ * This provides information about how and when to apply the transformation,
  * such as what phase it should be applied in.
  *
  * The allowed phase is a function of how the transformation is introduced
@@ -34,7 +34,7 @@ import java.lang.annotation.Target;
  * for the phase().  This is because the annotations are not inspected until
  * after the classes are all resolved.
  *
- * Also, only annotation types present durring the SEMANTIC_ANALYSIS phase
+ * Also, only annotation types present during the SEMANTIC_ANALYSIS phase
  * will be handled.  Transformations adding other annotations that are
  * transformable will have those new annotations only considered in
  * latter phases, and only if the type was present in the source unit
