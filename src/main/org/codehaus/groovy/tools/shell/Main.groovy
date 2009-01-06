c088646a5f52a54898caf1fd3a9b8765595d5d08
Jim White <jim@pagesmiths.com>
2009-01-06 15:48:04 +0000

    GROOVY-3260 : Merge in fix from trunk.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_6_X@14891 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/tools/shell/Main.groovy
+++ src/main/org/codehaus/groovy/tools/shell/Main.groovy
@@ -16,9 +16,6 @@
 
 package org.codehaus.groovy.tools.shell
 
-import java.awt.HeadlessException
-import javax.swing.UIManager
-
 import org.codehaus.groovy.runtime.InvokerHelper
 import org.codehaus.groovy.tools.shell.util.ANSI
 import org.codehaus.groovy.tools.shell.util.HelpFormatter
@@ -112,11 +109,6 @@ class Main
             io.flush()
         }
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-        } catch (HeadlessException he) {
-            io.err.println('@|red WARNING:| Running in AWT Headless mode, \'inspect\' will fail')
-        }
         // Boot up the shell... :-)
         Groovysh shell = new Groovysh(io)
 
