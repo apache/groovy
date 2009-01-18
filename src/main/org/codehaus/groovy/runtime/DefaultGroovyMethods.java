90e0cebf786bc4f46310d4b24bc12730f804f187
Jim White <jim@pagesmiths.com>
2009-01-18 02:04:15 +0000

    GROOVY-3264 : Yet another tweak.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@15094 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -9147,15 +9147,17 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
         
         final StringBuilder sb = new StringBuilder((110 * len) / 100);
 
-        for (int i = 0; i < len; ++i) {
-            final char ch = self.charAt(i);
+        int i = 0;
+        
+        while (i < len) {
+            final char ch = self.charAt(i++);
 
             switch (ch) {
                 case '\r':
                     sb.append(lineSeparator);
                     
                     // Eat the following LF if any.
-                    if ((i + 1 < len) && (self.charAt(i + 1) == '\n')) {
+                    if ((i < len) && (self.charAt(i) == '\n')) {
                         ++i;
                     }
                     
