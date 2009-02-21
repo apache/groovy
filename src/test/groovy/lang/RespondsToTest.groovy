80b0b8379374786836b8b71ba7f36bd58e08d8f8
Paul King <paulk@asert.com.au>
2009-02-21 00:51:23 +0000

    formatting only in prep for refactor/GROOVY-3363 fix
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@15413 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/lang/RespondsToTest.groovy
+++ src/test/groovy/lang/RespondsToTest.groovy
@@ -1,23 +1,35 @@
-/**
- * Tests the respondsTo functionality of Groovy
- 
- * @author Graeme Rocher
- * @since 1.1
-  *
- * Created: Sep 7, 2007
- * Time: 4:35:19 PM
- * 
+/*
+ * Copyright 2003-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package groovy.lang
+
+/**
+ * Tests the respondsTo functionality of Groovy
+ *
+ * @author Graeme Rocher
+ * @since 1.6.0
+ */
 class RespondsToTest extends GroovyTestCase {
 
     void testRespondsToForMethodEvaluation() {
         RespondsToTestClass.metaClass.invokeMethod = {String name, args ->
-            def methods = RespondsToTestClass.metaClass.respondsTo(delegate,name, args*.getClass() as Object[])
+            def methods = RespondsToTestClass.metaClass.respondsTo(delegate, name, args*.getClass() as Object[])
             def result
-            if(methods) {
+            if (methods) {
                 // only way to get var-args to work is to do this at the moment. Yuck!
-                if(methods[0].parameterTypes.length == 1 && methods[0].parameterTypes[0].theClass == Object[].class)
+                if (methods[0].parameterTypes.length == 1 && methods[0].parameterTypes[0].theClass == Object[].class)
                     result = methods[0].invoke(delegate, [args] as Object[])
                 else
                     result = methods[0].invoke(delegate, args)
@@ -30,11 +42,11 @@ class RespondsToTest extends GroovyTestCase {
 
         def t = new RespondsToTestClass()
         assertEquals "one", t.noArgsMethod()
-        assertEquals "two", t.varArgsMethod(1,2)
-        assertEquals "two", t.varArgsMethod(null,null)
-        assertEquals "three", t.typedArgsMethod("one",1)
-        assertEquals "three", t.typedArgsMethod(null,1)
-        assertEquals "three", t.typedArgsMethod(null,null)
+        assertEquals "two", t.varArgsMethod(1, 2)
+        assertEquals "two", t.varArgsMethod(null, null)
+        assertEquals "three", t.typedArgsMethod("one", 1)
+        assertEquals "three", t.typedArgsMethod(null, 1)
+        assertEquals "three", t.typedArgsMethod(null, null)
         assertEquals "four", t.overloadedMethod("one")
         assertEquals "five", t.overloadedMethod(1)
         assertEquals "four", t.overloadedMethod(null)
@@ -43,62 +55,72 @@ class RespondsToTest extends GroovyTestCase {
     }
 
     void testRespondsTo() {
-
         RTTest2.metaClass.newM = {-> "foo" }
-
         def t = new RTTest2()
-
-        assert t.metaClass.respondsTo(t,"one")
-        assert t.metaClass.respondsTo(t,"three")
-        assert t.metaClass.respondsTo(t,"one", String)
-        assert t.metaClass.respondsTo(t,"foo", String)
-        assert t.metaClass.respondsTo(t,"bar", String)
-        assert t.metaClass.respondsTo(t,"stuff")
-        //assert t.metaClass.respondsTo(t,"two") THIS DOESN'T WORK! Should responds to deal with closure properties?
-        assert t.metaClass.respondsTo(t,"getFive")
-        assert t.metaClass.respondsTo(t,"setFive")
-        assert t.metaClass.respondsTo(t,"setFive", String)
-        assert t.metaClass.respondsTo(t,"newM")
-        assert !t.metaClass.respondsTo(t,"one", String, Integer)
+        assert t.metaClass.respondsTo(t, "one")
+        assert t.metaClass.respondsTo(t, "three")
+        assert t.metaClass.respondsTo(t, "one", String)
+        assert t.metaClass.respondsTo(t, "foo", String)
+        assert t.metaClass.respondsTo(t, "bar", String)
+        assert t.metaClass.respondsTo(t, "stuff")
+        //assert t.metaClass.respondsTo(t, "two") // THIS DOESN'T WORK! Should responds to deal with closure properties?
+        assert t.metaClass.respondsTo(t, "getFive")
+        assert t.metaClass.respondsTo(t, "setFive")
+        assert t.metaClass.respondsTo(t, "setFive", String)
+        assert t.metaClass.respondsTo(t, "newM")
+        assert !t.metaClass.respondsTo(t, "one", String, Integer)
     }
 
     void testHasProperty() {
         RTTest2.metaClass.getNewProp = {-> "new" }
         def t = new RTTest2()
-
-        assert t.metaClass.hasProperty(t,"two")
-        assert t.metaClass.hasProperty(t,"five")
-        assert t.metaClass.hasProperty(t,"six")
-        assert t.metaClass.hasProperty(t,"seven")
-        assert t.metaClass.hasProperty(t,"eight")
-        assert t.metaClass.hasProperty(t,"newProp")
-
+        assert t.metaClass.hasProperty(t, "two")
+        assert t.metaClass.hasProperty(t, "five")
+        assert t.metaClass.hasProperty(t, "six")
+        assert t.metaClass.hasProperty(t, "seven")
+        assert t.metaClass.hasProperty(t, "eight")
+        assert t.metaClass.hasProperty(t, "newProp")
     }
 }
+
 class RespondsToTestClass {
     def noArgsMethod() { "one" }
+
     def varArgsMethod(Object[] args) { "two" }
+
     def typedArgsMethod(String one, Integer two) { "three" }
+
     def overloadedMethod(String one) { "four" }
+
     def overloadedMethod(Integer one) { "five" }
+
     def overloadedMethod() { "six" }
 }
+
 class RTTest1 {
     String five
     def two = { "three" }
+
     def one() { "two"}
+
     def one(String one) { "two: $one" }
+
     def three(String one) { "four" }
+
     def three(Integer one) { "four" }
+
     def foo(String name) {
         "bar"
     }
 
     String getSeven() { "seven" }
 }
+
 class RTTest2 extends RTTest1 {
     String six
+
     def bar(String name) { "foo" }
+
     static stuff() { "goodie" }
 
     String getEight() { "eight" }
