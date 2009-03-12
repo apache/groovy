7af2c446570115a85850c23e8b79b1ec07d9cf81
Roshan Dawrani <roshandawrani@gmail.com>
2009-03-12 19:48:14 +0000

    GROOVY-3403 Same call site caching issue as in GROOVY-3139 but this one was for stubbing of static calls. In case of Stubs/Mocks, instead of using cached MockProxyMetaClass instance, the one from the meta-class registry should be used so that latest interceptor and its call count are used and not the cached one's.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_6_X@15530 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/callsite/StaticMetaClassSite.java
+++ src/main/org/codehaus/groovy/runtime/callsite/StaticMetaClassSite.java
@@ -3,7 +3,9 @@ package org.codehaus.groovy.runtime.callsite;
 import org.codehaus.groovy.runtime.ScriptBytecodeAdapter;
 
 import groovy.lang.GroovyRuntimeException;
+import groovy.lang.GroovySystem;
 import groovy.lang.MetaClass;
+import groovy.lang.ProxyMetaClass;
 
 /**
  * Call site for invoking static methods
@@ -20,7 +22,13 @@ public class StaticMetaClassSite extends MetaClassSite {
     public final Object call(Object receiver, Object[] args) throws Throwable {
         if(receiver == metaClass.getTheClass()) {
             try {
-                return metaClass.invokeStaticMethod(receiver, name, args);
+				MetaClass metaClassToInvoke = metaClass;
+				// if it is ProxyMetaClass, use the one from the registry and not the cached one
+				// as the mock/stub infrastructure replaces metaClass at runtime in the context of use {..}
+				if(metaClass instanceof ProxyMetaClass) {
+					metaClassToInvoke = GroovySystem.getMetaClassRegistry().getMetaClass(metaClass.getTheClass()); 
+				}
+                return metaClassToInvoke.invokeStaticMethod(receiver, name, args);
             } catch (GroovyRuntimeException gre) {
                 throw ScriptBytecodeAdapter.unwrap(gre);
             }
