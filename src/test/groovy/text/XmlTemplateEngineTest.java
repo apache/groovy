97164fd9141f6918542f10815240ab54ee0c5d69
Paul King <paulk@asert.com.au>
2009-06-20 01:38:43 +0000

    merge changeset 16750 from trunk (GROOVY-3269, GROOVY-3505)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_6_X@16751 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/text/XmlTemplateEngineTest.java
+++ src/test/groovy/text/XmlTemplateEngineTest.java
@@ -12,16 +12,16 @@ public class XmlTemplateEngineTest extends TestCase {
         binding.put("Christian", "Stein");
 
         XmlTemplateEngine xmlTemplateEngine = new XmlTemplateEngine();
-        String xmlScript = "<!-- Just a comment. -->\n" //
-                + "<xml xmlns:gsp=\"http://groovy.codehaus.org/2005/gsp\">" //
-                + "  ${Christian}" //
-                + "  <gsp:expression>Christian</gsp:expression>" //
-                + "  <gsp:scriptlet>println Christian</gsp:scriptlet>" //
+        String xmlScript = "<!-- Just a comment. -->\n"
+                + "<xml xmlns:gsp=\"http://groovy.codehaus.org/2005/gsp\">"
+                + "  ${Christian}"
+                + "  <gsp:expression>Christian</gsp:expression>"
+                + "  <gsp:scriptlet>println Christian</gsp:scriptlet>"
                 + "</xml>";
-        String xmlResult = "<xml>\n" //
-                + "  Stein\n" //
-                + xmlTemplateEngine.getIndentation() + "Stein\n" //
-                + "Stein" + System.getProperty("line.separator") //
+        String xmlResult = "<xml>\n"
+                + "  Stein\n"
+                + xmlTemplateEngine.getIndentation() + "Stein\n"
+                + "Stein" + System.getProperty("line.separator")
                 + "</xml>\n";
         Template template = xmlTemplateEngine.createTemplate(xmlScript);
         assertEquals(xmlResult, template.make(binding).toString());
@@ -32,17 +32,39 @@ public class XmlTemplateEngineTest extends TestCase {
         binding.put("Christian", "Stein");
 
         XmlTemplateEngine xmlTemplateEngine = new XmlTemplateEngine();
-        String xmlScript = "<!-- Just a comment. -->\n" //
-                + "<xml xmlns:gsp=\"http://groovy.codehaus.org/2005/gsp\">" //
-                + "  ${Christian + \" \" + Christian}" //
-                + "  <gsp:expression>Christian + \" \" + Christian</gsp:expression>" //
-                + "  <gsp:scriptlet>println Christian</gsp:scriptlet>" //
+        String xmlScript = "<!-- Just a comment. -->\n"
+                + "<xml xmlns:mygsp=\"http://groovy.codehaus.org/2005/gsp\">"
+                + "  ${Christian + \" \" + Christian}"
+                + "  <mygsp:expression>Christian + \" \" + Christian</mygsp:expression>"
+                + "  <mygsp:scriptlet>println Christian</mygsp:scriptlet>"
                 + "</xml>";
-        String xmlResult = "<xml>\n" //
-                + "  Stein Stein\n" //
-                + xmlTemplateEngine.getIndentation() + "Stein Stein\n" //
-                + "Stein" + System.getProperty("line.separator") //
+        String xmlResult = "<xml>\n"
+                + "  Stein Stein\n"
+                + xmlTemplateEngine.getIndentation() + "Stein Stein\n"
+                + "Stein" + System.getProperty("line.separator")
                 + "</xml>\n";
+        Template template = xmlTemplateEngine.createTemplate(xmlScript);
+        assertEquals(xmlResult, template.make(binding).toString());
+    }
+
+    public void testNamespaces() throws Exception {
+        Map binding = new HashMap();
+        binding.put("Christian", "Stein");
+
+        XmlTemplateEngine xmlTemplateEngine = new XmlTemplateEngine();
+        String xmlScript = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                + "<foo:bar xmlns:foo='urn:baz' xmlns:mygsp=\"http://groovy.codehaus.org/2005/gsp\">"
+                + "${Christian + \" \" + Christian}"
+                + "<mygsp:expression>Christian + \" \" + Christian</mygsp:expression>"
+                + "<nonamespace><mygsp:scriptlet>println Christian</mygsp:scriptlet></nonamespace>"
+                + "</foo:bar>";
+        String xmlResult = "<foo:bar xmlns:foo='urn:baz'>\n"
+                + "  Stein Stein\n"
+                + xmlTemplateEngine.getIndentation() + "Stein Stein\n"
+                + xmlTemplateEngine.getIndentation() + "<nonamespace>\n"
+                + "Stein" + System.getProperty("line.separator")
+                + xmlTemplateEngine.getIndentation() + "</nonamespace>\n"
+                + "</foo:bar>\n";
         Template template = xmlTemplateEngine.createTemplate(xmlScript);
         assertEquals(xmlResult, template.make(binding).toString());
     }
