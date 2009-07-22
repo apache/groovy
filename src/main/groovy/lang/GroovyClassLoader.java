f48ace59a1f56ffcd5b7ef2927ac1d9bad474846
Jochen Theodorou <blackdrag@gmx.org>
2009-07-22 16:14:12 +0000

    GROOVY-3125, reenable the fix for class caching
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@16983 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/lang/GroovyClassLoader.java
+++ src/main/groovy/lang/GroovyClassLoader.java
@@ -701,7 +701,7 @@ public class GroovyClassLoader extends URLClassLoader {
                     // if recompilation fails, we want cls==null
                     Class oldClass = cls;
                     cls = null;
-                    cls = recompile(source, name, cls);
+                    cls = recompile(source, name, oldClass);
                 } catch (IOException ioe) {
                     last = new ClassNotFoundException("IOException while opening groovy source: " + name, ioe);
                 } finally {
