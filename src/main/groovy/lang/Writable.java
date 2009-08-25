03d2335985a3ee7b229912502e5dc3be39f682ec
Thom Nichols <tmnichols@gmail.com>
2009-08-25 01:32:08 +0000

    javadoc
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@17425 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/lang/Writable.java
+++ src/main/groovy/lang/Writable.java
@@ -20,9 +20,14 @@ import java.io.Writer;
 
 
 /**
- * Represents an object which is capable of writing itself to a text stream
+ * <p>Represents an object which is capable of writing itself to a text stream
  * in a more efficient format than just creating a toString() representation
  * of itself. This mechanism is particularly useful for templates and such like.
+ * </p>
+ * 
+ * <p>It is worth noting that writable implementations often override their 
+ * {@link #toString()} implementation as well to allow rendering the same result 
+ * directly to a String; however this is not required.</p>
  * 
  * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
  * @version $Revision$
@@ -30,7 +35,21 @@ import java.io.Writer;
 public interface Writable {
 
     /**
-     * writes this object to the given stream
+     * <p>Writes this object to the given writer.</p>
+     * 
+     * <p>This is used to defer content creation until the point when it is 
+     * streamed to the output destination.  Oftentimes, content will be defined 
+     * but not necessarily created (as is may be the case with a Closure 
+     * definition.)  In that case, the output is then 'deferred' to the point 
+     * when it is serialized to the writer. This class may be used whenever an 
+     * object should be responsible for creating its own textual representation, 
+     * but creating the entire output as a single String would be inefficient
+     * (such as outputting a multi-gigabyte XML document.)</p>  
+     * 
+     * @param out the Writer to which this Writable should output its data.
+     * @return the Writer that was passed  
+     * @throws IOException if an error occurred while outputting data to the 
+     * writer
      */
     Writer writeTo(Writer out) throws IOException;
         
