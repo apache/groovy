72dc5227f860a7d5cf1122a97e5426d03490cdea
Roshan Dawrani <roshandawrani@gmail.com>
2009-09-21 13:51:11 +0000

    GROOVY-3770 Curried closure did not deal with setting of delegate and resolveStrategy correctly. Fixed it.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@17693 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy3770Bug.groovy
@@ -0,0 +1,31 @@
+package groovy.bugs
+
+class Groovy3770Bug extends GroovyTestCase {
+    void testSetDelegateAndResolveStrategyOnACurriedClosure() {
+        assertScript """
+            void hello(who) {
+                println ("Hello " + who)
+            }
+            
+            def c = { x ->
+                hello(x)
+            }
+            
+            def d = c.curry("Ian")
+            d.call()
+            
+            d.delegate = null
+
+            assert d.delegate == null
+
+            d.resolveStrategy = Closure.DELEGATE_ONLY
+
+            try {
+                d.call()
+                throw new RuntimeException("The curried closure call should have failed here with MME")
+            } catch(MissingMethodException ex) {
+                // ok if closure call returned in an exception (MME)
+            }
+        """
+    }
+}
