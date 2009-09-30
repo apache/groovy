c9aeb80ed76fcb34bfd7ecae593d2575a3bebba7
Guillaume Laforge <glaforge@gmail.com>
2009-09-30 20:32:52 +0000

    GROOVY-3669: Can't use several times the same JSR-223 ScriptContext for different groovy scripts
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@17816 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/jsr223/JSR223Test.groovy
+++ src/test/org/codehaus/groovy/jsr223/JSR223Test.groovy
@@ -1,95 +1,125 @@
-package org.codehaus.groovy.jsr223;
+package org.codehaus.groovy.jsr223
 
-import junit.framework.TestCase;
+import junit.framework.TestCase
 
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptException;
+import javax.script.ScriptEngineManager
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineFactory
+import javax.script.ScriptException
 
-import org.codehaus.groovy.runtime.InvokerHelper;
+import org.codehaus.groovy.runtime.InvokerHelper
 
-import java.util.List;
-import java.util.Iterator;
+import java.util.Iterator
+import javax.script.SimpleScriptContext
 
 /**
  * Tests JSR-223 Groovy engine implementation.
+ *
  * @author Jim White
+ * @author Guillaume Laforge
  */
 public class JSR223Test extends TestCase {
-    protected ScriptEngineManager manager;
+    protected ScriptEngineManager manager
 
     protected void setUp() {
-        manager = new ScriptEngineManager();
+        manager = new ScriptEngineManager()
     }
 
-    public void testGetEngineByEngineName() {
-        ScriptEngine engine = manager.getEngineByName("groovy");
-        assertNotNull(engine);
+    void testGetEngineByEngineName() {
+        ScriptEngine engine = manager.getEngineByName("groovy")
+        assertNotNull(engine)
     }
 
-    public void testGetEngineByLanguageName() {
-        ScriptEngine engine = manager.getEngineByName("Groovy");
-        assertNotNull(engine);
+    void testGetEngineByLanguageName() {
+        ScriptEngine engine = manager.getEngineByName("Groovy")
+        assertNotNull(engine)
     }
 
-    public void testGetEngineByExtension() {
-        ScriptEngine engine = manager.getEngineByExtension("groovy");
-        assertNotNull(engine);
+    void testGetEngineByExtension() {
+        ScriptEngine engine = manager.getEngineByExtension("groovy")
+        assertNotNull(engine)
     }
 
-    public void testGetEngineByMIMEType() {
-        ScriptEngine engine = manager.getEngineByMimeType("application/x-groovy");
-        assertNotNull(engine);
+    void testGetEngineByMIMEType() {
+        ScriptEngine engine = manager.getEngineByMimeType("application/x-groovy")
+        assertNotNull(engine)
     }
 
-    public void testCheckParameters() {
-        ScriptEngine engine = manager.getEngineByName("groovy");
-        assertNotNull(engine);
+    void testCheckParameters() {
+        ScriptEngine engine = manager.getEngineByName("groovy")
+        assertNotNull(engine)
         
-        ScriptEngineFactory factory = engine.getFactory();
-        assertNotNull(factory);
+        ScriptEngineFactory factory = engine.getFactory()
+        assertNotNull(factory)
 
-        boolean gotname = false;
+        boolean gotname = false
 
-        Iterator names = factory.getNames().iterator();
+        Iterator names = factory.getNames().iterator()
         while (names.hasNext()) {
-            if (names.next().equals("groovy")) gotname = true;
+            if (names.next().equals("groovy")) gotname = true
         }
-        assertTrue("Short name missing from factory", gotname);
+        assertTrue("Short name missing from factory", gotname)
 
-        assertEquals("Groovy", factory.getLanguageName());
-        assertNotNull(factory.getEngineVersion());
-        assertNotNull(factory.getExtensions());
-        assertNotNull(factory.getMimeTypes());
+        assertEquals("Groovy", factory.getLanguageName())
+        assertNotNull(factory.getEngineVersion())
+        assertNotNull(factory.getExtensions())
+        assertNotNull(factory.getMimeTypes())
 
-        assertEquals(InvokerHelper.getVersion(), factory.getLanguageVersion());
+        assertEquals(InvokerHelper.getVersion(), factory.getLanguageVersion())
     }
 
-    public void testSimpleExpr() throws ScriptException {
-        ScriptEngine engine = manager.getEngineByName("groovy");
-        assertNotNull(engine);
+    void testSimpleExpr() throws ScriptException {
+        ScriptEngine engine = manager.getEngineByName("groovy")
+        assertNotNull(engine)
 
-        assertEquals(Integer.valueOf(3), engine.eval("1 + 2"));
+        assertEquals(Integer.valueOf(3), engine.eval("1 + 2"))
     }
 
-    public void testSyntaxError() {
-        ScriptEngine engine = manager.getEngineByName("groovy");
-        assertNotNull(engine);
+    void testSyntaxError() {
+        ScriptEngine engine = manager.getEngineByName("groovy")
+        assertNotNull(engine)
 
         try {
-            Object x =  engine.eval("z");
-            assertFalse("Didn't get ScriptException for syntax error", true);
+            Object x =  engine.eval("z")
+            assertFalse("Didn't get ScriptException for syntax error", true)
         } catch (ScriptException e) {
         }
     }
 
-    public void testEmptyScriptWithJustImports() {
-        ScriptEngine engine = manager.getEngineByName("groovy");
-        assertNotNull(engine);
+    void testEmptyScriptWithJustImports() {
+        ScriptEngine engine = manager.getEngineByName("groovy")
+        assertNotNull(engine)
 
         // GROOVY-3711: The eval of the following script earlier resulted in NPE
         // as groovy did not generate a script class in this case
-        engine.eval("import java.lang.*");
+        engine.eval("import java.lang.*")
+    }
+
+    /**
+     * Fix for GROOVY-3669:
+     * Can't use several times the same JSR-223 ScriptContext for different groovy script 
+     */
+    void testGroovy3669() {
+        def scriptContext = new SimpleScriptContext()
+
+        def sem = new ScriptEngineManager()
+
+        def engine1 = sem.getEngineByName("groovy")
+        def sw1 = new StringWriter()
+        scriptContext.writer = sw1
+        engine1.eval("print 'one'", scriptContext)
+
+        assert sw1.toString() == 'one', "'one' should have been printed"
+
+        def engine2 = sem.getEngineByName("groovy")
+        def sw2 = new StringWriter()
+        scriptContext.writer = sw2
+        engine2.eval("print 'two'", scriptContext)
+
+        assert sw2.toString() == 'two', "'two' should have been printed instead of '${sw2.toString()}'"
+        assert sw1.toString() == 'one', "The output shouldn't have changed and still be 'one' instead of '${sw1.toString()}'"
+
+        assert !scriptContext.getAttribute("out"), "Groovy's 'out' variable should not be left in the script context"
+        assert !scriptContext.getAttribute("context"), "The 'context' should not be left in the script context"
     }
 }
