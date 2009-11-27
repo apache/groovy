e47d35fd6de757f828bd84268a32409763449611
Jochen Theodorou <blackdrag@gmx.org>
2009-11-27 08:46:04 +0000

    ensure no NPE can happen while getting the release information
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@18421 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/util/ReleaseInfo.java
+++ src/main/org/codehaus/groovy/util/ReleaseInfo.java
@@ -39,20 +39,26 @@ public class ReleaseInfo {
         if(!loaded) {
             URL url = null;
             ClassLoader cl = ReleaseInfo.class.getClassLoader();
-            if(cl instanceof URLClassLoader) {
+            // we need no security check for getting the system class
+            // loader since if we do the invoker has a null loader,
+            // in which case no security check will be done
+            if (cl==null) cl = ClassLoader.getSystemClassLoader();
+            if (cl instanceof URLClassLoader) {
                 // this avoids going through the parent classloaders/bootstarp
                 url = ((URLClassLoader) cl).findResource(RELEASE_INFO_FILE);
             } else {
                 // fallback option as ClassLoader#findResource() is protected
                 url = cl.getResource(RELEASE_INFO_FILE);
             }
-            try {
-                InputStream is = url.openStream();
-                if(is != null) {
-                    releaseInfo.load(is);
+            if (url!=null) {
+                try {
+                    InputStream is = url.openStream();
+                    if(is != null) {
+                        releaseInfo.load(is);
+                    }
+                } catch(IOException ioex) {
+                    // ignore. In case of some exception, release info is not available
                 }
-            } catch(IOException ioex) {
-                // ignore. In case of some exception, release info is not available
             }
             loaded = true;
         }
