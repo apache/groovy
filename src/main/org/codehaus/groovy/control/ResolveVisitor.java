4a57361acb9e394e7237ee1cf2e7f83cc6ed858b
Roshan Dawrani <roshandawrani@gmail.com>
2009-12-28 18:00:46 +0000

    GROOVY-3953 Static * import on a module import class was failing. Fixed it.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@18742 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/control/ResolveVisitor.java
+++ src/main/org/codehaus/groovy/control/ResolveVisitor.java
@@ -1131,7 +1131,7 @@ public class ResolveVisitor extends ClassCodeExpressionTransformer {
             }
             for (ImportNode importNode : module.getStaticStarImports().values()) {
                 ClassNode type = importNode.getType();
-                if (resolve(type, false, false, true)) continue;
+                if (resolve(type, true, false, true)) continue;
                 // May be this type belongs in the same package as the node that is doing the
                 // static import. In that case, the package may not have been explicitly specified.
                 // Try with the node's package too. If still not found, revert to original type name.
