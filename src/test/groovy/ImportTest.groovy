aa230671f7fea5f144340f8c5898587efeb560ba
Roshan Dawrani <roshandawrani@gmail.com>
2009-12-31 09:09:55 +0000

    GROOVY-3953 Made a change that makes non-static imports consistent with static imports - in that they also now support resolving an import using an earlier import.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@18830 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/ImportTest.groovy
+++ src/test/groovy/ImportTest.groovy
@@ -1,4 +1,6 @@
 package groovy
+import org.codehaus.groovy.runtime.metaclass.MemoryAwareConcurrentReadMap
+import MemoryAwareConcurrentReadMap.Entry
 
 class ImportTest extends GroovyTestCase {
 
@@ -21,5 +23,9 @@ class ImportTest extends GroovyTestCase {
         def y = [1, 2, 3]
         assert y instanceof List
         assert y.getClass().name.startsWith("java.util.")
+    }
+    
+    void testImportResolutionThroughAnEarlierImport() {
+        assert MemoryAwareConcurrentReadMap.Entry.class != null        
     }
 }
