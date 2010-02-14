3da7b28b83b90a0b8f5eabfa40b04666389ed742
Paul King <paulk@asert.com.au>
2010-02-14 22:09:34 +0000

    GROOVY-1746: Add default to Map
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@19301 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/main/groovy/lang/MapWithDefault.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.lang;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A wrapper for Map which allows a default value to be specified.
+ *
+ * @author Paul King
+ */
+public class MapWithDefault<K, V> implements Map<K, V> {
+
+    private Map<K, V> delegate;
+    private Closure initClosure;
+
+    private MapWithDefault(Map<K, V> m, Closure initClosure) {
+        delegate = m;
+        this.initClosure = initClosure;
+    }
+
+    public static <K, V> Map<K, V> newInstance(Map<K, V> m, Closure initClosure) {
+        return new MapWithDefault<K, V>(m, initClosure);
+    }
+
+    public int size() {
+        return delegate.size();
+    }
+
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    public boolean containsKey(Object key) {
+        return delegate.containsKey(key);
+    }
+
+    public boolean containsValue(Object value) {
+        return delegate.containsValue(value);
+    }
+
+    public V get(Object key) {
+        if (!delegate.containsKey(key)) {
+            delegate.put((K)key, (V)initClosure.call(new Object[]{key}));
+        }
+        return delegate.get(key);
+    }
+
+    public V put(K key, V value) {
+        return delegate.put(key, value);
+    }
+
+    public V remove(Object key) {
+        return delegate.remove(key);
+    }
+
+    public void putAll(Map<? extends K, ? extends V> m) {
+        delegate.putAll(m);
+    }
+
+    public void clear() {
+        delegate.clear();
+    }
+
+    public Set<K> keySet() {
+        return delegate.keySet();
+    }
+
+    public Collection<V> values() {
+        return delegate.values();
+    }
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        return delegate.entrySet();
+    }
+}
