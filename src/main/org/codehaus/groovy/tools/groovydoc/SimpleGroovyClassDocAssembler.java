63fda14996af2a211cc92da8d30a5e12a68cebf3
Paul King <paulk@asert.com.au>
2010-02-16 06:49:05 +0000

    merge changeset 19308 from trunk (GROOVY-3748: problem with shown methods for GroovyShell)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@19309 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/tools/groovydoc/SimpleGroovyClassDocAssembler.java
+++ src/main/org/codehaus/groovy/tools/groovydoc/SimpleGroovyClassDocAssembler.java
@@ -272,8 +272,7 @@ public class SimpleGroovyClassDocAssembler extends VisitorAdapter implements Gro
     }
 
     private boolean isNested() {
-        GroovySourceAST gpn = getGrandParentNode();
-        return getParentNode() != null && gpn != null && (gpn.getType() == CLASS_DEF || gpn.getType() == INTERFACE_DEF);
+        return getCurrentClassDoc() != null;
     }
 
     private boolean isTopLevelConstruct(GroovySourceAST node) {
@@ -617,11 +616,13 @@ public class SimpleGroovyClassDocAssembler extends VisitorAdapter implements Gro
     }
 
     private SimpleGroovyClassDoc getCurrentClassDoc() {
-        GroovySourceAST pn = getParentNode();
-        if (isTopLevelConstruct(pn)) return foundClasses.get(getIdentFor(pn));
-        GroovySourceAST gpn = getGrandParentNode();
-        if (isTopLevelConstruct(gpn)) return foundClasses.get(getIdentFor(gpn));
-        return null;
+        if (stack.isEmpty()) return null;
+        GroovySourceAST node = getParentNode();
+        if (isTopLevelConstruct(node)) return foundClasses.get(getIdentFor(node));
+        GroovySourceAST saved = stack.pop();
+        SimpleGroovyClassDoc result = getCurrentClassDoc();
+        stack.push(saved);
+        return result;
     }
 
     private String getIdentFor(GroovySourceAST gpn) {
