cb67d81a473e1540bd1991578e123e6acad55deb
Paul King <paulk@asert.com.au>
2010-02-18 15:27:38 +0000

    refactor ExecuteTest to be more robust
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@19384 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/execute/ExecuteTest.groovy
+++ src/test/groovy/execute/ExecuteTest.groovy
@@ -88,6 +88,8 @@ class ExecuteTest extends GroovyTestCase {
         def value = process.exitValue()
         int count = sbout.toString().readLines().size()
         println "Heaps of time case: Exit value: $value, Err lines: ${sberr.toString().readLines().size()}, Out lines: $count"
+        System.err.println 'std err: ' + sberr.toString()
+        System.err.println 'std out: ' + sbout.toString()
         assert sbout.toString().contains('Done'), "Expected 'Done' but found: " + sbout.toString() + " with error: " + sberr.toString()
         assert value == 0
 
