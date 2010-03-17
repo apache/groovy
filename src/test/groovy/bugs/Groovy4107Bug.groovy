1dafad4016852ebad8b75d9ed5ab74096b7ca2f1
Roshan Dawrani <roshandawrani@gmail.com>
2010-03-17 17:31:33 +0000

    GROOVY-4107 ResolveVisitor was resolving an unqualified import against itself by mistake. Fixed it.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@19623 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4107Bug.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+class Groovy4107Bug extends GroovyTestCase {
+    void testUnqualifiedImportResolveBug() {
+        try {
+            new GroovyShell().parse """
+                import Roshan4107
+                
+                Roshan4107.foo()
+            """
+            fail('The compilation should have failed as the class Roshan4107 is non-existent')
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains('unable to resolve class Roshan4107')
+        }
+    }
+}
