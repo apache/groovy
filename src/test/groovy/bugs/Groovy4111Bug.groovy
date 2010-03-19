2b5911a6480638de9e769dfb00f452073e77bda6
Roshan Dawrani <roshandawrani@gmail.com>
2010-03-19 13:39:03 +0000

    GROOVY-4111 Constructor calls on abstract types are now rejected earlier (ResolveVisitor) than before (ClassCompletionVerifier).
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@19632 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4111Bug.groovy
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+class Groovy4111Bug extends GroovyTestCase {
+    void testConstructorCallOnAnAbstractType() {
+        try {
+            new GroovyShell().parse """
+                class Test4111 {
+                    static main(args) {
+                        def obj = new I4111()
+                    }
+                }
+                
+                interface I4111 {}
+            """
+            fail('The compilation should have failed as the constructor call is on an interface')
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains('You cannot create an instance from the abstract interface')
+        }
+    }
+}
