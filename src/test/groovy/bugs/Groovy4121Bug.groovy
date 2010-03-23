ceec83a73b60e9cd91dfe38f48b37e6ccba806e1
Roshan Dawrani <roshandawrani@gmail.com>
2010-03-23 07:41:24 +0000

    GROOVY-4121 Pushed the checkFinalFieldAccess a bit - from VariableScopeVisitor (resolve) to ClassCompletionVerifier (classgen) - so that it now covers the fields made final by @Immutable.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@19659 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4121Bug.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+class Groovy4121Bug extends GroovyTestCase {
+    void testAssignmentToAFieldMadeFinalByImmutable() {
+        try {
+            new GroovyShell().parse """
+                @Immutable
+                class Account4121 {
+                    BigDecimal balance
+                    String customer
+                    
+                    Account4121 deposit(amount) {
+                        balance = balance + amount
+                        this
+                    }
+                }
+                
+                def acc = new Account4121(0.0, "Test")
+                acc.deposit(3.1)
+                assert 3.1 == acc.balance
+            """
+            fail('The compilation should have failed as a final field is being assigned to.')
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("cannot modify final field 'balance' outside of constructor")
+        }
+    }
+}
