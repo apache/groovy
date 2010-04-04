d75f7d80a1471b1b46391cef049ec12052a5128f
Paul King <paulk@asert.com.au>
2010-04-04 01:49:42 +0000

    merge changeset 19751 from trunk (GROOVY-4146: Groovy's Sql classes could support batch operations by leveraging the java.sql.Statement.executeBatch() command.)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@19752 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/sql/BatchingStatementWrapper.java
+++ src/main/groovy/sql/BatchingStatementWrapper.java
@@ -1,5 +1,8 @@
 package groovy.sql;
 
+import groovy.lang.GroovyObjectSupport;
+import org.codehaus.groovy.runtime.InvokerHelper;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,7 +18,7 @@ import java.util.logging.Logger;
  * this Statement does an executeBatch() automatically. If batchSize is
  * zero, then no batching is performed.
  */
-public class BatchingStatementWrapper implements Statement {
+public class BatchingStatementWrapper extends GroovyObjectSupport {
     private Statement delegate;
     private int batchSize;
     private int batchCount;
@@ -32,6 +35,11 @@ public class BatchingStatementWrapper implements Statement {
         results = new ArrayList<Integer>();
     }
 
+    @Override
+    public Object invokeMethod(String name, Object args) {
+        return InvokerHelper.invokeMethod(delegate, name, args);
+    }
+
     public void addBatch(String sql) throws SQLException {
         delegate.addBatch(sql);
         batchCount++;
@@ -45,110 +53,6 @@ public class BatchingStatementWrapper implements Statement {
         }
     }
 
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-        return delegate.unwrap(iface);
-    }
-
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        return delegate.isWrapperFor(iface);
-    }
-
-    public ResultSet executeQuery(String sql) throws SQLException {
-        return delegate.executeQuery(sql);
-    }
-
-    public int executeUpdate(String sql) throws SQLException {
-        return delegate.executeUpdate(sql);
-    }
-
-    public void close() throws SQLException {
-        delegate.close();
-    }
-
-    public int getMaxFieldSize() throws SQLException {
-        return delegate.getMaxFieldSize();
-    }
-
-    public void setMaxFieldSize(int max) throws SQLException {
-        delegate.setMaxFieldSize(max);
-    }
-
-    public int getMaxRows() throws SQLException {
-        return delegate.getMaxRows();
-    }
-
-    public void setMaxRows(int max) throws SQLException {
-        delegate.setMaxRows(max);
-    }
-
-    public void setEscapeProcessing(boolean enable) throws SQLException {
-        delegate.setEscapeProcessing(enable);
-    }
-
-    public int getQueryTimeout() throws SQLException {
-        return delegate.getQueryTimeout();
-    }
-
-    public void setQueryTimeout(int seconds) throws SQLException {
-        delegate.setQueryTimeout(seconds);
-    }
-
-    public void cancel() throws SQLException {
-        delegate.cancel();
-    }
-
-    public SQLWarning getWarnings() throws SQLException {
-        return delegate.getWarnings();
-    }
-
-    public void clearWarnings() throws SQLException {
-        delegate.clearWarnings();
-    }
-
-    public void setCursorName(String name) throws SQLException {
-        delegate.setCursorName(name);
-    }
-
-    public boolean execute(String sql) throws SQLException {
-        return delegate.execute(sql);
-    }
-
-    public ResultSet getResultSet() throws SQLException {
-        return delegate.getResultSet();
-    }
-
-    public int getUpdateCount() throws SQLException {
-        return delegate.getUpdateCount();
-    }
-
-    public boolean getMoreResults() throws SQLException {
-        return delegate.getMoreResults();
-    }
-
-    public void setFetchDirection(int direction) throws SQLException {
-        delegate.setFetchDirection(direction);
-    }
-
-    public int getFetchDirection() throws SQLException {
-        return delegate.getFetchDirection();
-    }
-
-    public void setFetchSize(int rows) throws SQLException {
-        delegate.setFetchSize(rows);
-    }
-
-    public int getFetchSize() throws SQLException {
-        return delegate.getFetchSize();
-    }
-
-    public int getResultSetConcurrency() throws SQLException {
-        return delegate.getResultSetConcurrency();
-    }
-
-    public int getResultSetType() throws SQLException {
-        return delegate.getResultSetType();
-    }
-
     public void clearBatch() throws SQLException {
         if (batchSize != 0) {
             results = new ArrayList<Integer>();
@@ -158,7 +62,9 @@ public class BatchingStatementWrapper implements Statement {
 
     public int[] executeBatch() throws SQLException {
         if (batchSize == 0) {
-            return delegate.executeBatch();
+            int[] result = delegate.executeBatch();
+            log.fine("Successfully executed batch with " + result.length + " command(s)");
+            return result;
         } else {
             int[] lastResult = delegate.executeBatch();
             for (int i : lastResult) {
@@ -174,55 +80,7 @@ public class BatchingStatementWrapper implements Statement {
         }
     }
 
-    public Connection getConnection() throws SQLException {
-        return delegate.getConnection();
-    }
-
-    public boolean getMoreResults(int current) throws SQLException {
-        return delegate.getMoreResults(current);
-    }
-
-    public ResultSet getGeneratedKeys() throws SQLException {
-        return delegate.getGeneratedKeys();
-    }
-
-    public int executeUpdate(String sql, int autoGeneratedKeys) throws SQLException {
-        return delegate.executeUpdate(sql, autoGeneratedKeys);
-    }
-
-    public int executeUpdate(String sql, int[] columnIndexes) throws SQLException {
-        return delegate.executeUpdate(sql, columnIndexes);
-    }
-
-    public int executeUpdate(String sql, String[] columnNames) throws SQLException {
-        return delegate.executeUpdate(sql, columnNames);
-    }
-
-    public boolean execute(String sql, int autoGeneratedKeys) throws SQLException {
-        return delegate.execute(sql, autoGeneratedKeys);
-    }
-
-    public boolean execute(String sql, int[] columnIndexes) throws SQLException {
-        return delegate.execute(sql, columnIndexes);
-    }
-
-    public boolean execute(String sql, String[] columnNames) throws SQLException {
-        return delegate.execute(sql, columnNames);
-    }
-
-    public int getResultSetHoldability() throws SQLException {
-        return delegate.getResultSetHoldability();
-    }
-
-    public boolean isClosed() throws SQLException {
-        return delegate.isClosed();
-    }
-
-    public void setPoolable(boolean poolable) throws SQLException {
-        delegate.setPoolable(poolable);
-    }
-
-    public boolean isPoolable() throws SQLException {
-        return delegate.isPoolable();
+    public void close() throws SQLException {
+        delegate.close();
     }
 }
