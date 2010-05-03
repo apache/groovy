207483f50b0d6d304b3e92ac97575d5af694b926
Paul King <paulk@asert.com.au>
2010-05-03 04:26:10 +0000

    merge changeset 19923 from trunk (GROOVY-4186: Possible syntactic sugar for JSR-223)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@19924 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/jsr223/vm6/JavascriptTest.groovy
+++ src/test/org/codehaus/groovy/jsr223/vm6/JavascriptTest.groovy
@@ -7,9 +7,13 @@ class JavascriptTest extends GroovyTestCase {
         def binding = new Binding()
         binding.x = 10
         binding.y = 5
-        def js = ScriptEngineManager.javascript.&eval.rcurry(binding)
-        assert js('2 * x + y') == 25
-        js 'z = x + y'
-        assert binding.z == 15
+        def js = ScriptEngineManager.javascript
+        if (!js) System.err.println("Warning: JavaScript not available on this JVM - test ignored")
+        else {
+            def eval = js.&eval.rcurry(binding)
+            assert eval('2 * x + y') == 25
+            eval 'z = x + y'
+            assert binding.z == 15
+        }
     }
 }
