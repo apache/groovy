717de4346f9c296aae018b0e815eacc83cde03a5
Roshan Dawrani <roshandawrani@gmail.com>
2010-05-06 03:35:22 +0000

    GROOVY-4188 If the class defines field "metaClass" of the wrong type, it results in a compilation error now, instead of NoSuchFieldError later at runtime.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@19950 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4188Bug.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+class Groovy4188Bug extends GroovyTestCase {
+    void testClassProvidingMetaClassFieldOfWrongType() {
+        try {
+            new GroovyShell().parse """
+				class Foo4188 {
+				  private String metaClass
+				}
+				  
+				new Foo4188()
+            """
+            fail("The compilation should have failed as the field 'metaClass' is not of type groovy.lang.MetaClass.")
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("it needs to be of the type groovy.lang.MetaClass for internal groovy purposes")
+        }
+    }
+}
