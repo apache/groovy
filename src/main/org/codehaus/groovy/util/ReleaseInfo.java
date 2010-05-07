99ba3fc01e637eb899aaaf1b441f2b34bd63dd95
Roshan Dawrani <roshandawrani@gmail.com>
2010-05-07 14:15:19 +0000

    GROOVY-4192 Class ReleaseInfo's thread-safety improved.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@19964 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/util/ReleaseInfo.java
+++ src/main/org/codehaus/groovy/util/ReleaseInfo.java
@@ -27,55 +27,49 @@ import java.util.Properties;
  * @author Roshan Dawrani
  */
 public class ReleaseInfo {
-    private static Properties releaseInfo = new Properties();
-    private static String RELEASE_INFO_FILE = "META-INF/groovy-release-info.properties";
-    private static String KEY_IMPLEMENTATION_VERSION = "ImplementationVersion";
-    private static String KEY_BUNDLE_VERSION = "BundleVersion";
-    private static String KEY_BUILD_DATE = "BuildDate";
-    private static String KEY_BUILD_TIME = "BuildTime";
-    private static boolean loaded = false;
+    private static final Properties releaseInfo = new Properties();
+    private static final String RELEASE_INFO_FILE = "META-INF/groovy-release-info.properties";
+    private static final String KEY_IMPLEMENTATION_VERSION = "ImplementationVersion";
+    private static final String KEY_BUNDLE_VERSION = "BundleVersion";
+    private static final String KEY_BUILD_DATE = "BuildDate";
+    private static final String KEY_BUILD_TIME = "BuildTime";
 
+    static {
+        URL url = null;
+        ClassLoader cl = ReleaseInfo.class.getClassLoader();
+        // we need no security check for getting the system class
+        // loader since if we do the invoker has a null loader,
+        // in which case no security check will be done
+        if (cl == null) cl = ClassLoader.getSystemClassLoader();
+        if (cl instanceof URLClassLoader) {
+            // this avoids going through the parent classloaders/bootstarp
+            url = ((URLClassLoader) cl).findResource(RELEASE_INFO_FILE);
+        } else {
+            // fallback option as ClassLoader#findResource() is protected
+            url = cl.getResource(RELEASE_INFO_FILE);
+        }
+        if (url != null) {
+            try {
+                InputStream is = url.openStream();
+                if(is != null) {
+                    releaseInfo.load(is);
+                }
+            } catch(IOException ioex) {
+                // ignore. In case of some exception, release info is not available
+            }
+        }
+    }
+    
     public static String getVersion() {
         return get(KEY_IMPLEMENTATION_VERSION);
     }
     
     public static Properties getAllProperties() {
-        loadInfo();
         return releaseInfo; 
     }
     
     private static String get(String propName) {
-        loadInfo();
         String propValue = releaseInfo.getProperty(propName);
         return (propValue == null ? "" : propValue);
-    }
-    
-    private static void loadInfo() {
-        if(!loaded) {
-            URL url = null;
-            ClassLoader cl = ReleaseInfo.class.getClassLoader();
-            // we need no security check for getting the system class
-            // loader since if we do the invoker has a null loader,
-            // in which case no security check will be done
-            if (cl==null) cl = ClassLoader.getSystemClassLoader();
-            if (cl instanceof URLClassLoader) {
-                // this avoids going through the parent classloaders/bootstarp
-                url = ((URLClassLoader) cl).findResource(RELEASE_INFO_FILE);
-            } else {
-                // fallback option as ClassLoader#findResource() is protected
-                url = cl.getResource(RELEASE_INFO_FILE);
-            }
-            if (url!=null) {
-                try {
-                    InputStream is = url.openStream();
-                    if(is != null) {
-                        releaseInfo.load(is);
-                    }
-                } catch(IOException ioex) {
-                    // ignore. In case of some exception, release info is not available
-                }
-            }
-            loaded = true;
-        }
     }
 }
