56d33505505c45c13034fa350283216753afc535
Paul King <paulk@asert.com.au>
2010-05-08 11:51:20 +0000

    merge changesets 19980 from trunk (GROOVY-1525: groovy -help doesn't mention -cp option)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@19981 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/tools/shell/Main.groovy
+++ src/main/org/codehaus/groovy/tools/shell/Main.groovy
@@ -50,6 +50,8 @@ class Main
 
         def cli = new CliBuilder(usage : 'groovysh [options] [...]', formatter: new HelpFormatter(), writer: io.out)
 
+        cli.classpath(messages['cli.option.classpath.description'])
+        cli.cp(longOpt: 'classpath', messages['cli.option.cp.description'])
         cli.h(longOpt: 'help', messages['cli.option.help.description'])
         cli.V(longOpt: 'version', messages['cli.option.version.description'])
         cli.v(longOpt: 'verbose', messages['cli.option.verbose.description'])
