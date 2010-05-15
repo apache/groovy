e439820a66aacec5df8e1e3d1b13142ddcb919e5
Paul King <paulk@asert.com.au>
2010-05-15 07:45:26 +0000

    merge changeset 20033 from trunk (GROOVY-4204: BigDecimal division is currently implemented using pre Java 1.5 code)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20034 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/typehandling/BigDecimalMath.java
+++ src/main/org/codehaus/groovy/runtime/typehandling/BigDecimalMath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 the original author or authors.
+ * Copyright 2003-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,70 +17,62 @@
 package org.codehaus.groovy.runtime.typehandling;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 /**
  * BigDecimal NumberMath operations
- * 
+ *
  * @author Steve Goetze
  */
 public final class BigDecimalMath extends NumberMath {
 
-	//This is an arbitrary value, picked as a reasonable choice for a rounding point
-	//for typical user math.
-	public static final int MAX_DIVISION_SCALE = 10;
-	
-	public static final BigDecimalMath INSTANCE = new BigDecimalMath();
-	
-	private BigDecimalMath() {}
+    // This is an arbitrary value, picked as a reasonable choice for a precision
+    // for typical user math when a non-terminating result would otherwise occur.
+    public static final int ADDITIONAL_PRECISION = 10;
 
-	protected Number absImpl(Number number) {
-		return toBigDecimal(number).abs();
-	}
-	
-	public Number addImpl(Number left, Number right) {
-		return toBigDecimal(left).add(toBigDecimal(right));
-	}
+    //This is an arbitrary value, picked as a reasonable choice for a rounding point
+    //for typical user math.
+    public static final int MAX_DIVISION_SCALE = 10;
 
-	public Number subtractImpl(Number left, Number right) {
-		return toBigDecimal(left).subtract(toBigDecimal(right));
-	}
+    public static final BigDecimalMath INSTANCE = new BigDecimalMath();
 
-	public Number multiplyImpl(Number left, Number right) {
-		return toBigDecimal(left).multiply(toBigDecimal(right));
-	}
+    private BigDecimalMath() {
+    }
 
-	public Number divideImpl(Number left, Number right) {
-		//Hack until Java 1.5 BigDecimal is available.  For now, pick
-		//a result scale which is the maximum of the scale of the
-		//two operands and an arbitrary maximum (similar to what a
-		//handheld calculator would do).  Then, normalize the result
-		//by removing any trailing zeros.
-		BigDecimal bigLeft = toBigDecimal(left);
-		BigDecimal bigRight = toBigDecimal(right);
-		int scale = Math.max(bigLeft.scale(), bigRight.scale());
-		return normalize(bigLeft.divide(bigRight, Math.max(scale, MAX_DIVISION_SCALE), BigDecimal.ROUND_HALF_UP));
-	}
-	
-	public int compareToImpl(Number left, Number right) {
-		return toBigDecimal(left).compareTo(toBigDecimal(right));
-	}
-	
-	private BigDecimal normalize(BigDecimal number) {
-        // we have to take care of the case number==0, because 0 can have every
-        // scale and the test in the while loop would never end
-        if (number.signum()==0) {
-            // the smallest scale for 0 is 0
-            return number.setScale(0);
+    protected Number absImpl(Number number) {
+        return toBigDecimal(number).abs();
+    }
+
+    public Number addImpl(Number left, Number right) {
+        return toBigDecimal(left).add(toBigDecimal(right));
+    }
+
+    public Number subtractImpl(Number left, Number right) {
+        return toBigDecimal(left).subtract(toBigDecimal(right));
+    }
+
+    public Number multiplyImpl(Number left, Number right) {
+        return toBigDecimal(left).multiply(toBigDecimal(right));
+    }
+
+    public Number divideImpl(Number left, Number right) {
+        BigDecimal bigLeft = toBigDecimal(left);
+        BigDecimal bigRight = toBigDecimal(right);
+        try {
+            return bigLeft.divide(bigRight);
+        } catch (ArithmeticException e) {
+            // set a DEFAULT precision if otherwise non-terminating
+            int precision = Math.max(bigLeft.precision(), bigRight.precision()) + ADDITIONAL_PRECISION;
+            BigDecimal result = bigLeft.divide(bigRight, new MathContext(precision));
+            int scale = Math.max(Math.max(bigLeft.scale(), bigRight.scale()), MAX_DIVISION_SCALE);
+            if (result.scale() > scale) result = result.setScale(scale, BigDecimal.ROUND_HALF_UP);
+            return result;
         }
-        // rescale until we found the smallest possible scale
-		try {
-			while (true) {
-				number = number.setScale(number.scale()-1);
-			} 
-		} catch (ArithmeticException e) {
-			return number;
-		}
-	}
+    }
+
+    public int compareToImpl(Number left, Number right) {
+        return toBigDecimal(left).compareTo(toBigDecimal(right));
+    }
 
     protected Number unaryMinusImpl(Number left) {
         return toBigDecimal(left).negate();
