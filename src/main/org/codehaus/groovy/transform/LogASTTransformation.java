d5b187daaac2e2f4a06614224bd6e4b5eec25698
Paul King <paulk@asert.com.au>
2010-05-20 22:48:42 +0000

    remove tabs
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20090 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/transform/LogASTTransformation.java
+++ src/main/org/codehaus/groovy/transform/LogASTTransformation.java
@@ -38,115 +38,116 @@ import org.objectweb.asm.Opcodes;
  */
 @GroovyASTTransformation(phase = CompilePhase.CANONICALIZATION)
 public class LogASTTransformation implements ASTTransformation {
-	
-	public void visit(ASTNode[] nodes, final SourceUnit source) {
-		if (nodes.length != 2 || !(nodes[0] instanceof AnnotationNode) || !(nodes[1] instanceof AnnotatedNode)) {
+
+    public void visit(ASTNode[] nodes, final SourceUnit source) {
+        if (nodes.length != 2 || !(nodes[0] instanceof AnnotationNode) || !(nodes[1] instanceof AnnotatedNode)) {
             addError("Internal error: expecting [AnnotationNode, AnnotatedNode] but got: " + Arrays.asList(nodes), nodes[0], source);
         }
 
-		AnnotatedNode targetClass = (AnnotatedNode) nodes[1];
-		AnnotationNode logAnnotation = (AnnotationNode) nodes[0];
+        AnnotatedNode targetClass = (AnnotatedNode) nodes[1];
+//        AnnotationNode logAnnotation = (AnnotationNode) nodes[0];
 
-		if (!(targetClass instanceof ClassNode)) throw new GroovyBugError("Class annotation @Log annotated no Class, this must not happen.");
+        if (!(targetClass instanceof ClassNode))
+            throw new GroovyBugError("Class annotation @Log annotated no Class, this must not happen.");
 
-		final ClassNode classNode = (ClassNode) targetClass;
+        final ClassNode classNode = (ClassNode) targetClass;
 
-		ClassCodeExpressionTransformer transformer = new ClassCodeExpressionTransformer() {
-			private FieldNode logNode;
+        ClassCodeExpressionTransformer transformer = new ClassCodeExpressionTransformer() {
+            private FieldNode logNode;
 
-			@Override
-			protected SourceUnit getSourceUnit() {
-				return source;
-			}
+            @Override
+            protected SourceUnit getSourceUnit() {
+                return source;
+            }
 
-			public Expression transform(Expression exp) {
-				if (exp==null) return null;
-				if (exp instanceof MethodCallExpression) {
-					return transformMethodCallExpression(exp);
-				}
-				return super.transform(exp);
-			}
+            public Expression transform(Expression exp) {
+                if (exp == null) return null;
+                if (exp instanceof MethodCallExpression) {
+                    return transformMethodCallExpression(exp);
+                }
+                return super.transform(exp);
+            }
 
-			@Override
-			public void visitClass(ClassNode node) {
-				FieldNode logField = node.getField("log");
-				if (logField != null) {
-					addError("Class annotated with Log annotation cannot have log field declared",
-							logField);
-				} else {
-					ClassNode loggerClassNode = new ClassNode(Logger.class);
-					logNode = node.addField("log",
-							Opcodes.ACC_FINAL | Opcodes.ACC_TRANSIENT | Opcodes.ACC_STATIC | Opcodes.ACC_PRIVATE,
-							loggerClassNode,
-							new MethodCallExpression(
-									new ClassExpression(loggerClassNode),
-									"getLogger",
-									new ConstantExpression(node.getName())));
-				}
-				super.visitClass(node);
-			}
+            @Override
+            public void visitClass(ClassNode node) {
+                FieldNode logField = node.getField("log");
+                if (logField != null) {
+                    addError("Class annotated with Log annotation cannot have log field declared",
+                            logField);
+                } else {
+                    ClassNode loggerClassNode = new ClassNode(Logger.class);
+                    logNode = node.addField("log",
+                            Opcodes.ACC_FINAL | Opcodes.ACC_TRANSIENT | Opcodes.ACC_STATIC | Opcodes.ACC_PRIVATE,
+                            loggerClassNode,
+                            new MethodCallExpression(
+                                    new ClassExpression(loggerClassNode),
+                                    "getLogger",
+                                    new ConstantExpression(node.getName())));
+                }
+                super.visitClass(node);
+            }
 
-			private Expression transformMethodCallExpression(Expression exp) {
-				MethodCallExpression mce = (MethodCallExpression) exp;
-				if (!(mce.getObjectExpression() instanceof VariableExpression)) {
-					return exp;
-				}
-				VariableExpression variableExpression = (VariableExpression) mce.getObjectExpression();
-				if (!variableExpression.getName().equals("log") 
-						|| !(variableExpression.getAccessedVariable() instanceof DynamicVariable)) {
-					return exp;
-				}					
-				String methodName = mce.getMethodAsString();
-				if (methodName == null) return exp;
-				if (usesSimpleMethodArgumentsOnly(mce)) return exp;
+            private Expression transformMethodCallExpression(Expression exp) {
+                MethodCallExpression mce = (MethodCallExpression) exp;
+                if (!(mce.getObjectExpression() instanceof VariableExpression)) {
+                    return exp;
+                }
+                VariableExpression variableExpression = (VariableExpression) mce.getObjectExpression();
+                if (!variableExpression.getName().equals("log")
+                        || !(variableExpression.getAccessedVariable() instanceof DynamicVariable)) {
+                    return exp;
+                }
+                String methodName = mce.getMethodAsString();
+                if (methodName == null) return exp;
+                if (usesSimpleMethodArgumentsOnly(mce)) return exp;
 
-				variableExpression.setAccessedVariable(logNode);
+                variableExpression.setAccessedVariable(logNode);
 
-				ArgumentListExpression args = new ArgumentListExpression();
-				ClassNode levelClass = new ClassNode("java.util.logging.Level",0,ClassHelper.OBJECT_TYPE);
-				AttributeExpression logLevelExpression = new AttributeExpression(
-						new ClassExpression(levelClass),
-						new ConstantExpression(methodName.toUpperCase()));
-				args.addExpression(logLevelExpression);
-				MethodCallExpression condition = new MethodCallExpression(variableExpression, "isLoggable", args);
-				BooleanExpression booleanExpression = new BooleanExpression(condition);
+                ArgumentListExpression args = new ArgumentListExpression();
+                ClassNode levelClass = new ClassNode("java.util.logging.Level", 0, ClassHelper.OBJECT_TYPE);
+                AttributeExpression logLevelExpression = new AttributeExpression(
+                        new ClassExpression(levelClass),
+                        new ConstantExpression(methodName.toUpperCase()));
+                args.addExpression(logLevelExpression);
+                MethodCallExpression condition = new MethodCallExpression(variableExpression, "isLoggable", args);
+                BooleanExpression booleanExpression = new BooleanExpression(condition);
 
-				TernaryExpression isLoggableExpr = new TernaryExpression(booleanExpression,
-						exp, ConstantExpression.NULL);
+                TernaryExpression isLoggableExpr = new TernaryExpression(booleanExpression,
+                        exp, ConstantExpression.NULL);
 
-				return isLoggableExpr;
-			}
+                return isLoggableExpr;
+            }
 
-			private boolean usesSimpleMethodArgumentsOnly(MethodCallExpression mce) {
-				Expression arguments = mce.getArguments();
-				if (arguments instanceof TupleExpression) {
-					TupleExpression tuple = (TupleExpression) arguments;
-					for (Expression exp : tuple.getExpressions()) {
-						if (!isSimpleExpression(exp)) return false;
-					}
-					return true;
-				}
-				return !isSimpleExpression(arguments);
-			}
+            private boolean usesSimpleMethodArgumentsOnly(MethodCallExpression mce) {
+                Expression arguments = mce.getArguments();
+                if (arguments instanceof TupleExpression) {
+                    TupleExpression tuple = (TupleExpression) arguments;
+                    for (Expression exp : tuple.getExpressions()) {
+                        if (!isSimpleExpression(exp)) return false;
+                    }
+                    return true;
+                }
+                return !isSimpleExpression(arguments);
+            }
 
-			private boolean isSimpleExpression(Expression exp) {
-				if (exp instanceof ConstantExpression) return true;
-				if (exp instanceof VariableExpression) return true;
-				return false;					
-			}
+            private boolean isSimpleExpression(Expression exp) {
+                if (exp instanceof ConstantExpression) return true;
+                if (exp instanceof VariableExpression) return true;
+                return false;
+            }
 
-		};        	
-		transformer.visitClass(classNode);
+        };
+        transformer.visitClass(classNode);
 
-	}
+    }
 
-	public void addError(String msg, ASTNode expr, SourceUnit source){
+    public void addError(String msg, ASTNode expr, SourceUnit source) {
         int line = expr.getLineNumber();
         int col = expr.getColumnNumber();
         source.getErrorCollector().addErrorAndContinue(
                 new SyntaxErrorMessage(new SyntaxException(msg + '\n', line, col), source)
         );
-	}
-	
-	
+    }
+
+
 }
