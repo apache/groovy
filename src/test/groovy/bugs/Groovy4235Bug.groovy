b5d31a7919c6e8798c6cb35ee2b993c6be3d23c6
Roshan Dawrani <roshandawrani@gmail.com>
2010-06-03 03:35:46 +0000

    GROOVY-4235 Accessing static property from a closure returned from a static context was erroring out with ClassCastException. Fixed it.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20202 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4235Bug.groovy
@@ -0,0 +1,15 @@
+package groovy.bugs
+
+class Groovy4235Bug extends GroovyTestCase {
+    void testAccessStaticPropInsideClosure() {
+    	assertScript """
+			class Foo4235 {
+				static prop = "sadfs"
+				static foo() {
+					return { -> this.prop }
+				}
+			}
+			assert Foo4235.foo().call() == 'sadfs'
+    	"""
+    }
+}
