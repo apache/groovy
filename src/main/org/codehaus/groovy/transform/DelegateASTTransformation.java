17e078480e0f77e6787e9e17902460f6a4bdc05e
Roshan Dawrani <roshandawrani@gmail.com>
2010-06-09 16:07:12 +0000

    GROOVY-4244 Setting of non final properties didn't work through @Delegate. Fixed it.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20262 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/transform/DelegateASTTransformation.java
+++ src/main/org/codehaus/groovy/transform/DelegateASTTransformation.java
@@ -91,7 +91,7 @@ public class DelegateASTTransformation implements ASTTransformation, Opcodes {
 
     private void addSetterIfNeeded(FieldNode fieldNode, ClassNode owner, PropertyNode prop, String name) {
         String setterName = "set" + Verifier.capitalize(name);
-        if ((prop.getModifiers() & ACC_FINAL) != 0 && owner.getSetterMethod(setterName) == null) {
+        if ((prop.getModifiers() & ACC_FINAL) == 0 && owner.getSetterMethod(setterName) == null) {
             owner.addMethod(setterName,
                     ACC_PUBLIC,
                     ClassHelper.VOID_TYPE,
