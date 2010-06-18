a70bf84180b5a54dc716cea38290b636bc96c855
Paul King <paulk@asert.com.au>
2010-06-18 06:41:09 +0000

    merge changeset 20298 from trunk: package refactor for grape test
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20304 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/grape/GrabErrorIsolationTest.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.grape
+
+/**
+ * Test for GROOVY-3853. Kept with other grab tests and not normally
+ * run unless -Djunit.network=true is set on command line.
+ */
+class GrabErrorIsolationTest extends GroovyTestCase {
+
+    void testConsecutiveGrabCallsWithASharedLoaderWhereFirstGrabFails() {
+        def classLoader = new GroovyClassLoader()
+
+        grabThatShouldFail(classLoader)
+
+        // prior to fix this call failed because GrapeIvy maintained state from previous failure
+        grabThatShouldGoThrough(classLoader)
+    }
+
+    def grabThatShouldFail(classLoader) {
+        try {
+            // just an unlikely grab, that's all
+            classLoader.parseClass """
+                @Grab(group="roshan", module="dawrani", version="0.0.7")
+                class Foo3853V1 {}
+            """
+            fail('This @Grab usage should have failed')
+        } catch (ex) {
+            // fine if it failed
+            assert ex.message.contains('unresolved dependency')
+        }
+    }
+
+    def grabThatShouldGoThrough(classLoader) {
+        classLoader.parseClass """
+            @Grab(group="junit", module="junit", version="4.7")
+            class Foo3853V2 {}
+        """
+    }
+}
