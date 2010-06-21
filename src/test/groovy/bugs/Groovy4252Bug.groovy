1395e9b0153b836276b836d99ba36d3fb60d32ce
Roshan Dawrani <roshandawrani@gmail.com>
2010-06-21 11:56:23 +0000

    GROOVY-4252 Compiler crashed with a BUG! when the traditional java-like for loop had a leading '.'. Fixed it. Also removed the testcase for GROOVY-4252 as that usage of ClosureListExpression is disallowed now.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20312 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4252Bug.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+class Groovy4252Bug extends GroovyShellTestCase {
+    void testClosureListExprUsageInvalid1() {
+        try {
+            shell.parse """
+                [].bar(1;2;3)
+            """
+            fail("The compilation should have failed as expression list of form (a;b;c) is not supported in this context")
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("Expression list of the form (a; b; c) is not supported in this context")
+        }
+    }
+
+    void testClosureListExprUsageInvalid2() {
+        try {
+            shell.parse """
+                def x = (1;2;3)
+            """
+            fail("The compilation should have failed as expression list of form (a;b;c) is not supported in this context")
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("Expression list of the form (a; b; c) is not supported in this context")
+        }
+    }
+
+    void testClosureListExprUsageInvalid3() {
+        try {
+            shell.parse """
+                [].for(1;2;3){println "in loop"}
+            """
+            fail("The compilation should have failed as expression list of form (a;b;c) is not supported in this context")
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("Expression list of the form (a; b; c) is not supported in this context")
+        }
+    }
+
+    void testClosureListExprUsageInvalid4() {
+        try {
+            shell.parse """
+                class Crasher {
+                    public void m() {
+                        def fields = [1,2,3]
+                        def expectedFieldNames = ["patentnumber", "status"].
+                        for (int i=0; i<fields.size(); i++) {
+                            Object f = fields[i] 
+                            System.out.println(f); 
+                        }
+                    }
+                }
+            """
+            fail("The compilation should have failed as expression list of form (a;b;c) is not supported in this context")
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("Expression list of the form (a; b; c) is not supported in this context")
+        }
+    }
+
+    void testClosureListExprUsageValid() {
+        shell.parse """
+            for(int i = 0; i < 5; i++){println i}
+        """
+    }
+}
