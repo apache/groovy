527e4200641b169fe5473e2fd201bceae575f8fd
Paul King <paulk@asert.com.au>
2010-06-24 05:26:02 +0000

    merge changeset 20350 from trunk: GROOVY-4260 improved GCE error message
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20351 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/typehandling/GroovyCastException.java
+++ src/main/org/codehaus/groovy/runtime/typehandling/GroovyCastException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 the original author or authors.
+ * Copyright 2003-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,16 +17,29 @@ package org.codehaus.groovy.runtime.typehandling;
 
 public class GroovyCastException extends ClassCastException {
 
+    /**
+     * @param objectToCast object we tried to cast
+     * @param classToCastTo class we tried to cast to
+     * @param cause not kept but we pass on message from this Exception if any
+     */
     public GroovyCastException(Object objectToCast, Class classToCastTo, Exception cause) {
-        super(makeMessage(objectToCast,classToCastTo) + " due to: " + cause.getMessage());
+        super(makeMessage(objectToCast,classToCastTo) + " due to: " +
+                cause.getClass().getName() + (cause.getMessage() == null ? "" : ": " + cause.getMessage()));
     }
 
+    /**
+     * @param objectToCast object we tried to cast
+     * @param classToCastTo class we tried to cast to
+     */
     public GroovyCastException(Object objectToCast, Class classToCastTo) {
         super(makeMessage(objectToCast,classToCastTo));
     }
 
-    public GroovyCastException(String string) {
-        super(string);
+    /**
+     * @param message custom Exception message
+     */
+    public GroovyCastException(String message) {
+        super(message);
     }
 
     private static String makeMessage(Object objectToCast, Class classToCastTo) {
