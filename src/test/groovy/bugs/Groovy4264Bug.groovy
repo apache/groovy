85c788f7b6b3dd5a882f2c7504f270604adaa1a6
Roshan Dawrani <roshandawrani@gmail.com>
2010-07-03 19:54:58 +0000

    GROOVY-4264 Fixed the class ordering issue when a file contains multiple classes related by inheritence.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20418 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4264Bug.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+class Groovy4264Bug extends GroovyTestCase {
+    void testSubClassHavingMainEntryPoint() {
+        try {
+            assertScript """
+                class B4264 extends A4264 {
+                    static main(args){
+                        throw new RuntimeException('B4264#main() called correctly')
+                    }
+                }
+                
+                class A4264 {}
+            """
+            fail('B4264 execution should have made the script fail')
+        } catch(RuntimeException ex) {
+            assert ex.message.contains('B4264#main() called correctly')
+        }
+    }
+
+    // this scenario used to work correctly. Just added to ensure that there is no change in its behavior
+    void testScriptExecutionTakingPreferenceOverClasses() {
+        try {
+            assertScript """
+                class Y4264 extends X4264 {
+                    static main(args){
+                        throw new RuntimeException('X4264#main() called correctly')
+                    }
+                }
+                
+                class X4264 {}
+                throw new RuntimeException('Script class executed correctly')
+            """
+            fail('Script class execution should have caused a failure')
+        } catch(RuntimeException ex) {
+            assert ex.message.contains('Script class executed correctly')
+        }
+    }
+
+    // this scenario used to work correctly. Just added to ensure that there is no change in its behavior
+    void testExecutionOfJustAScript() {
+        try {
+            assertScript """
+                throw new RuntimeException('Script class executed correctly')
+            """
+            fail('Script class execution should have caused a failure')
+        } catch(RuntimeException ex) {
+            assert ex.message.contains('Script class executed correctly')
+        }
+    }
+}
