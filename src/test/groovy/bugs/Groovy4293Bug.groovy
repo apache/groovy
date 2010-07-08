df535d0a3a48f31d8782522930d00121f9cafe5b
Roshan Dawrani <roshandawrani@gmail.com>
2010-07-08 01:46:15 +0000

    GROOVY-4293 Fixed the compiler so it throws a IllegalArgumentException with a informative message instead of BUG! in case of a null scrip passed to it for compilation.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20429 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4293Bug.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+class Groovy4293Bug extends GroovyTestCase {
+    void testNoBUGMessageForNullScriptPassedToCompiler() {
+        try {
+            assertScript """
+                def params = new Expando()
+                params.newValue = 23
+                def trendMap = ['worsening': (-2), 'slightly worsening':(-1), 'neutral':0 ,'slightly improving': 1,'improving':2]
+                
+                def obj = []
+                def el = [obj]
+                if (!el[1]) el[1] = "1"
+                
+                Eval.x(el[0],"x[1]="+ el[2] ? trendMap[params.newValue.toInteger()] : params.newValue.toInteger() )
+            """
+            fail('Eval call should have failed as null script is passed to it')
+        } catch(IllegalArgumentException ex) {
+            assert ex.message.contains('Script text to compile cannot be null!')
+        }
+    }
+}
