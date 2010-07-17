cd9db4bfcaa62ba6ac839a2a9d77c740758b194f
Paul King <paulk@asert.com.au>
2010-07-17 10:07:31 +0000

    refactor: change some uses of FieldExpression to Variable/PropertyExpression
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20455 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/control/StaticImportVisitor.java
+++ src/main/org/codehaus/groovy/control/StaticImportVisitor.java
@@ -143,10 +143,11 @@ public class StaticImportVisitor extends ClassCodeExpressionTransformer {
         boolean oldInLeftExpression;
         Expression right = transform(be.getRightExpression());
         be.setRightExpression(right);
+        Expression left;
         if (type == Types.EQUAL) {
             oldInLeftExpression = inLeftExpression;
             inLeftExpression = true;
-            Expression left = transform(be.getLeftExpression());
+            left = transform(be.getLeftExpression());
             inLeftExpression = oldInLeftExpression;
             if (left instanceof StaticMethodCallExpression) {
                 StaticMethodCallExpression smce = (StaticMethodCallExpression) left;
@@ -154,14 +155,9 @@ public class StaticImportVisitor extends ClassCodeExpressionTransformer {
                 result.setSourcePosition(be);
                 return result;
             }
-            if (left instanceof FieldExpression) {
-                FieldExpression fe = (FieldExpression) left;
-                fe.setSourcePosition(left);
-                be.setLeftExpression(left);
-                return be;
-            }
+        } else {
+            left = transform(be.getLeftExpression());            
         }
-        Expression left = transform(be.getLeftExpression());
         be.setLeftExpression(left);
         return be;
     }
@@ -193,10 +189,6 @@ public class StaticImportVisitor extends ClassCodeExpressionTransformer {
                 Expression constant = findConstant(type.getField(pe.getPropertyAsString()));
                 if (constant != null) return constant;
             }
-        } else if (exp instanceof FieldExpression) {
-            FieldExpression fe = (FieldExpression) exp;
-            Expression constant = findConstant(fe.getField());
-            if (constant != null) return constant;
         } else if (exp instanceof ListExpression) {
             ListExpression le = (ListExpression) exp;
             ListExpression result = new ListExpression();
@@ -314,19 +306,6 @@ public class StaticImportVisitor extends ClassCodeExpressionTransformer {
         foundArgs = null;
         foundConstant = null;
         Expression objectExpression = transform(pe.getObjectExpression());
-
-        // check for static field access in a static method in the class containing the field
-        if (objectExpression instanceof ClassExpression && currentMethod != null && currentMethod.isStatic()) {
-            ClassExpression ce = (ClassExpression) objectExpression;
-            if (ce.getType().getName().equals(currentClass.getName())) {
-                FieldNode field = currentClass.getDeclaredField(pe.getPropertyAsString());
-                if (field != null && field.isStatic()) {
-                    Expression expression = new FieldExpression(field);
-                    expression.setSourcePosition(pe);
-                    return expression;
-                }
-            }
-        }
 
         if (foundArgs != null && foundConstant != null) {
             Expression result = findStaticMethodImportFromModule(foundConstant, foundArgs);
@@ -512,9 +491,8 @@ public class StaticImportVisitor extends ClassCodeExpressionTransformer {
         if (staticImportType.isPrimaryClassNode() || staticImportType.isResolved()) {
             staticImportType.getFields(); // force init
             FieldNode field = staticImportType.getField(fieldName);
-            if (field != null && field.isStatic() && field.isEnum())
+            if (field != null && field.isStatic())
                 return new PropertyExpression(new ClassExpression(staticImportType), fieldName);
-            if (field != null && field.isStatic()) return new FieldExpression(field);
         }
         return null;
     }
