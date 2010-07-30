dc9dad3961977bf0ce4b3514325a9f3a25c2fa33
Paul King <paulk@asert.com.au>
2010-07-30 03:07:39 +0000

    formatting only
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20568 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/groovy/bugs/Groovy249_Bug.groovy
+++ src/test/groovy/bugs/Groovy249_Bug.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package groovy.bugs
 
 import groovy.xml.MarkupBuilder
@@ -9,57 +24,57 @@ import groovy.xml.MarkupBuilder
 class Groovy249_Bug extends GroovyTestCase {
 
     void testBug() {
-		def t = new Bean249()
-		t.b = "hello"
-		println t.b
-		println "test: ${t.b}"
-		
-		def xml = new MarkupBuilder()
-		def root = xml.foo {
-			bar {
-				// works
-				baz("test")
-				// fails
-				baz(t.b)
-				// fails
-				baz("${t.b}")
-			}
-		} 
-	}
-	
+        def t = new Bean249()
+        t.b = "hello"
+        println t.b
+        println "test: ${t.b}"
+        
+        def xml = new MarkupBuilder()
+        def root = xml.foo {
+            bar {
+                // works
+                baz("test")
+                // fails
+                baz(t.b)
+                // fails
+                baz("${t.b}")
+            }
+        } 
+    }
+    
 /** @todo don't know why this fails
 
     void testBugInScript() {
-    	assertScript <<<EOF
-			import groovy.xml.MarkupBuilder;
-			
-			class Bean {
-				String b
-			};
-			
-			def t = new Bean()
-			t.b = "hello"
-			println t.b
-			println "test: ${t.b}"
-			
-			def xml = new MarkupBuilder()
-			root = xml.foo {
-				bar {
-					// works
-					baz("test")
-					// fails
-					baz(t.b)
-					// fails
-					baz("${t.b}")
-				}
-			} 
+        assertScript <<<EOF
+            import groovy.xml.MarkupBuilder;
+            
+            class Bean {
+                String b
+            };
+            
+            def t = new Bean()
+            t.b = "hello"
+            println t.b
+            println "test: ${t.b}"
+            
+            def xml = new MarkupBuilder()
+            root = xml.foo {
+                bar {
+                    // works
+                    baz("test")
+                    // fails
+                    baz(t.b)
+                    // fails
+                    baz("${t.b}")
+                }
+            } 
 
-EOF    	
-	}
+EOF        
+    }
 */
    
 }
 
 class Bean249 {
-	String b
+    String b
 }
--- src/test/groovy/bugs/Groovy4006Bug.groovy
+++ src/test/groovy/bugs/Groovy4006Bug.groovy
@@ -1,131 +1,146 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package groovy.bugs
 
 class Groovy4006Bug extends GroovyTestCase {
     void testOuterThisReferenceImplicitPassingToInnerClassConstructorNoArg() {
-    	try {
-	        assertScript """
-				class MyOuterClass4006V1 {
-					def outerName = 'OC1' 
-					def foo() {
-						def ic = new MyInnerClass4006V1()
-						ic.bar()
-					}
-					class MyInnerClass4006V1 {
-						def innerName
-						MyInnerClass4006V1() {
-							this.innerName = 'IC1'
-						}
-						def bar() {
-							assert this.innerName == 'IC1'
-							assert this.outerName == 'OC1'
-							this.outerName = 'OC1New'
-							assert this.outerName == 'OC1New'
-							throw new RuntimeException('V1 - Inner class now successfully refers to implicitly passed outer this reference!')
-						}
-					}
-				}
-				def oc = new MyOuterClass4006V1()
-				oc.foo()
-	        """
-	        fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
-    	} catch (RuntimeException ex) {
-    		assert ex.message == 'V1 - Inner class now successfully refers to implicitly passed outer this reference!'
-    	}
+        try {
+            assertScript """
+                class MyOuterClass4006V1 {
+                    def outerName = 'OC1' 
+                    def foo() {
+                        def ic = new MyInnerClass4006V1()
+                        ic.bar()
+                    }
+                    class MyInnerClass4006V1 {
+                        def innerName
+                        MyInnerClass4006V1() {
+                            this.innerName = 'IC1'
+                        }
+                        def bar() {
+                            assert this.innerName == 'IC1'
+                            assert this.outerName == 'OC1'
+                            this.outerName = 'OC1New'
+                            assert this.outerName == 'OC1New'
+                            throw new RuntimeException('V1 - Inner class now successfully refers to implicitly passed outer this reference!')
+                        }
+                    }
+                }
+                def oc = new MyOuterClass4006V1()
+                oc.foo()
+            """
+            fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
+        } catch (RuntimeException ex) {
+            assert ex.message == 'V1 - Inner class now successfully refers to implicitly passed outer this reference!'
+        }
     }
 
     void testOuterThisReferenceImplicitPassingToInnerClassConstructorWithArg() {
-    	try {
-	        assertScript """
-				class MyOuterClass4006V2 {
-					def outerName = 'OC2' 
-					def foo() {
-						def ic = new MyInnerClass4006V1('IC2')
-						ic.bar()
-					}
-					class MyInnerClass4006V1 {
-						def innerName
-						MyInnerClass4006V1(innerName) {
-							this.innerName = innerName
-						}
-						def bar() {
-							assert this.innerName == 'IC2'
-							assert this.outerName == 'OC2'
-							this.outerName = 'OC2New'
-							assert this.outerName == 'OC2New'
-							throw new RuntimeException('V2 - Inner class now successfully refers to implicitly passed outer this reference!')
-						}
-					}
-				}
-				def oc = new MyOuterClass4006V2()
-				oc.foo()
-	        """
-	        fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
-    	} catch (RuntimeException ex) {
-    		assert ex.message == 'V2 - Inner class now successfully refers to implicitly passed outer this reference!'
-    	}
+        try {
+            assertScript """
+                class MyOuterClass4006V2 {
+                    def outerName = 'OC2' 
+                    def foo() {
+                        def ic = new MyInnerClass4006V1('IC2')
+                        ic.bar()
+                    }
+                    class MyInnerClass4006V1 {
+                        def innerName
+                        MyInnerClass4006V1(innerName) {
+                            this.innerName = innerName
+                        }
+                        def bar() {
+                            assert this.innerName == 'IC2'
+                            assert this.outerName == 'OC2'
+                            this.outerName = 'OC2New'
+                            assert this.outerName == 'OC2New'
+                            throw new RuntimeException('V2 - Inner class now successfully refers to implicitly passed outer this reference!')
+                        }
+                    }
+                }
+                def oc = new MyOuterClass4006V2()
+                oc.foo()
+            """
+            fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
+        } catch (RuntimeException ex) {
+            assert ex.message == 'V2 - Inner class now successfully refers to implicitly passed outer this reference!'
+        }
     }
 
     void testOuterThisReferenceImplicitPassingToInnerClassConstructorWithArgInAProp() {
-    	try {
-	        assertScript """
-				class MyOuterClass4006V3 {
-					def outerName = 'OC3' 
-					def icField = new MyInnerClass4006V3('IC3');
-					def foo() {
-						icField.bar()
-					}
-					class MyInnerClass4006V3 {
-						def innerName
-						MyInnerClass4006V3(innerName) {
-							this.innerName = innerName
-						}
-						def bar() {
-							assert this.innerName == 'IC3'
-							assert this.outerName == 'OC3'
-							this.outerName = 'OC3New'
-							assert this.outerName == 'OC3New'
-							throw new RuntimeException('V3 - Inner class now successfully refers to implicitly passed outer this reference!')
-						}
-					}
-				}
-				def oc = new MyOuterClass4006V3()
-				oc.foo()
-	        """
-	        fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
-    	} catch (RuntimeException ex) {
-    		assert ex.message == 'V3 - Inner class now successfully refers to implicitly passed outer this reference!'
-    	}
+        try {
+            assertScript """
+                class MyOuterClass4006V3 {
+                    def outerName = 'OC3' 
+                    def icField = new MyInnerClass4006V3('IC3');
+                    def foo() {
+                        icField.bar()
+                    }
+                    class MyInnerClass4006V3 {
+                        def innerName
+                        MyInnerClass4006V3(innerName) {
+                            this.innerName = innerName
+                        }
+                        def bar() {
+                            assert this.innerName == 'IC3'
+                            assert this.outerName == 'OC3'
+                            this.outerName = 'OC3New'
+                            assert this.outerName == 'OC3New'
+                            throw new RuntimeException('V3 - Inner class now successfully refers to implicitly passed outer this reference!')
+                        }
+                    }
+                }
+                def oc = new MyOuterClass4006V3()
+                oc.foo()
+            """
+            fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
+        } catch (RuntimeException ex) {
+            assert ex.message == 'V3 - Inner class now successfully refers to implicitly passed outer this reference!'
+        }
     }
 
     void testOuterThisReferenceImplicitPassingToInnerClassConstructorWithArgInAField() {
-    	try {
-	        assertScript """
-				class MyOuterClass4006V4 {
-					def outerName = 'OC4' 
-					private def icField = new MyInnerClass4006V4('IC4');
-					def foo() {
-						icField.bar()
-					}
-					class MyInnerClass4006V4 {
-						def innerName
-						MyInnerClass4006V4(innerName) {
-							this.innerName = innerName
-						}
-						def bar() {
-							assert this.innerName == 'IC4'
-							assert this.outerName == 'OC4'
-							this.outerName = 'OC4New'
-							assert this.outerName == 'OC4New'
-							throw new RuntimeException('V4 - Inner class now successfully refers to implicitly passed outer this reference!')
-						}
-					}
-				}
-				def oc = new MyOuterClass4006V4()
-				oc.foo()
-	        """
-	        fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
-    	} catch (RuntimeException ex) {
-    		assert ex.message == 'V4 - Inner class now successfully refers to implicitly passed outer this reference!'
-    	}
+        try {
+            assertScript """
+                class MyOuterClass4006V4 {
+                    def outerName = 'OC4' 
+                    private def icField = new MyInnerClass4006V4('IC4');
+                    def foo() {
+                        icField.bar()
+                    }
+                    class MyInnerClass4006V4 {
+                        def innerName
+                        MyInnerClass4006V4(innerName) {
+                            this.innerName = innerName
+                        }
+                        def bar() {
+                            assert this.innerName == 'IC4'
+                            assert this.outerName == 'OC4'
+                            this.outerName = 'OC4New'
+                            assert this.outerName == 'OC4New'
+                            throw new RuntimeException('V4 - Inner class now successfully refers to implicitly passed outer this reference!')
+                        }
+                    }
+                }
+                def oc = new MyOuterClass4006V4()
+                oc.foo()
+            """
+            fail('The script run should have failed with RuntimeException, coming from bar() of inner class')
+        } catch (RuntimeException ex) {
+            assert ex.message == 'V4 - Inner class now successfully refers to implicitly passed outer this reference!'
+        }
     }
 }
