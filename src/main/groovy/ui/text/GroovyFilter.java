761a98a51b5dafd4512a5f81843f2b9123b50ad7
Guillaume Laforge <glaforge@gmail.com>
2010-09-08 10:37:48 +0000

    GROOVY-2701: Support for syntax highlighting of dollar slashy strings $/.../$ in the Groovy swing console
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20709 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/ui/text/GroovyFilter.java
+++ src/main/groovy/ui/text/GroovyFilter.java
@@ -52,7 +52,7 @@ public class GroovyFilter extends StructuredSyntaxDocumentFilter {
             "(?ms:\"{3}(?!\\\"{1,3}).*?(?:\"{3}|\\z))|(?:\"{1}(?!\\\").*?(?:\"|\\Z))";
     public static final String SINGLE_QUOTES =
             "(?ms:'{3}(?!'{1,3}).*?(?:'{3}|\\z))|(?:'[^'].*?(?:'|\\z))";
-    public static final String SLASHY_QUOTES = "/[^/*].*?/";
+    public static final String SLASHY_QUOTES = "(?:/[^/*].*?/|(?ms:\\$/.*?(?:/\\$|\\z)))";
     public static final String DIGIT = "\\d+?[efld]?";
 
     public static final String IDENT = "[\\w\\$&&[\\D]][\\w\\$]*";
