2a2335be6d4e2c882f790b1819c97e1e81cc671f
Roshan Dawrani <roshandawrani@gmail.com>
2010-09-13 09:22:23 +0000

    GROOVY-4416 Resolved the NPEs in property/method dispatch from inner class to outer.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20736 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4416Bug.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+class Groovy4416Bug extends GroovyTestCase {
+    void testImplicitThisPassingInNonStaticInnerClassesBug() {
+        assertScript """
+            class Dummy4416 {
+               static final PROPERTY_VALUE = "property_value"
+               def foo(){"foo()"}
+               class Bean {
+                   String property1 = PROPERTY_VALUE
+                   String property2 = foo()
+               }
+               def bean = new Bean()
+            }
+            def b = new Dummy4416().bean
+            
+            assert b.property1 == "property_value"
+            assert b.property2 == "foo()"
+        """
+    }
+}
