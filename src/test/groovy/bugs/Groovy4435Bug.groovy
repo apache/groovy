ce26f444bb2d377a1762bb893cae683e36292fd2
Roshan Dawrani <roshandawrani@gmail.com>
2010-09-29 19:02:02 +0000

    GROOVY-4435 Fixed inconsistency in handling of final fields' initialization outside constructors.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20869 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4435Bug.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.bugs
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+class Groovy4435Bug extends GroovyTestCase {
+    void testAssignmentToAFinalFieldThroughPropertyAccess() {
+        try {
+            new GroovyShell().parse """
+                class Test4435 {
+                    private final String prop = ""
+                    void setProp(val) {
+                        this.prop = val
+                    }
+                }
+            """
+            fail('The compilation should have failed as a final field is being assigned to.')
+        } catch (MultipleCompilationErrorsException e) {
+            def syntaxError = e.errorCollector.getSyntaxError(0)
+            assert syntaxError.message.contains("cannot modify final field 'prop' outside of constructor")
+        }
+    }
+}
