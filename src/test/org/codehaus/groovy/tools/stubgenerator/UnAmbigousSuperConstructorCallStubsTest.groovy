d31eb88836081cfa68b119e63c54f242b6d996a3
Roshan Dawrani <roshandawrani@gmail.com>
2010-10-02 11:16:59 +0000

    GROOVY-4400 Added a test for ambiguous super constructor calls in generated stubs.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20896 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/UnAmbigousSuperConstructorCallStubsTest.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * GROOVY-4400 - call to super class Exception constructor should be rendered as 
+ * "super ((java.lang.Throwable)null);" and not as "super (null);"
+ *
+ * @author Roshan Dawrani
+ */
+class UnAmbigousSuperConstructorCallStubsTest extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+            'UserAuthException.groovy': '''
+                public class UserAuthException extends java.lang.Exception {
+                  private static String message = "User Authentication Failed";
+                
+                  public UserAuthException() {
+                    super(message);
+                  }
+                
+                  public String getError() {
+                    return message;
+                  }
+                
+                }
+            ''',
+            'Dummy.java': 'interface Dummy{}'
+        ]
+    }
+
+    void verifyStubs() {
+        assert classes['UserAuthException'] != null
+        String stubSource = stubJavaSourceFor('UserAuthException')
+        assert !stubSource.contains('super (null)')
+        println 'UnAmbigousSuperConstructorCallStubsTest - verified'
+    }
+}
