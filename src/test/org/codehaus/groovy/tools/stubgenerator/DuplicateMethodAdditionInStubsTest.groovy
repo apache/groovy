06e9a24c53dc7fd1f68e045357815ca99c025fdc
Roshan Dawrani <roshandawrani@gmail.com>
2010-10-02 06:45:30 +0000

    GROOVY-4453 Joint compilation stub generator added duplicate methods. Fixed it.
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@20891 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/DuplicateMethodAdditionInStubsTest.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ *  GROOVY-4453: property methods and methods added indirectly due parameters-with-default-values 
+ *  were resulting in duplicate methods in generated stubs.
+ *
+ * @author Roshan Dawrani
+ */
+class DuplicateMethodAdditionInStubsTest extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+            'de/me/App4453.java': '''
+                package de.me;
+                
+                import de.app.User4453;
+                
+                public class App4453 {
+                    public void make() {
+                        User4453 u = new User4453();
+                    }
+                }
+            ''',
+            'de/app/User4453.groovy': '''
+                package de.app
+                class User4453 {
+                    String name
+                
+                    public void setName(String name, String t = "") {
+                          this.name = name
+                    }
+                }
+            '''
+        ]
+    }
+
+    void verifyStubs() {
+        assert classes.size() == 2
+        assert classes['de.app.User4453'].methods['setName'].size() == 2
+        println 'DuplicateMethodAdditionInStubsTest - verified'
+    }
+}
