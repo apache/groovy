16bde640ae6f0131b269b19db545f5b2c5efbff1
Paul King <paulk@asert.com.au>
2010-10-12 19:33:15 +0000

    GROOVY-4469: partial fix covering EncodingGroovyMethods
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@20940 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/EncodingGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/EncodingGroovyMethods.java
@@ -138,40 +138,6 @@ public class EncodingGroovyMethods {
         return encodeBase64(data, false);
     }
 
-    private static final byte[] TRANSLATE_TABLE = (
-            //
-            "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
-                    //                    \t    \n                \r
-                    + "\u0042\u0042\u0041\u0041\u0042\u0042\u0041\u0042"
-                    //
-                    + "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
-                    //
-                    + "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
-                    //        sp    !     "     #     $     %     &     '
-                    + "\u0041\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
-                    //         (    )     *     +     ,     -     .     /
-                    + "\u0042\u0042\u0042\u003E\u0042\u0042\u0042\u003F"
-                    //         0    1     2     3     4     5     6     7
-                    + "\u0034\u0035\u0036\u0037\u0038\u0039\u003A\u003B"
-                    //         8    9     :     ;     <     =     >     ?
-                    + "\u003C\u003D\u0042\u0042\u0042\u0040\u0042\u0042"
-                    //         @    A     B     C     D     E     F     G
-                    + "\u0042\u0000\u0001\u0002\u0003\u0004\u0005\u0006"
-                    //         H    I   J K   L     M   N   O
-                    + "\u0007\u0008\t\n\u000B\u000C\r\u000E"
-                    //         P    Q     R     S     T     U     V    W
-                    + "\u000F\u0010\u0011\u0012\u0013\u0014\u0015\u0016"
-                    //         X    Y     Z     [     \     ]     ^    _
-                    + "\u0017\u0018\u0019\u0042\u0042\u0042\u0042\u0042"
-                    //         '    a     b     c     d     e     f     g
-                    + "\u0042\u001A\u001B\u001C\u001D\u001E\u001F\u0020"
-                    //        h   i   j     k     l     m     n     o    p
-                    + "\u0021\"\u0023\u0024\u0025\u0026\u0027\u0028"
-                    //        p     q     r     s     t     u     v     w
-                    + "\u0029\u002A\u002B\u002C\u002D\u002E\u002F\u0030"
-                    //        x     y     z
-                    + "\u0031\u0032\u0033").getBytes();
-
     /**
      * Decode the String from Base64 into a byte array.
      *
@@ -187,7 +153,7 @@ public class EncodingGroovyMethods {
 
         for (int i = 0; i != value.length(); i++) {
             final char c = value.charAt(i);
-            final int sixBit = (c < 123) ? TRANSLATE_TABLE[c] : 66;
+            final int sixBit = (c < 123) ? EncodingGroovyMethodsSupport.TRANSLATE_TABLE[c] : 66;
 
             if (sixBit < 64) {
                 if (done)
---
+++ src/main/org/codehaus/groovy/runtime/EncodingGroovyMethodsSupport.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.runtime;
+
+/**
+ * Keep this constant in a separate file as it is troublesome for Antlr to parse for doc purposes.
+ */
+public class EncodingGroovyMethodsSupport {
+    static final byte[] TRANSLATE_TABLE = (
+            "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+                    //                    \t    \n                \r
+                    + "\u0042\u0042\u0041\u0041\u0042\u0042\u0041\u0042"
+                    //
+                    + "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+                    //
+                    + "\u0042\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+                    //        sp    !     "     #     $     %     &     '
+                    + "\u0041\u0042\u0042\u0042\u0042\u0042\u0042\u0042"
+                    //         (    )     *     +     ,     -     .     /
+                    + "\u0042\u0042\u0042\u003E\u0042\u0042\u0042\u003F"
+                    //         0    1     2     3     4     5     6     7
+                    + "\u0034\u0035\u0036\u0037\u0038\u0039\u003A\u003B"
+                    //         8    9     :     ;     <     =     >     ?
+                    + "\u003C\u003D\u0042\u0042\u0042\u0040\u0042\u0042"
+                    //         @    A     B     C     D     E     F     G
+                    + "\u0042\u0000\u0001\u0002\u0003\u0004\u0005\u0006"
+                    //         H    I   J K   L     M   N   O
+                    + "\u0007\u0008\t\n\u000B\u000C\r\u000E"
+                    //         P    Q     R     S     T     U     V    W
+                    + "\u000F\u0010\u0011\u0012\u0013\u0014\u0015\u0016"
+                    //         X    Y     Z     [     \     ]     ^    _
+                    + "\u0017\u0018\u0019\u0042\u0042\u0042\u0042\u0042"
+                    //         '    a     b     c     d     e     f     g
+                    + "\u0042\u001A\u001B\u001C\u001D\u001E\u001F\u0020"
+                    //        h   i   j     k     l     m     n     o    p
+                    + "\u0021\"\u0023\u0024\u0025\u0026\u0027\u0028"
+                    //        p     q     r     s     t     u     v     w
+                    + "\u0029\u002A\u002B\u002C\u002D\u002E\u002F\u0030"
+                    //        x     y     z
+                    + "\u0031\u0032\u0033").getBytes();
+}
