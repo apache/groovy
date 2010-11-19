10816269e75dd4abd8c8615f7958c52ca38965ea
Peter Niederwieser <pniederw@gmail.com>
2010-11-19 19:54:25 +0000

    javadoc formatting and escaping
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21078 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/beans/ListenerList.groovy
+++ src/main/groovy/beans/ListenerList.groovy
@@ -11,6 +11,7 @@ import org.codehaus.groovy.transform.GroovyASTTransformationClass
  * This annotation adds Java-style listener support to a class based on the annotated Collection-property.
  * 
  * Given the following example:
+ *
  * <pre>
  * interface TestListener {
  *     void eventOccurred(TestEvent event)
@@ -27,11 +28,13 @@ import org.codehaus.groovy.transform.GroovyASTTransformationClass
  * }
  * 
  * class TestClass {
- *     @ListenerList
+ *     &#064;ListenerList
  *     List<TestListener> listeners
  * }
  * </pre>
+ *
  * It adds the following methods:
+ *
  * <pre>
  * synchronized void addTestListener(TestListener listener)
  * synchronized void removeTestListener(TestListener listener)
@@ -39,7 +42,9 @@ import org.codehaus.groovy.transform.GroovyASTTransformationClass
  * void fireEventOccurred(TestEvent event)
  * void fireEventOccurred(Object source, String message)
  * </pre>
- * The Annotation can get the following parameters:
+ *
+ * The annotation can get the following parameters:
+ *
  * listener    = type of the Listener instance.
  *               Default:
  *                 If the annotated collection has a single, concrete (not wildcarded) generic subtype this subtype is
@@ -55,20 +60,21 @@ import org.codehaus.groovy.transform.GroovyASTTransformationClass
  *                 If the second part is omitted, the first part is used for both.
  *               Default:
  *                 All abstract methods with a single parameter derived from type of event
- * name        = name as suffix for creating the add, remove and get Methods
+ * name        = name as suffix for creating the add, remove and get methods
  *               Default:
  *                 Name of the listener type
  * synchronize = Whether or not the methods created should be synchronized at the method level. 
  *               Default:
  *                 false
  * </pre>
- * You can add the same annotation to the class itself aswell, but need to specify at least listener and event.
- * Then it autocreates a private variable called <name>List (starting lowerCase) and uses this to store the listeners.
- * If compatible variable named this way, it will be used instead.
+ *
+ * <p>You can add the same annotation to the class itself as well, but need to specify at least listener and event.
+ * Then it autocreates a private variable called <tt><name>List</tt> (starting lowercase) and uses this to store the listeners.
+ * If there already exists a compatible variable named this way, it will be used instead.
  * You can add multiple annotations to the class.
  * 
- * More info on fire-method creation:
- * For each entry in the given or default created list a fire-method name will be created: <pre>"fire${suffix.capitalize()}"</pre>.
+ * <p>More info on fire-method creation:
+ * For each entry in the given or default created list a fire-method name will be created: <tt>fire${suffix.capitalize()}</tt>.
  * For each fire-method name a set of fire-methods will be created with the event type as single parameter and all the
  * parameter lists from the declared constructors of the event type.
  *
