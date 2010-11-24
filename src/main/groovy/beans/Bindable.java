822569cc35f2b9203ab66166b7ea15eb0d14868c
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-11-24 15:45:56 +0000

    Added code examples to Javadoc of groovy.beans ast transforms.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21111 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/beans/Bindable.java
+++ src/main/groovy/beans/Bindable.java
@@ -28,20 +28,81 @@ import java.lang.annotation.Target;
  *
  * When annotating a property it indicates that the property should be a
  * bound property according to the JavaBeans spec, announcing to listeners
- * that the value has changed.
+ * that the value has changed. <br/><br/>
  *
  * When annotating a class it indicates that all groovy properties in that
  * class should be bound as though each property had the annotation (even
- * if it already has it explicitly).
+ * if it already has it explicitly).<br/><br/>
  *
  * It is a compilation error to place this annotation on a field (that is
- * not a property, i.e. has scope visibility modifiers).
+ * not a property, i.e. has scope visibility modifiers).<br/><br/>
  *
  * If a property with a user defined setter method is annotated the code
- * block is wrapped with the needed code to fire off the event.
+ * block is wrapped with the needed code to fire off the event.<br/><br/>
  *
- * //TODO discuss generated fields and methods
+ * The following example shows how you can use this annotation on fields
+ * of a class: 
+ * <pre>
+ * class Person {
+ *    @groovy.beans.Bindable
+ *    String firstName
  *
+ *    @groovy.beans.Bindable
+ *    def zipCode
+ * }
+ * </pre>
+ * The above example will generate code that is similar to the next snippet. 
+ * Notice the difference between a String property and a def/Object property: 
+ * <pre>
+ * public class Person { 
+ * 
+ *     @groovy.beans.Bindable
+ *     private java.lang.String firstName 
+ *     @groovy.beans.Bindable
+ *     private java.lang.Object zipCode 
+ *     final private java.beans.PropertyChangeSupport this$propertyChangeSupport 
+ * 
+ *     public Person() {
+ *         this$propertyChangeSupport = new java.beans.PropertyChangeSupport(this)
+ *     }
+ * 
+ *     public void addPropertyChangeListener(java.beans.PropertyChangeListener listener) {
+ *         this$propertyChangeSupport.addPropertyChangeListener(listener)
+ *     }
+ * 
+ *     public void addPropertyChangeListener(java.lang.String name, java.beans.PropertyChangeListener listener) {
+ *         this$propertyChangeSupport.addPropertyChangeListener(name, listener)
+ *     }
+ * 
+ *     public void removePropertyChangeListener(java.beans.PropertyChangeListener listener) {
+ *         this$propertyChangeSupport.removePropertyChangeListener(listener)
+ *     }
+ * 
+ *     public void removePropertyChangeListener(java.lang.String name, java.beans.PropertyChangeListener listener) {
+ *         this$propertyChangeSupport.removePropertyChangeListener(name, listener)
+ *     }
+ * 
+ *     public void firePropertyChange(java.lang.String name, java.lang.Object oldValue, java.lang.Object newValue) {
+ *         this$propertyChangeSupport.firePropertyChange(name, oldValue, newValue)
+ *     }
+ * 
+ *     public java.beans.PropertyChangeListener[] getPropertyChangeListeners() {
+ *         return this$propertyChangeSupport.getPropertyChangeListeners()
+ *     }
+ * 
+ *     public java.beans.PropertyChangeListener[] getPropertyChangeListeners(java.lang.String name) {
+ *         return this$propertyChangeSupport.getPropertyChangeListeners(name)
+ *     }
+ * 
+ *     public void setFirstName(java.lang.String value) {
+ *         this.firePropertyChange('firstName', firstName, firstName = value )
+ *     }
+ * 
+ *     public void setZipCode(java.lang.Object value) {
+ *         this.firePropertyChange('zipCode', zipCode, zipCode = value )
+ *     }
+ * }
+ * </pre>
  * @see BindableASTTransformation
  *
  * @author Danno Ferrin (shemnon)
--- src/main/groovy/beans/Vetoable.java
+++ src/main/groovy/beans/Vetoable.java
@@ -24,24 +24,82 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotates a groovy property or a class.
+ * Annotates a groovy property or a class. <br/><br/>
  *
  * When annotating a property it indicates that the property should be a
  * constrained property according to the JavaBeans spec, subject to
- * listeners vetoing the property change.
+ * listeners vetoing the property change.<br/><br/>
  *
  * When annotating a class it indicates that all groovy properties in that
  * class should be bound as though each property had the annotation (even
- * if it already has it explicitly).
+ * if it already has it explicitly).<br/><br/>
  *
  * It is a compilation error to place this annotation on a field (that is
- * not a property, i.e. has scope visibility modifiers).
+ * not a property, i.e. has scope visibility modifiers).<br/><br/>
  *
  * If a property with a user defined setter method is annotated the code
- * block is wrapped with the needed code to fire off the event.
+ * block is wrapped with the needed code to fire off the event.<br/><br/>
  *
- * //TODO discuss generated fields and methods
- *
+ * Here is a simple example of how to annotate a class with Vetoable: 
+ * <pre>
+ * @groovy.beans.Vetoable
+ * class Person {
+ *     String firstName
+ *     def zipCode
+ * }
+ * </pre>
+ * This code is transformed by the compiler into something resembling the following
+ * snippet. Notice the difference between a String and a def/Object property: 
+ * <pre>
+ * public class Person implements groovy.lang.GroovyObject { 
+ * 
+ *     private java.lang.String firstName 
+ *     private java.lang.Object zipCode 
+ *     final private java.beans.VetoableChangeSupport this$vetoableChangeSupport 
+ * 
+ *     public Person() {
+ *         this$vetoableChangeSupport = new java.beans.VetoableChangeSupport(this)
+ *     }
+ * 
+ *     public void addVetoableChangeListener(java.beans.VetoableChangeListener listener) {
+ *         this$vetoableChangeSupport.addVetoableChangeListener(listener)
+ *     }
+ * 
+ *     public void addVetoableChangeListener(java.lang.String name, java.beans.VetoableChangeListener listener) {
+ *         this$vetoableChangeSupport.addVetoableChangeListener(name, listener)
+ *     }
+ * 
+ *     public void removeVetoableChangeListener(java.beans.VetoableChangeListener listener) {
+ *         this$vetoableChangeSupport.removeVetoableChangeListener(listener)
+ *     }
+ * 
+ *     public void removeVetoableChangeListener(java.lang.String name, java.beans.VetoableChangeListener listener) {
+ *         this$vetoableChangeSupport.removeVetoableChangeListener(name, listener)
+ *     }
+ * 
+ *     public void fireVetoableChange(java.lang.String name, java.lang.Object oldValue, java.lang.Object newValue) throws java.beans.PropertyVetoException {
+ *         this$vetoableChangeSupport.fireVetoableChange(name, oldValue, newValue)
+ *     }
+ * 
+ *     public java.beans.VetoableChangeListener[] getVetoableChangeListeners() {
+ *         return this$vetoableChangeSupport.getVetoableChangeListeners()
+ *     }
+ * 
+ *     public java.beans.VetoableChangeListener[] getVetoableChangeListeners(java.lang.String name) {
+ *         return this$vetoableChangeSupport.getVetoableChangeListeners(name)
+ *     }
+ * 
+ *     public void setFirstName(java.lang.String value) throws java.beans.PropertyVetoException {
+ *         this.fireVetoableChange('firstName', firstName, value)
+ *         firstName = value 
+ *     }
+ * 
+ *     public void setZipCode(java.lang.Object value) throws java.beans.PropertyVetoException {
+ *         this.fireVetoableChange('zipCode', zipCode, value)
+ *         zipCode = value 
+ *     }
+ * }
+ * </pre>
  * @see VetoableASTTransformation
  *
  * @author Danno Ferrin (shemnon)
