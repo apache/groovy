760671f2e7f95e803ee1fef139f7a74b16998c0b
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-11-30 20:31:24 +0000

    Updated ArrayUtil javadoc one more time so that people quite wondering what it is used for.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21152 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/ArrayUtil.java
+++ src/main/org/codehaus/groovy/runtime/ArrayUtil.java
@@ -16,10 +16,38 @@
 package org.codehaus.groovy.runtime;
 
 /**
-* This is a generated class used internally during the writing of bytecode
-* within the CallSiteWriter logic. 
-* This is not a class exposed to users, as is the case with almost all
-* classes in the org.codehaus.groovy packages. 
+* This is a generated class used internally during the writing of bytecode within the CallSiteWriter logic. <br/>
+* This is not a class exposed to users, as is the case with almost all classes in the org.codehaus.groovy packages. <br/> <br/>
+* 
+* The purpose is the reduction of the size of the bytecode. Consider creating a three element Object[] with null values: <br/>
+* <pre>
+*  ANEWARRAY java/lang/Object    
+*  DUP
+*  ICONST_0
+*  ACONST_NULL
+*  AASTORE
+*  DUP
+*  ICONST_1
+*  ACONST_NULL
+*  AASTORE
+*  DUP
+*  ICONST_2
+*  ACONST_NULL
+*  AASTORE
+* </pre>
+* with ArrayUtils you can have it like this:
+* <pre>
+*  ACONST_NULL
+*  ACONST_NULL
+*  ACONST_NULL
+*  INVOKESTATIC ArrayUtils.createArray(Object,Object,Object)
+* </pre>
+* The number of needed instructions is thus reduced from 15 to 4. For every entry we save 3 bytecode instructions. 
+* This allows better readable bytecode and it allows the JIT to see less bytecode to optimize, helping under the 
+* inlining threshold here or there.  <br/> <br/>
+*
+* So even though the class is ugly, there are good reason to have this in Groovy, even if the class makes
+* absolutely no sense in normal Java. But it is not used in normal Java, but from the bytecode. 
 */ 
 public class ArrayUtil {
     private static final Object[] EMPTY = new Object[0]
