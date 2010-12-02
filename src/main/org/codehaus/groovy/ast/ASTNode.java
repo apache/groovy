aecac8188fbcfaa6d472ba59407b13f76b99c43f
Jochen Theodorou <blackdrag@gmx.org>
2010-12-02 18:15:52 +0000

    allows storage of meta data an an ASTNode
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21182 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/ast/ASTNode.java
+++ src/main/org/codehaus/groovy/ast/ASTNode.java
@@ -15,11 +15,30 @@
  */
 package org.codehaus.groovy.ast;
 
+import java.util.Map;
+
 
 /**
- * Base class for any AST node
- * 
+ * Base class for any AST node. This class supports basic information used in all
+ * nodes of the AST<ul>
+ * <li> line and column number information. Usually a node represents a certain
+ * area in a text file determined by a starting position and an ending position.
+ * For nodes that do not represent this, this information will be -1. A node can
+ * also be configured in its line/col information using another node through 
+ * setSourcePosition(otherNode).</li>
+ * <li> every node can store meta data. A phase operation or transform can use 
+ * this to transport arbitrary information to another phase operation or 
+ * transform. The only requirement is that the other phase operation or transform
+ * runs after the part storing the information. To save memory this map is null
+ * by default. If the information transport is done and the map does not store
+ * any information anymore, it is strongly recommended to set a null map again.</li> 
+ * </ul>
+ * <li> a text representation of this node trough getText(). This was in the 
+ * past used for assertion messages. Since the usage of power asserts this 
+ * method will not be called for this purpose anymore and might be removed in
+ * future versions of Groovy</li>
  * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
+ * @author <a href="maito:blackdrag@gmx.org>Jochen "blackdrag" Theodorou</a>
  * @version $Revision$
  */
 public class ASTNode {
@@ -28,6 +47,7 @@ public class ASTNode {
     private int columnNumber = -1;
     private int lastLineNumber = -1;
     private int lastColumnNumber = -1;
+    private Map nodeMetaData = null; 
 
     public void visit(GroovyCodeVisitor visitor) {
         throw new RuntimeException("No visit() method implemented for class: " + getClass().getName());
@@ -81,5 +101,23 @@ public class ASTNode {
         this.lastLineNumber = node.getLastLineNumber();
         this.lastColumnNumber = node.getLastColumnNumber();
         this.lineNumber = node.getLineNumber();
+    }
+    
+    /**
+     * Gets the node meta data. By default this map will be null.
+     * We do this to save memory for the AST.
+     * @return the node meta data map
+     */
+    public Map getNodeMetaData() {
+        return nodeMetaData;
+    }
+    
+    /**
+     * Sets the node meta data. Note: if all the meta is deleted from
+     * the map, this method should be called with null.
+     * @param m - the node meta data map
+     */
+    public void setNodeMeataData(Map m) {
+        this.nodeMetaData = m;
     }
 }
