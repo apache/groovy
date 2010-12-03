62dd6014b8c1839495ba01a5c63d6a0fee3be321
Guillaume Laforge <glaforge@gmail.com>
2010-12-03 15:02:18 +0000

    Added a test for comparisons on ints within an if condition
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21217 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/org/codehaus/groovy/classgen/IfComparisonOnIntTest.groovy
@@ -0,0 +1,22 @@
+package org.codehaus.groovy.classgen
+
+/**
+ * @author Guillaume Laforge
+ */
+class IfComparisonOnIntTest extends AbstractBytecodeTestCase {
+
+    /**
+     * The "optimized" bytecode should contain an int comparison (IF_ICMPGE)
+     * after having loaded the int i on the stack, and the constant int 100.
+     */
+    void assertBytecode() {
+        assert compile("""\
+            int i = 0
+            if (i < 100) println "true"
+        """).hasSequence([
+                "ILOAD",
+                "LDC 100",
+                "IF_ICMPGE"
+        ])
+    }
+}
--- src/test/org/codehaus/groovy/classgen/PrintlnLoadsAConstantTest.groovy
+++ src/test/org/codehaus/groovy/classgen/PrintlnLoadsAConstantTest.groovy
@@ -1,5 +1,8 @@
 package org.codehaus.groovy.classgen
 
+/**
+ * @author Guillaume Laforge
+ */
 class PrintlnLoadsAConstantTest extends AbstractBytecodeTestCase {
     void assertBytecode() {
         assert compile(''' println "true" ''').hasSequence(['LDC "true"'])
