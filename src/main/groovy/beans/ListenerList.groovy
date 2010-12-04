8781c84bbade545db1cea92d27fcd1fb87e77f58
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-12-04 22:02:36 +0000

    updated @listenerlist javadoc
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21239 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/beans/ListenerList.groovy
+++ src/main/groovy/beans/ListenerList.groovy
@@ -8,84 +8,87 @@ import org.codehaus.groovy.transform.GroovyASTTransformationClass
 
 
 /**
- * This annotation adds Java-style listener support to a class based on the annotated Collection-property.
- * 
- * Given the following example:
+ * This annotation adds Java-style listener support to a class based on an annotated Collection-property. <br/><br/>
  *
+ * For any given Collection property, several methods will be written into the enclosing class during the compile phase. These
+ * changes are visible from Java or other languages. The List is intended to hold listeners of some sort, and the methods
+ * addListener, removeListener, and getListeners are all added to the class. The actual methods names depend on the generic
+ * type of the collection.
+ *
+ * Given the following example:<br/>
  * <pre>
- * interface TestListener {
- *     void eventOccurred(TestEvent event)
+ * class MyClass {
+ *     &#064;groovy.beans.ListenerList
+ *     List&lt;java.awt.event.ActionListener&gt; listeners
  * }
- * 
- * class TestEvent {
- *     def source
- *     String message
- * 
- *     TestEvent(def source, String message) {
- *         this.source = source
- *         this.message = message
+ * </pre>
+ * The following code is generated:
+ * <pre>
+ * public class MyClass extends java.lang.Object {
+ *
+ *     &#064;groovy.beans.ListenerList
+ *     private java.util.List&lt;java.awt.event.ActionListener&gt; listeners
+ *
+ *     public void addActionListener(java.awt.event.ActionListener listener) {
+ *         if ( listener == null) {
+ *             return null
+ *         }
+ *         if ( listeners == null) {
+ *             listeners = []
+ *         }
+ *         listeners.add(listener)
+ *     }
+ *
+ *     public void removeActionListener(java.awt.event.ActionListener listener) {
+ *         if ( listener == null) {
+ *             return null
+ *         }
+ *         if ( listeners == null) {
+ *             listeners = []
+ *         }
+ *         listeners.remove(listener)
+ *     }
+ *
+ *     public java.awt.event.ActionListener[] getActionListeners() {
+ *         java.lang.Object __result = []
+ *         if ( listeners != null) {
+ *             __result.addAll(listeners)
+ *         }
+ *         return (( __result ) as java.awt.event.ActionListener[])
+ *     }
+ *
+ *     public void fireActionPerformed(java.awt.event.ActionEvent param0) {
+ *         if ( listeners != null) {
+ *             def __list = new java.util.ArrayList(listeners)
+ *             for (java.lang.Object listener : __list ) {
+ *                 listener.actionPerformed(param0)
+ *             }
+ *         }
  *     }
  * }
+ * </pre>
+ * A fire method is created for each public method in the target class. In this case, ActionListener only has one
+ * method. For a four method interface, four fire methods would be created. <br/><br/>
  * 
- * class TestClass {
- *     &#064;ListenerList
- *     List<TestListener> listeners
- * }
- * </pre>
- *
- * It adds the following methods:
- *
+ * The annotation can take the following parameters:
  * <pre>
- * synchronized void addTestListener(TestListener listener)
- * synchronized void removeTestListener(TestListener listener)
- * synchronized TestListener[] getTestListeners()
- * void fireEventOccurred(TestEvent event)
- * void fireEventOccurred(Object source, String message)
- * </pre>
+ * name        = a suffix for creating the add, remove, and get methods.
+ *               Default: Name of the listener type
+ *               In the above example, if name is set to MyListener, then the class will have an addMyListener,
+ *               removeMyListener, and getMyListeners methods. 
  *
- * The annotation can get the following parameters:
- *
- * <pre>
- * fire        = One or many names of event-methods to fire
- *               Format: <suffix for fire-method>[-><event-method to be called>]?
- *                 e.g. fire="eventOccurred" or fire="eventOccurred2->eventOccurred"
- *                 If the second part is omitted, the first part is used for both.
- *               Default:
- *                 All abstract methods with a single parameter derived from type of event
- * name        = name as suffix for creating the add, remove and get methods
- *               Default:
- *                 Name of the listener type
  * synchronize = Whether or not the methods created should be synchronized at the method level. 
- *               Default:
- *                 false
+ *               Default: false
  * </pre>
  *
- * <p>You can add the same annotation to the class itself as well, but you need to specify at least listener and event.
- * Then it autocreates a private variable called <tt>&lt;name&gt;List</tt> (starting lowercase) and uses this to store the listeners.
- * If there already exists a compatible variable named this way, it will be used instead.
- * You can add multiple annotations to the class.
- *
- * <p>Another example:
- *
- * <pre>
- * import groovy.beans.*
- *
- * class GreetingClass {
- *   &#064;ListenerList(name = "helloListener", fire = "welcome", event = Date)
- *   def listeners
- * }
- *
- * def greet = new GreetingClass()
- * greet.addHelloListener([welcome: { e -> println "Received: $e" }])
- * greet.fireWelcome(new Date())
- * // => Received: Sat Nov 20 09:46:22 EST 2010
- * </pre>
- *
- * <p>More info on fire-method creation:
- * For each entry in the given or default created list a fire-method name will be created: <tt>fire${suffix.capitalize()}</tt>.
- * For each fire-method name a set of fire-methods will be created with the event type as single parameter and all the
- * parameter lists from the declared constructors of the event type.
- *
+ * <strong>Compilation Errors</strong> - Using this annotation incorrectly results in compilation errors rather
+ * than runtime errors. A list of potential problems includes:
+ * <ul>
+ * <li>This annotation can only be applied to a field of type Collection</li>
+ * <li>The annotated Collection field must have a generic type</li>
+ * <li>The annotated Collection field must not have a generic wildcard declared</li>
+ * <li>The generated methods must not already exist</li>
+ * </ul>
  * @see ListenerListASTTransformation
  *
  * @author Alexander Klein
