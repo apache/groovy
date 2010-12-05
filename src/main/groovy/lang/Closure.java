b9c85a2c03d17d55e7ec50524f5ee252ef629df1
Paul King <paulk@asert.com.au>
2010-12-05 10:40:39 +0000

    revert: remove Closure#call() convenience constructor - leave it just in trunk for now
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@21242 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/groovy/lang/Closure.java
+++ src/main/groovy/lang/Closure.java
@@ -118,14 +118,6 @@ public abstract class Closure extends GroovyObjectSupport implements Cloneable, 
     }
 
     /**
-     * Constructor only used for rare cases where owner and "this" object are both null.
-     * Not used in normal Groovy contexts.
-     */
-    public Closure() {
-        this(null);
-    }
-
-    /**
      * Sets the strategy which the closure uses to resolve property references. The default is Closure.OWNER_FIRST
      *
      * @param resolveStrategy The resolve strategy to set
