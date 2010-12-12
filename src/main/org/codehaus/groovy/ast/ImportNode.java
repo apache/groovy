2f6c228e29b0db019122d6ed04b1d54b2da21132
Paul King <paulk@asert.com.au>
2010-12-12 10:47:47 +0000

    GROOVY-4578: static star ImportNode has invalid constructor?
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21302 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/ast/ImportNode.java
+++ src/main/org/codehaus/groovy/ast/ImportNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2009 the original author or authors.
+ * Copyright 2003-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,8 @@ import org.objectweb.asm.Opcodes;
 /**
  * Represents an import statement of a single class
  * <p/>
- * author Jochen Theodorou
- * author Paul King
- *
+ * @author Jochen Theodorou
+ * @author Paul King
  * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
  */
 public class ImportNode extends AnnotatedNode implements Opcodes {
@@ -36,7 +35,7 @@ public class ImportNode extends AnnotatedNode implements Opcodes {
     private final boolean isStatic;
 
     /**
-     * Represent an import of an entire package, i.e. import package.Classname
+     * Represent an import of an entire package, i.e.&nbsp;import package.Classname
      *
      * @param type  the referenced class
      * @param alias optional alias
@@ -51,7 +50,7 @@ public class ImportNode extends AnnotatedNode implements Opcodes {
     }
 
     /**
-     * Represent an import of an entire package, i.e. import package.*
+     * Represent an import of an entire package, i.e.&nbsp;import package.*
      *
      * @param packageName the name of the package
      */
@@ -65,7 +64,7 @@ public class ImportNode extends AnnotatedNode implements Opcodes {
     }
 
     /**
-     * Represent a static import of a Class, i.e. import static package.Classname.*
+     * Represent a static import of a Class, i.e.&nbsp;import static package.Classname.*
      *
      * @param type the referenced class
      */
@@ -73,13 +72,13 @@ public class ImportNode extends AnnotatedNode implements Opcodes {
         this.type = type;
         this.alias = null;
         this.isStar = true;
-        this.isStatic = false;
+        this.isStatic = true;
         this.packageName = null;
         this.fieldName = null;
     }
 
     /**
-     * Represent a static import of a field or method, i.e. import static package.Classname.name
+     * Represent a static import of a field or method, i.e.&nbsp;import static package.Classname.name
      *
      * @param type      the referenced class
      * @param fieldName the field name
