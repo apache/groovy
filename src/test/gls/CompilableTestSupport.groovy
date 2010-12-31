b9a83b165478725c8fe034237de80ee3eb8c6e5a
Paul King <paulk@asert.com.au>
2010-12-31 10:11:31 +0000

    merge changeset 21367 from trunk: minor refactor
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@21368 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/gls/CompilableTestSupport.groovy
+++ src/test/gls/CompilableTestSupport.groovy
@@ -1,15 +1,29 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gls
 
 import org.codehaus.groovy.control.CompilationFailedException
 
 public class CompilableTestSupport extends GroovyTestCase {
-    protected void shouldNotCompile(String script) {
+    protected shouldNotCompile(String script) {
         try {
             GroovyClassLoader gcl = new GroovyClassLoader()
             gcl.parseClass(script, getTestClassName())
         } catch (CompilationFailedException cfe) {
-            assert true
-            return
+            return cfe.message
         }
         fail("the compilation succeeded but should have failed")
     }
@@ -24,7 +38,6 @@ public class CompilableTestSupport extends GroovyTestCase {
         try {
             def shell = new GroovyShell()
             shell.evaluate(script, getTestClassName())
-            return
         } catch (Throwable thrown) {
             if (th == null) return
             if (th != thrown.getClass()) {
