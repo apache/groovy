40548f6af27f5876dcef0e124a333b591d1fd5ae
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-03-10 19:58:03 +0000

    weeding out codenarc rules and trying to find actual, potential bugs.
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@21787 a5544e8c-8a19-0410-ba12-f9af4593a198

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -20,6 +20,8 @@ ruleset {
     ruleset('rulesets/naming.xml') {
 //        exclude 'PropertyName'
 //        exclude 'PackageName'   // TODO is plugAndPlay package correct? 
+        exclude 'ClassName'
+        exclude 'ClassNameRule'
 //        'ClassName' {
 //            regex = '.*'  // '^[A-Z][a-zA-Z0-9]*$'   // TODO: refine correct regex
 //        }
@@ -36,21 +38,45 @@ ruleset {
 //        }
     }
     ruleset('rulesets/grails.xml')
-    ruleset('rulesets/logging.xml') 
+    ruleset('rulesets/logging.xml') {
+        exclude 'Println'
+        exclude 'SystemOutPrint'
+    }
     ruleset('rulesets/braces.xml') {
         exclude 'IfStatementBraces' // if statements without braces seems acceptable in our coding standards
         exclude 'ElseBlockBraces'   // else statements without braces seems acceptable in our coding standards
     }
-    ruleset('rulesets/basic.xml') 
+    ruleset('rulesets/basic.xml') {
+        exclude 'ConsecutiveStringConcatenation'  // defect in CodeNarc 0.13    
+    }
     ruleset('rulesets/size.xml') 
-    ruleset('rulesets/junit.xml') 
+    ruleset('rulesets/junit.xml') {
+        exclude 'JUnitPublicNonTestMethod'  // too many to worry about
+        exclude 'JUnitStyleAssertions'      // too many to worry about
+        exclude 'JUnitTestMethodWithoutAssert'  // too many to worry about
+    }
     ruleset('rulesets/concurrency.xml') 
     ruleset('rulesets/unnecessary.xml') {
-        exclude 'UnnecessaryObjectReferences' // CodeNarc 0.12 has a bug
+        exclude 'UnnecessaryReturnKeywordRule'  // too many to worry about
+        exclude 'UnnecessaryReturnKeyword'  // too many to worry about
+        exclude 'UnnecessaryGetter'    // too many to worry about
+        exclude 'UnnecessaryGString'    // too many to worry about
+        exclude 'UnnecessarySemicolon'  // too many to worry about
+        exclude 'UnnecessaryPublicModifier' // too many to worry about
     }
-    ruleset('rulesets/dry.xml') 
+    ruleset('rulesets/dry.xml') {
+        'DuplicateNumberLiteral' {
+            ignoreNumbers = '0,1,2,3,4,5,6,10,11,12,16,24,33,34,48,55,77,97,100,123,456,10.0'        
+        }            
+        exclude 'DuplicateStringLiteralRule'    // too many to worry about
+    }
     ruleset('rulesets/design.xml') 
-    ruleset('rulesets/exceptions.xml') 
+    ruleset('rulesets/exceptions.xml') {
+        exclude 'ThrowRuntimeException'
+        exclude 'ThrowException'
+        exclude 'CatchThrowable'
+        exclude 'CatchRuntimeException'
+    }
     ruleset('rulesets/unused.xml') 
 }
 
