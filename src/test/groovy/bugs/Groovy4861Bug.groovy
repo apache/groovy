caed75445e63b437fc10db15354353d24ff3cd2c
Paul King <paulk@asert.com.au>
2011-06-05 11:34:08 +0000

    merge changeset 22287 from trunk: GROOVY-4861: Duplicate entry in InnerClasses
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_8_X@22288 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/bugs/Groovy4861Bug.groovy
@@ -0,0 +1,19 @@
+package groovy.bugs
+
+class Groovy4861Bug extends GroovyTestCase {
+    void testCallSitesUsageInNestedInterface() {
+        assert Foo4861.A.j == 3
+        assert Foo4861.A.j2 == 7
+        assert new Foo4861()
+    }
+}
+
+private class Foo4861 {
+    static interface A {
+        static Integer j = 3
+        static Integer j2 = j + 4
+    }
+    static interface B{}
+    static class Inner<X> {}
+    static Inner<A> method() { null }
+}
