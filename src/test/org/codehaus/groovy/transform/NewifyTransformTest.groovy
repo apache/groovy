a0c8d2418286d46182ee647d5f3c6d48212a508f
Paul King <paulk@asert.com.au>
2011-06-12 13:00:09 +0000

    GROOVY-4876: @Newify works for pre-defined classes but not for classes defined in the same script
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@22318 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/transform/NewifyTransformTest.groovy
+++ src/test/org/codehaus/groovy/transform/NewifyTransformTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 the original author or authors.
+ * Copyright 2008-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ class NewifyTransformTest extends GroovyShellTestCase {
                     assert Integer.new(40) == 40
                 }
             }
-          """
+        """
 
         evaluate """
             @Newify(Integer) class Pythonesque {
@@ -49,7 +49,7 @@ class NewifyTransformTest extends GroovyShellTestCase {
                     assert Integer(41) == 41
                 }
             }
-          """
+        """
     }
 
     void testMethodLevelNewification() {
@@ -62,7 +62,7 @@ class NewifyTransformTest extends GroovyShellTestCase {
                     assert Integer.new(42) == 42
                 }
             }
-          """
+        """
 
         evaluate """
             class Pythonesque {
@@ -73,7 +73,7 @@ class NewifyTransformTest extends GroovyShellTestCase {
                     assert Integer(43) == 43
                 }
             }
-          """
+        """
     }
 
     void testNewificationInProperties() {
@@ -83,7 +83,7 @@ class NewifyTransformTest extends GroovyShellTestCase {
                     assert Integer.new(44).class == Integer
                 }
             }
-          """
+        """
 
         evaluate """
             class Pythonesque {
@@ -91,6 +91,18 @@ class NewifyTransformTest extends GroovyShellTestCase {
                     assert Integer(45).class == Integer
                 }
             }
-          """
+        """
+    }
+
+    void testNewificationUsingClassesWithinScript() {
+        evaluate """
+            import groovy.transform.Immutable
+            abstract class Tree {}
+            @Immutable class Branch extends Tree { Tree left, right }
+            @Immutable class Leaf extends Tree { int val }
+            @Newify([Branch, Leaf])
+            def t = Branch(Leaf(1), Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+            assert t.toString() == 'Branch(Leaf(1), Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))'
+        """
     }
 }
