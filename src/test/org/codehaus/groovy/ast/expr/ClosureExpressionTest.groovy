5bd8abfad3bb518ab60211502bd2e7766d6574cb
Paul King <paulk@asert.com.au>
2011-06-20 20:40:14 +0000

    minor refacoring (see patch attached to GROOVY-4890)
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@22368 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/test/org/codehaus/groovy/ast/expr/ClosureExpressionTest.groovy
+++ src/test/org/codehaus/groovy/ast/expr/ClosureExpressionTest.groovy
@@ -1,8 +1,6 @@
-    package org.codehaus.groovy.ast.expr
+package org.codehaus.groovy.ast.expr
 
 import org.codehaus.groovy.ast.builder.AstBuilder
-import org.codehaus.groovy.syntax.ASTHelper
-import org.codehaus.groovy.ast.AstToTextHelper
 
 /**
  * 
@@ -11,32 +9,32 @@ import org.codehaus.groovy.ast.AstToTextHelper
 class ClosureExpressionTest extends GroovyTestCase {
 
     void testGetText_Simple() {
-
-        def ast = new AstBuilder().buildFromString ''' return { it * it } '''
-        assert '{ -> ... }' == ast[0].statements[0].expression.text
+        def expression = buildFromString 'return { it * it }'
+        assert expression.text == '{ -> ... }'
     }
 
     void testGetText_Parameter() {
-
-        def ast = new AstBuilder().buildFromString ''' return { x -> x * x } '''
-        assert '{ java.lang.Object x -> ... }' == ast[0].statements[0].expression.text
+        def expression = buildFromString 'return { x -> x * x }'
+        assert expression.text == '{ java.lang.Object x -> ... }'
     }
 
     void testGetText_MultipleParameters() {
-
-        def ast = new AstBuilder().buildFromString ''' return { x, y -> x * y } '''
-        assert '{ java.lang.Object x, java.lang.Object y -> ... }' == ast[0].statements[0].expression.text
+        def expression = buildFromString 'return { x, y -> x * y }'
+        assert expression.text == '{ java.lang.Object x, java.lang.Object y -> ... }'
     }
 
     void testGetText_TypedParameter() {
-
-        def ast = new AstBuilder().buildFromString ''' return { String x -> x * x } '''
-        assert '{ java.lang.String x -> ... }' == ast[0].statements[0].expression.text
+        def expression = buildFromString 'return { String x -> x * x }'
+        assert expression.text == '{ java.lang.String x -> ... }'
     }
 
     void testGetText_MultipleTypedParameters() {
+        def expression = buildFromString 'return { String x, Integer y -> x * y }'
+        assert expression.text == '{ java.lang.String x, java.lang.Integer y -> ... }'
+    }
 
-        def ast = new AstBuilder().buildFromString ''' return { String x, Integer y -> x * y } '''
-        assert '{ java.lang.String x, java.lang.Integer y -> ... }' == ast[0].statements[0].expression.text
+    private Expression buildFromString(String source) {
+        def ast = new AstBuilder().buildFromString(source)
+        ast[0].statements[0].expression
     }
 }
