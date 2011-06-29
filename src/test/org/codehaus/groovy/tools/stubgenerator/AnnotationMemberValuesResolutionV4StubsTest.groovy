2237ac08186d5fb8a4ac848f154f1655047b697f
Paul King <paulk@asert.com.au>
2011-06-29 09:12:48 +0000

    merge changesets 22463-22464 from 1_7_X: minor refactor + GROOVY-4768
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/groovy-core@22466 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/AnnotationMemberValuesResolutionV4StubsTest.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2003-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * Test that FQN appears in generated stub when an annotation node
+ *  is used as an annotation member value.
+ *
+ * @author Paul King
+ */
+class AnnotationMemberValuesResolutionV4StubsTest extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+            'foo/MainAnno4768.java': '''
+                package foo;
+                
+                import java.lang.annotation.*;
+                import static java.lang.annotation.ElementType.*;
+                import static java.lang.annotation.RetentionPolicy.*;
+
+                @Retention(RUNTIME) @Target({TYPE})
+                public @interface MainAnno4768 {
+                    OtherAnno4768 other();
+                }
+            ''',
+            'foo/OtherAnno4768.java': '''
+                package foo;
+
+                import java.lang.annotation.*;
+                import static java.lang.annotation.ElementType.*;
+                import static java.lang.annotation.RetentionPolicy.*;
+
+                @Retention(RUNTIME) @Target({METHOD, FIELD})
+                public @interface OtherAnno4768 {
+                    String name() default "";
+                }
+            ''',
+            'Main4768.groovy': '''
+                import foo.*
+
+                @MainAnno4768(other = @OtherAnno4768(name='baz'))
+                class Main4768 {}
+            '''
+        ]
+    }
+
+    void verifyStubs() {
+        classes['Main4768'].with {
+            assert annotations[0].getProperty('other').getProperty('name').value == 'baz'
+        }
+    }
+}
