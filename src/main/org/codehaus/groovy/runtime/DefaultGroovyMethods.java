b803e2e9476ce800df0f8b9583ef45e7dbd395dd
Paul King <paulk@asert.com.au>
2011-07-09 11:22:57 +0000

    merge changeset 22516 from trunk: GROOVY-4916: Set equality fails if null elements are present
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_8_X@22517 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -7685,6 +7685,9 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
         if (right == null) {
             return false;
         }
+        if (left == right) {
+            return true;
+        }
         if (left.length != right.length) {
             return false;
         }
@@ -7777,6 +7780,9 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
         if (right == null) {
             return false;
         }
+        if (left == right) {
+            return true;
+        }
         if (left.size() != right.size()) {
             return false;
         }
@@ -7803,7 +7809,8 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
      * for example 2 == 2L.  If both sets are <code>null</code>, the result
      * is true; otherwise if either set is <code>null</code>, the result
      * is <code>false</code>. Example usage:
-     * <pre class="groovyTestCase">Set s1 = ["a", 2]
+     * <pre class="groovyTestCase">
+     * Set s1 = ["a", 2]
      * def s2 = [2, 'a'] as Set
      * Set s3 = [3, 'a']
      * def s4 = [2.0, 'a'] as Set
@@ -7818,29 +7825,34 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
      * @return <tt>true</tt> if the contents of both sets are identical
      * @since 1.8.0
      */
-    public static boolean equals(Set self, Set other) {
+    public static <T> boolean equals(Set<T> self, Set<T> other) {
         if (self == null) {
             return other == null;
         }
         if (other == null) {
             return false;
         }
+        if (self == other) {
+            return true;
+        }
         if (self.size() != other.size()) {
             return false;
         }
-        final Iterator it1 = self.iterator();
-        Collection otherItems = new HashSet(other);
+        final Iterator<T> it1 = self.iterator();
+        Collection<T> otherItems = new HashSet<T>(other);
         while (it1.hasNext()) {
             final Object o1 = it1.next();
-            final Iterator it2 = otherItems.iterator();
-            Object foundItem = null;
+            final Iterator<T> it2 = otherItems.iterator();
+            T foundItem = null;
+            boolean found = false;
             while (it2.hasNext() && foundItem == null) {
-                final Object o2 = it2.next();
+                final T o2 = it2.next();
                 if (coercedEquals(o1, o2)) {
                     foundItem = o2;
+                    found = true;
                 }
             }
-            if (foundItem == null) return false;
+            if (!found) return false;
             otherItems.remove(foundItem);
         }
         return otherItems.size() == 0;
@@ -7864,15 +7876,16 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
         if (other == null) {
             return false;
         }
+        if (self == other) {
+            return true;
+        }
         if (self.size() != other.size()) {
             return false;
         }
         if (!self.keySet().equals(other.keySet())) {
             return false;
         }
-        final Iterator it = self.keySet().iterator();
-        while (it.hasNext()) {
-            final Object key = it.next();
+        for (Object key : self.keySet()) {
             if (!coercedEquals(self.get(key), other.get(key))) {
                 return false;
             }
---
+++ src/test/groovy/SetTest.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2003-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy
+
+class SetTest extends GroovyTestCase {
+
+    void testSetPlus() {
+        Set s1 = [6, 4, 5, 1, 7, 2]
+        def s2 = [6, 4, 5, 1, 7, [4,5]]
+        def s3 = s1 + s2
+        assert s3 == [1, 2, 4, 5, 6, 7, [4,5]] as Set
+    }
+
+    void testSetSimpleMinus() {
+        Set s1 = [1, 1, 2, 2, 3, 3, 3, 4, 5, 3, 5]
+        def s2 = s1 - [1, 4]
+        assert s2 == [2, 3, 5] as Set
+        def s3 = s1 - 4.0
+        assert s3 == [1, 2, 3, 5] as Set
+    }
+
+    void testSetFlatten() {
+        Set orig = [[[4, 5, 6, [46, 7, "erer"] as Set] as Set, 4, [3, 6, 78] as Set] as Set, 4]
+        Set flat = orig.flatten()
+        assert flat == [3, 4, 5, 6, 7, 46, 78, "erer"] as Set
+    }
+
+    void testFlattenSetOfMapsWithClosure() {
+        Set orig = [[a:1, b:2], [c:3, d:4]] as Set
+        Set flat = orig.flatten{ it instanceof Map ? it.values() : it }
+        assert flat == [1, 2, 3 ,4] as Set
+        flat = orig.flatten{ it instanceof Map ? it.keySet() : it }
+        assert flat == ["a", "b", "c", "d"] as Set
+    }
+
+    void testSetEquality() {
+        def a = [1, 'a', null] as Set
+        def b = ['a', null, 1] as Set
+        assert a == a
+        assert a.equals(a)
+        assert a == b
+        assert a.equals(b)
+    }
+}
