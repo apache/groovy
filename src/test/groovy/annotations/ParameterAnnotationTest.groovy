1e6bdaad46777f1ef416f5416a0299b09fae1aa7
Jochen Theodorou <blackdrag@gmx.org>
2011-07-14 15:53:01 +0000

    GROOVY-4732: missing annotations on parameters, fix and testcase by Andre Steingress
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_8_X@22571 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/groovy/annotations/ParameterAnnotationTest.groovy
@@ -0,0 +1,62 @@
+/*
+* Copyright 2003-2009 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import org.codehaus.groovy.control.*
+
+class ParameterAnnotationTest extends GroovyTestCase {
+    void testParameterAnnotation() {
+        
+                def gcl = new GroovyClassLoader()
+        
+                gcl.parseClass """
+                    import java.lang.annotation.*
+        
+                    @Target(ElementType.METHOD)
+                    @Retention(RetentionPolicy.RUNTIME)
+                    @interface MethodAnnotation {}
+        
+                    @Target(ElementType.PARAMETER)
+                    @Retention(RetentionPolicy.RUNTIME)
+                    @interface ParameterAnnotation {}
+        
+                    interface MyInterface {
+                        @MethodAnnotation
+                        def method(@ParameterAnnotation def param)
+                    }
+                """
+        
+                GroovyCodeSource codeSource = new GroovyCodeSource("""
+                    class MyInterfaceImpl implements MyInterface {
+                        def method(def param) {}
+                    }
+                """, "script" + System.currentTimeMillis() + ".groovy", "/groovy/script")
+        
+                def cu = new CompilationUnit(CompilerConfiguration.DEFAULT, codeSource.codeSource, gcl)
+                cu.addSource(codeSource.getName(), codeSource.scriptText);
+                cu.compile(CompilePhase.FINALIZATION.phaseNumber)
+        
+                def classNode = cu.getClassNode("MyInterfaceImpl")
+                def interfaceClassNode = classNode.getInterfaces().find { it.nameWithoutPackage == 'MyInterface' }
+        
+                def methodNode = interfaceClassNode.getDeclaredMethods("method")[0]
+        
+                // check if the AnnotationNode for 'MethodAnnotation' has been created
+                assert methodNode.getAnnotations().any { an -> an.classNode.nameWithoutPackage == 'MethodAnnotation' }
+                
+                // this one will fail, since parameter annotations are ignored by Java5Plugin (and above)
+                assert methodNode.getParameters()[0].getAnnotations().any { an -> an.classNode.nameWithoutPackage == 'ParameterAnnotation' }
+        }
+}
