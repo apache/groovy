c0fb1b9e4a68c90a5ceb9dee7d6d54a4e98f5f3d
Guillaume Laforge <glaforge@gmail.com>
2011-07-19 10:29:58 +0000

    GROOVY-4925: JsonBuilder with Writer support would be handy (merge rev 22608)
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_8_X@22609 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/main/groovy/json/StreamingJsonBuilder.groovy
@@ -0,0 +1,284 @@
+/*
+ * Copyright 2003-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.json
+
+/**
+ * A builder for creating JSON payloads.
+ * <p>
+ * This builder supports the usual builder syntax made of nested method calls and closures,
+ * but also some specific aspects of JSON data structures, such as list of values, etc.
+ * Please make sure to have a look at the various methods provided by this builder
+ * to be able to learn about the various possibilities of usage.
+ * <p>
+ * Unlike the JsonBuilder class which creates a data structure in memory,
+ * which is handy in those situations where you want to alter the structure programmatically before output,
+ * the StreamingJsonBuilder streams to a writter directly without any memory data structure.
+ * So if you don't need to modify the structure, and want a more memory-efficient approach,
+ * please use the StreamingJsonBuilder.
+ * <p>
+ * Example:
+ * <pre class="groovyTestCase">
+ *     new StringWriter().with { w ->
+ *         def builder = new groovy.json.StreamingJsonBuilder( w )
+ *         builder.people {
+ *             person {
+ *                 firstName 'Tim'
+ *                 lastName 'Yates'
+ *                 // Named arguments are valid values for objects too
+ *                 address(
+ *                     city: 'Manchester',
+ *                     country: 'UK',
+ *                     zip: 'M1 2AB',
+ *                 )
+ *                 living true
+ *                 eyes 'left', 'right'
+ *             }
+ *         }
+ *
+ *         assert w.toString() == '{"people":{"person":{"firstName":"Tim","lastName":"Yates","address":{"city":"Manchester","country":"UK","zip":"M1 2AB"},"living":true,"eyes":["left","right"]}}}'
+ *    }
+ * </pre>
+ *
+ * @author Tim Yates
+ * @since 1.8.1
+ */
+class StreamingJsonBuilder {
+
+    Writer writer
+    
+    /**
+     * Instantiates a JSON builder, possibly with some existing data structure.
+     *
+     * @param writer A writer to which Json will be written
+     * @param content a pre-existing data structure, default to null
+     */
+    StreamingJsonBuilder( Writer writer, content = null) {
+        this.writer = writer
+        if( content ) writer.write( JsonOutput.toJson( content ) )
+    }
+
+    /**
+     * Named arguments can be passed to the JSON builder instance to create a root JSON object
+     * <p>
+     * Example:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *   def json = new groovy.json.StreamingJsonBuilder( w )
+     *   json name: "Tim", age: 31
+     *
+     *   assert w.toString() == '{"name":"Tim","age":31}'
+     * }
+     * </pre>
+     *
+     * @param m a map of key / value pairs
+     * @return a map of key / value pairs
+     */
+    def call(Map m) {
+        writer.write JsonOutput.toJson( m )
+        return m
+    }
+
+    /**
+     * A list of elements as arguments to the JSON builder creates a root JSON array
+     * <p>
+     * Example:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *   def json = new groovy.json.StreamingJsonBuilder( w )
+     *   def result = json([1, 2, 3])
+     *
+     *   assert result == [ 1, 2, 3 ]
+     *   assert w.toString() == "[1,2,3]"
+     * }
+     * </pre>
+     *
+     * @param l a list of values
+     * @return a list of values
+     */
+    def call(List l) {
+        writer.write( JsonOutput.toJson( l ) )
+        return l
+    }
+
+    /**
+     * Varargs elements as arguments to the JSON builder create a root JSON array
+     * <p>
+     * Example:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *   def json = new groovy.json.StreamingJsonBuilder( w )
+     *   def result = json 1, 2, 3
+     *
+     *   assert result instanceof List
+     *   assert w.toString() == "[1,2,3]"
+     * }
+     * </pre>
+
+     * @param args an array of values
+     * @return a list of values
+     */
+    def call(Object... args) {
+        def l = args.toList()
+        writer.write JsonOutput.toJson( l )
+        return l
+    }
+
+    /**
+     * A closure passed to a JSON builder will create a root JSON object
+     * <p>
+     * Example:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *   def json = new groovy.json.StreamingJsonBuilder( w )
+     *   json {
+     *      name "Tim"
+     *      age 39
+     *   }
+     *
+     *   assert w.toString() == '{"name":"Tim","age":39}'
+     * }
+     * </pre>
+     *
+     * @param c a closure whose method call statements represent key / values of a JSON object
+     */
+    def call(Closure c) {
+        writer.write( '{' )
+        StreamingJsonDelegate.cloneDelegateAndGetContent( writer, c )
+        writer.write( '}' )
+    }
+
+    /**
+     * A method call on the JSON builder instance will create a root object with only one key
+     * whose name is the name of the method being called.
+     * This method takes as arguments:
+     * <ul>
+     *     <li>a closure</li>
+     *     <li>a map (ie. named arguments)</li>
+     *     <li>a map and a closure</li>
+     *     <li>or no argument at all</li>
+     * </ul>
+     * <p>
+     * Example with a classicala builder-style:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *     def json = new groovy.json.StreamingJsonBuilder( w )
+     *     json.person {
+     *         name "Tim"
+     *          age 28
+     *     }
+     *
+     *     assert w.toString() == '{"person":{"name":"Tim","age":28}}'
+     * }
+     * </pre>
+     *
+     * Or alternatively with a method call taking named arguments:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *     def json = new groovy.json.StreamingJsonBuilder( w )
+     *     json.person name: "Tim", age: 32
+     *
+     *     assert w.toString() == '{"person":{"name":"Tim","age":32}}'
+     * }
+     * </pre>
+     *
+     * If you use named arguments and a closure as last argument,
+     * the key/value pairs of the map (as named arguments)
+     * and the key/value pairs represented in the closure
+     * will be merged together &mdash;
+     * the closure properties overriding the map key/values
+     * in case the same key is used.
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *     def json = new groovy.json.StreamingJsonBuilder( w )
+     *     json.person(name: "Tim", age: 35) { town "Manchester" }
+     *
+     *     assert w.toString() == '{"person":{"name":"Tim","age":35,"town":"Manchester"}}'
+     * }
+     * </pre>
+     *
+     * The empty args call will create a key whose value will be an empty JSON object:
+     * <pre class="groovyTestCase">
+     * new StringWriter().with { w ->
+     *     def json = new groovy.json.StreamingJsonBuilder( w )
+     *     json.person()
+     *
+     *     assert w.toString() == '{"person":{}}'
+     * }
+     * </pre>
+     *
+     * @param name the single key
+     * @param args the value associated with the key
+     */
+    def invokeMethod(String name, Object args) {
+        if (args?.size() == 0) {
+            writer.write JsonOutput.toJson( [(name): [:]] )
+        } else if (args?.size() == 1) {
+            if (args[0] instanceof Closure) {
+                writer.write "{${JsonOutput.toJson( name )}:"
+                this.call( args[0] )
+                writer.write '}'
+            } else if (args[0] instanceof Map) {
+                writer.write JsonOutput.toJson( [(name): args[0]] )
+            }
+            else {
+                throw new JsonException("Expected no arguments, a single map, a single closure, or a map and closure as arguments.")
+            }
+        } else if (args?.size() == 2 && args[0] instanceof Map && args[1] instanceof Closure) {
+            writer.write "{${JsonOutput.toJson( name )}:{"
+            args[0].eachWithIndex { it, idx ->
+                if( idx > 0 ) writer.write ','
+                writer.write JsonOutput.toJson( it.key )
+                writer.write ':'
+                writer.write JsonOutput.toJson( it.value )
+            }
+            StreamingJsonDelegate.cloneDelegateAndGetContent( writer, args[1], !args[0].size() )
+            writer.write '}}'
+        }
+        else {
+            throw new JsonException("Expected no arguments, a single map, a single closure, or a map and closure as arguments.")
+        }
+    }
+}
+
+class StreamingJsonDelegate {
+    Writer writer
+    boolean first
+    
+    StreamingJsonDelegate( Writer w, boolean first ) {
+        this.writer = w
+        this.first = first
+    }
+    
+    def invokeMethod(String name, Object args) {
+        if (args) {
+            if( !first ) {
+                writer.write ','
+            }
+            writer.write JsonOutput.toJson( name )
+            writer.write ':'
+            writer.write JsonOutput.toJson( args.size() == 1 ? args[0] : args.toList() )
+            first = false
+        }
+    }
+
+    static cloneDelegateAndGetContent(Writer w, Closure c, boolean first=true ) {
+        def delegate = new StreamingJsonDelegate( w, first )
+        Closure cloned = c.clone()
+        cloned.delegate = delegate
+        cloned.resolveStrategy = Closure.DELEGATE_FIRST
+        cloned()
+    }
+}
---
+++ src/test/groovy/json/StreamingJsonBuilderTest.groovy
@@ -0,0 +1,300 @@
+/*
+ * Copyright 2003-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.json
+
+/**
+ * @author Tim Yates
+ * @author Guillaume Laforge
+ */
+class StreamingJsonBuilderTest extends GroovyTestCase {
+
+    void testJsonBuilderConstructor() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w, [a: 1, b: true])
+            assert w.toString() == '{"a":1,"b":true}'
+        }
+    }
+
+    void testEmptyArray() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json([])
+            assert w.toString() == '[]'
+        }
+    }
+
+    void testSimpleArray() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json 1, 2, "a", "b"
+
+            assert w.toString() == '[1,2,"a","b"]'
+        }
+    }
+
+    void testComplexArray() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json 1, 2, [k: true], "a", "b", [3, "c"]
+
+            assert w.toString() == '[1,2,{"k":true},"a","b",[3,"c"]]'
+        }
+    }
+
+    void testMap() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json a: 1, b: 2
+
+            assert w.toString() == '{"a":1,"b":2}'
+        }
+    }
+
+    void testEmptyObject() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json {}
+
+            assert w.toString() == '{}'
+        }
+    }
+
+    void testBasicObject() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json {
+                a 1
+                b true
+                c null
+            }
+
+            assert w.toString() == '{"a":1,"b":true,"c":null}'
+        }
+    }
+    
+    void testNestedObjects() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json {
+                a {
+                    b {
+                        c 1
+                    }
+                }
+            }
+
+            assert w.toString() == '{"a":{"b":{"c":1}}}'
+        }
+    }
+
+    void testStandardBuilderStyle() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json.person {
+                name "Guillaume"
+                age 33
+            }
+
+            assert w.toString() == '{"person":{"name":"Guillaume","age":33}}'
+        }
+    }
+
+    void testMethodCallWithNamedArguments() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json.person name: "Guillaume", age: 33
+
+            assert w.toString() == '{"person":{"name":"Guillaume","age":33}}'
+        }
+    }
+
+    void testThrowAnExceptionWhenPassingSomethingElseThanAClosure() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+
+            shouldFail(JsonException) {
+                json.something 1, 2, 3
+            }
+        }
+    }
+
+    void testListWithAnEmptyObject() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json([[:]])
+
+            assert w.toString() == '[{}]'
+        }
+    }
+
+    void testListOfObjects() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json([name: "Guillaume"], [name: "Jochen"], [name: "Paul"])
+
+            assert w.toString() == '[{"name":"Guillaume"},{"name":"Jochen"},{"name":"Paul"}]'
+        }
+    }
+
+    void testElementHasListOfObjects() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json.response {
+                results 1, [a: 2]
+            }
+
+            assert w.toString() == '{"response":{"results":[1,{"a":2}]}}'
+        }
+    }
+
+    void testComplexStructureFromTheGuardian() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json.response {
+                status "ok"
+                userTier "free"
+                total 2413
+                startIndex 1
+                pageSize 10
+                currentPage 1
+                pages 242
+                orderBy "newest"
+                results([
+                    id: "world/video/2011/jan/19/tunisia-demonstrators-democracy-video",
+                    sectionId: "world",
+                    sectionName: "World news",
+                    webPublicationDate: "2011-01-19T15:12:46Z",
+                    webTitle: "Tunisian demonstrators demand new democracy - video",
+                    webUrl: "http://www.guardian.co.uk/world/video/2011/jan/19/tunisia-demonstrators-democracy-video",
+                    apiUrl: "http://content.guardianapis.com/world/video/2011/jan/19/tunisia-demonstrators-democracy-video"
+                ],
+                [
+                    id: "world/gallery/2011/jan/19/tunisia-protests-pictures",
+                    sectionId: "world",
+                    sectionName: "World news",
+                    webPublicationDate: "2011-01-19T15:01:09Z",
+                    webTitle: "Tunisia protests continue � in pictures ",
+                    webUrl: "http://www.guardian.co.uk/world/gallery/2011/jan/19/tunisia-protests-pictures",
+                    apiUrl: "http://content.guardianapis.com/world/gallery/2011/jan/19/tunisia-protests-pictures"
+                ])
+            }
+
+            assert w.toString() ==
+                '''{"response":{"status":"ok","userTier":"free","total":2413,"startIndex":1,"pageSize":10,"currentPage":1,"pages":242,"orderBy":"newest","results":[{"id":"world\\/video\\/2011\\/jan\\/19\\/tunisia-demonstrators-democracy-video","sectionId":"world","sectionName":"World news","webPublicationDate":"2011-01-19T15:12:46Z","webTitle":"Tunisian demonstrators demand new democracy - video","webUrl":"http:\\/\\/www.guardian.co.uk\\/world\\/video\\/2011\\/jan\\/19\\/tunisia-demonstrators-democracy-video","apiUrl":"http:\\/\\/content.guardianapis.com\\/world\\/video\\/2011\\/jan\\/19\\/tunisia-demonstrators-democracy-video"},{"id":"world\\/gallery\\/2011\\/jan\\/19\\/tunisia-protests-pictures","sectionId":"world","sectionName":"World news","webPublicationDate":"2011-01-19T15:01:09Z","webTitle":"Tunisia protests continue � in pictures ","webUrl":"http:\\/\\/www.guardian.co.uk\\/world\\/gallery\\/2011\\/jan\\/19\\/tunisia-protests-pictures","apiUrl":"http:\\/\\/content.guardianapis.com\\/world\\/gallery\\/2011\\/jan\\/19\\/tunisia-protests-pictures"}]}}'''
+        }
+    }
+
+    void testNestedListMap() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json.content {
+                list([:],[another:[a:[1,2,3]]])
+            }
+
+            assert w.toString() == '''{"content":{"list":[{},{"another":{"a":[1,2,3]}}]}}'''
+        }
+    }
+
+    void testEmptyList() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json()
+
+            assert w.toString() == '''[]'''
+        }
+    }
+
+    void testTrendsFromTwitter() {
+        new StringWriter().with { w ->
+            def json = new StreamingJsonBuilder( w )
+            json.trends {
+                "2010-06-22 17:20" ([
+                        name: "Groovy rules",
+                        query: "Groovy rules"
+                ], {
+                        name "#worldcup"
+                        query "#worldcup"
+                }, [
+                        name: "Uruguai",
+                        query: "Uruguai"
+                ])
+                "2010-06-22 06:20" ({
+                    name "#groovy"
+                    query "#groovy"
+                }, [
+                    name: "#java",
+                    query: "#java"
+                ])
+            }
+            assert w.toString() == '''{"trends":{"2010-06-22 17:20":[{"name":"Groovy rules","query":"Groovy rules"},{"name":"#worldcup","query":"#worldcup"},{"name":"Uruguai","query":"Uruguai"}],"2010-06-22 06:20":[{"name":"#groovy","query":"#groovy"},{"name":"#java","query":"#java"}]}}'''
+        }
+    }
+
+    void testExampleFromTheGep7Page() {
+        new StringWriter().with { w ->
+            def builder = new StreamingJsonBuilder( w )
+            builder.people {
+                person {
+                    firstName 'Guillame'
+                    lastName 'Laforge'
+                    // Maps are valid values for objects too
+                    address(
+                        city: 'Paris',
+                        country: 'France',
+                        zip: 12345,
+                    )
+                    married true
+                    conferences 'JavaOne', 'Gr8conf'
+                }
+            }
+
+            assert w.toString() == '{"people":{"person":{"firstName":"Guillame","lastName":"Laforge","address":{"city":"Paris","country":"France","zip":12345},"married":true,"conferences":["JavaOne","Gr8conf"]}}}'
+        }
+    }
+
+    void testEdgeCases() {
+        new StringWriter().with { w ->
+            def builder = new StreamingJsonBuilder( w )
+            builder { elem 1, 2, 3 }
+            
+            assert w.toString() == '{"elem":[1,2,3]}'
+        }
+        new StringWriter().with { w ->
+            def builder = new StreamingJsonBuilder( w )
+            builder.elem()
+            
+            assert w.toString() == '{"elem":{}}'
+        }
+        new StringWriter().with { w ->
+            def builder = new StreamingJsonBuilder( w )
+            builder.elem(a: 1, b: 2) { c 3 }
+            
+            assert w.toString() == '{"elem":{"a":1,"b":2,"c":3}}'
+        }
+        new StringWriter().with { w ->
+            def builder = new StreamingJsonBuilder( w )
+            builder.elem( [:] ) { c 3 }
+            
+            assert w.toString() == '{"elem":{"c":3}}'
+        }
+        new StringWriter().with { w ->
+            def builder = new StreamingJsonBuilder( w )
+            shouldFail(JsonException) {
+                builder.elem(a: 1, b: 2, "ABCD")
+            }
+        }
+    }
+}
