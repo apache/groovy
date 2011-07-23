d9e0d0ddeee4a19590dbf7d70a6c40f97aba8c25
Paul King <paulk@asert.com.au>
2011-07-23 02:09:17 +0000

    merge changeset 22657 from trunk: GROOVY-4939: groovyc compilation problem for return values with generics
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_8_X@22661 a5544e8c-8a19-0410-ba12-f9af4593a198

--- src/main/org/codehaus/groovy/ast/GenericsType.java
+++ src/main/org/codehaus/groovy/ast/GenericsType.java
@@ -53,16 +53,29 @@ public class GenericsType extends ASTNode {
     }
 
     public String toString() {
-        String ret = (type == null || placeholder || wildcard) ? name : type.getName();
+        String ret = (type == null || placeholder || wildcard) ? name : genericsBounds(type);
         if (upperBounds != null) {
             ret += " extends ";
             for (int i = 0; i < upperBounds.length; i++) {
-                ret += upperBounds[i].getName();
+                ret += genericsBounds(upperBounds[i]);
                 if (i + 1 < upperBounds.length) ret += " & ";
             }
         } else if (lowerBound != null) {
-            ret += " super " + lowerBound.getName();
+            ret += " super " + genericsBounds(lowerBound);
         }
+        return ret;
+    }
+
+    private String genericsBounds(ClassNode theType) {
+        String ret = theType.getName();
+        GenericsType[] genericsTypes = theType.getGenericsTypes();
+        if (genericsTypes == null || genericsTypes.length == 0) return ret;
+        ret += "<";
+        for (int i = 0; i < genericsTypes.length; i++) {
+            if (i != 0) ret += ", ";
+            ret += genericsTypes[i].toString();
+        }
+        ret += ">";
         return ret;
     }
 
