f846cd1878a3d9201b479c590b7bd89acad44b52
Paul King <paulk@asert.com.au>
2011-07-23 03:10:21 +0000

    merge changeset 22657 from trunk: GROOVY-4939: groovyc compilation problem for return values with generics
    git-svn-id: http://svn.codehaus.org/groovy/branches/GROOVY_1_7_X@22664 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/NestedGenericsTypesStubTest.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2003-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * Checks that nested generic types correctly appear within stubs.
+ *
+ * @author Paul King
+ */
+class NestedGenericsTypesStubTest extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+            'MyClass.groovy': '''
+                class MyClass {
+                   List<List<String>> getManyStringLists() { null }
+                }
+            ''',
+
+            'Main.java': '''
+                import java.util.List;
+                public class Main {
+                   public void main() {
+                      List<List<String>> result = new MyClass().getManyStringLists();
+                   }
+                }
+            '''
+        ]
+    }
+
+    void verifyStubs() {
+        def stubSource = stubJavaSourceFor('MyClass')
+        assert stubSource.contains('java.util.List<java.util.List<java.lang.String>> getManyStringLists()')
+    }
+}
