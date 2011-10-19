397364163a6448dcb00a0d9d2b584cb59b7dbbc1
Cedric Champeau <cedric.champeau@gmail.com>
2011-10-19 23:04:30 +0200

    Handle closure return type correctly when dealing with various primitive types

---
+++ src/test/groovy/transform/stc/ClosuresSTCTest.groovy
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.transform.stc
+
+import org.codehaus.groovy.util.ListHashMap
+
+/**
+ * Unit tests for static type checking : closures.
+ *
+ * @author Cedric Champeau
+ */
+class ClosuresSTCTest extends StaticTypeCheckingTestCase {
+
+    void testClosureWithoutArguments() {
+        assertScript '''
+        def clos = { println "hello!" }
+
+        println "Executing the Closure:"
+        clos() //prints "hello!"
+        '''
+    }
+
+    void testClosureWithArguments() {
+        assertScript '''
+            def printSum = { int a, int b -> print a+b }
+            printSum( 5, 7 ) //prints "12"
+        '''
+
+        shouldFailWithMessages '''
+            def printSum = { int a, int b -> print a+b }
+            printSum( '5', '7' ) //prints "12"
+        ''', 'Closure argument types: [int, int] do not match with parameter types: [java.lang.String, java.lang.String]'
+    }
+
+    void testClosureWithArgumentsAndNoDef() {
+        assertScript '''
+            { int a, int b -> print a+b }(5,7)
+        '''
+    }
+
+    void testClosureWithArgumentsNoDefAndWrongType() {
+        shouldFailWithMessages '''
+            { int a, int b -> print a+b }('5',7)
+        ''', 'Closure argument types: [int, int] do not match with parameter types: [java.lang.String, int]'
+    }
+
+    void testClosureReturnTypeInferrence() {
+        assertScript '''
+            def closure = { int x, int y -> return x+y }
+            int total = closure(2,3)
+        '''
+
+        shouldFailWithMessages '''
+            def closure = { int x, int y -> return x+y }
+            int total = closure('2',3)
+        ''', 'Closure argument types: [int, int] do not match with parameter types: [java.lang.String, int]'
+    }
+
+    void testClosureReturnTypeInferrenceWithoutDef() {
+        assertScript '''
+            int total = { int x, int y -> return x+y }(2,3)
+        '''
+    }
+
+    void testClosureReturnTypeInference() {
+        shouldFailWithMessages '''
+            def cl = { int x ->
+                if (x==0) {
+                    1L
+                } else {
+                    x // int
+                }
+            }
+            byte res = cl(0) // should throw an error because return type inference should be a Number
+        ''', 'Cannot assign value of type java.lang.Number to variable of type java.lang.Byte'
+    }
+
+}
+
