95c69a78bad6e4442c8c0aeaa70593be7f58558f
Cedric Champeau <cedric.champeau@gmail.com>
2011-10-25 16:44:53 +0200

    Avoid calls to DTT.intUnbox when top operand is a number type

--- src/main/org/codehaus/groovy/classgen/asm/OperandStack.java
+++ src/main/org/codehaus/groovy/classgen/asm/OperandStack.java
@@ -332,7 +332,11 @@ public class OperandStack {
         } else {
             castToTypeIfNecessary(top, targetType);
         }
-        BytecodeHelper.doCast(mv,targetType);
+        if (ClassHelper.isNumberType(top) && primTarget && ClassHelper.isNumberType(targetType)) {
+            BytecodeHelper.doCastToPrimitive(mv, top, targetType);
+        } else {
+            BytecodeHelper.doCast(mv,targetType);
+        }
         replace(targetType);
     }
 
