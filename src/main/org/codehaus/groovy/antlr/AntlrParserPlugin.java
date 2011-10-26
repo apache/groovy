f095b9e16fbaa4550b9c37dd6ec66e63389bff0e
Cedric Champeau <cedric.champeau@gmail.com>
2011-10-26 17:59:05 +0200

    Make double constants primitive

--- src/main/org/codehaus/groovy/antlr/AntlrParserPlugin.java
+++ src/main/org/codehaus/groovy/antlr/AntlrParserPlugin.java
@@ -2719,7 +2719,8 @@ public class AntlrParserPlugin extends ASTHelper implements ParserPlugin, Groovy
     protected ConstantExpression integerExpression(AST node) {
         String text = node.getText();
         Object number = Numbers.parseInteger(text);
-        ConstantExpression constantExpression = new ConstantExpression(number, number instanceof Integer);
+        boolean keepPrimitive = number instanceof Integer || number instanceof Long;
+        ConstantExpression constantExpression = new ConstantExpression(number, keepPrimitive);
         configureAST(constantExpression, node);
         return constantExpression;
     }
