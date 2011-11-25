2bdafa6e7d9e5acebd89995cc7d1c88722ac3277
Jochen Theodorou <blackdrag@gmx.org>
2011-11-25 21:14:23 +0100

    refactoring more into methods and adding basic coercion and wrapping code

--- src/main/org/codehaus/groovy/vmplugin/v7/IndyInterface.java
+++ src/main/org/codehaus/groovy/vmplugin/v7/IndyInterface.java
@@ -15,16 +15,21 @@
  */
 package org.codehaus.groovy.vmplugin.v7;
 
+import groovy.lang.GString;
 import groovy.lang.GroovyObject;
+import groovy.lang.MetaClass;
 import groovy.lang.MetaClassImpl;
 import groovy.lang.MetaMethod;
 
 import java.lang.invoke.*;
 import java.lang.invoke.MethodHandles.Lookup;
+import java.math.BigInteger;
 
 import org.codehaus.groovy.GroovyBugError;
 import org.codehaus.groovy.reflection.CachedMethod;
 import org.codehaus.groovy.runtime.InvokerHelper;
+import org.codehaus.groovy.runtime.MetaClassHelper;
+import org.codehaus.groovy.runtime.wrappers.Wrapper;
 
 /**
  * Bytecode level interface for bootstrap methods used by invokedynamic.
@@ -44,8 +49,20 @@ public class IndyInterface {
             }
         }
         private static MethodType GENERAL_INVOKER_SIGNATURE = MethodType.methodType(Object.class, Object.class, Object[].class);
-    
-    
+        private static MethodType INVOKE_METHOD_SIGNATURE = MethodType.methodType(Object.class, String.class, Object[].class);
+        private static MethodType O2O = MethodType.methodType(Object.class, Object.class);
+        private static MethodHandle UNWRAP_METHOD, TO_STRING, TO_BYTE, TO_BIGINT;
+        static {
+            try {
+                UNWRAP_METHOD = LOOKUP.findStatic(IndyInterface.class, "unwrap", O2O);
+                TO_STRING = LOOKUP.findStatic(IndyInterface.class, "coerceToString", O2O);
+                TO_BYTE = LOOKUP.findStatic(IndyInterface.class, "coerceToByte", O2O);
+                TO_BIGINT = LOOKUP.findStatic(IndyInterface.class, "coerceToBigInt", O2O);
+            } catch (Exception e) {
+                throw new GroovyBugError(e);
+            }
+        }
+        
         public static CallSite bootstrap(Lookup caller, String name, MethodType type) {
             // since indy does not give us the runtime types
             // we produce first a dummy call site, which then changes the target to one,
@@ -62,37 +79,142 @@ public class IndyInterface {
             return mc;
         }
         
-        public static Object selectMethod(MutableCallSite callSite, Class sender, String methodName, Object receiver, Object[] arguments) {
-            MetaClassImpl mc = null;
+        private static MetaClass getMetaClass(Object receiver) {
             if (receiver instanceof GroovyObject) {
-                mc = (MetaClassImpl) ((GroovyObject) receiver).getMetaClass(); 
+                return ((GroovyObject) receiver).getMetaClass(); 
             } else {
-                mc = (MetaClassImpl) InvokerHelper.getMetaClass(receiver);
+                return InvokerHelper.getMetaClass(receiver);
             }
-            MetaMethod m = mc.getMethodWithCaching(receiver.getClass(), methodName, arguments, false);
-            MethodType targetType = callSite.type();
-            if (m instanceof CachedMethod) {
-                CachedMethod cm = (CachedMethod) m;
-                MethodHandle mh;
+        }
+        
+        private static class CallInfo {
+            public Object[] args;
+            public MetaMethod method;
+            public MethodType targetType;
+            public String methodName;
+            public Object receiver;
+            public MethodHandle handle;
+            public boolean useMetaClass = false;
+        }
+        
+        private static void setHandleForMetaMethod(CallInfo info) {
+            if (info.method instanceof CachedMethod) {
+                CachedMethod cm = (CachedMethod) info.method;
                 try {
-                    mh = LOOKUP.unreflect(cm.getCachedMethod()).asType(targetType);
+                    info.handle = LOOKUP.unreflect(cm.getCachedMethod());
                 } catch (IllegalAccessException e) {
                     throw new GroovyBugError(e);
                 }
-                callSite.setTarget(mh);
             } else {
                 // receiver, args
-                MethodHandle mh;
                 try {
-                    mh = LOOKUP.findVirtual(m.getClass(), "invoke", GENERAL_INVOKER_SIGNATURE);
+                    info.handle = LOOKUP.findVirtual(info.method.getClass(), "invoke", GENERAL_INVOKER_SIGNATURE);
                 } catch (Exception e) {
                     throw new GroovyBugError(e);
                 }
-                mh = mh.bindTo(m).
-                        asCollector(Object[].class, targetType.parameterCount()-1).
-                        asType(targetType);
-                callSite.setTarget(mh);
+                info.handle = info.handle.bindTo(info.method).
+                                asCollector(Object[].class, info.targetType.parameterCount()-1);
             }
-            return m.invoke(receiver, arguments);
+        }
+        
+        private static void chooseMethod(MetaClass mc, CallInfo ci) {
+            if (mc instanceof MetaClassImpl) {
+                MetaClassImpl mci = (MetaClassImpl) mc;
+                ci.method = mci.getMethodWithCaching(ci.receiver.getClass(), ci.methodName, ci.args, false);
+            }
+        }
+        
+        private static void setMetaClassCallHandleIfNedded(MetaClass mc, CallInfo ci) {
+            if (ci.handle!=null) return;
+            try {
+                ci.useMetaClass = true;
+                ci.handle = LOOKUP.findVirtual(mc.getClass(), "invokeMethod", INVOKE_METHOD_SIGNATURE);
+            } catch (Exception e) {
+                throw new GroovyBugError(e);
+            }
+            ci.handle = ci.handle.bindTo(mc).
+                        asCollector(Object[].class, ci.targetType.parameterCount()-1);
+        }
+        
+        /**
+         * called by handle
+         */
+        public static Object unwrap(Object o) {
+            Wrapper w = (Wrapper) o;
+            return w.unwrap();
+        }
+        
+        /**
+         * called by handle
+         */
+        public static Object coerceToString(Object o) {
+            return o.toString();
+        }
+        
+        /**
+         * called by handle
+         */
+        public static Object coerceToByte(Object o) {
+            return new Byte(((Number) o).byteValue());
+        }
+        
+        /**
+         * called by handle
+         */
+        public static Object coerceToBigInt(Object o) {
+            return new BigInteger(String.valueOf((Number) o));
+        }
+        
+        private static void correctWrapping(CallInfo ci) {
+            if (ci.useMetaClass) return;
+            for (int i=0; i<ci.args.length; i++) {
+                if (ci.args[i] instanceof Wrapper) { 
+                    ci.handle = MethodHandles.filterArguments(ci.handle, i+1, UNWRAP_METHOD);
+                }
+            }
+        }
+        
+        private static void correctCoerce(CallInfo ci) {
+            if (ci.useMetaClass) return;
+            Class[] parameters = ci.targetType.parameterArray();
+            for (int i=0; i<ci.args.length; i++) {
+                Class got = ci.args[i].getClass(); 
+                if (got == GString.class && parameters[i+1] == String.class) {
+                    ci.handle = MethodHandles.filterArguments(ci.handle, i+1, TO_STRING);                    
+                } else if (parameters[i+1] == Byte.class && got != Byte.class) {
+                    ci.handle = MethodHandles.filterArguments(ci.handle, i+1, TO_BYTE);
+                } else if (parameters[i+1] == BigInteger.class && got != BigInteger.class) {
+                    ci.handle = MethodHandles.filterArguments(ci.handle, i+1, TO_BIGINT);
+                }
+            }
+        }
+        
+        public static Object selectMethod(MutableCallSite callSite, Class sender, String methodName, Object receiver, Object[] arguments) throws Throwable {
+            CallInfo callInfo = new CallInfo();
+            callInfo.targetType = callSite.type();
+            callInfo.methodName = methodName;
+            callInfo.receiver = receiver;
+            callInfo.args = arguments;
+            MetaClass mc = getMetaClass(receiver);
+            chooseMethod(mc, callInfo);
+            setHandleForMetaMethod(callInfo);
+            setMetaClassCallHandleIfNedded(mc, callInfo);
+            correctWrapping(callInfo);
+            correctCoerce(callInfo);
+            
+            callInfo.handle = callInfo.handle.asType(callInfo.targetType);
+            callSite.setTarget(callInfo.handle);
+            
+            MetaClassHelper.unwrap(arguments);
+            return callInfo.handle.invokeWithArguments(repack(receiver, arguments));
+        }
+        
+        private static Object[] repack(Object o, Object[] args) {
+            Object[] ar = new Object[args.length+1];
+            ar[0] = o;
+            for (int i=0; i<args.length; i++) {
+                ar[i+1] = args[i];
+            }
+            return ar;
         }
 }
