50150218deb9b54e894572c17f8267d15a56a3b3
Paul King <paulk@asert.com.au>
2011-12-08 23:06:14 +1000

    GROOVY-5176: incorrect handling of generics placeholders in JavaStubGenerator

--- src/main/org/codehaus/groovy/ast/GenericsType.java
+++ src/main/org/codehaus/groovy/ast/GenericsType.java
@@ -17,6 +17,7 @@
 package org.codehaus.groovy.ast;
 
 import org.codehaus.groovy.ast.tools.GenericsUtils;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -63,6 +64,7 @@ public class GenericsType extends ASTNode {
         Set<String> visited = new HashSet<String>();
         return toString(visited);
     }
+
     private String toString(Set<String> visited) {
         if (placeholder) visited.add(name);
         String ret = (type == null || placeholder || wildcard) ? name : genericsBounds(type, visited);
@@ -82,6 +84,10 @@ public class GenericsType extends ASTNode {
         String ret = theType.isArray()?theType.getComponentType().getName()+"[]":theType.getName();
         GenericsType[] genericsTypes = theType.getGenericsTypes();
         if (genericsTypes == null || genericsTypes.length == 0) return ret;
+        // TODO instead of catching Object<T> here stop it from being placed into type in first place
+        if (genericsTypes.length == 1 && genericsTypes[0].isPlaceholder() && theType.getName().equals("java.lang.Object")) {
+            return genericsTypes[0].getName();
+        }
         ret += "<";
         for (int i = 0; i < genericsTypes.length; i++) {
             if (i != 0) ret += ", ";
