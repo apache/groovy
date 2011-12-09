d705f4b0f7dfaf5aca0a3238dd94a268835c0873
Cedric Champeau <cedric.champeau@gmail.com>
2011-12-09 10:00:39 +0100

    GROOVY-4457: Generic types from owner class must be propagated to non static inner classes only

--- src/test/groovy/bugs/Groovy4457GenericTypeDeclarationLeakTest.groovy
+++ src/test/groovy/bugs/Groovy4457GenericTypeDeclarationLeakTest.groovy
@@ -31,4 +31,17 @@ class Groovy4457GenericTypeDeclarationLeakTest extends GroovyTestCase {
             assert A.B.declaredMethods.find { it.name == "foo" }.parameterTypes[0].name.contains("String")
         """
     }
+
+    void testNonStaticInnerClassGenerics() {
+        assertScript '''
+            class A<T> {
+                void bar(T s) {}
+                class B {
+                  void foo(T s) {}
+                }
+            }
+            assert A.class.methods.find{it.name=="bar"}.parameterTypes[0].name.contains("java.lang.Object")
+            assert A.B.class.methods.find{it.name=="foo"}.parameterTypes[0].name.contains("java.lang.Object")
+        '''
+    }
 }
