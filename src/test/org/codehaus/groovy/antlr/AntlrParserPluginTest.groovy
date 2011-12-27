3e8a8f65ea686172b4719c1d3824ebe0b2dc12bf
Paul King <paulk@asert.com.au>
2011-12-27 15:28:27 +1000

    GROOVY-5214: Source location incorrect for enums

--- src/test/org/codehaus/groovy/antlr/AntlrParserPluginTest.groovy
+++ src/test/org/codehaus/groovy/antlr/AntlrParserPluginTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2010 the original author or authors.
+ * Copyright 2003-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 package org.codehaus.groovy.antlr
 
 import org.codehaus.groovy.ast.builder.AstBuilder
+import org.codehaus.groovy.ast.*
+import static org.codehaus.groovy.control.CompilePhase.CONVERSION
 
 /**
  * Test for AntlrParserPlugin.
@@ -26,17 +28,30 @@ class AntlrParserPluginTest extends GroovyTestCase {
 
     void testInnerClassLineNumbers() {
 
-        def result = new AstBuilder().buildFromString(org.codehaus.groovy.control.CompilePhase.CONVERSION, false,  '''
+        def result = new AstBuilder().buildFromString CONVERSION, false, '''
             new Object() {
 
             }
-        ''')
+        '''
 
-        assert result[2].getClass() == org.codehaus.groovy.ast.InnerClassNode
+        assert result[2].getClass() == InnerClassNode
         assert result[2].lineNumber == 2
         assert result[2].lastLineNumber == 4
         assert result[2].columnNumber == 26
         assert result[2].lastColumnNumber == 14
+    }
 
+    void testEnumLineNumbers() {
+        def result = new AstBuilder().buildFromString CONVERSION, false, '''
+            enum Color {
+
+            }
+        '''
+
+        assert result[1].getClass() == ClassNode
+        assert result[1].lineNumber == 2
+        assert result[1].lastLineNumber == 4
+        assert result[1].columnNumber == 13
+        assert result[1].lastColumnNumber == 14
     }
 }
