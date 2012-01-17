a8ba177f4309b92730f446db3b31a7e9f9cf97d5
Jochen Theodorou <blackdrag@gmx.org>
2012-01-17 13:18:29 +0100

    fix mixup of boolean flags for super call and inside call (super was true before always)

--- src/main/org/codehaus/groovy/vmplugin/v7/IndyInterface.java
+++ src/main/org/codehaus/groovy/vmplugin/v7/IndyInterface.java
@@ -239,7 +239,7 @@ public class IndyInterface {
                 } else {
                     ci.handle = LOOKUP.findVirtual(MetaClass.class, "invokeMethod", INVOKE_METHOD_SIGNATURE);
                     ci.handle = ci.handle.bindTo(mc).bindTo(receiver.getClass());
-                    ci.handle = MethodHandles.insertArguments(ci.handle, ci.handle.type().parameterCount()-2, true, false);
+                    ci.handle = MethodHandles.insertArguments(ci.handle, ci.handle.type().parameterCount()-2, false, true);
                     if (receiver instanceof GroovyObject) {
                         // if the meta class call fails we may still want to fall back to call
                         // GroovyObject#invokeMethod if the receiver is a GroovyObject
