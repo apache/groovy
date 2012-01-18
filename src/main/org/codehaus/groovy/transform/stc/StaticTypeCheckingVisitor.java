14b42bf53a48e04966bdfb3843c6ea5d5a2272a4
Cedric Champeau <cedric.champeau@gmail.com>
2012-01-18 21:25:16 +0100

    Fixed inferred method return type which could be a generic placeholder instead of Object

--- src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -1894,6 +1894,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
             }
         }
         if (returnType.equals(OBJECT_TYPE)) {
+            if (copy[0].getType().isGenericsPlaceHolder()) return OBJECT_TYPE;
             return copy[0].getType();
         }
         if (returnType.isArray()) {
