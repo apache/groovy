c994e0a709f393f763faf2b14d6a3d19e932a70d
Tiago Fernandez <tiago.fernandez@gmail.com>
2012-01-30 15:15:57 +0100

    Add a test suite to JSR-223 module.
    [junit] Running UberTestCaseJSR223
    [junit] Tests run: 3, Failures: 0, Errors: 0, Time elapsed: 0.281 sec
    
    ...
    
    Class UberTestCaseJSR223
    
    Name                  Tests  Errors  Failures  Time(s)
    UberTestCaseJSR223      3       0       0      0.250
    
    should_forbid_an_instruction_when_overriding_GroovyClassLoader_using_reflection	 Success 0.203
    should_forbid_an_instruction_when_overriding_GroovyClassLoader_using_injection	 Success 0.000
    should_forbid_an_instruction_when_overriding_GroovyClassLoader_using_constructor Success 0.000

---
+++ subprojects/groovy-jsr223/src/test/java/UberTestCaseJSR223.java
@@ -0,0 +1,9 @@
+import org.codehaus.groovy.jsr223.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    JSR223SecurityTest.class
+})
+public class UberTestCaseJSR223 {}
