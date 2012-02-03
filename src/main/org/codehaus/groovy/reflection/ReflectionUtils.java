188441e28acd4d2801f209994363e6eef59a7a32
Jochen Theodorou <blackdrag@gmx.org>
2012-02-03 15:07:40 +0100

    add to be ignored packages for call class finding utility

--- src/main/org/codehaus/groovy/reflection/ReflectionUtils.java
+++ src/main/org/codehaus/groovy/reflection/ReflectionUtils.java
@@ -39,6 +39,8 @@ public class ReflectionUtils {
         IGNORED_PACKAGES.add("org.codehaus.groovy.runtime.metaclass");
         IGNORED_PACKAGES.add("org.codehaus.groovy.runtime");
         IGNORED_PACKAGES.add("sun.reflect");
+        IGNORED_PACKAGES.add("java.lang.invoke");
+        IGNORED_PACKAGES.add("org.codehaus.groovy.vmplugin.v7");
     }
 
     private static final Method MAGIC_METHOD;
