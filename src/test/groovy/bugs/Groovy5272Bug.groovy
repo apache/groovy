38ccd309c3cc0981a7f95e1253eebde314dd931e
Cedric Champeau <cedric.champeau@gmail.com>
2012-02-06 18:49:00 +0100

    Fixed GROOVY-5272: the patch didn't work for non inherited properties. Added more test cases with and without expando.

--- src/test/groovy/bugs/Groovy5272Bug.groovy
+++ src/test/groovy/bugs/Groovy5272Bug.groovy
@@ -39,6 +39,52 @@ class Groovy5272Bug extends GroovyTestCase {
             assert A.FOO == "Foo C"
             '''
         }
-
     }
+    
+    void testResolveConstantInSuperInterfaceWithExpando() {
+        assertScript '''
+            ExpandoMetaClass.enableGlobally()
+            interface Foo {
+                String FOO = 'FOO'
+            }
+            interface Bar extends Foo { }
+            assert Bar.FOO == 'FOO'
+            ExpandoMetaClass.disableGlobally()
+        '''
+    }
+
+    void testResolveConstantInSuperInterfaceWithoutExpando() {
+        assertScript '''
+            interface Foo {
+                String FOO = 'FOO'
+            }
+            interface Bar extends Foo { }
+            assert Bar.FOO == 'FOO'
+        '''
+    }
+
+    void testResolveConstantInClassWithSuperInterfaceWithoutExpando() {
+        assertScript '''
+            interface Foo {
+                String FOO = 'FOO'
+            }
+            interface Bar extends Foo { }
+            class Baz implements Bar {}
+            assert Baz.FOO == 'FOO'
+        '''
+    }
+
+    void testResolveConstantInClassWithSuperInterfaceWithExpando() {
+        assertScript '''
+            ExpandoMetaClass.enableGlobally()
+            interface Foo {
+                String FOO = 'FOO'
+            }
+            interface Bar extends Foo { }
+            class Baz implements Bar {}
+            assert Baz.FOO == 'FOO'
+            ExpandoMetaClass.disableGlobally()
+        '''
+    }
+
 }
