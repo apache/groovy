15683c26b710547e2ea6966253fd3e0e403d73ff
Paul King <paulk@asert.com.au>
2012-02-15 14:56:47 +1000

    manual merge of be61849 and 0aafa73 from master

--- src/main/org/codehaus/groovy/tools/groovydoc/GroovyDocTool.java
+++ src/main/org/codehaus/groovy/tools/groovydoc/GroovyDocTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2010 the original author or authors.
+ * Copyright 2007-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import org.codehaus.groovy.tools.shell.util.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -50,7 +51,10 @@ public class GroovyDocTool {
 
     public GroovyDocTool(ResourceManager resourceManager, String[] sourcepaths, String[] docTemplates, String[] packageTemplates, String[] classTemplates, List<LinkArgument> links, Properties properties) {
         rootDocBuilder = new GroovyRootDocBuilder(this, sourcepaths, links, properties);
+        String charset = properties.getProperty("charset");
+        properties.setProperty("charset", charset != null && charset.length() != 0 ? charset : Charset.defaultCharset().name());
         this.properties = properties;
+
         if (resourceManager == null) {
             templateEngine = null;
         } else {
