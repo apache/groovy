c851ef8aaeb16a6af017aa8b677e34eba324093f
Cedric Champeau <cedric.champeau@gmail.com>
2012-03-15 16:23:48 +0100

    Remove dependency on templating module

--- src/test/groovy/io/PlatformLineWriterTest.java
+++ src/test/groovy/io/PlatformLineWriterTest.java
@@ -1,26 +1,28 @@
 package groovy.io;
 
-import groovy.text.SimpleTemplateEngine;
-import groovy.text.Template;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 import junit.framework.TestCase;
 
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
 
 public class PlatformLineWriterTest extends TestCase {
 
     public void testPlatformLineWriter() throws IOException, ClassNotFoundException {
         String LS = System.getProperty("line.separator");
-        Map binding = new HashMap();
-        binding.put("first", "Tom");
-        binding.put("last", "Adams");
-        Template template = new SimpleTemplateEngine().createTemplate("<%= first %>\n<% println last %>");
+        Binding binding = new Binding();
+        binding.setVariable("first", "Tom");
+        binding.setVariable("last", "Adams");
         StringWriter stringWriter = new StringWriter();
         Writer platformWriter = new PlatformLineWriter(stringWriter);
-        template.make(binding).writeTo(platformWriter);
+        GroovyShell shell = new GroovyShell(binding);
+        platformWriter.write(shell.evaluate("\"$first\\n$last\\n\"").toString());
+        assertEquals("Tom" + LS + "Adams" + LS, stringWriter.toString());
+        stringWriter = new StringWriter();
+        platformWriter = new PlatformLineWriter(stringWriter);
+        platformWriter.write(shell.evaluate("\"$first\\r\\n$last\\r\\n\"").toString());
         assertEquals("Tom" + LS + "Adams" + LS, stringWriter.toString());
     }
 }
