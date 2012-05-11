21197a20b3384adf320c1b0fadd573dd4e8c918f
Cedric Champeau <cedric.champeau@gmail.com>
2012-05-11 15:59:59 +0200

    Removed unnecessary reference to bug class

--- src/test/org/codehaus/groovy/classgen/RunBugsTest.java
+++ src/test/org/codehaus/groovy/classgen/RunBugsTest.java
@@ -77,11 +77,6 @@ public class RunBugsTest extends TestSupport {
         object.invokeMethod("testInvisibleVariable", null);
     }
 
-    public void testStaticMarkupBug() throws Exception {
-        GroovyObject object = compile("src/test/groovy/bugs/StaticMarkupBug.groovy");
-        object.invokeMethod("testBug", null);
-    }
-
     public void testOverloadInvokeMethodBug() throws Exception {
         GroovyObject object = compile("src/test/groovy/bugs/OverloadInvokeMethodBug.groovy");
         object.invokeMethod("testBug", null);
