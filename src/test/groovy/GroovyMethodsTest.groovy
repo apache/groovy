31fde69ca6b3114f21108adfd33cdb2d1ea9ec01
Paul King <paulk@asert.com.au>
2012-06-11 10:17:32 +1000

    test tweak - make test slightly more robust

--- src/test/groovy/GroovyMethodsTest.groovy
+++ src/test/groovy/GroovyMethodsTest.groovy
@@ -804,8 +804,9 @@ class GroovyMethodsTest extends GroovyTestCase {
 
     void testThreadNaming() {
         def t = Thread.start("MyNamedThread"){
-            sleep 200 // give ourselves time to find the thread
+            sleep 300 // give ourselves time to find the thread
         }
+        sleep 50 // a little bit of time for t to start
         assert Thread.allStackTraces.keySet().any{ thread -> thread.name == 'MyNamedThread' }
         t.join()
     }
