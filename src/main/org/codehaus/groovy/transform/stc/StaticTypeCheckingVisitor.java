c85dd637dd49005dcda5a86c2ebdd70a0e4de5aa
Cedric Champeau <cedric.champeau@gmail.com>
2012-07-16 11:17:00 +0200

    GROOVY-5601: Fix side effect when left type of assignment doesn't use generics

--- src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -459,7 +459,8 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
     private void inferDiamondType(final ConstructorCallExpression cce, final ClassNode lType) {
         // check if constructor call expression makes use of the diamond operator
         ClassNode node = cce.getType();
-        if (node.isUsingGenerics() && node instanceof InnerClassNode && ((InnerClassNode) node).isAnonymous()) {
+        if (node.isUsingGenerics() && node instanceof InnerClassNode && ((InnerClassNode) node).isAnonymous()
+                && (node.getGenericsTypes()==null || node.getGenericsTypes().length==0) && lType.isUsingGenerics()) {
             // InterfaceA<Foo> obj = new InterfaceA<>() { ... }
             // InterfaceA<Foo> obj = new ClassA<>() { ... }
             // ClassA<Foo> obj = new ClassA<>() { ... }
