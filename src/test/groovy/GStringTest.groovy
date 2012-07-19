2b972826a02fd3cdadb3945901e2a19c923a6531
Jochen Theodorou <blackdrag@gmx.org>
2012-07-19 11:23:50 +0200

    GROOVY-5605: fix more indy method call transformations

--- src/test/groovy/GStringTest.groovy
+++ src/test/groovy/GStringTest.groovy
@@ -548,4 +548,11 @@ class GStringTest extends GroovyTestCase {
         assert gs.split() == ['The', 'quick', 'brown', 'fox'] as String[]
         assert gs.split('o') == ['The quick br', 'wn f', 'x'] as String[]
     }
+
+    def foo(String s) {1}
+    void testGStringArgumentForStringParameter() {
+        def a = 1
+        def b = "$a"
+        assert foo(b) == 1 
+    }
 }
