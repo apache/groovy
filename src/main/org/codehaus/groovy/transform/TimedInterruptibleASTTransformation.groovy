b744f215914f489082860c0502dea5f7babeb11e
Paul King <paulk@asert.com.au>
2012-09-07 21:27:28 +1000

    GROOVY-5691: TimeoutException immediately thrown when using both TimedInterrupt and with {} closure on a Map

--- src/main/org/codehaus/groovy/transform/TimedInterruptibleASTTransformation.groovy
+++ src/main/org/codehaus/groovy/transform/TimedInterruptibleASTTransformation.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2010 the original author or authors.
+ * Copyright 2008-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -140,7 +140,7 @@ public class TimedInterruptibleASTTransformation implements ASTTransformation {
       new IfStatement(
               new BooleanExpression(
                       new BinaryExpression(
-                              new VariableExpression('TimedInterrupt$expireTime'),
+                              new PropertyExpression(new VariableExpression("this"), 'TimedInterrupt$expireTime'),
                               new Token(Types.COMPARE_LESS_THAN, '<', -1, -1),
                               new StaticMethodCallExpression(
                                       ClassHelper.make(System),
@@ -155,7 +155,7 @@ public class TimedInterruptibleASTTransformation implements ASTTransformation {
                                               new ConstantExpression(
                                                       'Execution timed out after ' + maximum + ' units. Start time: '),
                                               new Token(Types.PLUS, '+', -1, -1),
-                                              new VariableExpression('TimedInterrupt$startTime'),
+                                              new PropertyExpression(new VariableExpression("this"), 'TimedInterrupt$startTime'),
                                       )
 
                               )
