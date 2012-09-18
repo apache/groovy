a7ef1ef39a0d95fc794f79dcef8c531c0d00361f
Guillaume Laforge <glaforge@gmail.com>
2012-09-18 09:07:09 +0200

    GROOVY-5710: Stub generator should not use raw types when casting default return values

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/DefaultValueReturnTypeShouldUseGenericsStubsTest.groovy
@@ -0,0 +1,34 @@
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * GROOVY-5710: Stub generator should not use raw types when casting default return values
+ *
+ * @author Guillaume Laforge
+ */
+class DefaultValueReturnTypeShouldUseGenericsStubsTest extends StringSourcesStubTestCase  {
+
+    protected void setUp() {
+        super.setUp()
+        debug = true;
+    }
+
+    Map<String, String> provideSources() {
+        [
+                'GantBinding.groovy': '''
+                    class GantBinding {
+                        private List<BuildListener> buildListeners = []
+                        List<BuildListener> getBuildListeners ( ) { buildListeners }
+                    }
+                ''',
+                'BuildListener.java': '''
+                    public class BuildListener {}
+                '''
+        ]
+    }
+
+    void verifyStubs() {
+        def stubSource = stubJavaSourceFor('GantBinding')
+
+        assert stubSource.contains('return (java.util.List<BuildListener>)null')
+    }
+}
