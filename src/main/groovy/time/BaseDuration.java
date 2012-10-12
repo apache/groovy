f84db0464eb9db93c326509cfd3694946ea2784d
Paul King <paulk@asert.com.au>
2012-10-12 19:04:43 +1000

    merge/port GROOVY-5751: TimeCategory toString bug

--- src/main/groovy/time/BaseDuration.java
+++ src/main/groovy/time/BaseDuration.java
@@ -102,8 +102,11 @@ public abstract class BaseDuration implements Comparable<BaseDuration> {
         if (this.hours      != 0) buffer.add(this.hours   + " hours");
         if (this.minutes    != 0) buffer.add(this.minutes + " minutes");
 
-        if (this.seconds != 0 || this.millis != 0)
-            buffer.add((seconds == 0 ? (millis < 0 ? "-0" : "0") : seconds) + "." + DefaultGroovyMethods.padLeft("" + Math.abs(millis), 3, "0")  + " seconds");
+        if (this.seconds != 0 || this.millis != 0) {
+            int norm_millis = millis % 1000;
+            int norm_seconds = seconds + DefaultGroovyMethods.intdiv(millis - norm_millis, 1000).intValue();
+            buffer.add((norm_seconds == 0 ? (norm_millis < 0 ? "-0" : "0") : norm_seconds) + "." + DefaultGroovyMethods.padLeft("" + Math.abs(norm_millis), 3, "0") + " seconds");
+        }
 
         if (buffer.size()!=0) {
             return DefaultGroovyMethods.join(buffer, ", ");
