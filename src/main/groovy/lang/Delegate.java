f9b7e12134a8e8f7dd3b93e90fe7936dd4df5267
Andre Steingress <me@andresteingress.com>
2013-01-08 23:26:51 +0100

    GROOVY-5446: Make @Delegate carry over method annotations

--- src/main/groovy/lang/Delegate.java
+++ src/main/groovy/lang/Delegate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2012 the original author or authors.
+ * Copyright 2008-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -140,4 +140,14 @@ public @interface Delegate {
      * @return true if owner class should delegate to methods annotated with @Deprecated
      */
     boolean deprecated() default false;
+
+    /**
+     * @return true if generated delegate methods should keep method annotations
+     */
+    boolean methodAnnotations() default false;
+
+    /**
+     * @return true if generated delegate methods should keep parameter annotations
+     */
+    boolean parameterAnnotations() default false;
 }
