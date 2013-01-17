5ef65881d793989d5d0cc71418e5540003cccf57
Andre Steingress <me@andresteingress.com>
2013-01-17 11:53:44 +0100

    GROOVY-5907 Object.with seems to lose typing when generics are used (minor tweaks)

--- src/test/groovy/transform/stc/WithSTCTest.groovy
+++ src/test/groovy/transform/stc/WithSTCTest.groovy
@@ -27,7 +27,6 @@ class WithSTCTest extends StaticTypeCheckingTestCase {
     // GROOVY-5907
     void testGenericReturnType() {
         assertScript '''
-            @groovy.transform.CompileStatic
             class Test {
               static List<String> a( String s ) {
                 s.with { String it -> [ "$it" ] }
@@ -40,7 +39,6 @@ class WithSTCTest extends StaticTypeCheckingTestCase {
 
     void testStringReturnType() {
         assertScript '''
-            @groovy.transform.CompileStatic
             class Test {
               static String a( String s ) {
                 s.with { String it -> it.toLowerCase() }
