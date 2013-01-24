9153a326ad53c1159960fe01935a2e4d6620c672
Paul King <paulk@asert.com.au>
2013-01-24 20:03:11 +1000

    spike for GROOVY-4701

--- src/main/org/codehaus/groovy/runtime/InvokerHelper.java
+++ src/main/org/codehaus/groovy/runtime/InvokerHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2012 the original author or authors.
+ * Copyright 2003-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,6 @@ import java.util.regex.Pattern;
  * A static helper class to make bytecode generation easier and act as a facade over the Invoker
  *
  * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
- * @version $Revision$
  */
 public class InvokerHelper {
     private static final Object[] EMPTY_MAIN_ARGS = new Object[]{new String[0]};
@@ -639,6 +638,10 @@ public class InvokerHelper {
     }
 
     private static String formatList(Collection collection, boolean verbose, int maxSize) {
+        return formatList(collection, verbose, maxSize, false);
+    }
+
+    private static String formatList(Collection collection, boolean verbose, int maxSize, boolean safe) {
         StringBuilder buffer = new StringBuilder("[");
         boolean first = true;
         for (Object item : collection) {
@@ -654,7 +657,20 @@ public class InvokerHelper {
             if (item == collection) {
                 buffer.append("(this Collection)");
             } else {
-                buffer.append(format(item, verbose, sizeLeft(maxSize, buffer)));
+                String str;
+                try {
+                    str = format(item, verbose, sizeLeft(maxSize, buffer));
+                } catch (Exception ex) {
+                    if (!safe) throw new GroovyRuntimeException(ex);
+                    String hash;
+                    try {
+                        hash = Integer.toHexString(item.hashCode());
+                    } catch (Exception ignored) {
+                        hash = "????";
+                    }
+                    str = "<" + item.getClass().getName() + "@" + hash + ">";
+                }
+                buffer.append(str);
             }
         }
         buffer.append("]");
@@ -720,12 +736,24 @@ public class InvokerHelper {
      * @return the string representation of the collection
      */
     public static String toListString(Collection arg, int maxSize) {
-        return formatList(arg, false, maxSize);
+        return toListString(arg, maxSize, false);
+    }
+
+    /**
+     * A helper method to return the string representation of a list with bracket boundaries "[" and "]".
+     *
+     * @param arg     the collection to process
+     * @param maxSize stop after approximately this many characters and append '...'
+     * @param safe    whether to use a default object representation for any item in the collection if an exception occurs when generating its toString
+     * @return the string representation of the collection
+     */
+    public static String toListString(Collection arg, int maxSize, boolean safe) {
+        return formatList(arg, false, maxSize, safe);
     }
 
     /**
      * A helper method to return the string representation of an array of objects
-     * with brace boundaries "{" and "}".
+     * with brace boundaries "[" and "]".
      *
      * @param arguments the array to process
      * @return the string representation of the array
@@ -745,6 +773,19 @@ public class InvokerHelper {
         }
         argBuf.append(ebdry);
         return argBuf.toString();
+    }
+
+    /**
+     * A helper method to return the string representation of an array of objects
+     * with brace boundaries "[" and "]".
+     *
+     * @param arguments the array to process
+     * @param maxSize stop after approximately this many characters and append '...'
+     * @param safe    whether to use a default object representation for any item in the array if an exception occurs when generating its toString
+     * @return the string representation of the array
+     */
+    public static String toArrayString(Object[] arguments, int maxSize, boolean safe) {
+        return toListString(DefaultTypeTransformation.asCollection(arguments), maxSize, safe);
     }
 
     public static List createRange(Object from, Object to, boolean inclusive) {
