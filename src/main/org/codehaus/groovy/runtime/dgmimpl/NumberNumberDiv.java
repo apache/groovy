8cb5a1ab84dcc1e9fe22b602ccdd3f46af0705eb
Jochen Theodorou <blackdrag@gmx.org>
2013-01-28 17:43:05 +0100

    GROOVY-4458 fix call site caching bug in that a numeric operation did not check the receiver to change to for exmaple null

--- src/main/org/codehaus/groovy/runtime/dgmimpl/NumberNumberDiv.java
+++ src/main/org/codehaus/groovy/runtime/dgmimpl/NumberNumberDiv.java
@@ -98,7 +98,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Integer) receiver).doubleValue() / ((Float) arg).doubleValue());
                 }
             }
@@ -115,7 +115,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Integer) receiver).intValue() / ((Double) arg).doubleValue());
                 }
             }
@@ -132,7 +132,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Long) receiver).doubleValue() / ((Float) arg).doubleValue());
                 }
             }
@@ -149,7 +149,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Long) receiver).doubleValue() / ((Double) arg).doubleValue());
                 }
             }
@@ -166,7 +166,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() / ((Integer) arg).doubleValue());
                 }
             }
@@ -183,7 +183,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() / ((Long) arg).doubleValue());
                 }
             }
@@ -200,7 +200,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() / ((Float) arg).doubleValue());
                 }
             }
@@ -225,7 +225,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() / ((Double) arg).doubleValue());
                 }
             }
@@ -242,7 +242,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() / ((Integer) arg).doubleValue());
                 }
             }
@@ -267,7 +267,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() / ((Long) arg).doubleValue());
                 }
             }
@@ -284,7 +284,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() / ((Float) arg).doubleValue());
                 }
             }
@@ -301,7 +301,7 @@ public final class NumberNumberDiv extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() / ((Double) arg).doubleValue());
                 }
             }
--- src/main/org/codehaus/groovy/runtime/dgmimpl/NumberNumberMinus.java
+++ src/main/org/codehaus/groovy/runtime/dgmimpl/NumberNumberMinus.java
@@ -107,7 +107,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() - ((Double) arg).doubleValue());
                 }
             }
@@ -124,7 +124,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() - ((Float) arg).doubleValue());
                 }
             }
@@ -141,7 +141,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() - ((Long) arg).doubleValue());
                 }
             }
@@ -158,7 +158,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Double) receiver).doubleValue() - ((Integer) arg).intValue());
                 }
             }
@@ -175,7 +175,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() - ((Double) arg).doubleValue());
                 }
             }
@@ -192,7 +192,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() - ((Float) arg).doubleValue());
                 }
             }
@@ -209,7 +209,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() - ((Long) arg).doubleValue());
                 }
             }
@@ -226,7 +226,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Float) receiver).doubleValue() - ((Integer) arg).doubleValue());
                 }
             }
@@ -243,7 +243,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Long) receiver).doubleValue() - ((Double) arg).doubleValue());
                 }
             }
@@ -260,7 +260,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Long) receiver).doubleValue() - ((Float) arg).doubleValue());
                 }
             }
@@ -277,7 +277,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Long(((Long) receiver).longValue() - ((Long) arg).longValue());
                 }
             }
@@ -294,7 +294,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Long(((Long) receiver).longValue() - ((Integer) arg).longValue());
                 }
             }
@@ -311,7 +311,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Integer) receiver).doubleValue() - ((Double) arg).doubleValue());
                 }
             }
@@ -328,7 +328,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Double(((Integer) receiver).doubleValue() - ((Float) arg).doubleValue());
                 }
             }
@@ -345,7 +345,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                     return new Long(((Integer) receiver).longValue() - ((Long) arg).longValue());
                 }
             }
@@ -362,7 +362,7 @@ public final class NumberNumberMinus extends NumberNumberMetaMethod {
 
         public final Object call(Object receiver, Object arg) throws Throwable {
             try {
-                if (checkPojoMetaClass()) {
+                if (checkCall(receiver)) {
                   return new Integer(((Integer) receiver).intValue() - ((Integer) arg).intValue());
                 }
             }
