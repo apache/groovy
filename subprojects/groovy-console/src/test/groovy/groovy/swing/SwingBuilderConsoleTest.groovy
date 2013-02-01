93e721d602487ef141b9bc1dfdd744de4cf68ccb
Paul King <paulk@asert.com.au>
2013-02-01 10:35:57 +1000

    fix test on Windows

--- subprojects/groovy-console/src/test/groovy/groovy/swing/SwingBuilderConsoleTest.groovy
+++ subprojects/groovy-console/src/test/groovy/groovy/swing/SwingBuilderConsoleTest.groovy
@@ -1,6 +1,6 @@
 package groovy.swing
 /*
- * Copyright 2003-2012 the original author or authors.
+ * Copyright 2003-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -320,19 +320,21 @@ class SwingBuilderConsoleTest extends GroovySwingTestCase {
             def swing = new SwingBuilder()
             swing.controller = new Console()
 
-            // we need to init the console actions - menu bar referes to them
+            // we need to init the console actions - menu bar refers to them
             swing.build(consoleActions)
 
             def basicMenuBar = swing.build(basicMenuBarScript)
             def macOSXMenuBar = swing.build(macOSXMenuBarScript)
 
-            // check if we have to same main menu items
-            assert macOSXMenuBar.subElements*.text == basicMenuBar.subElements*.text - 'Help'
+            // check if we have the same main menu items
+            if (macOSXMenuBar) { // null on windows
+                assert macOSXMenuBar.subElements*.text == basicMenuBar.subElements*.text - 'Help'
 
-            // check whether the amount of sub menu elements and their types complies to the basic menu bar
-            macOSXMenuBar.subElements.eachWithIndex { menu, i ->
-                assert menu.subElements.size() == basicMenuBar.subElements[i].subElements.size()
-                assert menu.subElements*.class == basicMenuBar.subElements[i].subElements*.class
+                // check whether the amount of sub menu elements and their types complies to the basic menu bar
+                macOSXMenuBar.subElements.eachWithIndex { menu, i ->
+                    assert menu.subElements.size() == basicMenuBar.subElements[i].subElements.size()
+                    assert menu.subElements*.class == basicMenuBar.subElements[i].subElements*.class
+                }
             }
         }
     }
