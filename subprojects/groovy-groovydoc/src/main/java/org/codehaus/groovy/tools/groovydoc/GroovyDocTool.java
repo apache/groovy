2d69b55490e4135ab110ce72007b9c860afd1dda
Paul King <paulk@asert.com.au>
2013-02-19 10:49:47 +1000

    GROOVY-6004: Groovydoc should process annotations more intelligently

--- subprojects/groovy-groovydoc/src/main/java/org/codehaus/groovy/tools/groovydoc/GroovyDocTool.java
+++ subprojects/groovy-groovydoc/src/main/java/org/codehaus/groovy/tools/groovydoc/GroovyDocTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2012 the original author or authors.
+ * Copyright 2007-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -104,7 +104,7 @@ public class GroovyDocTool {
     String getPath(String filename) {
         String path = new File(filename).getParent();
         // path length of 1 indicates that probably is 'default package' i.e. "/"
-        if (path == null || path.length() == 1) {
+        if (path == null || (path.length() == 1 && !Character.isJavaIdentifierStart(path.charAt(0)))) {
             path = "DefaultPackage"; // "DefaultPackage" for 'default package' path, rather than null...
         }
         return path;
