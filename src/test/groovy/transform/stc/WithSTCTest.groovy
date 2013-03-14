9251f1b2dd013244165e0ced56035ce744c33602
Cedric Champeau <cedric.champeau@gmail.com>
2013-03-14 11:15:57 +0100

    GROOVY-6051: Fix NPE in generics return type inference

--- src/test/groovy/transform/stc/WithSTCTest.groovy
+++ src/test/groovy/transform/stc/WithSTCTest.groovy
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package groovy.transform.stc
 
 /**
@@ -27,7 +29,7 @@ class WithSTCTest extends StaticTypeCheckingTestCase {
         assertScript '''
             class Test {
               static List<String> a( String s ) {
-                s.with { String it -> [ "$it" ] }
+                s.with { String it -> [ "$it".toString() ] }
               }
             }
 
