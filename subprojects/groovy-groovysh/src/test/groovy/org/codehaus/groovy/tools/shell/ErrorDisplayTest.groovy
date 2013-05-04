be8128c06e0aa22c4d47221f6cdf1654970db48f
Thibault Kruse <kruset@in.tum.de>
2013-05-04 21:43:16 +0200

    added missing License headers

--- subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/ErrorDisplayTest.groovy
+++ subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/ErrorDisplayTest.groovy
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.codehaus.groovy.tools.shell
 
 import jline.ConsoleReader
