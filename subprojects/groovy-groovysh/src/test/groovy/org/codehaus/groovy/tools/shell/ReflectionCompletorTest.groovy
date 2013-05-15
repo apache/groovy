8750ea1c5caa68262a3f0e37dfe4ee676fdcebf3
Thibault Kruse <kruset@in.tum.de>
2013-05-15 08:50:15 +0200

    test IO class

--- subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/ReflectionCompletorTest.groovy
+++ subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/ReflectionCompletorTest.groovy
@@ -23,6 +23,22 @@ import org.codehaus.groovy.tools.shell.commands.ImportCommand
 
 class GroovyshCompletorTest extends GroovyTestCase {
 
+    void testIOMock() {
+        IO testio
+        ByteArrayOutputStream mockOut
+        ByteArrayOutputStream mockErr
+        mockOut = new ByteArrayOutputStream();
+        mockErr = new ByteArrayOutputStream();
+        testio = new IO(
+                new ByteArrayInputStream(),
+                mockOut,
+                mockErr)
+        testio.out.println("mockResult")
+        assertTrue("stdout=" + mockOut.toString() + "\nstderr=" + mockErr.toString(), mockOut.toString().contains('mockResult'))
+        testio.err.println("mockErrResult")
+        assertTrue("stdout=" + mockOut.toString() + "\nstderr=" + mockErr.toString(), mockErr.toString().contains('mockErrResult'))
+    }
+
     void testLiveClass() {
         /* This test setup looks weird, but it is the only I found that can reproduce this behavior:
 groovy:000> class Foo extends HashSet implements Comparable {int compareTo(Object) {0}}
