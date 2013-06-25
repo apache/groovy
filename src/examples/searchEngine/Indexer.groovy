d9aed8ff187784c3fd82707feeebe17b3b7c1e3a
Cedric Champeau <cedric.champeau@gmail.com>
2013-06-25 14:20:28 +0200

    GROOVY-6216: Upgrade to ASM 4.1     * move version numbers to variables     * upgrade ASM to 4.1     * upgrade Lucene example to Lucene 4.3.1     * make "examples" task pass compilation     * make "examples" task triggered by the "check" task

--- src/examples/searchEngine/Indexer.groovy
+++ src/examples/searchEngine/Indexer.groovy
@@ -2,6 +2,12 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.document.Document
 import org.apache.lucene.document.Field
 import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.util.Version
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.store.FSDirectory
+import org.apache.lucene.document.TextField
+import org.apache.lucene.document.StringField
+import static org.apache.lucene.document.Field.Store.*
 
 /**
  * Indexer: traverses a file system and indexes .txt files
@@ -10,13 +16,14 @@ import org.apache.lucene.index.IndexWriter
  * based on examples in the wonderful 'Lucene in Action' book
  * by Erik Hatcher and Otis Gospodnetic ( http://www.lucenebook.com )
  *
- * requires a lucene-1.x.x.jar from http://lucene.apache.org
+ * June 25th, 2013: Updated for Lucene 4.3.1
+ * requires a lucene-4.3.x.jar from http://lucene.apache.org
  */
 
 if (args.size() != 2 ) {
     throw new Exception("Usage: groovy -cp lucene-1.4.3.jar Indexer <index dir> <data dir>")
 }
-def indexDir = new File(args[0]) // Create Lucene index in this directory
+def indexDir = FSDirectory.open(new File(args[0])) // Create Lucene index in this directory
 def dataDir = new File(args[1]) // Index files in this directory
 
 def start = new Date().time
@@ -29,16 +36,15 @@ def index(indexDir, dataDir) {
     if (!dataDir.exists() || !dataDir.directory) {
         throw new IOException("$dataDir does not exist or is not a directory")
     }
-    def writer = new IndexWriter(indexDir, new StandardAnalyzer(), true) // Create Lucene index
-    writer.useCompoundFile = false
+    def config = new IndexWriterConfig(Version.LUCENE_43, new StandardAnalyzer(Version.LUCENE_43))
+    def writer = new IndexWriter(indexDir, config) // Create Lucene index
 
     dataDir.eachFileRecurse {
         if (it.name =~ /.txt$/) { // Index .txt files only
             indexFile(writer,it)
         }
     }
-    def numIndexed = writer.docCount()
-    writer.optimize()
+    def numIndexed = writer.numDocs()
     writer.close() // Close index
     return numIndexed
 }
@@ -50,10 +56,10 @@ void indexFile(writer, f) {
     def doc = new Document()
 
     // Construct a Field that is tokenized and indexed, but is not stored in the index verbatim.
-    doc.add(Field.Text("contents", new FileReader(f)))
+    doc.add(new TextField("contents", f.newReader()))
 
     // Construct a Field that is not tokenized, but is indexed and stored.
-    doc.add(Field.Keyword("filename",f.canonicalPath))
+    doc.add(new StringField("filename",f.canonicalPath, YES))
 
     writer.addDocument(doc) // Add document to Lucene index
 }
