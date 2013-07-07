3ceebc09c9711d5c41d5edd8d9815532e0f36463
PascalSchumacher <pascalschumacher@gmx.net>
2013-07-07 08:52:46 +0200

    fixed package declaration, updated copyright

--- subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/completion/ReflectionCompletorTest.groovy
+++ subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/completion/ReflectionCompletorTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 the original author or authors.
+ * Copyright 2003-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,11 @@
  * limitations under the License.
  */
 
-package org.codehaus.groovy.tools.shell
+package org.codehaus.groovy.tools.shell.completion
 
 import org.codehaus.groovy.antlr.parser.GroovyLexer
 import org.codehaus.groovy.tools.shell.completion.ReflectionCompletor
+import org.codehaus.groovy.tools.shell.Interpreter
 
 import static org.codehaus.groovy.tools.shell.completion.TokenUtilTest.tokenList
 import static org.codehaus.groovy.tools.shell.completion.TokenUtilTest.tokensString
