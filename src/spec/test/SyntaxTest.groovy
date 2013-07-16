d98c5f35ce063be7db7c13cdfc97f6a5b75f9d8b
Guillaume Laforge <glaforge@gmail.com>
2013-07-16 11:44:03 +0200

    Section on identifiers

--- src/spec/test/SyntaxTest.groovy
+++ src/spec/test/SyntaxTest.groovy
@@ -1,4 +1,25 @@
-class SyntaxTest extends GroovyTestCase {
+import gls.CompilableTestSupport
+
+class SyntaxTest extends CompilableTestSupport {
+
+    void testValidIdentifiers() {
+        // tag::valid_identifiers[]
+        def name
+        def item3
+        def with_underscore
+        def $dollarStart
+        // end::valid_identifiers[]
+    }
+
+    void testInvalidIdentifiers() {
+        shouldNotCompile '''
+            // tag::invalid_identifiers[]
+            def 3tier
+            def a+b
+            def a#b
+            // end::invalid_identifiers[]
+        '''
+    }
 
     void testVariableDefinition() {
         // tag::variable_definition[]
