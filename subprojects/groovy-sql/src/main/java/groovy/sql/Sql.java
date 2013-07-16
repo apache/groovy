5cc2797255eb7d3fbc3a29f4e79e8df2a7712954
PascalSchumacher <pascalschumacher@gmx.net>
2013-07-16 20:00:32 +0200

    GROOVY-6262: Sql.withTransaction setAutoCommit in finally not wrapped in try/catch

--- subprojects/groovy-sql/src/main/java/groovy/sql/Sql.java
+++ subprojects/groovy-sql/src/main/java/groovy/sql/Sql.java
@@ -3054,7 +3054,14 @@ public class Sql {
             handleError(connection, e);
             throw e;
         } finally {
-            if (connection != null) connection.setAutoCommit(savedAutoCommit);
+            if (connection != null) {
+                try {
+                    connection.setAutoCommit(savedAutoCommit);
+                }
+                catch (SQLException e) {
+                    LOG.finest("Caught exception resetting auto commit: " + e.getMessage() + " - continuing");
+                }
+            }
             cacheConnection = false;
             closeResources(connection, null);
             cacheConnection = savedCacheConnection;
