c008184ce4f207a9505821c8aad248fd456af3a9
Andrey Bloschetsov <bloschetsov@gmail.com>
2013-08-02 12:16:48 +0400

    Underscore in literal added

--- src/spec/test/SyntaxTest.groovy
+++ src/spec/test/SyntaxTest.groovy
@@ -2,6 +2,19 @@ import gls.CompilableTestSupport
 
 class SyntaxTest extends CompilableTestSupport {
 
+    void testUnderscoreInNumber() {
+        // tag::underscore_in_number_example
+        long creditCardNumber = 1234_5678_9012_3456L
+        long socialSecurityNumbers = 999_99_9999L
+        double monetaryAmount = 12_345_132.12
+        long hexBytes = 0xFF_EC_DE_5E
+        long hexWords = 0xFFEC_DE5E
+        long maxLong = 0x7fff_ffff_ffff_ffffL
+        long alsoMaxLong = 9_223_372_036_854_775_807L
+        long bytes = 0b11010010_01101001_10010100_10010010
+        // end::underscore_in_number_example
+    }
+
     void testNumberTypeSuffixes() {
         // tag::number_type_suffixes_example[]
         assert 42I == new Integer("42")
