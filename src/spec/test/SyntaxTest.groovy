1318c312d52087f54a3c02d0d9296334836be641
Andrey Bloschetsov <bloschetsov@gmail.com>
2013-08-02 17:17:20 +0400

    Number type suffixes improved

--- src/spec/test/SyntaxTest.groovy
+++ src/spec/test/SyntaxTest.groovy
@@ -86,14 +86,19 @@ class SyntaxTest extends CompilableTestSupport {
 
     void testNumberTypeSuffixes() {
         // tag::number_type_suffixes_example[]
-        assert 42I == new Integer("42")
-        assert 123L == new Long("123")
-        assert 2147483648 == new Long("2147483648") //Long type used, value too large for an Integer
-        assert 456G == new java.math.BigInteger("456")
-        assert 123.45 == new java.math.BigDecimal("123.45") //default BigDecimal type used
-        assert 1.200065D == new Double("1.200065")
-        assert 1.234F == new Float("1.234")
-        assert 1.23E23D == new Double("1.23E23")
+        assert 42I == new Integer('42')
+        assert 42i == new Integer('42') //lowercase i more readable
+        assert 123L == new Long("123") //uppercase L more readable
+        assert 2147483648 == new Long('2147483648') //Long type used, value too large for an Integer
+        assert 456G == new java.math.BigInteger('456')
+        assert 456g == new java.math.BigInteger('456')
+        assert 123.45 == new java.math.BigDecimal('123.45') //default BigDecimal type used
+        assert 1.200065D == new Double('1.200065')
+        assert 1.234F == new Float('1.234')
+        assert 1.23E23D == new Double('1.23E23')
+        assert 0b1111L.class == Long // binary
+        assert 0xFFi.class == Integer // hexadecimal
+        assert 034G.class == BigInteger // ocatal
         // end::number_type_suffixes_example[]
     }
 
