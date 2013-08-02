abbfe8a2d0b94e1c3568b4467627a3c9ee474367
Guillaume Laforge <glaforge@gmail.com>
2013-08-02 16:56:00 +0200

    Non-needed FQN since Big(Decimal|Integer) are imported by default in Groovy

--- src/spec/test/SyntaxTest.groovy
+++ src/spec/test/SyntaxTest.groovy
@@ -90,9 +90,9 @@ class SyntaxTest extends CompilableTestSupport {
         assert 42i == new Integer('42') //lowercase i more readable
         assert 123L == new Long("123") //uppercase L more readable
         assert 2147483648 == new Long('2147483648') //Long type used, value too large for an Integer
-        assert 456G == new java.math.BigInteger('456')
-        assert 456g == new java.math.BigInteger('456')
-        assert 123.45 == new java.math.BigDecimal('123.45') //default BigDecimal type used
+        assert 456G == new BigInteger('456')
+        assert 456g == new BigInteger('456')
+        assert 123.45 == new BigDecimal('123.45') //default BigDecimal type used
         assert 1.200065D == new Double('1.200065')
         assert 1.234F == new Float('1.234')
         assert 1.23E23D == new Double('1.23E23')
