b138c5ea6048479fce660a44778705d59c9d1ef9
Cedric Champeau <cedric.champeau@gmail.com>
2013-08-09 10:08:38 +0200

    Fixed broken unit tests

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/Groovy5859Support.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2003-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.tools.stubgenerator;
+
+import java.util.Map;
+import java.util.SortedMap;
+
+public class Groovy5859Support<K,V> {
+    public Groovy5859Support(SortedMap<? extends K, ? extends V> map) {}
+
+    public void putAll(final Map<? extends K, ? extends V> m) {
+    }
+}
