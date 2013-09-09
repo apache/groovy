4cb3192235a162db8c8b7158d0152bcb8e391f7d
Jochen Theodorou <blackdrag@gmx.org>
2013-09-09 15:12:49 +0200

    GROOVY-6196: fix property name resolving

--- src/test/groovy/transform/stc/MiscSTCTest.groovy
+++ src/test/groovy/transform/stc/MiscSTCTest.groovy
@@ -290,7 +290,7 @@ class MiscSTCTest extends StaticTypeCheckingTestCase {
         '''
     }
 
-    // GROOVY-6165
+    // GROOVY-6165 && GROOVY-6196
     void testPropertyNameFromMethodName() {
         // too bad we're not using Spock!
 
@@ -306,7 +306,8 @@ class MiscSTCTest extends StaticTypeCheckingTestCase {
                 ['get', 'get_foo', '_foo'],
                 [null, 'foo', null],
                 ['foo', null, null],
-                [null,null,null]
+                [null,null,null],
+                ['get', 'getaList', 'aList']
         ]
         tests.each { prefix, methodName, expectation ->
             assert StaticTypeCheckingVisitor.extractPropertyNameFromMethodName(prefix, methodName) == expectation
