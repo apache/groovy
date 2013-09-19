f15cbb8885a74011cf7ec5bd05d6894e55f0b024
Tim Yates <tyates@picr.man.ac.uk>
2013-09-19 14:34:15 +0100

    CompileStatic fix

--- src/test/groovy/transform/ImmutableTest.groovy
+++ src/test/groovy/transform/ImmutableTest.groovy
@@ -2,7 +2,7 @@ package groovy.transform
 
 class ImmutableTest extends GroovyTestCase {
     public void testCopyWith() {
-        def tester = new GroovyClassLoader().parseClass( 
+        def tester = new GroovyClassLoader().parseClass(
           '''@groovy.transform.Immutable(addCopyWith = true)
             |class Person {
             |    String first, last
@@ -24,13 +24,61 @@ class ImmutableTest extends GroovyTestCase {
         assert !alice.is( tim )
     }
 
+    public void testStaticCopyWith() {
+        def tester = new GroovyClassLoader().parseClass(
+          '''@groovy.transform.Immutable(addCopyWith = true)
+            |@groovy.transform.CompileStatic
+            |class Person {
+            |    String first, last
+            |}
+            |'''.stripMargin() )
+
+        // One instance
+        def tim = tester.newInstance( first:'tim', last:'yates' )
+        assert tim.first == 'tim'
+
+        // This should be the same instance and no changes
+        def tim2 = tim.copyWith( first:'tim' )
+        assert tim.is( tim2 )
+
+        // This should be a new instance with a new firstname
+        def alice = tim.copyWith( first:'alice' )
+        assert tim != alice
+        assert alice.first == 'alice'
+        assert !alice.is( tim )
+    }
+
+    public void testTypedCopyWith() {
+        def tester = new GroovyClassLoader().parseClass(
+          '''@groovy.transform.Immutable(addCopyWith = true)
+            |@groovy.transform.TypeChecked
+            |class Person {
+            |    String first, last
+            |}
+            |'''.stripMargin() )
+
+        // One instance
+        def tim = tester.newInstance( first:'tim', last:'yates' )
+        assert tim.first == 'tim'
+
+        // This should be the same instance and no changes
+        def tim2 = tim.copyWith( first:'tim' )
+        assert tim.is( tim2 )
+
+        // This should be a new instance with a new firstname
+        def alice = tim.copyWith( first:'alice' )
+        assert tim != alice
+        assert alice.first == 'alice'
+        assert !alice.is( tim )
+    }
+
     public void testCopyWithSkipping() {
-        def tester = new GroovyClassLoader().parseClass( 
+        def tester = new GroovyClassLoader().parseClass(
           '''@groovy.transform.Immutable(addCopyWith = true)
             |class Person {
             |    String first, last
             |    List<Person> copyWith( i ) {
-            |        [ this ] * i    
+            |        (1..i).collect { this }
             |    }
             |}
             |'''.stripMargin() )
@@ -38,7 +86,51 @@ class ImmutableTest extends GroovyTestCase {
         // One instance
         def tim = tester.newInstance( first:'tim', last:'yates' )
         assert tim.first == 'tim'
-        
+
+        // Check original copyWith remains
+        def result = tim.copyWith( 2 )
+        assert result.size() == 2
+        assert result.first == [ 'tim', 'tim' ]
+    }
+
+    public void testStaticCopyWithSkipping() {
+        def tester = new GroovyClassLoader().parseClass(
+          '''@groovy.transform.Immutable(addCopyWith = true)
+            |@groovy.transform.CompileStatic
+            |class Person {
+            |    String first, last
+            |    List<Person> copyWith( i ) {
+            |        (1..i).collect { this }
+            |    }
+            |}
+            |'''.stripMargin() )
+
+        // One instance
+        def tim = tester.newInstance( first:'tim', last:'yates' )
+        assert tim.first == 'tim'
+
+        // Check original copyWith remains
+        def result = tim.copyWith( 2 )
+        assert result.size() == 2
+        assert result.first == [ 'tim', 'tim' ]
+    }
+
+    public void testTypedCopyWithSkipping() {
+        def tester = new GroovyClassLoader().parseClass(
+          '''@groovy.transform.Immutable(addCopyWith = true)
+            |@groovy.transform.CompileStatic
+            |class Person {
+            |    String first, last
+            |    List<Person> copyWith( i ) {
+            |        (1..i).collect { this }
+            |    }
+            |}
+            |'''.stripMargin() )
+
+        // One instance
+        def tim = tester.newInstance( first:'tim', last:'yates' )
+        assert tim.first == 'tim'
+
         // Check original copyWith remains
         def result = tim.copyWith( 2 )
         assert result.size() == 2
