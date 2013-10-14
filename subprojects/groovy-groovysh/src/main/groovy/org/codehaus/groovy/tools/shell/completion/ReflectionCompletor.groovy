7a38cefb608ad8c2f3fa7b5055d213d65b3010f5
Thibault Kruse <kruset@in.tum.de>
2013-10-14 18:44:24 +0200

    useless function call

--- subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/completion/ReflectionCompletor.groovy
+++ subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/completion/ReflectionCompletor.groovy
@@ -273,7 +273,6 @@ class ReflectionCompletor {
      */
     static Collection<String> getPublicFieldsAndMethods(Object instance, String prefix) {
         Set<String> rv = new HashSet<String>()
-        Set.getInterfaces()
         Class clazz = instance.getClass()
         if (clazz == null) {
             return rv;
