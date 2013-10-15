ebffac6e26ee36cc9f63ff45f4e4517f06000125
Pascal Schumacher <pascalschumacher@gmx.net>
2013-10-15 21:24:19 +0200

    make test succeed even if ANSI is not supported by test platform

--- subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/AnsiDetectorTest.groovy
+++ subprojects/groovy-groovysh/src/test/groovy/org/codehaus/groovy/tools/shell/AnsiDetectorTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 the original author or authors.
+ * Copyright 2003-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ class AnsiDetectorTest extends GroovyTestCase {
 
     void testAnsiCall() {
         AnsiDetector det = new AnsiDetector()
-        assert det.call()
+        assert det.call() instanceof Boolean
     }
 
 }
