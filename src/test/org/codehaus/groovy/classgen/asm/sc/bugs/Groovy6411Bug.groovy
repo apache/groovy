890081d2c02a4a3c52a340b89965e813b557d9a1
Cedric Champeau <cedric.champeau@gmail.com>
2013-11-22 15:00:57 +0100

    GROOVY-6411: Make direct method call to bridge methods from closures

---
+++ src/test/org/codehaus/groovy/classgen/asm/sc/bugs/Groovy6411Bug.groovy
@@ -0,0 +1,43 @@
+package org.codehaus.groovy.classgen.asm.sc.bugs
+
+import groovy.transform.stc.StaticTypeCheckingTestCase
+import org.codehaus.groovy.classgen.asm.sc.StaticCompilationTestSupport
+
+@Mixin(StaticCompilationTestSupport)
+class Groovy6411Bug extends StaticTypeCheckingTestCase {
+    @Override
+    protected void setUp() {
+        super.setUp()
+        extraSetup()
+    }
+
+    void testShouldNotThrowInvokerInvocationException() {
+            assertScript '''
+    class Client<T> {
+      static boolean caughtIOEx = false
+      static boolean caughtInvokerEx = false
+
+      static String method() {
+        return ({
+          try {
+            return doSomething()
+          } catch (IOException ioe) {
+            caughtIOEx = true
+          } catch (Exception e) {
+            caughtInvokerEx = true
+          }
+        })()
+      }
+
+      private static <T> T doSomething() throws IOException {
+        throw new IOException()
+      }
+    }
+
+    Client.method()
+    assert Client.caughtIOEx
+    assert !Client.caughtInvokerEx
+
+    '''
+    }
+}
