0268510504aed9a9752e0a516990131fcb2d340e
Guillaume Laforge <glaforge@gmail.com>
2013-11-26 17:38:22 +0100

    GROOVY-6268: a bit more coverage on numbers

--- src/spec/test/SyntaxTest.groovy
+++ src/spec/test/SyntaxTest.groovy
@@ -491,4 +491,86 @@ class SyntaxTest extends CompilableTestSupport {
                 ].each { dollarSlashy.contains(it) }
         // end::dollar_slashy_1[]
     }
+
+    void testIntegralNumberDeclarations() {
+        // tag::int_decl[]
+        // primitive types
+        byte  b = 1
+        char  c = 2
+        short s = 3
+        int   i = 4
+        long  l = 5
+
+        // infinite precision
+        BigInteger bi =  6
+        // end::int_decl[]
+
+        assert b  instanceof Byte
+        assert c  instanceof Character
+        assert s  instanceof Short
+        assert i  instanceof Integer
+        assert l  instanceof Long
+        assert bi instanceof BigInteger
+    }
+
+    void testDecimalNumberDeclarations() {
+        // tag::float_decl[]
+        // primitive types
+        float  f = 1.234
+        double d = 2.345
+
+        // infinite precision
+        BigDecimal bd =  3.456
+        // end::float_decl[]
+
+        assert f  instanceof Float
+        assert d  instanceof Double
+        assert bd instanceof BigDecimal
+    }
+
+    void testWideningIntegralValueTypes() {
+        // positive values
+        // tag::wide_int_positive[]
+        def a = 1
+        assert a instanceof Integer
+
+        // Integer.MAX_VALUE
+        def b = 2147483647
+        assert b instanceof Integer
+
+        // Integer.MAX_VALUE + 1
+        def c = 2147483648
+        assert c instanceof Long
+
+        // Long.MAX_VALUE
+        def d = 9223372036854775807
+        assert d instanceof Long
+
+        // Long.MAX_VALUE + 1
+        def e = 9223372036854775808
+        assert e instanceof BigInteger
+        // end::wide_int_positive[]
+
+        // negative values
+        // tag::wide_int_negative[]
+        def na = -1
+        assert na instanceof Integer
+
+        // Integer.MIN_VALUE
+        def nb = -2147483648
+        assert nb instanceof Integer
+
+        // Integer.MIN_VALUE - 1
+        def nc = -2147483649
+        assert nc instanceof Long
+
+        // Long.MIN_VALUE
+        def nd = -9223372036854775808
+        assert nd instanceof Long
+
+        // Long.MIN_VALUE - 1
+        def ne = -9223372036854775809
+        assert ne instanceof BigInteger
+        // end::wide_int_negative[]
+    }
 }
