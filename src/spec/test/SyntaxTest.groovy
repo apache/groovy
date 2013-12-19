3384ae4f69cbea40031f64883eca223b1bf3a472
Guillaume Laforge <glaforge@gmail.com>
2013-12-19 13:00:24 +0100

    GROOVY-6268: covering the power operator

--- src/spec/test/SyntaxTest.groovy
+++ src/spec/test/SyntaxTest.groovy
@@ -582,9 +582,45 @@ class SyntaxTest extends CompilableTestSupport {
         // end::wide_int_negative[]
     }
 
-    void testNumberDivision() {
-        // tag::number_division[]
+    void testNumberPower() {
+        // tag::number_power[]
+        // base and exponent are ints and the result can be represented by an Integer
+        assert    2    **   3    instanceof Integer    //  8
+        assert   10    **   9    instanceof Integer    //  1_000_000_000
 
-        // end::number_division[]
+        // the base is a long, so fit the result in a Long
+        // (although it could have fit in an Integer)
+        assert    5L   **   2    instanceof Long       //  25
+
+        // the result can't be represented as an Integer or Long, so return a BigInteger
+        assert  100    **  10    instanceof BigInteger //  10e20
+        assert 1234    ** 123    instanceof BigInteger //  170515806212727042875...
+
+        // the base is a BigDecimal and the exponent a negative int
+        // but the result can be represented as an Integer
+        assert    0.5  **  -2    instanceof Integer    //  4
+
+        // the base is an int, and the exponent a negative float
+        // but again, the result can be represented as an Integer
+        assert    1    **  -0.3f instanceof Integer    //  1
+
+        // the base is an int, and the exponent a negative int
+        // but the result will be calculated as a Double
+        // (both base and exponent are actually converted to doubles)
+        assert   10    **  -1    instanceof Double     //  0.1
+
+        // the base is a BigDecimal, and the exponent is an int, so return a BigDecimal
+        assert    1.2  **  10    instanceof BigDecimal //  6.1917364224
+
+        // the base is a float or double, and the exponent is an int
+        // but the result can only be represented as a Double value
+        assert    3.4f **   5    instanceof Double     //  454.35430372146965
+        assert    5.6d **   2    instanceof Double     //  31.359999999999996
+
+        // the exponent is a decimal value
+        // and the result can only be represented as a Double value
+        assert    7.8  **   1.9  instanceof Double     //  49.542708423868476
+        assert    2    **   0.1f instanceof Double     //  1.0717734636432956
+        // end::number_power[]
     }
 }
