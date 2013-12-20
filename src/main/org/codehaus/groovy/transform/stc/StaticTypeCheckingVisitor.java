043e8f60e09630b8ab07e3ed2aa30fea194818a8
Cedric Champeau <cedric.champeau@gmail.com>
2013-12-20 12:35:29 +0100

    GROOVY-6496: Compilation failure if an extension method uses a primitive type and a generic type

--- src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -3760,6 +3760,9 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
         }
 
         Map<String, GenericsType> resolvedPlaceholders = null;
+        if (isPrimitiveType(receiver) && !isPrimitiveType(declaringClass)) {
+            receiver = getWrapper(receiver);
+        }
         ClassNode current = receiver;
         while (true) {
             //extract the place holders
