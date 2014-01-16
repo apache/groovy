e112a0ae4ce908e9416e38458106ddbef8861b09
Cedric Champeau <cedric.champeau@gmail.com>
2014-01-16 10:33:25 +0100

    GROOVY-6521: GStringTemplate engine is sharing the binding

--- subprojects/groovy-templates/src/test/groovy/groovy/SimpleGStringTemplateEngineTest.groovy
+++ subprojects/groovy-templates/src/test/groovy/groovy/SimpleGStringTemplateEngineTest.groovy
@@ -20,4 +20,27 @@ public class SimpleGStringTemplateEngineTest extends GroovyTestCase
     template.make().writeTo(sw);
     assertEquals("\nHello World!", sw.toString());
   }
+
+    void testShouldNotShareBinding() {
+        String text = "<% println delegate.books; books = books.split(\",\"); out << books %>";
+
+        StringWriter sw = new StringWriter();
+        GStringTemplateEngine engine = new GStringTemplateEngine();
+
+        Template template = engine.createTemplate(text);
+
+        Map data = [books: 'a,b,c,d']
+
+
+        // round one sucess
+        template.make(data).writeTo(sw);
+        assert sw.toString() == '[a, b, c, d]'
+
+        sw = new StringWriter();
+        // round two fails
+        data = [books: 'e,f,g,h']
+        template.make(data).writeTo(sw);
+        assert sw.toString() == '[e, f, g, h]'
+
+    }
 }
