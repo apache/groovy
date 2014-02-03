72855e2bb0e6700248d21247767487989df32b7e
Richard Hightower <richardhightower@gmail.com>
2014-02-03 10:39:40 -0800

    I had no luck getting offset and count to work with JDK 1.6. I disable FastStringUtils if JDK 1.6 is encountered, i.e., if I find a Count and Offset, I turn off FastStrngUtils. This works, and we get the speed for JDK 1.7.
    I noticed you use Unsafe. From what I can tell it is almost optional,
    but not 100%. There is a chance to find a slightly different
    implementation of String out there.
    
    When you do
    https://github.com/groovy/groovy-core/pull/317/files#diff-d5083c2146fab6
    0490dfd6f620890fdfR61 I assume your thought is that either all or none
    of the fields there are available. Effectively if the field count is
    not available, your test for direct access still passes and then line
    93 would fail.
    
    To avoid checking against multiple things all the time I would suggest
    to add a boolean for this... or you reuse ENABLED for this - then you
    need to set the value later.
    
    what do you think? Jochen Theodorou

--- subprojects/groovy-json/src/main/java/groovy/json/internal/FastStringUtils.java
+++ subprojects/groovy-json/src/main/java/groovy/json/internal/FastStringUtils.java
@@ -22,6 +22,8 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 import java.nio.charset.*;
 
+import static groovy.json.internal.Exceptions.sputs;
+
 /**
  * @author Rick Hightower
  * @author Stephane Landelle
@@ -35,80 +37,93 @@ public class FastStringUtils {
     public static final boolean ENABLED;
 
     private static final boolean WRITE_TO_FINAL_FIELDS = Boolean.parseBoolean( System.getProperty( "groovy.json.faststringutils.write.to.final.fields", "false" ) );
-    private static final boolean DISABLE = Boolean.parseBoolean( System.getProperty( "groovy.json.faststringutils.disable", "true" ) );
+    private static final boolean DISABLE = Boolean.parseBoolean( System.getProperty( "groovy.json.faststringutils.disable", "false" ) );
+
 
     static {
 
-        if ( !DISABLE ) {
-            Unsafe unsafe;
+        boolean enabled = !DISABLE; //Check to see if it is forced to disabled.
+        Unsafe unsafe = null;
+        boolean hasCountAndOffset = false;
+        long valueFieldOffset = -1L;
+        long offsetFieldOffset = -1L;
+        long countFieldOffset = -1L;
+
+
+        if ( enabled ) {
             try {
+                /* Lookup unsafe field, if there are any problems abort. */
                 Field unsafeField = Unsafe.class.getDeclaredField( "theUnsafe" );
                 unsafeField.setAccessible( true );
                 unsafe = ( Unsafe ) unsafeField.get( null );
 
             } catch ( Throwable cause ) {
                 unsafe = null;
+                enabled = false;
+            }
+        }
+
+
+        /* Now that we know Unsafe works, let's grab the string fields. */
+        if ( enabled ) {
+            try {
+                /* Older strings have value, offset, and count. Newer strings only have value. */
+                valueFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "value" ) );
+                offsetFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "offset" ) );
+                countFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "count" ) );
+                hasCountAndOffset = true;
+            } catch ( Throwable cause ) {
+                hasCountAndOffset = false;
             }
 
-            UNSAFE = unsafe;
-            ENABLED = unsafe != null;
+            /* If for some reason we did not find value, then disable the whole thing. */
+            enabled = valueFieldOffset != -1;
+        }
 
-            long stringValueFieldOffset = -1L;
-            long stringOffsetFieldOffset = -1L;
-            long stringCountFieldOffset = -1L;
 
-            if ( ENABLED ) {
-                try {
-                    stringValueFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "value" ) );
-                    stringOffsetFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "offset" ) );
-                    stringCountFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "count" ) );
-                } catch ( Throwable cause ) {
-                }
-            }
+        STRING_VALUE_FIELD_OFFSET = valueFieldOffset;
+        STRING_OFFSET_FIELD_OFFSET = offsetFieldOffset;
+        STRING_COUNT_FIELD_OFFSET = countFieldOffset;
+        ENABLED = enabled && !hasCountAndOffset;
+        UNSAFE = unsafe;
 
-            STRING_VALUE_FIELD_OFFSET = stringValueFieldOffset;
-            STRING_OFFSET_FIELD_OFFSET = stringOffsetFieldOffset;
-            STRING_COUNT_FIELD_OFFSET = stringCountFieldOffset;
+    }
 
+
+    public static char[] toCharArray( final String string ) {
+        if ( ENABLED  ) {
+            return ( char[] ) UNSAFE.getObject( string, STRING_VALUE_FIELD_OFFSET );
         } else {
-            STRING_VALUE_FIELD_OFFSET = -1;
-            STRING_OFFSET_FIELD_OFFSET = -1;
-            STRING_COUNT_FIELD_OFFSET = -1;
-            UNSAFE = null;
-            ENABLED = false;
+            /* Here we just go ahead an use the default, the only downside is an extra buffer copy. */
+            return string.toCharArray();
         }
     }
 
-    public static boolean hasUnsafe() {
-        return ENABLED;
-    }
+    private static char[] toCharArrayWithCountOffset( String string ) {
 
-    public static char[] toCharArray( final String string ) {
-        if ( ENABLED ) {
+        try {
             char[] value = ( char[] ) UNSAFE.getObject( string, STRING_VALUE_FIELD_OFFSET );
 
-            if ( STRING_OFFSET_FIELD_OFFSET != -1 ) {
-                // old String version with offset and count
-                Integer offset = ( Integer ) UNSAFE.getObject( string, STRING_OFFSET_FIELD_OFFSET );
-                Integer count = ( Integer ) UNSAFE.getObject( string, STRING_COUNT_FIELD_OFFSET );
+                /* old String version with offset and count  */
+            Integer offset = ( Integer ) UNSAFE.getObject( string, STRING_OFFSET_FIELD_OFFSET );
+            Integer count = ( Integer ) UNSAFE.getObject( string, STRING_COUNT_FIELD_OFFSET );
 
-                if ( (offset==null || value == null) || (offset == 0 && count == value.length) ) {
-                    // no need to copy
+
+
+            if ( (offset == 0 && count == value.length) ) {
+                    /* no need to copy since the offset is 0 and the length and count are the same. */
                     return value;
 
-                } else {
+            } else {
+                    /* A subset of the string was used, so only copy a subset over. */
                     char result[] = new char[ count ];
                     System.arraycopy( value, offset, result, 0, count );
                     return result;
-                }
-
-            } else {
-                return value;
             }
-
-        } else {
-            return string.toCharArray();
+        } catch (Exception ex) {
+            return Exceptions.handle(char[].class, sputs( "STRING str", string, ex.getMessage() ), ex);
         }
+
     }
 
     public static char[] toCharArray( final CharSequence charSequence ) {
