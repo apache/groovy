b03926cf258b5bbdfbb52faebc36f51c5ee06ade
Guillaume Laforge <glaforge@gmail.com>
2014-02-20 17:04:45 +0100

    GROOVY-6268: assignment binary operators

--- src/spec/test/OperatorsTest.groovy
+++ src/spec/test/OperatorsTest.groovy
@@ -11,5 +11,63 @@ class OperatorsTest extends CompilableTestSupport {
         assert 10  % 3 == 1
         assert  2 ** 3 == 8
         // end::binary_arith_ops[]
+
+        // tag::unary_plus_minus[]
+        assert +3 == 3
+        assert -4 == 0 - 4
+
+        assert -(-1) == 1  // <1>
+        // end::unary_plus_minus[]
+
+        // tag::plusplus_minusminus[]
+        def a = 2
+        def b = a++ * 3             // <1>
+
+        assert a == 3 && b == 6
+
+        def c = 3
+        def d = c-- * 2             // <2>
+
+        assert c == 2 && d == 6
+
+        def e = 1
+        def f = ++e + 3             // <3>
+
+        assert e == 2 && f == 5
+
+        def g = 4
+        def h = --g + 1             // <4>
+
+        assert g == 3 && h == 4
+        // end::plusplus_minusminus[]
+    }
+
+    void testArithmeticOperatorsWithAssignment() {
+        // tag::binary_assign_operators[]
+        def a = 4
+        a += 3
+
+        assert a == 7
+
+        def b = 5
+        b -= 3
+
+        assert b == 2
+
+        def c = 5
+        c *= 3
+
+        assert c == 15
+
+        def d = 10
+        d /= 2
+
+        assert d == 5
+
+        def e = 10
+        e %= 3
+
+        assert e == 1
+        // end::binary_assign_operators[]
     }
 }
