9ac1d78071589466d9de6fb39160aa4accbec93c
Guillaume Laforge <glaforge@gmail.com>
2014-02-20 12:10:21 +0100

    GROOVY-6268: first blurb on the arithmetic operators

--- src/spec/test/OperatorsTest.groovy
+++ src/spec/test/OperatorsTest.groovy
@@ -2,6 +2,14 @@ import gls.CompilableTestSupport
 
 class OperatorsTest extends CompilableTestSupport {
 
-    void test() {
+    void testArithmeticOperators() {
+        // tag::binary_arith_ops[]
+        assert  1  + 2 == 3
+        assert  4  - 3 == 1
+        assert  3  * 5 == 15
+        assert  3  / 2 == 1.5
+        assert 10  % 3 == 1
+        assert  2 ** 3 == 8
+        // end::binary_arith_ops[]
     }
 }
