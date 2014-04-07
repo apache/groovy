6a106011209fb2bcde6e18b619ab25e5e2a3be25
Paul King <paulk@asert.com.au>
2014-04-07 12:57:29 +1000

    minor refactor

--- src/main/org/codehaus/groovy/transform/ReadWriteLockASTTransformation.java
+++ src/main/org/codehaus/groovy/transform/ReadWriteLockASTTransformation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2013 the original author or authors.
+ * Copyright 2008-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,16 +17,27 @@ package org.codehaus.groovy.transform;
 
 import groovy.transform.WithReadLock;
 import groovy.transform.WithWriteLock;
-import org.codehaus.groovy.ast.*;
-import org.codehaus.groovy.ast.expr.*;
-import org.codehaus.groovy.ast.stmt.*;
+import org.codehaus.groovy.GroovyBugError;
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.ast.AnnotatedNode;
+import org.codehaus.groovy.ast.AnnotationNode;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.ast.FieldNode;
+import org.codehaus.groovy.ast.MethodNode;
+import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.ast.stmt.Statement;
+import org.codehaus.groovy.ast.stmt.TryCatchStatement;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
-import org.codehaus.groovy.syntax.Token;
-import org.objectweb.asm.Opcodes;
 
-import java.util.Arrays;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import static org.codehaus.groovy.ast.ClassHelper.make;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.block;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.callX;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.ctorX;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.stmt;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.varX;
 
 
 /**
@@ -39,18 +50,16 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author Hamlet D'Arcy
  */
 @GroovyASTTransformation(phase = CompilePhase.CANONICALIZATION)
-public class ReadWriteLockASTTransformation implements ASTTransformation, Opcodes {
+public class ReadWriteLockASTTransformation extends AbstractASTTransformation {
 
-    private static final ClassNode READ_LOCK_TYPE = ClassHelper.make(WithReadLock.class);
-    private static final ClassNode WRITE_LOCK_TYPE = ClassHelper.make(WithWriteLock.class);
-    private static final ClassNode LOCK_TYPE = ClassHelper.make(ReentrantReadWriteLock.class);
-    private static final Token ASSIGN = Token.newSymbol("=", -1, -1);
+    private static final ClassNode READ_LOCK_TYPE = make(WithReadLock.class);
+    private static final ClassNode WRITE_LOCK_TYPE = make(WithWriteLock.class);
+    private static final ClassNode LOCK_TYPE = make(ReentrantReadWriteLock.class);
+    public static final String DEFAULT_STATIC_LOCKNAME = "$REENTRANTLOCK";
+    public static final String DEFAULT_INSTANCE_LOCKNAME = "$reentrantlock";
 
     public void visit(ASTNode[] nodes, SourceUnit source) {
-        if (nodes.length != 2 || !(nodes[0] instanceof AnnotationNode) || !(nodes[1] instanceof AnnotatedNode)) {
-            throw new RuntimeException("Internal error: expecting [AnnotationNode, AnnotatedNode] but got: " + Arrays.asList(nodes));
-        }
-
+        init(nodes, source);
         AnnotatedNode parent = (AnnotatedNode) nodes[1];
         AnnotationNode node = (AnnotationNode) nodes[0];
         final boolean isWriteLock;
@@ -59,74 +68,73 @@ public class ReadWriteLockASTTransformation implements ASTTransformation, Opcode
         } else if (WRITE_LOCK_TYPE.equals(node.getClassNode())) {
             isWriteLock = true;
         } else {
-            throw new RuntimeException("Internal error: expecting [" + READ_LOCK_TYPE.getName() + ", " + WRITE_LOCK_TYPE.getName() + "]" + " but got: " + node.getClassNode().getName());
+            throw new GroovyBugError("Internal error: expecting [" + READ_LOCK_TYPE.getName() + ", " + WRITE_LOCK_TYPE.getName() + "]" + " but got: " + node.getClassNode().getName());
         }
 
         String myTypeName = "@" + node.getClassNode().getNameWithoutPackage();
 
-        Expression valueExpr = node.getMember("value");
-        String value = null;
-        if (valueExpr instanceof ConstantExpression) {
-            ConstantExpression ce = (ConstantExpression) valueExpr;
-            Object valueObject = ce.getValue();
-            if (valueObject != null) value = valueObject.toString();
-        }
+        String value = getMemberStringValue(node, "value");
 
         if (parent instanceof MethodNode) {
             MethodNode mNode = (MethodNode) parent;
             ClassNode cNode = mNode.getDeclaringClass();
             String lockExpr = determineLock(value, cNode, mNode.isStatic(), myTypeName);
+            if (lockExpr == null) return;
 
             // get lock type
             final Expression lockType;
             if (isWriteLock) {
-                lockType = new MethodCallExpression(new VariableExpression(lockExpr), "writeLock", ArgumentListExpression.EMPTY_ARGUMENTS);
+                lockType = callX(varX(lockExpr), "writeLock");
             } else {
-                lockType = new MethodCallExpression(new VariableExpression(lockExpr), "readLock", ArgumentListExpression.EMPTY_ARGUMENTS);
+                lockType = callX(varX(lockExpr), "readLock");
             }
 
-            MethodCallExpression acquireLock = new MethodCallExpression(lockType, "lock", ArgumentListExpression.EMPTY_ARGUMENTS);
-            MethodCallExpression releaseLock = new MethodCallExpression(lockType, "unlock", ArgumentListExpression.EMPTY_ARGUMENTS);
+            Expression acquireLock = callX(lockType, "lock");
+            Expression releaseLock = callX(lockType, "unlock");
             Statement originalCode = mNode.getCode();
 
-            BlockStatement body = new BlockStatement();
-            body.addStatement(new ExpressionStatement(acquireLock));
-            body.addStatement(new TryCatchStatement(originalCode, new ExpressionStatement(releaseLock)));
-            mNode.setCode(body);
+            mNode.setCode(block(
+                    stmt(acquireLock),
+                    new TryCatchStatement(originalCode, stmt(releaseLock))));
         }
     }
 
     private String determineLock(String value, ClassNode targetClass, boolean isStatic, String myTypeName) {
-        if (value != null && value.length() > 0 && !value.equalsIgnoreCase("$reentrantlock")) {
-            if (targetClass.getDeclaredField(value) == null) {
-                throw new RuntimeException("Error during " + myTypeName + " processing: lock field with name '" + value + "' not found in class " + targetClass.getName());
+        if (value != null && value.length() > 0 && !value.equalsIgnoreCase(DEFAULT_INSTANCE_LOCKNAME)) {
+            FieldNode existingLockField = targetClass.getDeclaredField(value);
+            if (existingLockField == null) {
+                addError("Error during " + myTypeName + " processing: lock field with name '" + value + "' not found in class " + targetClass.getName(), targetClass);
+                return null;
             }
-            if (targetClass.getDeclaredField(value).isStatic() != isStatic) {
-                throw new RuntimeException("Error during " + myTypeName + " processing: lock field with name '" + value + "' should " + (isStatic ? "" : "not ") + "be static");
+            if (existingLockField.isStatic() != isStatic) {
+                addError("Error during " + myTypeName + " processing: lock field with name '" + value + "' should " + (isStatic ? "" : "not ") + "be static", existingLockField);
+                return null;
             }
             return value;
         }
         if (isStatic) {
-            FieldNode field = targetClass.getDeclaredField("$REENTRANTLOCK");
+            FieldNode field = targetClass.getDeclaredField(DEFAULT_STATIC_LOCKNAME);
             if (field == null) {
                 int visibility = ACC_PRIVATE | ACC_STATIC | ACC_FINAL;
-                targetClass.addField("$REENTRANTLOCK", visibility, LOCK_TYPE, createLockObject());
+                targetClass.addField(DEFAULT_STATIC_LOCKNAME, visibility, LOCK_TYPE, createLockObject());
             } else if (!field.isStatic()) {
-                throw new RuntimeException("Error during " + myTypeName + " processing: $REENTRANTLOCK field must be static");
+                addError("Error during " + myTypeName + " processing: " + DEFAULT_STATIC_LOCKNAME + " field must be static", field);
+                return null;
             }
-            return "$REENTRANTLOCK";
+            return DEFAULT_STATIC_LOCKNAME;
         }
-        FieldNode field = targetClass.getDeclaredField("$reentrantlock");
+        FieldNode field = targetClass.getDeclaredField(DEFAULT_INSTANCE_LOCKNAME);
         if (field == null) {
             int visibility = ACC_PRIVATE | ACC_FINAL;
-            targetClass.addField("$reentrantlock", visibility, LOCK_TYPE, createLockObject());
+            targetClass.addField(DEFAULT_INSTANCE_LOCKNAME, visibility, LOCK_TYPE, createLockObject());
         } else if (field.isStatic()) {
-            throw new RuntimeException("Error during " + myTypeName + " processing: $reentrantlock field must not be static");
+            addError("Error during " + myTypeName + " processing: " + DEFAULT_INSTANCE_LOCKNAME + " field must not be static", field);
+            return null;
         }
-        return "$reentrantlock";
+        return DEFAULT_INSTANCE_LOCKNAME;
     }
 
     private Expression createLockObject() {
-        return new ConstructorCallExpression(LOCK_TYPE, MethodCallExpression.NO_ARGUMENTS);
+        return ctorX(LOCK_TYPE);
     }
 }
