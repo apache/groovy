4d8f6b7243d8dc8f314666bfbe41c8ac534cf7a4
Cedric Champeau <cedric.champeau@gmail.com>
2014-04-11 15:31:56 +0200

    GROOVY-6650: Fix method selection in case of last argument being an array

---
+++ src/test/org/codehaus/groovy/classgen/asm/sc/bugs/Groovy6650Bug.groovy
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2003-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.codehaus.groovy.classgen.asm.sc.bugs
+
+import groovy.transform.stc.StaticTypeCheckingTestCase
+import org.codehaus.groovy.classgen.asm.sc.StaticCompilationTestSupport
+
+@Mixin(StaticCompilationTestSupport)
+class Groovy6650Bug extends StaticTypeCheckingTestCase {
+    @Override
+    protected void setUp() {
+        super.setUp()
+        extraSetup()
+    }
+
+    void testShouldChooseVargsMethod() {
+        assertScript '''import org.codehaus.groovy.transform.stc.ExtensionMethodNode
+
+def x = 'a,b,c'.split(',')
+def y = 'd,e,f'.split(',')
+
+@ASTTest(phase=INSTRUCTION_SELECTION,value={
+    def call = node.rightExpression.getNodeMetaData(DIRECT_METHOD_CALL_TARGET)
+    assert call instanceof ExtensionMethodNode
+    def emn = call.extensionMethodNode
+    def typeDesc = emn.typeDescriptor
+    assert typeDesc == '[Ljava.lang.Object; plus(java.lang.Object[], java.lang.Object[])'
+    println emn.typeDescriptor
+})
+String[] result = x + y
+assert result as List == ['a', 'b', 'c', 'd', 'e', 'f']
+'''
+    }
+}
