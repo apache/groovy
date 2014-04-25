774d4d1a13ba1171cdbdc6a27d8b8625bf67b370
Cedric Champeau <cedric.champeau@gmail.com>
2014-04-25 17:23:05 +0200

    GROOVY-6724: Wrong receiver type when checking generics on property access

--- src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -980,11 +980,11 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
 
                     FieldNode field = current.getDeclaredField(propertyName);
                     field  = allowStaticAccessToMember(field, staticOnly);
-                    if (storeField(field, isAttributeExpression, pexp, objectExpressionType, visitor, receiver.getData())) return true;
+                    if (storeField(field, isAttributeExpression, pexp, current, visitor, receiver.getData())) return true;
 
                     PropertyNode propertyNode = current.getProperty(propertyName);
                     propertyNode = allowStaticAccessToMember(propertyNode, staticOnly);
-                    if (storeProperty(propertyNode, pexp, objectExpressionType, visitor, receiver.getData())) return true;
+                    if (storeProperty(propertyNode, pexp, current, visitor, receiver.getData())) return true;
 
                     boolean isThisExpression = objectExpression instanceof VariableExpression && 
                                                 ((VariableExpression)objectExpression).isThisExpression();
@@ -1030,11 +1030,11 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
                     }
                     foundGetterOrSetter = foundGetterOrSetter || setter!=null || getter!=null;
 
-                    if (storeField(field, true, pexp, objectExpressionType, visitor, receiver.getData())) return true;
+                    if (storeField(field, true, pexp, current, visitor, receiver.getData())) return true;
                     // if the property expression is an attribute expression (o.@attr), then
                     // we stop now, otherwise we must check the parent class
                     if (/*!isAttributeExpression && */current.getSuperClass() != null) {
-                        queue.add(current.getSuperClass());
+                        queue.add(current.getUnresolvedSuperClass());
                     }
                 }
                 // GROOVY-5568, the property may be defined by DGM
