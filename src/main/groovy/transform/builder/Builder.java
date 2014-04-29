2271448b8f631f5afa6274498b955981bfdaec9b
Paul King <paulk@asert.com.au>
2014-04-29 21:23:20 +1000

    doco tweaks for @Builder

--- src/main/groovy/transform/builder/Builder.java
+++ src/main/groovy/transform/builder/Builder.java
@@ -30,8 +30,8 @@ import static org.codehaus.groovy.transform.BuilderASTTransformation.BuilderStra
  * The transform supports multiple building strategies to cover a range of cases and there are a number
  * of configuration options to customize the building process.
  *
- * In addition, a number of annotation attributes let you customise the building process. No all annotation attributes
- * are supported by all strategies. See the individual
+ * In addition, a number of annotation attributes let you customise the building process. Not all annotation attributes
+ * are supported by all strategies. See the individual strategy documentation for more details.
  * If you're an AST hacker, you can also define your own strategy class.
  *
  * The following strategies are bundled with Groovy:
