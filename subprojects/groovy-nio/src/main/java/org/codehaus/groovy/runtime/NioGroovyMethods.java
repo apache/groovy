e21d2e50cb43941875e443a77bd749fae2474d10
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-05-07 17:47:03 +0200

    Fixed issue 6770 - write method do not truncate the file content

--- subprojects/groovy-nio/src/main/java/org/codehaus/groovy/runtime/NioGroovyMethods.java
+++ subprojects/groovy-nio/src/main/java/org/codehaus/groovy/runtime/NioGroovyMethods.java
@@ -455,7 +455,7 @@ public class NioGroovyMethods extends DefaultGroovyMethodsSupport {
     public static void write(Path self, String text) throws IOException {
         Writer writer = null;
         try {
-            writer = new OutputStreamWriter(Files.newOutputStream(self, CREATE, APPEND), Charset.defaultCharset());
+            writer = new OutputStreamWriter(Files.newOutputStream(self), Charset.defaultCharset());
             writer.write(text);
             writer.flush();
 
