6949f9e598e137d0483c2cb177b07f3b7201995f
Pascal Schumacher <pascalschumacher@gmx.net>
2014-05-10 10:59:27 +0200

    GROOVY-6743: Update copyright in header

--- subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/commands/ImportCommand.groovy
+++ subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/commands/ImportCommand.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 the original author or authors.
+ * Copyright 2003-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,6 @@ import org.codehaus.groovy.tools.shell.util.PackageHelper
 /**
  * The 'import' command.
  *
- * @version $Id$
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  */
 class ImportCommand
