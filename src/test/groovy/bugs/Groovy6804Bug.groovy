3ab78a523f42c781502c75227f9c1ebcce8a444a
Sergey Egorov <bsideup@gmail.com>
2014-05-19 18:18:07 +0300

    GROOVY-6804

---
+++ src/test/groovy/bugs/Groovy6804Bug.groovy
@@ -0,0 +1,34 @@
+package groovy.bugs
+
+import groovy.transform.NotYetImplemented
+import groovy.transform.stc.StaticTypeCheckingTestCase
+
+class Groovy6804Bug extends StaticTypeCheckingTestCase {
+
+    @NotYetImplemented
+    void testOverloadedMethod() {
+        assertScript '''
+            class Base<K extends Serializable, V> {
+                void delete(K key) {}
+                
+                void delete(V value) {}
+            }
+            class Foo extends Base<String, Integer> {
+            }
+            public class Class1 {
+            
+            public static void main(String[] args){
+              
+            }
+            
+                Class1() {
+                    Foo foo = new Foo();
+                    
+                    foo.delete(1);
+                }
+            }
+            new Class1();
+        '''
+    }
+
+}
