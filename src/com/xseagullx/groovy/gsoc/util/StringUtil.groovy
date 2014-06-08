f592944b13a9debc90f0a5fd01ac6dfb24a1bbb9
Pavel Dionisiev <xSeagullx@gmail.com>
2014-06-08 18:47:35 +0400

    Refactor and cleanup StringUtil class testing.

--- src/com/xseagullx/groovy/gsoc/util/StringUtil.groovy
+++ src/com/xseagullx/groovy/gsoc/util/StringUtil.groovy
@@ -1,21 +1,6 @@
 package com.xseagullx.groovy.gsoc.util
 
 class StringUtil {
-    static { //test
-        def text = /\123 \1\34\5\377\678/
-        def resText = "\123 \1\34\5\377\678"
-        assert replaceOctalEscapes(text) == resText
-
-        text = '\\' + 'u1234' + '\\' + 'uaBcd'
-        resText = "\u1234\uabcd"
-        assert replaceHexEscapes(text) == resText
-
-
-        text = /\b\t\n\f\r\"\'\ /
-        resText = "\b\t\n\f\r\"\'\\ "
-        assert replaceStandardEscapes(text) == resText
-    }
-
     static String replaceHexEscapes(String text) {
         def p = ~/\\u([0-9abcdefABCDEF]{4})/
         text.replaceAll(p) { String _0, String _1 ->
@@ -43,12 +28,5 @@ class StringUtil {
         text.replaceAll(p) { String _0, String _1 ->
             standardEscapes[_1] ?: _1
         }
-    }
-
-    static void main(args) {
-        def \u1234 = 12
-        println(ሴ)
-        println("Self-testing completed!")
-
     }
 }
