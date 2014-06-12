d6b9cca12b4d8de1a20e20cbf808edf0cc308488
Cedric Champeau <cedric.champeau@gmail.com>
2014-06-12 14:32:10 +0200

    Documentation for object operators

--- src/spec/test/OperatorsTest.groovy
+++ src/spec/test/OperatorsTest.groovy
@@ -175,4 +175,81 @@ class OperatorsTest extends CompilableTestSupport {
         // end::conditional_op_elvis[]
 
     }
+
+    void testNullSafeOperator() {
+        // tag::nullsafe[]
+        def person = Person.find { it.id == 123 }           // <1>
+        def name = person?.name                             // <2>
+        assert name == null                                 // <3>
+        // end::nullsafe[]
+    }
+
+    OperatorsTest() {
+    }
+
+    void testDirectFieldAccess() {
+        assertScript '''
+// tag::direct_field_class[]
+class User {
+    public final String name                               // <1>
+    User(String name) { this.name = name}
+    String getName() { "Name: $name" }                     // <2>
+}
+def user = new User('Bob')
+assert user.name == 'Name: Bob'                            // <3>
+// end::direct_field_class[]
+// tag::direct_field_op[]
+assert user.@name == 'Bob'                                 // <1>
+// end::direct_field_op[]
+'''
+    }
+
+    void testMethodReference() {
+        // tag::method_reference[]
+        def str = 'example of method reference'            // <1>
+        def fun = str.&toUpperCase                         // <2>
+        def upper = fun()                                  // <3>
+        assert upper == str.toUpperCase()                  // <4>
+        // end::method_reference[]
+        assert fun instanceof Closure
+
+        assertScript '''
+            class Person {
+                String name
+                int age
+            }
+            // tag::method_reference_strategy[]
+            def transform(List elements, Closure action) {                                      // <1>
+                def result = []
+                elements.each {
+                    result << action(it)
+                }
+                result
+            }
+            String describe(Person p) {                                                         // <2>
+                "$p.name is $p.age"
+            }
+            def action = this.&describe                                                         // <3>
+            def list = [new Person(name:'Bob', age:42), new Person(name:'Julia',age:35)]        // <4>
+            assert transform(list, action) == ['Bob is 42', 'Julia is 35']                      // <5>
+
+            // end::method_reference_strategy[]
+        '''
+
+        assertScript '''
+            // tag::method_reference_dispatch[]
+            def doSomething(String str) { str.toUpperCase() }                                   // <1>
+            def doSomething(Integer x) { 2*x }                                                  // <2>
+            def reference = this.&doSomething                                                   // <3>
+            assert reference('foo') == 'FOO'                                                    // <4>
+            assert reference(123)   == 246                                                      // <5>
+            // end::method_reference_dispatch[]
+        '''
+    }
+
+    private static class Person {
+        Long id
+        String name
+        static Person find(Closure c) { null }
+    }
 }
