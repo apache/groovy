99f6e0795e46bba8cc17e94cf76b4c288f29d135
Paul King <paulk@asert.com.au>
2014-06-13 11:45:41 +1000

    GROOVY-6404: Incorrect stubs for enums without constants

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/Groovy6404Bug.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2003-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * Test that empty enums are compiled successfully.
+ *
+ * @author Paul King
+ */
+class Groovy6404Bug extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+                'foo/Main.java'    : '''
+                    package foo;
+
+                    import bar.MyEnum;
+
+                    public class Main {
+                        public static void main(String[] args) {
+                            System.out.println(MyEnum.values());
+                        }
+                    }
+                ''',
+                'bar/MyEnum.groovy': '''
+                    package bar
+
+                    enum MyEnum { }
+                '''
+        ]
+    }
+
+    void verifyStubs() {}
+}
