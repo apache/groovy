f11460221c7fcd747b62c032a4a2a0d2cea35a13
Paul King <paulk@asert.com.au>
2014-06-13 12:59:48 +1000

    GROOVY-6302: Stub generator chokes on multi-dimensional arrays

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/Groovy6302Bug.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2003-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * Test that multi-dimensional arrays are compiled successfully.
+ *
+ * @author Paul King
+ */
+class Groovy6302Bug extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+                'foo/Main.java'    : '''
+                    package foo;
+
+                    import bar.GroovyClass;
+
+                    public class Main {
+                        public static void main(String[] args) {
+                            new GroovyClass().foo();
+                        }
+                    }
+                ''',
+                'bar/GroovyClass.groovy': '''
+                    package bar
+
+                    class GroovyClass {
+                        Map<String, String[][]> foo() { null }
+                    }
+                '''
+        ]
+    }
+
+    void verifyStubs() {}
+}
