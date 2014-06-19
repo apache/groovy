dff4eb35e85ad595cb3f19b0932ebb31d0651004
Cedric Champeau <cedric.champeau@gmail.com>
2014-06-19 11:14:34 +0200

    Documentation of simple rules for type checking assignments

--- src/spec/test/typing/TypeCheckingTest.groovy
+++ src/spec/test/typing/TypeCheckingTest.groovy
@@ -58,5 +58,158 @@ class TypeCheckingTest extends StaticTypeCheckingTestCase {
         assert new Calculator().sum(4,5) == 9
         '''
     }
+
+    void testTypeCheckingAssignmentRules() {
+        assertScript '''
+            // tag::stc_assign_equals[]
+            Date now = new Date()
+            // end::stc_assign_equals[]
+        '''
+        assertScript '''
+            // tag::stc_assign_specialcase[]
+            String s = new Date() // implicit call to toString
+            Boolean boxed = 'some string'       // Groovy truth
+            boolean prim = 'some string'        // Groovy truth
+            Class clazz = 'java.lang.String'    // class coercion
+            // end::stc_assign_specialcase[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_null[]
+            String s = null         // passes
+            // end::stc_assign_null[]
+        '''
+
+        shouldFailWithMessages '''
+            // tag::stc_assign_null2prim[]
+            int i = null            // fails
+            // end::stc_assign_null2prim[]
+        ''', 'Cannot assign value of type java.lang.Object to variable of type int'
+
+        assertScript '''
+            // tag::stc_assign_array[]
+            int[] i = new int[4]        // passes
+            // end::stc_assign_array[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_array_list[]
+            int[] i = [1,2,3]               // passes
+            // end::stc_assign_array_list[]
+        '''
+
+        shouldFailWithMessages '''
+            // tag::stc_assign_array[]
+            int[] i = new String[4]     // fails
+            // end::stc_assign_array[]
+        ''', 'Cannot assign value of type java.lang.String[] to variable of type int[]'
+
+        shouldFailWithMessages '''
+            // tag::stc_assign_array_list_fail[]
+            int[] i = [1,2, new Date()]     // fails
+            // end::stc_assign_array_list_fail[]
+        ''', 'Cannot assign value of type java.util.Date into array of type int[]'
+
+        assertScript '''
+            // tag::stc_assign_superclass[]
+            AbstractList list = new ArrayList()     // passes
+            // end::stc_assign_superclass[]
+        '''
+
+        shouldFailWithMessages '''
+            // tag::stc_assign_superclass_fail[]
+            LinkedList list = new ArrayList()       // fails
+            // end::stc_assign_superclass_fail[]
+        ''', 'Cannot assign value of type java.util.ArrayList to variable of type java.util.LinkedList'
+
+        assertScript '''
+            // tag::stc_assign_interface[]
+            List list = new ArrayList()             // passes
+            // end::stc_assign_interface[]
+        '''
+
+        shouldFailWithMessages '''
+            // tag::stc_assign_interface_fail[]
+            RandomAccess list = new LinkedList()    // fails
+            // end::stc_assign_interface_fail[]
+        ''', 'Cannot assign value of type java.util.LinkedList to variable of type java.util.RandomAccess'
+
+        assertScript '''
+            // tag::stc_assign_prim[]
+            int i = 0
+            Integer bi = 1
+            int x = new Integer(123)
+            double d = new Float(5f)
+            // end::stc_assign_prim[]
+        '''
+
+        assertScript '''
+            // tag::stc_closure_coercion[]
+            Runnable r = { println 'Hello' }
+            interface SAMType {
+                int doSomething()
+            }
+            SAMType sam = { 123 }
+            assert sam.doSomething() == 123
+            abstract class AbstractSAM {
+                int calc() { 2* value() }
+                abstract int value()
+            }
+            AbstractSAM c = { 123 }
+            assert c.calc() == 246
+            // end::stc_closure_coercion[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_to_double[]
+            Double d1 = 4d
+            Double d2 = 4f
+            Double d3 = 4l
+            Double d4 = 4i
+            Double d5 = (short) 4
+            Double d6 = (byte) 4
+            // end::stc_assign_to_double[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_to_float[]
+            Float f1 = 4f
+            Float f2 = 4l
+            Float f3 = 4i
+            Float f4 = (short) 4
+            Float f5 = (byte) 4
+            // end::stc_assign_to_float[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_to_long[]
+            Long l1 = 4l
+            Long l2 = 4i
+            Long l3 = (short) 4
+            Long l4 = (byte) 4
+            // end::stc_assign_to_long[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_to_int[]
+            Integer i1 = 4i
+            Integer i2 = (short) 4
+            Integer i3 = (byte) 4
+            // end::stc_assign_to_int[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_to_short[]
+            Short s1 = (short) 4
+            Short s2 = (byte) 4
+            // end::stc_assign_to_short[]
+        '''
+
+        assertScript '''
+            // tag::stc_assign_to_byte[]
+            Byte b1 = (byte) 4
+            // end::stc_assign_to_byte[]
+        '''
+    }
 }
 
