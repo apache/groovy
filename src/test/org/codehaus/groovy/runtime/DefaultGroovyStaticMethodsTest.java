59787aed267c1ed58195fe0fd11746f49fcc2e15
Kent <kifs@hib.no>
2014-07-03 09:08:22 +0200

    GROOVY-6294 added System.currentTimeSeconds() which returns System.currentTimeMillis()/1000

---
+++ src/test/org/codehaus/groovy/runtime/DefaultGroovyStaticMethodsTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2003-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.runtime;
+
+import groovy.util.GroovyTestCase;
+
+/**
+ * @author Kent Inge Fagerland Simonsen
+ */
+public class DefaultGroovyStaticMethodsTest extends GroovyTestCase {
+
+    /**
+     *Tests System.currentTimeSeconds()
+     */
+    public void testCurrentTimeSeconds() {
+	long timeMillis = System.currentTimeMillis();
+        long timeSeconds = DefaultGroovyStaticMethods.currentTimeSeconds(null);
+        long timeMillis2 = System.currentTimeMillis();
+        assertTrue(timeMillis/1000 <= timeSeconds);
+        assertTrue(timeMillis2/1000 >= timeSeconds);
+    }
+}
