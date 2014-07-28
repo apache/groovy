027ec895f54928cd91937ec92ca831a136cecbca
Cedric Champeau <cedric.champeau@gmail.com>
2014-07-28 10:41:45 +0200

    Fix messed up documentation due to program structure section

--- src/spec/test/thirdpartylib/MultiplyTwo.groovy
+++ src/spec/test/thirdpartylib/MultiplyTwo.groovy
@@ -1,12 +1,10 @@
-//tag:third_party_lib[]
+// tag::third_party_lib[]
 package thirdpartylib
 
-public class MultiplyTwo
-{
-	def static multiplay(def value)
-	{
-		return value * 3 //intentionally wrong.
-	}
+public class MultiplyTwo {
+    def static multiply(def value) {
+        return value * 3 //intentionally wrong.
+    }
 }
 
-//end:third_party_lib[]
+// end::third_party_lib[]
