bd4331ad4a71e64c9e762e072260247932e9f8ad
Thibault Kruse <thibault.kruse@comsysto.com>
2014-09-21 12:06:06 +0200

    Groovysh: Documentation: how to use commands.xml to add commands

--- subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/util/XmlCommandRegistrar.groovy
+++ subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/util/XmlCommandRegistrar.groovy
@@ -21,10 +21,33 @@ import org.codehaus.groovy.tools.shell.Command
 
 /**
  * Registers {@link Command} classes from an XML file like:
- * <commands>
- *  <command>org.codehaus.groovy.tools.shell.commands.HelpCommand</command>
- * ...
- * </commands>
+ *
+ * <pre>
+  {@literal
+ <commands>
+   <!-- default commands -->
+   <command>org.codehaus.groovy.tools.shell.commands.HelpCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.ExitCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.ImportCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.DisplayCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.ClearCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.ShowCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.InspectCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.PurgeCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.EditCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.LoadCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.SaveCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.RecordCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.HistoryCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.AliasCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.SetCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.ShadowCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.RegisterCommand</command>
+   <command>org.codehaus.groovy.tools.shell.commands.DocCommand</command>
+   <!-- custom commands -->
+ </commands>
+ * }
+ * <pre>
  *
  * @version $Id$
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
@@ -32,40 +55,40 @@ import org.codehaus.groovy.tools.shell.Command
 class XmlCommandRegistrar
 {
     private final Logger log = Logger.create(this.class)
-    
+
     private final Shell shell
-    
+
     private final ClassLoader classLoader
-    
+
     XmlCommandRegistrar(final Shell shell, final ClassLoader classLoader) {
         assert shell != null
         assert classLoader != null
-        
+
         this.shell = shell
         this.classLoader = classLoader
     }
-    
+
     void register(final URL url) {
         assert url
-        
+
         if (log.debugEnabled) {
             log.debug("Registering commands from: $url")
         }
-        
+
         url.withReader {Reader reader ->
             groovy.util.Node doc = new groovy.util.XmlParser().parse(reader)
 
             doc.children().each {groovy.util.Node element ->
                 String classname = element.text()
-                
+
                 Class type = classLoader.loadClass(classname)
-                
+
                 Command command = type.newInstance(shell) as Command
-                
+
                 if (log.debugEnabled) {
                     log.debug("Created command '${command.name}': $command")
                 }
-                
+
                 shell << command
             }
         }
