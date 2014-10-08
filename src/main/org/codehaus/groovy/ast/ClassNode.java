59c4332763351d910f5bdc61164fdbab0bd05707
Cedric Champeau <cedric.champeau@gmail.com>
2014-10-08 10:52:09 +0200

    GROOVY-7081: Improve error message whenever there's a conflict between a static and a non static method in a class

--- src/main/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/org/codehaus/groovy/ast/ClassNode.java
@@ -400,6 +400,7 @@ public class ClassNode extends AnnotatedNode implements Opcodes {
         } else {
             return result;
         }
+
     }
 
     public List<MethodNode> getAllDeclaredMethods() {
