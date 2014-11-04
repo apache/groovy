6db5d23832f760058b625649435ab9189e5bd89a
Cedric Champeau <cedric.champeau@gmail.com>
2014-11-04 13:08:33 +0100

    GROOVY-7146: Incorrect stack size computation if a method takes a double or long as first argument and a single slot variable as last one

--- src/main/org/codehaus/groovy/runtime/ProxyGeneratorAdapter.java
+++ src/main/org/codehaus/groovy/runtime/ProxyGeneratorAdapter.java
@@ -598,7 +598,7 @@ public class ProxyGeneratorAdapter extends ClassVisitor implements Opcodes {
         Type[] args = Type.getArgumentTypes(desc);
         BytecodeHelper.pushConstant(mv, args.length);
         mv.visitTypeInsn(ANEWARRAY, "java/lang/Object");
-        size = 3;
+        size = 6;
         int idx = 1;
         for (int i = 0; i < args.length; i++) {
             Type arg = args[i];
@@ -615,7 +615,7 @@ public class ProxyGeneratorAdapter extends ClassVisitor implements Opcodes {
             } else {
                 mv.visitVarInsn(ALOAD, idx); // load argument i
             }
-            size = Math.max(6, 5+registerLen(arg));
+            size = Math.max(size, 5+registerLen(arg));
             idx += registerLen(arg);
             mv.visitInsn(AASTORE); // store value into array
         }
