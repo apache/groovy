297f90d9da1b6c1a1b416d8ed17bc32de6dabb03
Cedric Champeau <cedric.champeau@gmail.com>
2014-11-05 10:25:24 +0100

    GROOVY-7039: Static compiler fails to resolve public field defined in an interface

---
+++ src/test/org/codehaus/groovy/classgen/asm/sc/bugs/Groovy7039Bug.groovy
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2003-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+
+
+package org.codehaus.groovy.classgen.asm.sc.bugs
+
+import groovy.transform.stc.StaticTypeCheckingTestCase
+import org.codehaus.groovy.classgen.asm.sc.StaticCompilationTestSupport
+
+class Groovy7039Bug extends StaticTypeCheckingTestCase implements StaticCompilationTestSupport {
+    void testAccessToPublicStaticFieldOfInterfaceFromClass() {
+        assertScript '''
+            interface Intf {
+                public static final int TRUTH = 42
+            }
+
+            class A implements Intf {}
+
+            int truth() { A.TRUTH }
+
+            assert truth() == 42
+
+        '''
+    }
+
+    void testAccessToPublicStaticFieldOfInterfaceFromSubClass() {
+        assertScript '''
+            interface Intf {
+                public static final int TRUTH = 42
+            }
+
+            class A implements Intf {}
+            class B extends A {}
+
+            int truth() { B.TRUTH }
+
+            assert truth() == 42
+
+        '''
+    }
+
+    void testAccessToPublicStaticFieldOfInterfaceFromSuperInterface() {
+        assertScript '''
+            interface Intf {
+                public static final int TRUTH = 42
+            }
+            interface SubIntf extends Intf {}
+
+            class A implements SubIntf {}
+            class B extends A {}
+
+            int truth() { B.TRUTH }
+
+            assert truth() == 42
+
+        '''
+    }
+}
