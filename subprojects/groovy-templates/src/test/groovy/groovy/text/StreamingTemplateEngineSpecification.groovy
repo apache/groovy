16f739b4b64241cc57d2b48c003261f918ad3fa2
Matias Bjarland <matias@iteego.com>
2014-11-07 18:10:12 +0100

    GROOVY-3457: Fixing issues in the replacement StreamingTemplateEngine
    o The template engine now handles $var style variable reference
    o Fixed invalid line numbers reported when template errors were encountered
    o Fixed failing execution of the example given in the javadoc...doh
    o Improved unit test coverage:
      - added the javadoc example as a spock test
      - added tests for $var expressions
      - improved coverage for other expressions
      - added test for correct line numbers in exception on template errors
      - added tests to validate evaluation of permutations of adjacent expressions
    o Improved template exception messaging with three lines of
      context so that the exception message contains the erring lines.

--- subprojects/groovy-templates/src/test/groovy/groovy/text/StreamingTemplateEngineSpecification.groovy
+++ subprojects/groovy-templates/src/test/groovy/groovy/text/StreamingTemplateEngineSpecification.groovy
@@ -4,24 +4,42 @@ import spock.lang.*
 
 import static StreamingTemplateEngineSpecification.EngineType.*
 
+/**
+ * http://code.google.com/p/spock/wiki/SpockBasics
+ *
+ * Author: Matias Bjarland
+ */
 class StreamingTemplateEngineSpecification extends Specification {
-
-  enum EngineType { 
+  enum EngineType {
     STREAMING('StreamingTemplateEngine'),
-    SIMPLE('SimpleTemplateEngine'), 
+    SIMPLE('SimpleTemplateEngine'),
     GSTRING('GStringTemplateEngine')
     String displayString
-    
+
     EngineType(displayString) {
       this.displayString = displayString
     }
-    
+
     String toString() {
-      displayString      
+      displayString
     }
   }
 
   @Shared Map defaultBinding = [alice: 'Alice', rabbit: 'Rabbit', queen: 'Queen', desk: 'writing desk']
+  @Shared String SIXTY_FOUR_K_OF_A
+
+  // run before the first feature method
+  def setupSpec() {
+    StringBuilder b = new StringBuilder()
+    def sixtyFourAs = "a"*64
+    (1..1024).each {
+      b.append(sixtyFourAs)
+    }
+    SIXTY_FOUR_K_OF_A = b.toString()
+  }
+  def cleanupSpec() {}   // run after the last feature method
+  def setup() {}          // run before every feature method
+  def cleanup() {}        // run after every feature method
 
   private String template(EngineType type, String data, Map binding=null) {
     TemplateEngine engine
@@ -29,10 +47,10 @@ class StreamingTemplateEngineSpecification extends Specification {
       case STREAMING:
         engine = new StreamingTemplateEngine()
         break
-      case SIMPLE: 
+      case SIMPLE:
         engine = new SimpleTemplateEngine()
         break
-      case GSTRING: 
+      case GSTRING:
         engine = new GStringTemplateEngine()
         break
     }
@@ -45,33 +63,70 @@ class StreamingTemplateEngineSpecification extends Specification {
     sw
   }
 
+  //TODO: Handle dollarExpressionSlashAtStart case better below
   @Unroll
   def "#testName - #engineType should evaluate '#data' to '#expectedResult' using binding '#binding'"() {
-    expect: 
+    expect:
       template(engineType, data, binding) == expectedResult
 
     where:
-      data                   | expectedResult      | engineType     | binding        | testName
-      ''                     | ''                  | STREAMING      | null           | 'emptyStringNoBinding'
-      ''                     | ''                  | STREAMING      | defaultBinding | 'emptyStringWithBinding'
-      'bob'                  | 'bob'               | STREAMING      | null           | 'noExpressionsNoBinding'
-      'bob'                  | 'bob'               | STREAMING      | defaultBinding | 'noExpressionsWithBinding'
+      data                     | expectedResult      | engineType     | binding        | testName
+      ''                       | ''                  | STREAMING      | null           | 'emptyStringNoBinding'
+      ''                       | ''                  | STREAMING      | defaultBinding | 'emptyStringWithBinding'
+      'bob'                    | 'bob'               | STREAMING      | null           | 'noExpressionsNoBinding'
+      'bob'                    | 'bob'               | STREAMING      | defaultBinding | 'noExpressionsWithBinding'
 
-      '\\Hello World'        | '\\Hello World'     | STREAMING      | null           | 'noExpressionsNoBindingEscapingAtStart'
-      '\\Hello World'        | '\\Hello World'     | STREAMING      | defaultBinding | 'noExpressionsWithBindingEscapingAtStart'
-      '\\\\Hello World'      | '\\\\Hello World'   | STREAMING      | null           | 'noExpressionsNoBindingDoubleEscapingAtStart'
-      '\\\\Hello World'      | '\\\\Hello World'   | STREAMING      | defaultBinding | 'noExpressionsWithBindingDoubleEscapingAtStart'
-      '\\\\\\Hello World'    | '\\\\\\Hello World' | STREAMING      | null           | 'noExpressionsNoBindingTripleEscapingAtStart'
-      '\\\\\\Hello World'    | '\\\\\\Hello World' | STREAMING      | defaultBinding | 'noExpressionsWithBindingTripleEscapingAtStart'
+      '\\Hello World'          | '\\Hello World'     | STREAMING      | null           | 'noExpressionsNoBindingEscapingAtStart'
+      '\\Hello World'          | '\\Hello World'     | STREAMING      | defaultBinding | 'noExpressionsWithBindingEscapingAtStart'
+      '\\\\Hello World'        | '\\\\Hello World'   | STREAMING      | null           | 'noExpressionsNoBindingDoubleEscapingAtStart'
+      '\\\\Hello World'        | '\\\\Hello World'   | STREAMING      | defaultBinding | 'noExpressionsWithBindingDoubleEscapingAtStart'
+      '\\\\\\Hello World'      | '\\\\\\Hello World' | STREAMING      | null           | 'noExpressionsNoBindingTripleEscapingAtStart'
+      '\\\\\\Hello World'      | '\\\\\\Hello World' | STREAMING      | defaultBinding | 'noExpressionsWithBindingTripleEscapingAtStart'
 
-      'Hello World\\'        | 'Hello World\\'     | STREAMING      | null           | 'noExpressionsNoBindingEscapingAtEnd'
-      'Hello World\\'        | 'Hello World\\'     | STREAMING      | defaultBinding | 'noExpressionsWithBindingEscapingAtEnd'
-      'Hello World\\\\'      | 'Hello World\\\\'   | STREAMING      | null           | 'noExpressionsNoBindingDoubleEscapingAtEnd'
-      'Hello World\\\\'      | 'Hello World\\\\'   | STREAMING      | defaultBinding | 'noExpressionsWithBindingDoubleEscapingAtEnd'
-      'Hello World\\\\\\'    | 'Hello World\\\\\\' | STREAMING      | null           | 'noExpressionsNoBindingTripleEscapingAtEnd'
-      'Hello World\\\\\\'    | 'Hello World\\\\\\' | STREAMING      | defaultBinding | 'noExpressionsWithBindingTripleEscapingAtEnd'
+      'Hello World\\'          | 'Hello World\\'     | STREAMING      | null           | 'noExpressionsNoBindingEscapingAtEnd'
+      'Hello World\\'          | 'Hello World\\'     | STREAMING      | defaultBinding | 'noExpressionsWithBindingEscapingAtEnd'
+      'Hello World\\\\'        | 'Hello World\\\\'   | STREAMING      | null           | 'noExpressionsNoBindingDoubleEscapingAtEnd'
+      'Hello World\\\\'        | 'Hello World\\\\'   | STREAMING      | defaultBinding | 'noExpressionsWithBindingDoubleEscapingAtEnd'
+      'Hello World\\\\\\'      | 'Hello World\\\\\\' | STREAMING      | null           | 'noExpressionsNoBindingTripleEscapingAtEnd'
+      'Hello World\\\\\\'      | 'Hello World\\\\\\' | STREAMING      | defaultBinding | 'noExpressionsWithBindingTripleEscapingAtEnd'
+
+      'Hello $alice'           | 'Hello Alice'       | STREAMING      | defaultBinding | 'dollarExpressionAtEnd'
+      '$alice Hello'           | 'Alice Hello'       | STREAMING      | defaultBinding | 'dollarExpressionAtBeginning'
+      '$alice'                 | 'Alice'             | STREAMING      | defaultBinding | 'dollarExpressionByItself'
+      'a $alice b'             | 'a Alice b'         | STREAMING      | defaultBinding | 'dollarExpressionInBetweenStrings'
+
+      '$rabbit$alice'          | 'RabbitAlice'       | STREAMING      | defaultBinding | 'dollarExpressionTwoAdjacentButDifferent'
+      '$alice$alice'           | 'AliceAlice'        | STREAMING      | defaultBinding | 'dollarExpressionTwoAdjacentAndIdentical'
+      '$rabbit $alice'         | 'Rabbit Alice'      | STREAMING      | defaultBinding | 'dollarExpressionTwoAdjacentButDifferentWithSpace'
+      '$alice $alice'          | 'Alice Alice'       | STREAMING      | defaultBinding | 'dollarExpressionTwoAdjacentAndIdenticalWithSpace'
+      '$'                      | '$'                 | STREAMING      | defaultBinding | 'literalDollarSignByItself'
+      '\\$'                    | '\\$'               | STREAMING      | defaultBinding | 'literalDollarSignEscapingAtStart'
+      '$\\'                    | '$\\'               | STREAMING      | defaultBinding | 'literalDollarSignEscapingAtEnd'
+      '\\$alice'               | '\\$alice'          | STREAMING      | defaultBinding | 'dollarExpressionSlashAtStart'
+      '\$alice'                | 'Alice'             | STREAMING      | defaultBinding | 'dollarExpressionEscapingAtStart'
+
+      '${rabbit}${alice}'      | 'RabbitAlice'       | STREAMING      | defaultBinding | 'curlyExpressionTwoAdjacentButDifferent'
+      '${alice}${alice}'       | 'AliceAlice'        | STREAMING      | defaultBinding | 'curlyExpressionTwoAdjacentAndIdentical'
+      '${rabbit} ${alice}'     | 'Rabbit Alice'      | STREAMING      | defaultBinding | 'curlyExpressionTwoAdjacentButDifferentWithSpace'
+      '${alice} ${alice}'      | 'Alice Alice'       | STREAMING      | defaultBinding | 'curlyExpressionTwoAdjacentAndIdenticalWithSpace'
+      '${}'                    | 'null'              | STREAMING      | defaultBinding | 'curlyExpressionEmptyEvaluatesToNull'
+
+      "<% out.print alice %>"    | 'Alice'           | STREAMING      | defaultBinding | 'scriptletOutPrintBinding'
+      "a<% out.print alice %>b"  | 'aAliceb'         | STREAMING      | defaultBinding | 'scriptletOutPrintBindingBetween'
+      "<% out.print 'bob' %>"    | 'bob'             | STREAMING      | defaultBinding | 'scriptletOutPrintConstant'
+      "a<% out.print 'bob' %>b"  | 'abobb'           | STREAMING      | defaultBinding | 'scriptletOutPrintConstantBetween'
+
+      "<%= alice %>"             | 'Alice'           | STREAMING      | defaultBinding | 'outExprPrintBinding'
+      "a<%= alice %>b"           | 'aAliceb'         | STREAMING      | defaultBinding | 'outExprPrintBindingBetween'
+      "<%= 'bob' %>"             | 'bob'             | STREAMING      | defaultBinding | 'outExprPrintConstant'
+      "a<%= 'bob' %>b"           | 'abobb'           | STREAMING      | defaultBinding | 'outExprPrintConstantBetween'
   }
 
+  /**
+   * Validate fix of handling of \r\n line endings as reported by Wilfried Middleton 2014.02.12
+   * https://github.com/mbjarland/groovy-streaming-template-engine/issues/1
+   * Note: This was before the template engine got merged into groovy-core
+   */
   def "should handle \\r\\n line feeds correctly"() {
     setup:
       String basic = '<%\r\n' +
@@ -92,6 +147,11 @@ class StreamingTemplateEngineSpecification extends Specification {
       gStringTemplate == streamingTemplate
   }
 
+  /**
+   * Validate fix of handling of if statements as reported by Wilfried Middleton 2014.02.12
+   * https://github.com/mbjarland/groovy-streaming-template-engine/issues/2
+   * Note: This was before the template engine got merged into groovy-core
+   */
   def "should handle simple embedded if statements"() {
     setup:
       String templateText = 'before "<% if (false) { %>should not be included<% } else { %>should be included<% } %>" after'
@@ -104,7 +164,12 @@ class StreamingTemplateEngineSpecification extends Specification {
       result == 'before "should be included" after'
   }
 
-  def "should handle complex embedded if statements"() {
+  /**
+   * Validate fix of handling of if statements as reported by Wilfried Middleton 2014.02.12
+   * https://github.com/mbjarland/groovy-streaming-template-engine/issues/2
+   * Note: This was before the template engine got merged into groovy-core
+   */
+  def "should handle complex embedded if statements same as GStringTemplateEngine"() {
     setup:
       String templateText = 'first line text\n' +
                             '<%\n' +
@@ -130,4 +195,108 @@ class StreamingTemplateEngineSpecification extends Specification {
     then:
       streamingResult == gStringResult
   }
+
+  def "should execute the javadoc example with correct output"() {
+    setup:
+      def binding = [firstname : "Grace",
+                     lastname  : "Hopper",
+                     accepted  : false,
+                     title     : 'Groovy for COBOL programmers']
+      def text = '''\
+        |Dear <% out.print firstname %> ${lastname},
+        |
+        |We <% if (accepted) out.print 'are pleased' else out.print 'regret' %> to inform you that your paper entitled
+        |'$title' was ${ accepted ? 'accepted' : 'rejected' }.
+        |
+        |The conference committee.
+        '''.stripMargin()
+
+      def expected = '''\
+        |Dear Grace Hopper,
+        |
+        |We regret to inform you that your paper entitled
+        |'Groovy for COBOL programmers' was rejected.
+        |
+        |The conference committee.
+        '''.stripMargin()
+
+    when:
+      String result = template(STREAMING, text, binding)
+
+    then:
+     result == expected
+  }
+
+  def "should execute the javadoc example same as GStringTemplateEngine"() {
+    setup:
+    def binding = [firstname : "Grace",
+                   lastname  : "Hopper",
+                   accepted  : false,
+                   title     : 'Groovy for COBOL programmers']
+    def text = '''\
+        |Dear <% out.print firstname %> ${lastname},
+        |
+        |We <% if (accepted) out.print 'are pleased' else out.print 'regret' %> to inform you that your paper entitled
+        |'$title' was ${ accepted ? 'accepted' : 'rejected' }.
+        |
+        |The conference committee.
+        '''.stripMargin()
+
+    when:
+      String streaming = template(STREAMING, text, binding)
+      String gString   = template(GSTRING, text, binding)
+
+    then:
+    streaming == gString
+  }
+
+  def "should throw exception with correct line number on template errors"() {
+    setup:
+      def binding = [firstname : "Grace",
+                     lastname  : "Hopper",
+                     accepted  : false,
+                     title     : 'Groovy for COBOL programmers']
+    def text = '''\
+        |Dear <% out.print firstname %> ${lastname},
+        |
+        |We <% if (accepted) out.print 'are pleased' else out.print 'regret' %> to inform you that your paper entitled
+        |'$txitle' was ${ accepted ? 'accepted' : 'rejected' }.
+        |
+        |The conference committee.
+        '''.stripMargin()
+
+    when:
+      String result = template(STREAMING, text, binding)
+
+    then:
+      def e = thrown(TemplateExecutionException)
+      e.lineNumber == 4
+  }
+
+  @Unroll
+  def "should evaluate adjacent expressions '#expression' to '#expected'"() {
+    expect:
+      template(STREAMING, expression, defaultBinding) == expected
+
+    where:
+      //Create all permutations of the different kinds of expressions and validate that they
+      //evaluate to expected results. We have encountered a few too many edge cases with failing
+      //adjacent evaluations...this should cover most of them
+      [expression, expected] << [
+        ['constant'               , 'constant'],
+        ['${alice}'               , 'Alice'],
+        ['$rabbit '               , 'Rabbit '],
+        ['<%= queen %>'           , 'Queen'],
+        ['<% out << desk %>'      , 'writing desk'],
+      ].permutations().collect { p ->
+        p.inject(['','']) { acc, val ->
+          println "VAL0: ${val[0]}"
+          println "VAL1: ${val[1]}"
+          acc[0] += val[0]
+          acc[1] += val[1]
+          acc
+        }
+      }
+  }
+
 }
