c35a4dfdb7adcd0f18c082fa8cdf98f6a9e4385f
Paul King <paulk@asert.com.au>
2014-12-17 21:28:06 +1000

    minor doco typo

--- src/main/groovy/transform/stc/MapEntryOrKeyValue.java
+++ src/main/groovy/transform/stc/MapEntryOrKeyValue.java
@@ -47,7 +47,7 @@ import java.util.Map;
  *     <li><i>index=true or false</i>, by default false. If true, then an additional "int" parameter is added,
  *     for "withIndex" variants</li>
  * </ul>
- * <code>void doSomething(String str, Map&lt;K,&gt;V map, @ClosureParams(value=MapEntryOrKeyValue.class,options="1") Closure c) { ... }</code>
+ * <code>void doSomething(String str, Map&lt;K,&gt;V map, @ClosureParams(value=MapEntryOrKeyValue.class,options="argNum=1") Closure c) { ... }</code>
  */
 public class MapEntryOrKeyValue extends ClosureSignatureHint {
     private static final ClassNode MAPENTRY_TYPE = ClassHelper.make(Map.Entry.class);
