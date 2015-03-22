d39968b8b556cf2223ead11439784357f037ce12
Pascal Schumacher <pascalschumacher@gmx.net>
2015-03-22 19:27:15 +0100

    Documentation: add section on StreamingTemplateEngine

--- subprojects/groovy-templates/src/spec/test/TemplateEnginesTest.groovy
+++ subprojects/groovy-templates/src/spec/test/TemplateEnginesTest.groovy
@@ -100,6 +100,37 @@ class TemplateEnginesTest extends CompilableTestSupport {
         result = template.toString()
         assert result.readLines()[1].trim() == '\\'
     }
+    
+        void testStreamingTemplateEngine() {
+// tag::streaming_template_engine[]
+def text = '''\
+Dear <% out.print firstname %> ${lastname},
+
+We <% if (accepted) out.print 'are pleased' else out.print 'regret' %> \
+to inform you that your paper entitled
+'$title' was ${ accepted ? 'accepted' : 'rejected' }.
+
+The conference committee.'''
+
+def template = new groovy.text.StreamingTemplateEngine().createTemplate(text)
+
+def binding = [
+    firstname : "Grace",
+    lastname  : "Hopper",
+    accepted  : true,
+    title     : 'Groovy for COBOL programmers'
+]
+
+String response = template.make(binding)
+
+assert response == '''Dear Grace Hopper,
+
+We are pleased to inform you that your paper entitled
+'Groovy for COBOL programmers' was accepted.
+
+The conference committee.'''
+// end::streaming_template_engine[]
+    }
 
     void testGStringTemplateEngine() {
         def binding = [firstname: 'Sam', lastname: 'Pullara', city: 'New York', month: 'December', signed: 'Groovy-Dev']
