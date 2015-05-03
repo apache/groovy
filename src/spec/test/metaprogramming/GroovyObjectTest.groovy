4cd3ae4cf8b7517892c825ff361f6b15c6048263
pascalschumacher <pascalschumacher@gmx.net>
2015-05-03 18:28:37 +0200

    Documentation: some additions/improvements to the "runtime metaprogramming" section

--- src/spec/test/metaprogramming/GroovyObjectTest.groovy
+++ src/spec/test/metaprogramming/GroovyObjectTest.groovy
@@ -1,38 +1,41 @@
 package metaprogramming
 
-class GroovyObjectTest extends GroovyTestCase{
+class GroovyObjectTest extends GroovyTestCase {
     void testInvokeMethod() {
         assertScript '''
 // tag::groovy_invoke_method[]
 class SomeGroovyClass {
-    def invokeMethod(String name, Object args){
+
+    def invokeMethod(String name, Object args) {
         return "called invokeMethod $name $args"
     }
     
-    def test(){
-        return 'exist method'
+    def test() {
+        return 'method exists'
     }
 }
 
 def someGroovyClass = new SomeGroovyClass()
-assert someGroovyClass.test() == 'exist method'
+
+assert someGroovyClass.test() == 'method exists'
 assert someGroovyClass.someMethod() == 'called invokeMethod someMethod []'
 // end::groovy_invoke_method[]
 '''
     }
     
-    void testGetProperty (){
+    void testGetProperty () {
         assertScript '''
 // tag::groovy_get_property[]
 class SomeGroovyClass {
-    def field1 = 'ha'
+
+    def property1 = 'ha'
     def field2 = 'ho'
     def field4 = 'hu'
-    public def getField1(){
+    
+    def getField1() {
         return 'getHa'
     }
     
-    @Override
     def getProperty(String name) {
         if (name != 'field3')
             return metaClass.getProperty(this, name) // <1>
@@ -42,29 +45,76 @@ class SomeGroovyClass {
 }
 
 def someGroovyClass = new SomeGroovyClass()
-assert someGroovyClass.'field1' == 'getHa'
-assert someGroovyClass.'field2' == 'ho'
-assert someGroovyClass.'field3' == 'field3'
+
+assert someGroovyClass.field1 == 'getHa'
+assert someGroovyClass.field2 == 'ho'
+assert someGroovyClass.field3 == 'field3'
 assert someGroovyClass.field4 == 'hu'
 // end::groovy_get_property[]
 '''
     }
+    
+    void testSetProperty () {
+        assertScript '''
+// tag::groovy_set_property[]
+class POGO {
 
-    void testGetAttribute (){
+    String property 
+    
+    void setProperty(String name, Object value) {
+        this.@"$name" = 'overriden'
+    }
+}
+
+def pogo = new POGO()
+pogo.property = 'a'
+
+assert pogo.property == 'overriden'
+// end::groovy_set_property[]
+'''
+    }
+
+    void testGetAttribute () {
         assertScript '''
 // tag::groovy_get_attribute[]
 class SomeGroovyClass {
+
     def field1 = 'ha'
     def field2 = 'ho'
-    public def getField1(){
+    
+    def getField1() {
         return 'getHa'
     }
 }
 
 def someGroovyClass = new SomeGroovyClass()
+
 assert someGroovyClass.metaClass.getAttribute(someGroovyClass, 'field1') == 'ha'
 assert someGroovyClass.metaClass.getAttribute(someGroovyClass, 'field2') == 'ho'
 // end::groovy_get_attribute[]
+'''
+    }
+    
+    void testSetAttribute () {
+        assertScript '''
+// tag::groovy_set_attribute[]
+class POGO {
+
+    private String field
+    String property1
+    
+    void setProperty1(String property1) {
+        this.property1 = "setProperty1"
+    }
+}
+
+def pogo = new POGO()
+pogo.metaClass.setAttribute(pogo, 'field', 'ha')
+pogo.metaClass.setAttribute(pogo, 'property1', 'ho')
+
+assert pogo.field == 'ha'
+assert pogo.property1 == 'ho'
+// end::groovy_set_attribute[]
 '''
     }
 }
--- src/spec/test/metaprogramming/InterceptionThroughMetaClassTest.groovy
+++ src/spec/test/metaprogramming/InterceptionThroughMetaClassTest.groovy
@@ -4,20 +4,23 @@ import groovy.xml.Entity
 
 // tag::meta_class_interception[]
 class InterceptionThroughMetaClassTest extends GroovyTestCase {
+
     void testPOJOMetaClassInterception() {
         String invoking = 'ha'
-        invoking.metaClass.invokeMethod = {String name, Object args ->
+        invoking.metaClass.invokeMethod = { String name, Object args ->
             'invoked'
         }
+        
         assert invoking.length() == 'invoked'
         assert invoking.someMethod() == 'invoked'
     }
 
     void testPOGOMetaClassInterception() {
         Entity entity = new Entity('Hello')
-        entity.metaClass.invokeMethod = {String name, Object args ->
+        entity.metaClass.invokeMethod = { String name, Object args ->
             'invoked'
         }
+        
         assert entity.build(new Object()) == 'invoked'
         assert entity.someMethod() == 'invoked'
     }
