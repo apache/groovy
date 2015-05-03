e01517d7658de8e31dc5aee3ba9a0ec0c2faadb0
pascalschumacher <pascalschumacher@gmx.net>
2015-05-03 18:28:37 +0200

    Documentation: some additions/improvements to the "runtime metaprogramming" section

--- src/spec/test/metaprogramming/InterceptableTest.groovy
+++ src/spec/test/metaprogramming/InterceptableTest.groovy
@@ -2,19 +2,23 @@ package metaprogramming
 
 // tag::groovy_interceptable_test[]
 class InterceptableTest extends GroovyTestCase {
-	void testCheckInterception() {
-		def interception = new Interception()
-		assertEquals interception.definedMethod(), interception.someMethod()
-	}
+
+    void testCheckInterception() {
+        def interception = new Interception()
+
+        assert interception.definedMethod() == 'invokedMethod'
+        assert interception.someMethod() == 'invokedMethod'
+    }
 }
 // end::groovy_interceptable_test[]
 
 // tag::groovy_interceptable_object_example[]
 class Interception implements GroovyInterceptable {
-	def definedMethod() {}
-	@Override
-	def invokeMethod(String name, Object args) {
-		'invokedMethod'
-	}
+
+    def definedMethod() { }
+
+    def invokeMethod(String name, Object args) {
+        'invokedMethod'
+    }
 }
 // end::groovy_interceptable_object_example[]
