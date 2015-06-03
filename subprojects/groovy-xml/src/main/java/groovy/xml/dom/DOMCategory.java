37a8cbde92d183e67b8b0a23de615576a5e47f46
aalmiray <aalmiray@gmail.com>
2015-06-03 10:57:53 +0200

    Remove unused imports from DOMCategory.
    Testing the waters for committer access.

--- subprojects/groovy-xml/src/main/java/groovy/xml/dom/DOMCategory.java
+++ subprojects/groovy-xml/src/main/java/groovy/xml/dom/DOMCategory.java
@@ -37,7 +37,6 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
