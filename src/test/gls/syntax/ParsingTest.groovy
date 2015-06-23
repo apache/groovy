5a2ae99bee319c993a7f866bf8f710b63fec62a0
Paul King <paulk@asert.com.au>
2015-06-23 15:26:02 +1000

    GROOVY-5185: Cast operator precedence is incorrect

--- src/test/gls/syntax/ParsingTest.groovy
+++ src/test/gls/syntax/ParsingTest.groovy
@@ -53,6 +53,22 @@ public class ParsingTest extends gls.CompilableTestSupport {
         assert val4.class.componentType == short
     }
 
+    void testCastPrecedence_Groovy4421_Groovy5185() {
+        def i = (int)1/(int)2
+        assert i.class==BigDecimal
+
+        def result = (long)10.7 % 3L
+        assert result == 1 && result instanceof Long
+
+        assert '42' == (String) { -> 40 + 2 }.call()
+
+        def percentage = 5.3
+        assert '5%' == (int)Math.floor(percentage) + "%"
+
+        def someInt = Integer.MAX_VALUE
+        assert 4294967294L == (long)someInt + someInt
+    }
+
     void testExpressionParsingWithCastInFrontOfAMap() {
         shouldCompile """
             def m = (Map)[a:{ "foo"; println 'bar' }]
