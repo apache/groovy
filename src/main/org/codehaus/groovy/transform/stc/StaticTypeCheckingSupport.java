a93be2f67f5e7031901ae2d5cbe73fe0f0aefac8
Paul King <paulk@asert.com.au>
2015-06-27 11:01:35 +1000

    GROOVY-7477: CompileStatic-related "BUG! exception in phase 'instruction selection'"

--- src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingSupport.java
+++ src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingSupport.java
@@ -1599,7 +1599,7 @@ public abstract class StaticTypeCheckingSupport {
      */
     static void extractGenericsConnections(Map<String, GenericsType> connections, ClassNode type, ClassNode target) {
         if (target==null || type==target || !isUsingGenericsOrIsArrayUsingGenerics(target)) return;
-        if (type==UNKNOWN_PARAMETER_TYPE) return;
+        if (type == null || type==UNKNOWN_PARAMETER_TYPE) return;
         if (type.isArray() && target.isArray()) {
             extractGenericsConnections(connections, type.getComponentType(), target.getComponentType());
         } else if (target.isGenericsPlaceHolder() || type.equals(target) || !implementsInterfaceOrIsSubclassOf(type, target)) {
