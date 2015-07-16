f1f3973c70cf6590ee0d057a577c2b15b531d97a
paulk <paulk@asert.com.au>
2015-07-16 22:17:38 +1000

    further clarification for license/NOTICE files (not yet complete)

--- subprojects/groovy-jsr223/src/main/java/org/codehaus/groovy/jsr223/GroovyCompiledScript.java
+++ subprojects/groovy-jsr223/src/main/java/org/codehaus/groovy/jsr223/GroovyCompiledScript.java
@@ -1,4 +1,32 @@
-/**
+/*
+ * The initial contribution was derived from the reference implementation
+ * developed by Sun in consultation with the Groovy community. The reference
+ * implementation had the following license header:
+ *
+ *  Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ *  Use is subject to license terms.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met: Redistributions of source code
+ *  must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice, this list of
+ *  conditions and the following disclaimer in the documentation and/or other materials
+ *  provided with the distribution. Neither the name of the Sun Microsystems nor the names of
+ *  is contributors may be used to endorse or promote products derived from this software
+ *  without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ *  OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Subsequent modifications by the Groovy community have been done under the Apache License v2:
+ *
  *  Licensed to the Apache Software Foundation (ASF) under one
  *  or more contributor license agreements.  See the NOTICE file
  *  distributed with this work for additional information
@@ -27,25 +55,25 @@ import javax.script.ScriptException;
  * Used to represent compiled Groovy scripts.  Such scripts may be executed repeatedly
  * by Groovy's <code>ScriptEngine</code> using the <code>eval</code> method without reparsing overheads.
  *
- * @author Mike Grogan
- * @author A. Sundararajan
+ * @author Adapted from original by Mike Grogan
+ * @author Adapted from original by A. Sundararajan
  */
 public class GroovyCompiledScript extends CompiledScript {
-    
+
     private final GroovyScriptEngineImpl engine;
     private final Class clasz;
-    
+
     public GroovyCompiledScript(GroovyScriptEngineImpl engine, Class clazz) {
         this.engine = engine;
         this.clasz = clazz;
     }
-    
+
     public Object eval(ScriptContext context) throws ScriptException {
         return engine.eval(clasz, context);
-    }    
-    
+    }
+
     public ScriptEngine getEngine() {
         return engine;
     }
-    
+
 }
