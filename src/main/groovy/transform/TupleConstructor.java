e4c8a0c0364642c9dca0e567f15bb7ce6ef8bf13
paulk <paulk@asert.com.au>
2015-07-30 16:34:40 +1000

    GROOVY-7523: TupleConstructor with empty includes includes all (update doco - Closes #72)

--- src/main/groovy/transform/TupleConstructor.java
+++ src/main/groovy/transform/TupleConstructor.java
@@ -79,7 +79,9 @@ public @interface TupleConstructor {
      * List of field and/or property names to include within the constructor.
      * Must not be used if 'excludes' is used. For convenience, a String with comma separated names
      * can be used in addition to an array (using Groovy's literal list notation) of String values.
-     * The default value is a special marker value indicating that no includes are defined.
+     * The default value is a special marker value indicating that no includes are defined;
+     * all fields are included if includes remains undefined and excludes is explicitly or implicitly
+     * an empty list.
      */
     String[] includes() default {Undefined.STRING};
 
