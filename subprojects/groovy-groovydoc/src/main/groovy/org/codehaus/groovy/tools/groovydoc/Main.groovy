623a99e162cf174ba376f82962400eff374aa113
Emmanuel Bourg <ebourg@apache.org>
2015-09-28 00:49:16 +0200

    Added the notimestamp option to groovydoc

--- subprojects/groovy-groovydoc/src/main/groovy/org/codehaus/groovy/tools/groovydoc/Main.groovy
+++ subprojects/groovy-groovydoc/src/main/groovy/org/codehaus/groovy/tools/groovydoc/Main.groovy
@@ -43,6 +43,7 @@ class Main {
     private static Boolean author
     private static Boolean noScripts
     private static Boolean noMainForScripts
+    private static Boolean noTimestamp
     private static Boolean privateScope
     private static Boolean packageScope
     private static Boolean publicScope
@@ -71,6 +72,7 @@ class Main {
         cli.author(messages['cli.option.author.description'])
         cli.noscripts(messages['cli.option.noscripts.description'])
         cli.nomainforscripts(messages['cli.option.nomainforscripts.description'])
+        cli.notimestamp(messages['cli.option.notimestamp.description'])
         cli.overview(args:1, argName: 'file', messages['cli.option.overview.description'])
         cli.public(messages['cli.option.public.description'])
         cli.protected(messages['cli.option.protected.description'])
@@ -123,6 +125,7 @@ class Main {
         author = Boolean.valueOf(options.author) ?: false
         noScripts = Boolean.valueOf(options.noscripts) ?: false
         noMainForScripts = Boolean.valueOf(options.nomainforscripts) ?: false
+        noTimestamp = Boolean.valueOf(options.notimestamp) ?: false
         packageScope = Boolean.valueOf(options.package) ?: false
         privateScope = Boolean.valueOf(options.private) ?: false
         protectedScope = Boolean.valueOf(options.protected) ?: false
@@ -191,6 +194,7 @@ class Main {
         properties.put("author", author.toString())
         properties.put("processScripts", (!noScripts).toString())
         properties.put("includeMainForScripts", (!noMainForScripts).toString())
+        properties.put("timestamp", (!noTimestamp).toString())
         properties.put("overviewFile", overviewFile?.absolutePath ?: "")
 
         def links = new ArrayList<LinkArgument>();
