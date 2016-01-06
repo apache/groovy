c2bf181293cd73df478f4809ff64425fa61c7da3
John Wagenleitner <jwagenleitner@apache.org>
2016-01-05 21:11:22 -0800

    Remove checks for jdk5 in tests

--- src/test/groovy/bugs/Groovy3175_Bug.groovy
+++ src/test/groovy/bugs/Groovy3175_Bug.groovy
@@ -20,12 +20,7 @@ package groovy.bugs
 
 public class Groovy3175_Bug extends GroovyTestCase {
 
-   def getJavaVersionMajorMinor() { (System.getProperty('java.version') =~ /^\d+\.?\d*/)[0] as BigDecimal }
-
    void testSyntheticModifier() {
-     if (getJavaVersionMajorMinor() < 1.5)
-        return
-        
      assertScript """
         class MyService {
             private fio
