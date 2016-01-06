7bf00880840e456151197afc612c864004293cb8
pascalschumacher <pascalschumacher@gmx.net>
2016-01-06 21:09:45 +0100

    minimal clean-up of groovy.json.internal.Sys

--- subprojects/groovy-json/src/main/java/groovy/json/internal/Sys.java
+++ subprojects/groovy-json/src/main/java/groovy/json/internal/Sys.java
@@ -24,8 +24,7 @@ import java.util.regex.Pattern;
 
 class Sys {
 
-    private static final boolean is1_7OorLater;
-    private static final BigDecimal version;
+    private static final boolean is1_7OrLater;
     private static final boolean is1_7;
     private static final boolean is1_8;
 
@@ -37,20 +36,16 @@ class Sys {
             try {
                 String ver = split[0];
 
-                if (ver.startsWith("1.8")) {
-                    v = new BigDecimal("1.8");
+                if (ver.startsWith("1.6")) {
+                    v = new BigDecimal("1.6");
                 }
 
                 if (ver.startsWith("1.7")) {
                     v = new BigDecimal("1.7");
                 }
 
-                if (ver.startsWith("1.6")) {
-                    v = new BigDecimal("1.6");
-                }
-
-                if (ver.startsWith("1.5")) {
-                    v = new BigDecimal("1.5");
+                if (ver.startsWith("1.8")) {
+                    v = new BigDecimal("1.8");
                 }
 
                 if (ver.startsWith("1.9")) {
@@ -70,15 +65,13 @@ class Sys {
             }
         }
 
-        version = v;
-
-        is1_7OorLater = version.compareTo(new BigDecimal("1.7")) >= 0;
-        is1_7 = version.compareTo(new BigDecimal("1.7")) == 0;
-        is1_8 = version.compareTo(new BigDecimal("1.8")) == 0;
+        is1_7OrLater = v.compareTo(new BigDecimal("1.7")) >= 0;
+        is1_7 = v.compareTo(new BigDecimal("1.7")) == 0;
+        is1_8 = v.compareTo(new BigDecimal("1.8")) == 0;
     }
 
     public static boolean is1_7OrLater() {
-        return is1_7OorLater;
+        return is1_7OrLater;
     }
 
     public static boolean is1_7() {
