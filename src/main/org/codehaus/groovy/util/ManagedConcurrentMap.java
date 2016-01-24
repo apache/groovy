cf5b77156876ac2205d4555203f3f24990462657
John Wagenleitner <jwagenleitner@apache.org>
2016-01-24 11:12:38 -0800

    Fix finalizeReference method override for ManagedReference subclasses

--- src/main/org/codehaus/groovy/util/ManagedConcurrentMap.java
+++ src/main/org/codehaus/groovy/util/ManagedConcurrentMap.java
@@ -76,9 +76,18 @@ public class ManagedConcurrentMap<K,V> extends AbstractConcurrentMap<K,V> {
             return hash;
         }
 
-        public void finalizeRef() {
-            super.finalizeReference();
+        @Override
+        public void finalizeReference() {
             segment.removeEntry(this);
+            super.finalizeReference();
+        }
+
+        /**
+         * @deprecated use finalizeReference
+         */
+        @Deprecated
+        public void finalizeRef() {
+            finalizeReference();
         }
     }
 
@@ -90,18 +99,20 @@ public class ManagedConcurrentMap<K,V> extends AbstractConcurrentMap<K,V> {
             setValue(value);
         }
 
+        @Override
         public V getValue() {
             return value;
         }
 
+        @Override
         public void setValue(V value) {
             this.value = value;
         }
 
-
-        public void finalizeRef() {
+        @Override
+        public void finalizeReference() {
             value = null;
-            super.finalizeRef();
+            super.finalizeReference();
         }
     }
 }
---
+++ src/test/org/codehaus/groovy/util/ManagedConcurrentMapTest.groovy
@@ -0,0 +1,41 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.codehaus.groovy.util
+
+class ManagedConcurrentMapTest extends GroovyTestCase {
+
+    ManagedConcurrentMap<Object, String> map =
+            new ManagedConcurrentMap<Object, String>(ReferenceBundle.getHardBundle())
+
+    void testEntriesRemoveSelfFromMapWhenFinalized() {
+        List<ManagedReference<Object>> entries = []
+        for (int i = 0; i < 5; i++) {
+            entries << map.getOrPut(new Object(), "Object ${i}")
+        }
+
+        assert map.size() == 5
+        assert map.fullSize() == 5
+
+        entries*.finalizeReference()
+
+        assert map.size() == 0
+        assert map.fullSize() == 0
+    }
+
+}
