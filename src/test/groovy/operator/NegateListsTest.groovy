d154ade9e3e0e443488ce027fe0254a5056ec4f8
pascalschumacher <pascalschumacher@gmx.net>
2016-02-20 12:03:45 +0100

    remove printlns and commented-out code from NegateListsTest

--- src/test/groovy/operator/NegateListsTest.groovy
+++ src/test/groovy/operator/NegateListsTest.groovy
@@ -56,19 +56,16 @@ class NegateListsTest extends GroovyTestCase {
     void testEvenFunction() {
         def PI = Math.PI
 
-        /////////////////////////////////////////////////////////////////////
         // A case of partition having 10 subintervals.
         // x = [0.0*PI/2, 0.1*PI/2, 0.2*PI/2, 0.3*PI/2, 0.4*PI/2, 0.5*PI/2, 
         //               0.6*PI/2, 0.7*PI/2, 0.8*PI/2, 0.9*PI/2, 1.0*PI/2]
 
-        /////////////////////////////////////////////////////////////////////
         // Generate a domain of function used om testing.
         def n = 1000    // the number of partitions for the interval 0..2/PI
         def x = []
         for (i in 0..n) {
             x << i*PI/n
         }
-        // println x
 
         def cos = { Math.cos(it) }
         assertTrue(isEvenFn(cos, x))
@@ -81,16 +78,10 @@ class NegateListsTest extends GroovyTestCase {
     }
 
     boolean isEvenFn(f, domain) {
-        // println domain.collect(f)
-        // println( (-domain).collect(f) )
-        println( (domain.collect(f)) == ((-domain).collect(f)) )
-        return (domain.collect(f)) == ((-domain).collect(f))
+        domain.collect(f) == ((-domain).collect(f))
     }
 
     boolean isOddFn(f, domain) {
-        // println domain.collect(f)
-        // println(  (-domain).collect(f) )
-        println( domain.collect(f) == -((-domain).collect(f)) )
-        return domain.collect(f) == -((-domain).collect(f))
+        domain.collect(f) == -((-domain).collect(f))
     }
 }
