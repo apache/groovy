bd6ff0dafda6155890284eb9d78be4bd1f78f30b
paulk <paulk@asert.com.au>
2016-05-27 20:28:37 +1000

    GROOVY-7826: fix test name

--- src/test/org/codehaus/groovy/ast/Groovy7826Bug.java
+++ src/test/org/codehaus/groovy/ast/Groovy7826Bug.java
@@ -22,9 +22,9 @@ import groovy.lang.GroovyShell;
 import groovy.util.GroovyTestCase;
 import org.codehaus.groovy.control.CompilerConfiguration;
 
-public class Groovy7862Bug extends GroovyTestCase {
+public class Groovy7826Bug extends GroovyTestCase {
   public void testComplexTypeArguments() throws Exception {
-    String script = "def f(org.codehaus.groovy.ast.Groovy7862Bug.C1 c1) { }";
+    String script = "def f(org.codehaus.groovy.ast.Groovy7826Bug.C1 c1) { }";
 
     CompilerConfiguration config = new CompilerConfiguration();
     config.getOptimizationOptions().put("asmResolving", false);
