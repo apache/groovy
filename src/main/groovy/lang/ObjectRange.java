ab2b0ae36ecf1c21f7f7417e8cb22af8abe61d7d
paulk <paulk@asert.com.au>
2016-07-08 12:58:05 +1000

    trivial refactor - use consistent naming for iterator usage

--- src/main/groovy/lang/ObjectRange.java
+++ src/main/groovy/lang/ObjectRange.java
@@ -346,13 +346,13 @@ public class ObjectRange extends AbstractList implements Range {
      */
     @Override
     public boolean contains(Object value) {
-        final Iterator it = iterator();
+        final Iterator iter = iterator();
         if (value == null) {
             return false;
         }
-        while (it.hasNext()) {
+        while (iter.hasNext()) {
             try {
-                if (DefaultTypeTransformation.compareEqual(value, it.next())) return true;
+                if (DefaultTypeTransformation.compareEqual(value, iter.next())) return true;
             } catch (ClassCastException e) {
                 return false;
             }
