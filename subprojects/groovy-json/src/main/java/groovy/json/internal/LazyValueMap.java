58aef7a94caf9c6943ba8f5512309f56b0f2b1cd
John Wagenleitner <jwagenleitner@apache.org>
2016-07-11 11:04:04 -0700

    Fix check for potential hash function collisions
    Java 7 introduced the 'jdk.map.althashing.threshold' property that
    would apply an alternative hash function that would improve
    performance by avoiding large number of collisions.  The property
    was removed in Java 8 and improvements in handling collisions
    were made.
    
    https://docs.oracle.com/javase/8/docs/technotes/guides/collections/changes8.html

--- subprojects/groovy-json/src/main/java/groovy/json/internal/LazyValueMap.java
+++ subprojects/groovy-json/src/main/java/groovy/json/internal/LazyValueMap.java
@@ -196,7 +196,7 @@ public class LazyValueMap extends AbstractMap<String, Object> implements ValueMa
 
     private void buildMap() {
         /** added to avoid hash collision attack. */
-        if (Sys.is1_7OrLater() && System.getProperty("jdk.map.althashing.threshold") != null) {
+        if (Sys.is1_8OrLater() || (Sys.is1_7() && LazyMap.JDK_MAP_ALTHASHING_SYSPROP != null)) {
             map = new HashMap<String, Object>(items.length);
         } else {
             map = new TreeMap<String, Object>();
