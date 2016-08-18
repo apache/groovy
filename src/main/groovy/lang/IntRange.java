256a9f462b066cd4a2dfc124c0fb6c5b4edfccd8
Anand upadhyay <anand.upadhyay.consultant@nielsen.com>
2016-08-18 16:39:00 +0530

    Calculating hashCode of IntRange by pairing function

--- src/main/groovy/lang/IntRange.java
+++ src/main/groovy/lang/IntRange.java
@@ -415,4 +415,14 @@ public class IntRange extends AbstractList<Integer> implements Range<Integer> {
         step(step, adapter);
         return adapter.asList();
     }
+
+    @Override
+    public int hashCode(){
+        int hashCode;
+        final int from = this.getFrom();
+        final int to = this.getTo();
+
+        hashCode = new Integer(((from+to+1)*(from+to))/2).intValue()+to;
+        return hashCode;
+    }
 }
