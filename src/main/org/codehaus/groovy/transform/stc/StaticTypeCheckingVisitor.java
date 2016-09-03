f35bdac706f60b2af3ca937d749199d8a1b065a2
Jochen Theodorou <blackdrag@gmx.org>
2016-09-03 12:43:04 +0200

    GROOVY-7927: use inferred type from closure to find connection to SAM return type

--- src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -3593,7 +3593,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
         return null;
     }
 
-    private static ClassNode inferSAMTypeGenericsInAssignment(ClassNode samUsage, MethodNode sam, ClassNode closureType, ClosureExpression closureExpression) {
+    private ClassNode inferSAMTypeGenericsInAssignment(ClassNode samUsage, MethodNode sam, ClassNode closureType, ClosureExpression closureExpression) {
         // if the sam type or closure type do not provide generics information, 
         // we cannot infer anything, thus we simply return the provided samUsage
         GenericsType[] samGt = samUsage.getGenericsTypes();
@@ -3602,7 +3602,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
 
         // extract the generics from the return type
         Map<String,GenericsType> connections = new HashMap<String,GenericsType>();
-        extractGenericsConnections(connections,closureType,sam.getReturnType());
+        extractGenericsConnections(connections, getInferredReturnType(closureExpression),sam.getReturnType());
 
         // next we get the block parameter types and set the generics 
         // information just like before
