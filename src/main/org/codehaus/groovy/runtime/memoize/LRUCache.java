d30f059bf6dff6d7badb328d8595cee41d56ce25
Daniel Sun <sunlan@apache.org>
2016-11-16 00:47:15 +0800

    Fix GROOVY-7977

--- src/main/org/codehaus/groovy/runtime/memoize/LRUCache.java
+++ src/main/org/codehaus/groovy/runtime/memoize/LRUCache.java
@@ -18,22 +18,26 @@
  */
 package org.codehaus.groovy.runtime.memoize;
 
+import org.apache.groovy.util.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
+
 import java.lang.ref.SoftReference;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 
 /**
- * A cache backed by a Collections.SynchronizedMap
+ * A cache backed by a ConcurrentLinkedHashMap
  *
  * @author Vaclav Pech
+ * @author <a href="mailto:realbluesun@hotmail.com">Daniel.Sun</a>
  */
 public final class LRUCache implements MemoizeCache<Object, Object> {
-
     private final Map<Object, Object> cache;
 
     public LRUCache(final int maxCacheSize) {
-        cache = Collections.synchronizedMap(new LRUProtectionStorage(maxCacheSize));
+//        cache = Collections.synchronizedMap(new LRUProtectionStorage(maxCacheSize));
+        cache = new ConcurrentLinkedHashMap.Builder<>()
+                .maximumWeightedCapacity(maxCacheSize)
+                .build();
     }
 
     public Object put(final Object key, final Object value) {
@@ -45,15 +49,17 @@ public final class LRUCache implements MemoizeCache<Object, Object> {
     }
 
     /**
-     * Replying on the Collections.SynchronizedMap thread-safe iteration implementation the method will remove all entries holding
-     * SoftReferences to gc-evicted objects.
+     * Remove all entries holding SoftReferences to gc-evicted objects.
      */
     public void cleanUpNullReferences() {
         synchronized (cache) {
             final Iterator<Map.Entry<Object, Object>> iterator = cache.entrySet().iterator();
             while (iterator.hasNext()) {
                 final Map.Entry<Object, Object> entry = iterator.next();
-                if (((SoftReference) entry.getValue()).get() == null) iterator.remove();
+                final Object value = entry.getValue();
+
+                if (!(value instanceof SoftReference)) continue;
+                if (((SoftReference) value).get() == null) iterator.remove();
             }
         }
     }
