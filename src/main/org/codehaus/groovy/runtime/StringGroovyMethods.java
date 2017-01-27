598b93e04767c0511860758531bff56975e49e53
paulk <paulk@asert.com.au>
2017-01-27 16:58:50 +1000

    GROOVY-8026: Matcher indexed via IntRange with startIdx..-1 does not return "intermediate" range matches (closes #483)

--- src/main/org/codehaus/groovy/runtime/StringGroovyMethods.java
+++ src/main/org/codehaus/groovy/runtime/StringGroovyMethods.java
@@ -1348,6 +1348,11 @@ public class StringGroovyMethods extends DefaultGroovyMethodsSupport {
      */
     public static List getAt(Matcher self, Collection indices) {
         List result = new ArrayList();
+        if (indices instanceof IntRange) {
+            int size = (int) size(self);
+            RangeInfo info = subListBorders(size, (Range) indices);
+            indices = new IntRange(((IntRange)indices).getInclusive(), info.from, info.to - 1);
+        }
         for (Object value : indices) {
             if (value instanceof Range) {
                 result.addAll(getAt(self, (Range) value));
