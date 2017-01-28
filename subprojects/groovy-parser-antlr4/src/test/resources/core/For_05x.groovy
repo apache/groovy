a033231e223bc7fec108773bec200c5cfb33195b
sunlan <sunlan@apache.org>
2017-01-28 23:00:51 +0800

    Add more test cases for the standard "for" loop statement

--- subprojects/groovy-parser-antlr4/src/test/resources/core/For_05x.groovy
+++ subprojects/groovy-parser-antlr4/src/test/resources/core/For_05x.groovy
@@ -6,8 +6,16 @@ def testCS() {
     for (int i = 0, n = 5; i < n; i++) {
         result += i
     }
-
     assert 10 == result
+
+    result = 0;
+    int i;
+    int j;
+    for (i = 1, j = 5; i < j; i++, j--) {
+        result += i;
+        result += j;
+    }
+    assert 12 == result
 }
 testCS();
 
@@ -16,7 +24,15 @@ def test() {
     for (int i = 0, n = 5; i < n; i++) {
         result += i
     }
-
     assert 10 == result
+
+    result = 0;
+    int i;
+    int j;
+    for (i = 1, j = 5; i < j; i++, j--) {
+        result += i;
+        result += j;
+    }
+    assert 12 == result
 }
 test();
