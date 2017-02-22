764388a34b9f6fbf1c605fbbf02700b8ebed82d8
Christoffer Hammarström <christoffer.hammarstrom@linuxgods.com>
2017-02-22 02:18:52 +0100

    Add tests to prove that calling CompilerConfiguration.setScriptBaseClass with a Java class causes ModuleNode to generate a constructor that prevents Field initialization from Binding context.

--- src/test/groovy/lang/GroovyShellTest2.groovy
+++ src/test/groovy/lang/GroovyShellTest2.groovy
@@ -43,6 +43,60 @@ class GroovyShellTest2 extends GroovyTestCase {
         assert result == arg0
     }
 
+    void testBindingsInFieldInitializersWithAnnotatedJavaBaseScript() {
+        def shell = new GroovyShell();
+        def scriptText = '''
+        @groovy.transform.BaseScript groovy.lang.BindingScript baseScript
+        @groovy.transform.Field def script_args = getProperty('args')
+
+        assert script_args[0] == 'Hello Groovy'
+        script_args[0]
+'''
+
+        def arg0 = 'Hello Groovy'
+        def result = shell.run scriptText, 'TestBindingsInFieldInitializersWithAnnotatedJavaBaseScript.groovy', [arg0]
+        assert result == arg0
+    }
+
+    /**
+     This test fails because {@link org.codehaus.groovy.ast.ModuleNode#setScriptBaseClassFromConfig(org.codehaus.groovy.ast.ClassNode)}
+     calls .setSuperClass(ClassHelper.make(baseClassName)) on the Scripts ClassNode.
+
+     The ClassNode created for this scriptBaseClass has lazyInitDone = true and constructors = null so when
+     .getSuperClass().getDeclaredConstructor(SCRIPT_CONTEXT_CTOR) is called by {@link org.codehaus.groovy.ast.ModuleNode#createStatementsClass()}
+     then ClassNode.constructors is set to an empty ArrayList in {@link org.codehaus.groovy.ast.ClassNode#getDeclaredConstructors()}
+
+     The script constructor is then generated as
+         Constructor(Binding context) {
+             super();                                    // Fields are initialized after the call to super()
+             setBinding(context);                        // Fields are initalized before the call to setBinding(context)
+         }
+
+     instead of
+         Constructor(Binding context) {
+             super(context);                             // Fields are initialized after the call to super(context)
+         }
+
+     Fields are initialized between the call to super() and the setBinding(context)
+     which means Field initializers don't have access to the Binding context.
+     */
+    void testBindingsInFieldInitializersWithConfigJavaBaseScript() {
+        def config = new org.codehaus.groovy.control.CompilerConfiguration()
+        config.scriptBaseClass = BindingScript.class.name
+
+        def shell = new GroovyShell(config);
+        def scriptText = '''
+        @groovy.transform.Field def script_args = getProperty('args')
+
+        assert script_args[0] == 'Hello Groovy'
+        script_args[0]
+'''
+
+        def arg0 = 'Hello Groovy'
+        def result = shell.run scriptText, 'TestBindingsInFieldInitializersWithConfigJavaBaseScript.groovy', [arg0]
+        assert result == arg0
+    }
+
     void testBindingsInScriptFieldInitializers() {
         def shell = new GroovyShell();
         def scriptText = '''
