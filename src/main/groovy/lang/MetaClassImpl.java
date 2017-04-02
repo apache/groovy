22f327f6f76be62d67d121e77bcec76751ba69a2
John Wagenleitner <jwagenleitner@apache.org>
2017-04-02 08:34:47 -0700

    GROOVY-8140: Invoke method not returning MOP super method if isCallToSuper (closes #520)

--- src/main/groovy/lang/MetaClassImpl.java
+++ src/main/groovy/lang/MetaClassImpl.java
@@ -499,11 +499,6 @@ public class MetaClassImpl implements MetaClass, MutableMetaClass {
             boolean useThis;
 
             @Override
-            public boolean skipClass(Class clazz) {
-                return !useThis && clazz == theClass;
-            }
-
-            @Override
             public void methodNameAction(Class clazz, MetaMethodIndex.Entry e) {
                 if (useThis) {
                     if (e.methods == null)
