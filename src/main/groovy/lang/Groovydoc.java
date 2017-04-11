a5f442f7d3fa3ac7c41cbb820ad0cf10a5b82305
sunlan <sunlan@apache.org>
2017-04-11 22:08:22 +0800

    Add APL2 header and fix a typo.

--- src/main/groovy/lang/Groovydoc.java
+++ src/main/groovy/lang/Groovydoc.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package groovy.lang;
 
 import java.lang.annotation.Documented;
@@ -7,7 +25,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * An annotation to hold the groovydoc for the annotated element at runtime, we can it "Runtime Groovydoc".
+ * An annotation to hold the groovydoc for the annotated element at runtime, we call it "Runtime Groovydoc".
  * Runtime Groovydoc is a bit like Python's Documentation Strings and will be useful for IDE and developers who set a high value on documentations.
  *
  * The usage is very simple, just place @Groovydoc at the beginning of the content of groovydoc, then the new parser Parrot will attach the annotation Groovydoc automatically
