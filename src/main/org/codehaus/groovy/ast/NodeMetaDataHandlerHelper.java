5e6c850fb927e7d58f68467027db08624e92dca9
sunlan <sunlan@apache.org>
2017-06-18 23:18:17 +0800

    Refine the missing constructor for NodeMetaDataHandlerHelper

--- src/main/org/codehaus/groovy/ast/NodeMetaDataHandlerHelper.java
+++ src/main/org/codehaus/groovy/ast/NodeMetaDataHandlerHelper.java
@@ -20,7 +20,6 @@ package org.codehaus.groovy.ast;
 
 import org.codehaus.groovy.GroovyBugError;
 import org.codehaus.groovy.util.ListHashMap;
-import static org.apache.groovy.parser.antlr4.GroovyLangParser.GroovyParserRuleContext;
 
 import java.util.Collections;
 import java.util.Map;
@@ -29,11 +28,7 @@ public class NodeMetaDataHandlerHelper {
 
     private NodeMetaDataHandler delegate;
 
-    public NodeMetaDataHandlerHelper(ASTNode delegate) {
-        this.delegate = delegate;
-    }
-
-    public NodeMetaDataHandlerHelper(GroovyParserRuleContext delegate) {
+    public NodeMetaDataHandlerHelper(NodeMetaDataHandler delegate) {
         this.delegate = delegate;
     }
 
