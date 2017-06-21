dbf08acfb5a975b540187731a62a9696c22344c2
paulk <paulk@asert.com.au>
2017-06-21 12:03:40 +1000

    add missing license headers

--- src/main/org/codehaus/groovy/classgen/asm/util/LoggableTextifier.java
+++ src/main/org/codehaus/groovy/classgen/asm/util/LoggableTextifier.java
@@ -1,5 +1,22 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.codehaus.groovy.classgen.asm.util;
-
 
 import org.objectweb.asm.Attribute;
 import org.objectweb.asm.Handle;
@@ -13,6 +30,8 @@ import java.util.List;
 
 /**
  * Logging bytecode generation, which can make debugging easy
+ *
+ * @since 2.4.12
  */
 public class LoggableTextifier extends Textifier {
 //    private static final Logger LOGGER = Logger.getLogger(LoggableTextifier.class.getName());
