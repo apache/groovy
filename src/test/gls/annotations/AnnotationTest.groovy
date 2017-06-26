8f958c2348fc0876595f7b934dd447590dc4a6ef
paulk <paulk@asert.com.au>
2017-06-26 23:17:12 +1000

    GROOVY-8234: Add @Repeatable java8 annotation support (closes #567)

--- src/test/gls/annotations/AnnotationTest.groovy
+++ src/test/gls/annotations/AnnotationTest.groovy
@@ -705,6 +705,53 @@ class AnnotationTest extends CompilableTestSupport {
         '''
     }
 
+    // GROOVY-8234
+    void testAnnotationWithRepeatableSupported() {
+        assertScript '''
+            import java.lang.annotation.*
+
+            class MyClass {
+                private static final expected = '@MyAnnotationArray(value=[@MyAnnotation(value=val1), @MyAnnotation(value=val2)])'
+
+                // control
+                @MyAnnotationArray([@MyAnnotation("val1"), @MyAnnotation("val2")])
+                String method1() { 'method1' }
+
+                // duplicate candidate for auto collection
+                @MyAnnotation(value = "val1")
+                @MyAnnotation(value = "val2")
+                String method2() { 'method2' }
+
+                static void main(String... args) {
+                    MyClass myc = new MyClass()
+                    assert 'method1' == myc.method1()
+                    assert 'method2' == myc.method2()
+                    assert checkAnnos(myc, "method1") == expected
+                    assert checkAnnos(myc, "method2") == expected
+                }
+
+                private static String checkAnnos(MyClass myc, String name) {
+                    def m = myc.getClass().getMethod(name)
+                    List annos = m.getAnnotations()
+                    assert annos.size() == 1
+                    annos[0].toString()
+                }
+            }
+
+            @Target(ElementType.METHOD)
+            @Retention(RetentionPolicy.RUNTIME)
+            @Repeatable(MyAnnotationArray)
+            @interface MyAnnotation {
+                String value() default "val0"
+            }
+
+            @Retention(RetentionPolicy.RUNTIME)
+            @interface MyAnnotationArray {
+                MyAnnotation[] value()
+            }
+        '''
+    }
+
     //Parametrized tests in Spock would allow to make it much more readable
     private static String codeWithMetaAnnotationWithTarget(String targetElementTypeName) {
         """
