1edfb974dec232be1000629943d3437e7ce2f214
sunlan <sunlan@apache.org>
2017-08-06 23:04:36 +0800

    Validate number format

--- subprojects/parser-antlr4/src/test/groovy/org/apache/groovy/parser/antlr4/SyntaxErrorTest.groovy
+++ subprojects/parser-antlr4/src/test/groovy/org/apache/groovy/parser/antlr4/SyntaxErrorTest.groovy
@@ -197,6 +197,11 @@ class SyntaxErrorTest extends GroovyTestCase {
         TestUtils.doRunAndShouldFail('fail/VarArgParameter_01x.groovy');
     }
 
+    void "test groovy core - Number"() {
+        TestUtils.doRunAndShouldFail('fail/Number_01x.groovy');
+        TestUtils.doRunAndShouldFail('fail/Number_02x.groovy');
+    }
+
     /**************************************/
     static unzipScriptAndShouldFail(String entryName, List ignoreClazzList, Map<String, String> replacementsMap=[:], boolean toCheckNewParserOnly = false) {
         ignoreClazzList.addAll(TestUtils.COMMON_IGNORE_CLASS_LIST)
