4ca7598faa2c691abbce0aee1eed6457c10855df
sunlan <sunlan@apache.org>
2017-08-17 08:06:38 +0800

    Minor refactoring

--- subprojects/parser-antlr4/src/main/java/org/apache/groovy/parser/antlr4/util/StringUtils.java
+++ subprojects/parser-antlr4/src/main/java/org/apache/groovy/parser/antlr4/util/StringUtils.java
@@ -22,7 +22,6 @@ import groovy.lang.Closure;
 import org.apache.groovy.util.Maps;
 import org.codehaus.groovy.runtime.StringGroovyMethods;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -144,5 +143,11 @@ public class StringUtils {
 
 	public static long countChar(String text, char c) {
 		return text.chars().filter(e -> c == e).count();
+	}
+
+	public static String trimQuotations(String text, int quotationLength) {
+		int length = text.length();
+
+		return length == quotationLength * 2 ? "" : text.substring(quotationLength, length - quotationLength);
 	}
 }
