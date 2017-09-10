b1d964dbad396901f79e7a1901175562e95ecaeb
sunlan <sunlan@apache.org>
2017-09-10 14:02:06 +0800

    Add `getGroovydoc` DGMs
    (cherry picked from commit ec49133)

--- subprojects/parser-antlr4/src/test/resources/core/Groovydoc_01x.groovy
+++ subprojects/parser-antlr4/src/test/resources/core/Groovydoc_01x.groovy
@@ -62,9 +62,9 @@ class AA {
 
 }
 
-assert AA.class.getAnnotation(groovy.lang.Groovydoc).value().contains('class AA')
-assert AA.class.getMethod('m', new Class[0]).getAnnotation(groovy.lang.Groovydoc).value().contains('method m')
-assert AA.class.getConstructor().getAnnotation(groovy.lang.Groovydoc).value().contains('constructor AA')
-assert AA.class.getField('SOME_FIELD').getAnnotation(groovy.lang.Groovydoc).value().contains('field SOME_FIELD')
-assert AA.class.getDeclaredClasses().find {it.simpleName.contains('InnerClass')}.getAnnotation(groovy.lang.Groovydoc).value().contains('class InnerClass')
-assert BB.class.getAnnotation(groovy.lang.Groovydoc).value().contains('annotation BB')
+assert AA.class.groovydoc.contains('class AA')
+assert AA.class.getMethod('m', new Class[0]).groovydoc.contains('method m')
+assert AA.class.getConstructor().groovydoc.contains('constructor AA')
+assert AA.class.getField('SOME_FIELD').groovydoc.contains('field SOME_FIELD')
+assert AA.class.getDeclaredClasses().find {it.simpleName.contains('InnerClass')}.groovydoc.contains('class InnerClass')
+assert BB.class.groovydoc.contains('annotation BB')
