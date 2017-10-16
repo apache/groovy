5886dd20447fa9becec75d2a8cbab6ee39524c7d
paulk <paulk@asert.com.au>
2017-10-17 09:56:24 +1000

    GROOVY-8313: NullPointerException in TypeResolver when using generic array return type

--- src/main/org/codehaus/groovy/classgen/Verifier.java
+++ src/main/org/codehaus/groovy/classgen/Verifier.java
@@ -1306,7 +1306,7 @@ public class Verifier implements GroovyClassVisitor, Opcodes {
         MethodNode newMethod = new MethodNode(
                 oldMethod.getName(),
                 overridingMethod.getModifiers() | ACC_SYNTHETIC | ACC_BRIDGE,
-                oldMethod.getReturnType().getPlainNodeReference(),
+                cleanType(oldMethod.getReturnType()),
                 cleanParameters(oldMethod.getParameters()),
                 oldMethod.getExceptions(),
                 null
