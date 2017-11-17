4425a702b91addf334ed46199da5e70a435b4ae7
paulk <paulk@asert.com.au>
2017-11-17 15:56:58 +1000

    fix typo

--- src/main/org/codehaus/groovy/classgen/asm/sc/StaticInvocationWriter.java
+++ src/main/org/codehaus/groovy/classgen/asm/sc/StaticInvocationWriter.java
@@ -59,7 +59,7 @@ import static org.objectweb.asm.Opcodes.*;
 
 public class StaticInvocationWriter extends InvocationWriter {
     private static final ClassNode INVOKERHELPER_CLASSNODE = ClassHelper.make(InvokerHelper.class);
-    private static final Expression INVOKERHELER_RECEIVER = new ClassExpression(INVOKERHELPER_CLASSNODE);
+    private static final Expression INVOKERHELPER_RECEIVER = new ClassExpression(INVOKERHELPER_CLASSNODE);
     private static final MethodNode INVOKERHELPER_INVOKEMETHOD = INVOKERHELPER_CLASSNODE.getMethod(
             "invokeMethodSafe",
             new Parameter[]{
@@ -290,7 +290,7 @@ public class StaticInvocationWriter extends InvocationWriter {
                     // replace call with an invoker helper call
                     ArrayExpression arr = new ArrayExpression(ClassHelper.OBJECT_TYPE, args.getExpressions());
                     MethodCallExpression mce = new MethodCallExpression(
-                            INVOKERHELER_RECEIVER,
+                            INVOKERHELPER_RECEIVER,
                             target.isStatic() ? "invokeStaticMethod" : "invokeMethodSafe",
                             new ArgumentListExpression(
                                     target.isStatic() ?
