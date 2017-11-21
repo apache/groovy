3c91db36433c8db6f3983fb41e1a5e4c42e93cb5
paulk <paulk@asert.com.au>
2017-11-21 12:19:54 +1000

    fix maxRetries extension example

--- src/spec/test/support/MaxRetriesExtension.groovy
+++ src/spec/test/support/MaxRetriesExtension.groovy
@@ -21,18 +21,19 @@ package support
 // tag::instance_extension[]
 class MaxRetriesExtension {                                     // <1>
     static void maxRetries(Integer self, Closure code) {        // <2>
-        int retries = 0
-        Throwable e
-        while (retries<self) {
+        assert self >= 0
+        int retries = self
+        Throwable e = null
+        while (retries > 0) {
             try {
                 code.call()
                 break
             } catch (Throwable err) {
                 e = err
-                retries++
+                retries--
             }
         }
-        if (retries==0 && e) {
+        if (retries == 0 && e) {
             throw e
         }
     }
