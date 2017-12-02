56b1008c93740921d5b9685ae494b3edf5a760c2
sunlan <sunlan@apache.org>
2017-12-02 16:55:56 +0800

    Minor refactoring
    (cherry picked from commit 4aa686d)

--- src/main/groovy/lang/MetaClassImpl.java
+++ src/main/groovy/lang/MetaClassImpl.java
@@ -1943,8 +1943,7 @@ public class MetaClassImpl implements MetaClass, MutableMetaClass {
         // if it is not static and we do no static access
         if (genericGetMethod != null && !(!genericGetMethod.isStatic() && isStatic)) {
             method = genericGetMethod;
-            if (method != null)
-                return new GetMethodMetaProperty(name, method);
+            return new GetMethodMetaProperty(name, method);
         }
 
         //----------------------------------------------------------------------
