2bb54589853ddc419fad59a704818eb9e5955636
sunlan <sunlan@apache.org>
2017-12-08 10:07:15 +0800

    Make tuples Serializable

--- src/main/groovy/lang/Tuple8.java
+++ src/main/groovy/lang/Tuple8.java
@@ -25,6 +25,7 @@ package groovy.lang;
  * @since 2.5.0
  */
 public class Tuple8<T1, T2, T3, T4, T5, T6, T7, T8> extends Tuple {
+    private static final long serialVersionUID = -8895822084644138566L;
     private final T1 first;
     private final T2 second;
     private final T3 third;
