316329d24945293b5b82c80f57a950ec561bec3a
sunlan <sunlan@apache.org>
2017-12-09 20:22:57 +0800

    Minor refactoring: classCache and sourceCache
    (cherry picked from commit 2c0a3bf)

---
+++ src/main/org/codehaus/groovy/runtime/memoize/EvictableMemoizeCache.java
@@ -0,0 +1,68 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.codehaus.groovy.runtime.memoize;
+
+import java.util.Collection;
+
+/**
+ * Represents an evictable memoize cache with its essential methods
+ * @param <K> type of the keys
+ * @param <V> type of the values
+ */
+public interface EvictableMemoizeCache<K, V> extends MemoizeCache<K, V> {
+    /**
+     * Remove the cached value by the key
+     * @param key
+     * @return returns false if there was no matching key
+     */
+    boolean remove(K key);
+
+    /**
+     * Clear the cache
+     * @return returns cleared values
+     */
+    Collection<V> clear();
+
+    /**
+     * Try to get the value from cache.
+     * If not found, create the value by {@link ValueProvider} and put it into the cache, at last return the value
+     * @param key
+     * @return the cached value
+     */
+    V getAndPut(K key, ValueProvider<K, V> valueProvider);
+
+    /**
+     * Get all cached values
+     * @return all cached values
+     */
+    Collection<V> values();
+
+    /**
+     * Represents a provider used to create value
+     * @param <K> type of the key
+     * @param <V> type of the value
+     */
+    interface ValueProvider<K, V> {
+        /**
+         * Provide the created value
+         * @return
+         */
+        V provide(K key);
+    }
+}
---
+++ src/main/org/codehaus/groovy/runtime/memoize/SimpleCache.java
@@ -0,0 +1,152 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.codehaus.groovy.runtime.memoize;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ *
+ * Represents a simple cache, which is thread safe and backed by {@link java.util.HashMap}
+ *
+ * @param <K> type of the keys
+ * @param <V> type of the values
+ */
+public class SimpleCache<K, V> implements EvictableMemoizeCache<K, V> {
+    private final Map<K, V> map = new HashMap<>();
+    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private final ReentrantReadWriteLock.ReadLock readLock = rwl.readLock();
+    private final ReentrantReadWriteLock.WriteLock writeLock = rwl.writeLock();
+
+    @Override
+    public V get(K key) {
+        if (null == key) {
+            return null;
+        }
+
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        writeLock.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
+
+    }
+
+    @Override
+    public V getAndPut(K key, ValueProvider<K, V> valueProvider) {
+        return getAndPut(key, valueProvider, true);
+    }
+
+    public V getAndPut(K key, ValueProvider<K, V> valueProvider, boolean shouldCache) {
+        V value;
+
+        readLock.lock();
+        try {
+            value = map.get(key);
+            if (null != value) return value;
+        } finally {
+            readLock.unlock();
+        }
+
+        writeLock.lock();
+        try {
+            // try to find the cached value again
+            value = map.get(key);
+            if (null != value) return value;
+
+            value = valueProvider.provide(key);
+            if (shouldCache) map.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
+
+        return value;
+    }
+
+
+
+    @Override
+    public Collection<V> values() {
+        return map.values();
+    }
+
+    @Override
+    public boolean remove(K key) {
+        V removedValue;
+
+        writeLock.lock();
+        try {
+            removedValue = map.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
+
+        return null != removedValue;
+    }
+
+    @Override
+    public Collection<V> clear() {
+        Collection<V> values;
+
+        writeLock.lock();
+        try {
+            values = map.values();
+            map.clear();
+        } finally {
+            writeLock.unlock();
+        }
+
+        return values;
+    }
+
+    @Override
+    public void cleanUpNullReferences() {
+        writeLock.lock();
+        try {
+            List<K> keys = new LinkedList<>();
+
+            for (Map.Entry<K, V> entry : map.entrySet()) {
+                if (null == entry.getValue()) {
+                    keys.add(entry.getKey());
+                }
+            }
+
+            for (K key : keys) {
+                map.remove(key);
+            }
+        } finally {
+            writeLock.unlock();
+        }
+    }
+}
