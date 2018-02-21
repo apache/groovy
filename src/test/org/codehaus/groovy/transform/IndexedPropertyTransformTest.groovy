d11d564254741544c46dd3a6c649e25ad8410516
paulk <paulk@asert.com.au>
2018-02-21 10:18:24 +1000

    improve class comment

--- src/test/org/codehaus/groovy/transform/IndexedPropertyTransformTest.groovy
+++ src/test/org/codehaus/groovy/transform/IndexedPropertyTransformTest.groovy
@@ -19,7 +19,7 @@
 package org.codehaus.groovy.transform
 
 /**
- * @author Paul King
+ * Tests for the {@code @IndexedProperty} AST transformation.
  */
 class IndexedPropertyTransformTest extends GroovyShellTestCase {
 
