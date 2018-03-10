7b73c6aeb2d399512b6cdfbb6ff4fbef59c33f9a
danielsun1106 <realbluesun@hotmail.com>
2018-03-10 22:16:06 +0800

    GROOVY-8439: @CompileStatic cannot compile generic class's static method

--- src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -2738,6 +2738,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
                             }
                     }
                     boolean lastArg = i == length - 1;
+
                     if (lastArg && inferredType.isArray()) {
                         if (inferredType.getComponentType().equals(originType)) {
                             inferredType = originType;
@@ -2745,6 +2746,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
                     } else if (!typeCheckMethodArgumentWithGenerics(originType, inferredType, lastArg)) {
                         addError("Expected parameter of type "+ inferredType.toString(false)+" but got "+originType.toString(false), closureParam.getType());
                     }
+
                     typeCheckingContext.controlStructureVariables.put(closureParam, inferredType);
                 }
             }
