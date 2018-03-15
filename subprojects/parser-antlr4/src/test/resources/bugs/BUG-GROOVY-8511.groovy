4ba0b72b2c6c418f22fe2ab6fb7a8b4e5c36d381
sunlan <sunlan@apache.org>
2018-03-16 07:58:38 +0800

    Add one more test case for GROOVY-8511

--- subprojects/parser-antlr4/src/test/resources/bugs/BUG-GROOVY-8511.groovy
+++ subprojects/parser-antlr4/src/test/resources/bugs/BUG-GROOVY-8511.groovy
@@ -23,3 +23,12 @@ class XX {
 
 @groovy.transform.PackageScope
 int y() {}
+
+
+@groovy.transform.PackageScope(groovy.transform.PackageScopeTarget.METHODS)
+class Foo2 {
+    Object method1() {}
+    public Object method2() {}
+    private Object method3() {}
+    protected Object method4() {}
+}
