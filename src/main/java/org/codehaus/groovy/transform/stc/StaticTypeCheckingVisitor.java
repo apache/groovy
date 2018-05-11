8df09980dd4413966922fa7ef23236db809325af
danielsun1106 <realbluesun@hotmail.com>
2018-05-12 02:13:31 +0800

    GROOVY-7883: Static compiler prefers private constructor over public if private matches better

--- src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -4357,6 +4357,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
 
         // lookup in DGM methods too
         findDGMMethodsByNameAndArguments(getTransformLoader(), receiver, name, args, methods);
+        methods = filterMethodsByVisibility(receiver, methods);
         List<MethodNode> chosen = chooseBestMethod(receiver, methods, args);
         if (!chosen.isEmpty()) return chosen;
 
@@ -4381,6 +4382,36 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
         }
 
         return EMPTY_METHODNODE_LIST;
+    }
+
+    private List<MethodNode> filterMethodsByVisibility(ClassNode receiver, List<MethodNode> methods) {
+        List<MethodNode> result = new LinkedList<>();
+
+        ClassNode enclosingClassNode = typeCheckingContext.getEnclosingClassNode();
+        if (enclosingClassNode.equals(receiver)) {
+            for (MethodNode methodNode : methods) {
+                if (methodNode.isPrivate() && !enclosingClassNode.equals(methodNode.getDeclaringClass())) {
+                    continue;
+                }
+
+                result.add(methodNode);
+            }
+        } else {
+            boolean isDerived = enclosingClassNode.isDerivedFrom(receiver);
+            for (MethodNode methodNode : methods) {
+                if (methodNode.isPrivate()) {
+                    continue;
+                }
+
+                if (!isDerived && methodNode.isProtected()) {
+                    continue;
+                }
+
+                result.add(methodNode);
+            }
+        }
+
+        return result;
     }
 
     /**
