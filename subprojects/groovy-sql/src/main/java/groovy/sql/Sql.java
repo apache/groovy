394db983d51446f849205efc4f9cca21601cc47c
Daniel Sun <realbluesun@hotmail.com>
2018-05-12 12:35:01 +0800

    GROOVY-7883  Static compiler prefers private constructor over public if private matches be

--- subprojects/groovy-sql/src/main/java/groovy/sql/Sql.java
+++ subprojects/groovy-sql/src/main/java/groovy/sql/Sql.java
@@ -3973,7 +3973,7 @@ public class Sql {
      * @return the resulting list of rows
      * @throws SQLException if a database error occurs
      */
-    protected List<GroovyRowResult> asList(String sql, ResultSet rs,
+    public List<GroovyRowResult> asList(String sql, ResultSet rs,
                                            @ClosureParams(value=SimpleType.class, options="java.sql.ResultSetMetaData") Closure metaClosure) throws SQLException {
         return asList(sql, rs, 0, 0, metaClosure);
     }
