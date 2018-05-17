25a414f8232eb7092869baab4ee5645dc17f39fc
Paul King <paulk@asert.com.au>
2018-05-18 09:38:26 +1000

    remove unused deprecated constant

--- src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
+++ src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
@@ -82,9 +82,6 @@ public class CompilerConfiguration {
     /** An array of the valid targetBytecode values **/
     public static final String[] ALLOWED_JDKS = JDK_TO_BYTECODE_VERSION_MAP.keySet().toArray(new String[0]);
 
-    // Just call getVMVersion() once.
-    public static final String CURRENT_JVM_VERSION = getVMVersion();
-
     private static final String GROOVY_ANTLR4_OPT = "groovy.antlr4";
 
     /**
@@ -229,7 +226,7 @@ public class CompilerConfiguration {
         setScriptBaseClass(null);
         setRecompileGroovySource(false);
         setMinimumRecompilationInterval(100);
-        setTargetBytecode(getSystemPropertySafe("groovy.target.bytecode", getVMVersion()));
+        setTargetBytecode(getSystemPropertySafe("groovy.target.bytecode", getMinBytecodeVersion()));
         setDefaultScriptExtension(getSystemPropertySafe("groovy.default.scriptExtension", ".groovy"));
 
         // Source file encoding
@@ -777,7 +774,7 @@ public class CompilerConfiguration {
         return this.targetBytecode;
     }
 
-    private static String getVMVersion() {
+    private static String getMinBytecodeVersion() {
         return JDK8;
     }
 
