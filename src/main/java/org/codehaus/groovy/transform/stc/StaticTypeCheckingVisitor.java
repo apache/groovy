970162478eb3892ac45c40f6b02afa36ce0208e8
sunlan <sunlan@apache.org>
2018-05-23 08:48:13 +0800

    GROOVY-8595: Expected parameter of type XXX but got YYY static compile error
    (cherry picked from commit 1546b19)

--- src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -3245,7 +3245,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
                         if (typeCheckMethodsWithGenericsOrFail(chosenReceiver.getType(), args, mn.get(0), call)) {
                             returnType = adjustWithTraits(directMethodCallCandidate, chosenReceiver.getType(), args, returnType);
 
-                            if (null != typeCheckingContext.getEnclosingReturnStatement() && !inNestedMethodCall()) {
+                            if (null != typeCheckingContext.getEnclosingReturnStatement() && !isNestedOrSandwichedMethodCall()) {
                                 ClassNode inferredType = infer(returnType, typeCheckingContext.getEnclosingMethod().getReturnType());
                                 if (null != inferredType) {
                                     returnType = inferredType;
@@ -3318,7 +3318,12 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
         }
     }
 
-    private boolean inNestedMethodCall() {
+    /**
+     * e.g. a(b()),         b() is nested method call
+     *      a().b().c(),    a() and b() are sandwiched method call
+     *
+     */
+    private boolean isNestedOrSandwichedMethodCall() {
         return typeCheckingContext.getEnclosingMethodCalls().size() > 1;
     }
 
