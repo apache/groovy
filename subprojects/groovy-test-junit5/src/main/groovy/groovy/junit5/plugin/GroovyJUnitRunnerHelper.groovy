7347b519f36f31f814ba613fb2e64db28d70c804
Paul King <paulk@asert.com.au>
2018-05-24 22:23:29 +1000

    GROOVY-8601: Groovy should have a runner for JUnit5 tests (closes #723)

---
+++ subprojects/groovy-test-junit5/src/main/groovy/groovy/junit5/plugin/GroovyJUnitRunnerHelper.groovy
@@ -0,0 +1,48 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package groovy.junit5.plugin
+
+import org.junit.platform.launcher.Launcher
+import org.junit.platform.launcher.LauncherDiscoveryRequest
+import org.junit.platform.launcher.TestExecutionListener
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder
+import org.junit.platform.launcher.core.LauncherFactory
+import org.junit.platform.launcher.listeners.LoggingListener
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
+
+class GroovyJUnitRunnerHelper {
+    static Throwable execute(Class testClass) {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectClass(testClass)).build()
+        Launcher launcher = LauncherFactory.create()
+
+        TestExecutionListener listener = new SummaryGeneratingListener()
+        launcher.registerTestExecutionListeners(listener)
+        launcher.registerTestExecutionListeners(LoggingListener.forJavaUtilLogging())
+        launcher.execute(request)
+        println listener.summary.with{ "JUnit5 launcher: passed=$testsSucceededCount, failed=$testsFailedCount, skipped=$testsSkippedCount, time=${timeFinished-timeStarted}ms" }
+        if (listener.summary.failures) {
+            listener.summary.printFailuresTo(new PrintWriter(System.out, true))
+            return listener.summary.failures[0].exception
+        }
+        return null
+    }
+}
---
+++ subprojects/groovy-test-junit5/src/spec/test/MyTestViaRun.groovy
@@ -0,0 +1,81 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+class MyTestViaRun extends GroovyTestCase {
+  void testJunit5ViaRun() {
+    new GroovyShell().run '''
+//@Grab('org.junit.jupiter:junit-jupiter-params:5.2.0')
+import org.junit.jupiter.api.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
+import static org.junit.jupiter.api.Assertions.assertTrue
+import static org.junit.jupiter.api.DynamicTest.dynamicTest
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
+import java.util.logging.Logger
+import org.junit.platform.launcher.listeners.LoggingListener
+
+// tag::junit5_test_part1[]
+class MyTest {
+// end::junit5_test_part1[]
+  // tag::junit5_test_part3[]
+  @BeforeAll
+  static void init() {
+    def logger = Logger.getLogger(LoggingListener.name)
+    logger.level = Level.FINE
+    logger.addHandler(new ConsoleHandler(level: Level.FINE))
+  }
+  // end::junit5_test_part3[]
+
+// tag::junit5_test_part2[]
+  @Test
+  void streamSum() {
+    assertTrue(Stream.of(1, 2, 3)
+      .mapToInt(i -> i)
+      .sum() > 5, () -> "Sum should be greater than 5")
+  }
+
+  @RepeatedTest(value=2, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+  void streamSumRepeated() {
+    assert Stream.of(1, 2, 3).mapToInt(i -> i).sum() == 6
+  }
+
+  private boolean isPalindrome(s) { s == s.reverse()  }
+
+  @ParameterizedTest                                                              // <1>
+  @ValueSource(strings = [ "racecar", "radar", "able was I ere I saw elba" ])
+  void palindromes(String candidate) {
+    assert isPalindrome(candidate)
+  }
+
+  @TestFactory
+  def dynamicTestCollection() {[
+    dynamicTest("Add test") { -> assert 1 + 1 == 2 },
+    dynamicTest("Multiply Test", () -> { assert 2 * 3 == 6 })
+  ]}
+}
+// end::junit5_test_part2[]
+''', "MyTest", []
+  }
+/*
+// tag::junit5_test_output[]
+JUnit5 launcher: passed=8, failed=0, skipped=0, time=246ms
+// end::junit5_test_output[]
+*/
+}
