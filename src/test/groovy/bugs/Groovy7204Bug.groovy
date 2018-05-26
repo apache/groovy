c68c4c0e0213a50b91550404953eca14636cfa31
Daniel Sun <realbluesun@hotmail.com>
2018-05-26 13:44:26 +0800

    Minor refactoring: use another extracting placeholder logic

--- src/test/groovy/bugs/Groovy7204Bug.groovy
+++ src/test/groovy/bugs/Groovy7204Bug.groovy
@@ -16,25 +16,6 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- */
 package groovy.bugs
 
 class Groovy7204Bug extends GroovyTestCase {
@@ -541,6 +522,32 @@ class Groovy7204Bug extends GroovyTestCase {
         }
         
         test()
+        '''
+    }
+
+    void testCompileStatic7() {
+        assertScript '''
+        @groovy.transform.CompileStatic
+        class Trie<T> {}
+        
+        @groovy.transform.CompileStatic
+        class Base<T> {
+            protected List<Trie<T>> list
+            
+            Base() {
+                list = new ArrayList<Trie<T>>()
+                list.add(new Trie<String>())
+            }
+        }
+        
+        @groovy.transform.CompileStatic
+        class Derived extends Base<String> {
+            Trie<String> getFirstElement() {
+                list.get(0)
+            }
+        }
+        
+        assert new Derived().getFirstElement() instanceof Trie
         '''
     }
 }
