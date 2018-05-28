bd285f381576bc47a711f0275493c2dcd60cd8cf
Daniel Sun <realbluesun@hotmail.com>
2018-05-28 18:21:56 +0800

    Add one more test

--- src/test/groovy/bugs/Groovy8609Bug.groovy
+++ src/test/groovy/bugs/Groovy8609Bug.groovy
@@ -98,4 +98,26 @@ class Groovy8609Bug extends CompilableTestSupport {
 
         assert errMsg.contains('[Static type checking] - Cannot call A <ArrayList, HashMap>#getFirstRecord(T) with arguments [java.util.ArrayList <TreeMap>]')
     }
+
+    void testUpperBoundWithGenericsThroughWrongType2() {
+        def errMsg = shouldFail '''
+        @groovy.transform.CompileStatic
+        public class A<T extends List<E>, E extends Map<String, Integer>> {
+            E getFirstRecord(T recordList) {
+                return recordList.get(0)
+            }
+            
+            static void main(args) {
+                def list = new ArrayList<HashMap<String, Long>>()
+                def record = new HashMap<String, Long>()
+                list.add(record)
+                def a = new A<ArrayList<HashMap<String, Integer>>, HashMap<String, Integer>>()
+                assert record.is(a.getFirstRecord(list))
+            }
+        }
+        '''
+
+        // TODO we should print generics details, e.g. [Static type checking] - Cannot call A <ArrayList, HashMap<String, Integer>>#getFirstRecord(T) with arguments [java.util.ArrayList <HashMap<String, Long>>]
+        assert errMsg.contains('[Static type checking] - Cannot call A <ArrayList, HashMap>#getFirstRecord(T) with arguments [java.util.ArrayList <HashMap>]')
+    }
 }
