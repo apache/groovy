37f8a8e8b775a5d2232390edf9a3e6627eaff3bd
danielsun1106 <realbluesun@hotmail.com>
2018-06-02 20:55:52 +0800

    Move the test of GROOVY-8613 to parser-antlr4 sub-project
    (cherry picked from commit 5531e6c)

--- subprojects/parser-antlr4/src/test/resources/bugs/BUG-GROOVY-8613.groovy
+++ subprojects/parser-antlr4/src/test/resources/bugs/BUG-GROOVY-8613.groovy
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package groovy.bugs
+package bugs
 
 class Groovy8613Bug extends GroovyTestCase {
     void testElvisAssign() {
