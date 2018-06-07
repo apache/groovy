8d730e19a591c29fbbc1bb3e3a4c1731a6a0ed5b
sunlan <sunlan@apache.org>
2018-06-07 10:51:15 +0800

    Fix artifact path

--- subprojects/groovy-groovydoc/src/main/java/org/codehaus/groovy/tools/groovydoc/gstringTemplates/GroovyDocTemplateInfo.java
+++ subprojects/groovy-groovydoc/src/main/java/org/codehaus/groovy/tools/groovydoc/gstringTemplates/GroovyDocTemplateInfo.java
@@ -20,7 +20,7 @@ package org.codehaus.groovy.tools.groovydoc.gstringTemplates;
 
 public class GroovyDocTemplateInfo {
     private static final String TEMPLATE_BASEDIR = "org/codehaus/groovy/tools/groovydoc/gstringTemplates/";
-    private static final String DOCGEN_BASEDIR = "org/codehaus/groovy/tools/";
+    private static final String DOCGEN_BASEDIR = "org/apache/groovy/docgenerator/";
     public static final String[] DEFAULT_DOC_TEMPLATES = new String[]{ // top level templates
             TEMPLATE_BASEDIR + "topLevel/index.html",
             TEMPLATE_BASEDIR + "topLevel/overview-frame.html", // needs all package names
@@ -31,7 +31,7 @@ public class GroovyDocTemplateInfo {
             TEMPLATE_BASEDIR + "topLevel/deprecated-list.html",
             TEMPLATE_BASEDIR + "topLevel/stylesheet.css", // copy default one, may override later
             TEMPLATE_BASEDIR + "topLevel/inherit.gif",
-            DOCGEN_BASEDIR + "groovy.ico",
+            DOCGEN_BASEDIR   + "groovy.ico",
     };
     public static final String[] DEFAULT_PACKAGE_TEMPLATES = new String[]{ // package level templates
             TEMPLATE_BASEDIR + "packageLevel/package-frame.html",
