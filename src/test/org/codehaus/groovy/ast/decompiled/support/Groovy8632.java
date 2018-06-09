76cbe177c01b5dc8720a84ba5169701afaaff51b
John Wagenleitner <jwagenleitner@apache.org>
2018-06-08 17:00:16 -0700

    GROOVY-8632: Groovy 2.5.0 fails to compile Google Java Client sample code (closes #753)
    Class files can contain INNERCLASS references to other
    classes inner classes whose name may be the same name as a
    contained inner class. By storing modifiers in a map keyed
    by short class name there is a possibility for the wrong
    modifiers to be stored.
    
    Since generated class files for inner classes contain an INNERCLASS
    self reference, the logic can be simplified to look for a matching
    name and storing those access modifiers. This eliminates the need
    to search the outer class for the INNERCLASS reference.
    
    (cherry picked from commit 351544823ad7a4e250ce036d822ce3207c0edf98)

---
+++ src/test/org/codehaus/groovy/ast/decompiled/support/Groovy8632.java
@@ -0,0 +1,9 @@
+package org.codehaus.groovy.ast.decompiled.support;
+
+public class Groovy8632 extends Groovy8632Abstract {
+    Groovy8632(Builder builder) { super(builder); }
+    public static final class Builder extends Groovy8632Abstract.Builder {}
+    public class InnerBuilder extends Groovy8632Abstract.InnerBuilder {}
+    private class InnerPrivate {}
+    protected class InnerProtected {}
+}
---
+++ src/test/org/codehaus/groovy/ast/decompiled/support/Groovy8632Abstract.java
@@ -0,0 +1,7 @@
+package org.codehaus.groovy.ast.decompiled.support;
+
+public abstract class Groovy8632Abstract {
+    Groovy8632Abstract(Builder builder) {}
+    public abstract static class Builder {}
+    public abstract class InnerBuilder {}
+}
