9b4d18b66899aa14bdfc40c03d1760db872ebe07
Paul King <paulk@asert.com.au>
2018-06-23 16:47:12 +1000

    show result of expanding unicode as comment

--- src/test/groovy/EscapedUnicodeTest.groovy
+++ src/test/groovy/EscapedUnicodeTest.groovy
@@ -51,3 +51,31 @@ class EscapedUnicodeTest extends GroovyTestCase {
 \u007d
 */
 
+/*
+class EscapedUnicodeTest extends GroovyTestCase {
+
+    void testAssert() {
+        def x = "abc"
+
+        assert x != "foo"
+        assert x !=  null
+        assert x != "def"
+        assert x == "abc"
+
+        assert x.equals("abc")
+	}
+
+    void testUnknownVariable() {
+        try {
+	        def y = this.x
+	        fail("x is undefined, should throw an exception")
+        }
+        catch (MissingPropertyException e) {
+			assert e.getProperty() == "x"
+            def text = e.message
+            assert text == "No such property: x for class: EscapedUnicodeTest"
+        }
+    }
+
+}
+ */
