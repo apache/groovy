3ed6a85ba3e7d12588ff27223195008e5bb72c91
sunlan <sunlan@apache.org>
2018-07-03 17:34:15 +0800

    Trivial formatting(closes #762)
    just to close PR#762

--- src/main/java/org/codehaus/groovy/runtime/typehandling/BigDecimalMath.java
+++ src/main/java/org/codehaus/groovy/runtime/typehandling/BigDecimalMath.java
@@ -40,8 +40,7 @@ public final class BigDecimalMath extends NumberMath {
 
     public static final BigDecimalMath INSTANCE = new BigDecimalMath();
 
-    private BigDecimalMath() {
-    }
+    private BigDecimalMath() {}
 
     protected Number absImpl(Number number) {
         return toBigDecimal(number).abs();
