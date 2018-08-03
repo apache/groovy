6afe5dd62cfcb72fa47fdb6a9ed3bf08f66675fb
Daniel Sun <sunlan@apache.org>
2018-08-04 07:05:12 +0800

    Optimize imports
    (cherry picked from commit 1e41047c39799498dc2a4bd9b88db4774a9f3494)

--- src/main/java/org/codehaus/groovy/ast/decompiled/ClassStub.java
+++ src/main/java/org/codehaus/groovy/ast/decompiled/ClassStub.java
@@ -19,7 +19,6 @@
 package org.codehaus.groovy.ast.decompiled;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
