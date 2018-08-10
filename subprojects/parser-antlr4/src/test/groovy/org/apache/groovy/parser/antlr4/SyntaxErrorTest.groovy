c405e6a998a9ae1dc57882d994fcdd96f3d6d628
Daniel Sun <sunlan@apache.org>
2018-08-11 03:31:20 +0800

    GROOVY-8743: Abstract method in trait should not have method body
    (cherry picked from commit 45bd8295ebe85b63d8b0bd425fc03a530c6868b7)

--- subprojects/parser-antlr4/src/test/groovy/org/apache/groovy/parser/antlr4/SyntaxErrorTest.groovy
+++ subprojects/parser-antlr4/src/test/groovy/org/apache/groovy/parser/antlr4/SyntaxErrorTest.groovy
@@ -236,6 +236,10 @@ class SyntaxErrorTest extends GroovyTestCase {
         TestUtils.doRunAndShouldFail('fail/UnaryOperator_02x.groovy');
     }
 
+    void "test groovy core - Trait"() {
+        TestUtils.shouldFail('fail/Trait_01.groovy');
+    }
+
     /**************************************/
     static unzipScriptAndShouldFail(String entryName, List ignoreClazzList, Map<String, String> replacementsMap=[:], boolean toCheckNewParserOnly = false) {
         ignoreClazzList.addAll(TestUtils.COMMON_IGNORE_CLASS_LIST)
