d890d571c139fc23f2a30b0fa0b773c5bb9663de
Paul King <paulk@asert.com.au>
2018-08-25 23:35:11 +1000

    GROOVY-8757: Incorrect bytecode produced after compiling class implementing trait with generic method (closes #788)

---
+++ src/test/groovy/bugs/groovy8757/UsageTest.groovy
@@ -0,0 +1,31 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package groovy.bugs.groovy8757
+
+class UsageTest extends GroovyTestCase {
+    void testAccessingPrecompiledTraitWithMethodGenerics() {
+        def c0 = new GroovyShell().evaluate('''
+            import groovy.bugs.groovy8757.T0
+            class C0 implements T0 {}
+            new C0()
+        ''')
+        assertEquals(42, c0.foo(Integer))
+        assertEquals(42L, c0.foo(Long))
+    }
+}
