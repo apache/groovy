c71f4e323ab022429d925c03a11b10d56905f4f2
Paul King <paulk@asert.com.au>
2018-09-14 14:04:22 +1000

    add missing license header

--- src/spec/test/metaprogramming/StaticPropertyMissingAndMethodMissingTest.groovy
+++ src/spec/test/metaprogramming/StaticPropertyMissingAndMethodMissingTest.groovy
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package metaprogramming
 
 class StaticPropertyMissingAndMethodMissingTest extends GroovyTestCase {
@@ -13,11 +31,10 @@ class StaticPropertyMissingAndMethodMissingTest extends GroovyTestCase {
 
             assert Foo.bar() == 'Missing static method name is bar'
             // end::static_method_missing[]
-'''
+        '''
     }
 
     void testStaticPropertyMissing() {
-
         assertScript '''
             // tag::static_property_missing[]
             class Foo {
@@ -28,6 +45,6 @@ class StaticPropertyMissingAndMethodMissingTest extends GroovyTestCase {
 
             assert Foo.foobar == 'Missing static property name is foobar'
             // end::static_property_missing[]
-'''
+        '''
     }
 }
