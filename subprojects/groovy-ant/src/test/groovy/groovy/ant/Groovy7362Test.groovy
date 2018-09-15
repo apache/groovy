3c11d4d2407260e18256b08eb1d39d33da8ffedc
Paul King <paulk@asert.com.au>
2018-09-15 13:03:04 +1000

    GROOVY-7362: Can't compile Java class extending Groovy abstract class with explicit GroovyObject interface (already fixed - add test case)

---
+++ subprojects/groovy-ant/src/test/groovy/groovy/ant/Groovy7362Test.groovy
@@ -0,0 +1,48 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package groovy.ant
+
+class Groovy7362Test extends AntTestCase {
+    void testCompileJavaClassExtendingAbstractGroovyClass() {
+//        def debugLogger = new org.apache.tools.ant.DefaultLogger()
+//        debugLogger.setMessageOutputLevel(4)
+//        debugLogger.setOutputPrintStream(System.out)
+//        debugLogger.setErrorPrintStream(System.err)
+
+        doInTmpDir { ant, baseDir ->
+            baseDir.src {
+                'ConcreteClass.groovy'('''
+                    class ConcreteClass { }
+                ''')
+                'AbstractClass.groovy'('''
+                    abstract class AbstractClass extends ConcreteClass implements GroovyObject { }
+                ''')
+                'JavaClass.java'('''
+                    public class JavaClass extends AbstractClass { }
+                ''')
+            }
+//            ant.project.addBuildListener(debugLogger)
+            ant.mkdir(dir: 'build')
+            def cp = System.getProperty('java.class.path') + ':build'
+            ant.taskdef(name: 'groovyc', classname: 'org.codehaus.groovy.ant.Groovyc')
+            ant.groovyc(srcdir: 'src', destdir: 'build')
+            ant.javac(classpath: cp, destdir: 'build', srcdir: 'src', includeantruntime: 'false', fork: 'true')
+        }
+    }
+}
