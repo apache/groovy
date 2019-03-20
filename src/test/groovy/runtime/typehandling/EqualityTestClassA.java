c9fdc50cc8623c3e957f0d252310d153a3e260bf
Paul King <paulk@asert.com.au>
2019-03-20 22:42:21 +1000

    move new tests to existing subdirectory

--- src/test/groovy/runtime/typehandling/EqualityTestClassA.java
+++
@@ -1,9 +0,0 @@
-package groovy.runtime.typehandling;
-
-public class EqualityTestClassA extends EqualityTestAbstractClass {
-
-    public EqualityTestClassA(int id, String value) {
-        super(id, value);
-    }
-
-}
--- src/test/groovy/runtime/typehandling/EqualityTestClassB.java
+++
@@ -1,9 +0,0 @@
-package groovy.runtime.typehandling;
-
-public class EqualityTestClassB extends EqualityTestAbstractClass {
-
-    public EqualityTestClassB(int id, String value) {
-        super(id, value);
-    }
-
-}
