5d34fd3abfafdb55ab58ca3f0f7bc2290da7d948
Paul King <paulk@asert.com.au>
2019-04-16 19:42:27 +1000

    remove codenarc warnings (take 2)

--- src/test/groovy/bugs/Groovy4078Bug.groovy
+++ src/test/groovy/bugs/Groovy4078Bug.groovy
@@ -21,15 +21,15 @@ package groovy.bugs
 class Groovy4078Bug extends GroovyTestCase {
     void testInfiniteLoopDetectionInStepUsage() {
         (2..2).step 0, {assert it != null} //IntRange
-        
+
         ('b'..'b').step 0, {assert it != null} //ObjectRange
-        
+
         5.step( 5, 1 ) { assert it != null } // DGM.step(), int
-        
+
         5.0.step (5.0, 1 ) { assert it != null } // DGM.step(), BigDecimal
-        
-        def from = new BigInteger(5)
-        def to = new BigInteger(5)
+
+        def from = BigInteger.valueOf(5)
+        def to = BigInteger.valueOf(5)
         from.step (to, 1 ) { assert it != null }  // DGM.step(), BigInteger
 
         try{
@@ -38,7 +38,7 @@ class Groovy4078Bug extends GroovyTestCase {
         } catch(ex) {
             assert ex.message.contains('Infinite loop detected due to step size of 0')
         }
-                      
+
         try{
             ('a'..'b').step 0, {assert it != null} // ObjectRange
             fail('Should have failed as step size 0 causes infinite loop')
