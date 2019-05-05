a185f4be026dbb05acbc59de95fce7c4bb935c46
Paul King <paulk@asert.com.au>
2019-05-05 14:19:29 +1000

    minor refactor: rename test

--- subprojects/groovy-test-junit5/src/test/groovy/JUnit5Test.groovy
+++ subprojects/groovy-test-junit5/src/test/groovy/JUnit5Test.groovy
@@ -23,29 +23,31 @@ import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 import static org.junit.jupiter.api.DynamicTest.dynamicTest
 
-class MyTestJUnit5 {
+class JUnit5Test {
 
-  @Test
-  void streamSum() {
-    assert Stream.of(1, 2, 3).mapToInt{ i -> i }.sum() > 5
-  }
+    @Test
+    void streamSum() {
+        assert Stream.of(1, 2, 3).mapToInt { i -> i }.sum() > 5
+    }
 
-  @RepeatedTest(value=2, name = "{displayName} {currentRepetition}/{totalRepetitions}")
-  void streamSumRepeated() {
-    assert Stream.of(1, 2, 3).mapToInt{ i -> i }.sum() == 6
-  }
+    @RepeatedTest(value = 2, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    void streamSumRepeated() {
+        assert Stream.of(1, 2, 3).mapToInt { i -> i }.sum() == 6
+    }
 
-  private boolean isPalindrome(s) { s == s.reverse()  }
+    private boolean isPalindrome(s) { s == s.reverse() }
 
-  @ParameterizedTest
-  @ValueSource(strings = [ "racecar", "radar", "able was I ere I saw elba" ])
-  void palindromes(String candidate) {
-    assert isPalindrome(candidate)
-  }
+    @ParameterizedTest
+    @ValueSource(strings = ["racecar", "radar", "able was I ere I saw elba"])
+    void palindromes(String candidate) {
+        assert isPalindrome(candidate)
+    }
 
-  @TestFactory
-  def dynamicTestCollection() {[
-    dynamicTest("Add test") { -> assert 1 + 1 == 2 },
-    dynamicTest("Multiply Test") { -> assert 2 * 3 == 6 }
-  ]}
+    @TestFactory
+    def dynamicTestCollection() {
+        [
+                dynamicTest("Add test") { -> assert 1 + 1 == 2 },
+                dynamicTest("Multiply Test") { -> assert 2 * 3 == 6 }
+        ]
+    }
 }
