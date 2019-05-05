a21e1ca9381f11198ab7d04225a160f3085d5db7
Paul King <paulk@asert.com.au>
2019-05-05 14:13:20 +1000

    minor refactor: move test

--- src/test/org/codehaus/groovy/transform/packageScope/PackageScopeTransformTest.groovy
+++ src/test/org/codehaus/groovy/transform/packageScope/PackageScopeTransformTest.groovy
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.codehaus.groovy.transform
+package org.codehaus.groovy.transform.packageScope
 
 import java.lang.reflect.Modifier
 
