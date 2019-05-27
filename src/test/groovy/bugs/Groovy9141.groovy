d06812d99109ad6919513855f6c3f696356418d0
Paul King <paulk@asert.com.au>
2019-05-27 16:23:43 +1000

    refactor test

--- src/test/groovy/bugs/Groovy9141.groovy
+++ src/test/groovy/bugs/Groovy9141.groovy
@@ -26,31 +26,39 @@ import static org.codehaus.groovy.control.ParserVersion.V_2
 
 @CompileStatic
 final class Groovy9141 extends CompilableTestSupport {
-    private static final String METHOD_DEF = '''
-        abstract meth() {
-            println 42
-        }
+    private static final String ABSTRACT_METHOD_WITH_BODY = '''
+        abstract meth() { }
     '''
 
+    // not a language requirement but script-level check takes precedence in current implementation
     void testAbstractMethodWithBodyInScript() {
-        def err = shouldNotCompile METHOD_DEF
-        assert err =~ / You cannot define an abstract method\[meth\] in the script. Try removing the 'abstract' /
+        def err = shouldNotCompile ABSTRACT_METHOD_WITH_BODY
+        assert err =~ / You cannot define an abstract method\[meth] in the script. Try removing the 'abstract' /
     }
 
     void testAbstractMethodWithBodyInClass() {
         def err = shouldNotCompile """
             class Main {
-                $METHOD_DEF
+                $ABSTRACT_METHOD_WITH_BODY
             }
         """
         // not a language requirement but class level check takes precedence in current implementation
         assert err =~ / Can't have an abstract method in a non-abstract class. /
     }
 
+    void testAbstractMethodWithBodyInCAbstractlass() {
+        def err = shouldNotCompile """
+            abstract class Main {
+                $ABSTRACT_METHOD_WITH_BODY
+            }
+        """
+        assert err =~ / You defined an abstract method\[meth] with a body. Try removing the method body @ line /
+    }
+
     void testAbstractMethodWithBodyInScript_oldParser() {
         def cc = new CompilerConfiguration(parserVersion: V_2)
         def err = shouldFail {
-            new GroovyShell(cc).evaluate METHOD_DEF
+            new GroovyShell(cc).evaluate ABSTRACT_METHOD_WITH_BODY
         }
         assert err =~ / Abstract methods do not define a body/
     }
