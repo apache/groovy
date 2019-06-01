f66b264754d190d877bbae06b77e92194cce08fe
Eric Milles <eric.milles@thomsonreuters.com>
2019-06-01 10:50:20 -0500

    GROOVY-9184 (clone of GROOVY-4438): enumConstants: reduce ambiguity within loop (closes #940)

---
+++ src/test/groovy/bugs/Groovy9184.groovy
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package groovy.bugs
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+import static org.codehaus.groovy.control.ParserPluginFactory.antlr2
+
+@CompileStatic
+@RunWith(JUnit4)
+final class Groovy9184 {
+
+    @Test(timeout=1500L)
+    void testEnumWithinEnum() {
+        def config = new CompilerConfiguration(pluginFactory: antlr2())
+
+        new GroovyShell(config).evaluate('''
+            enum Outer {
+                A, B
+                enum Inner {
+                    X, Y
+                }
+            }
+            assert Outer.A.name() == 'A'
+            assert Outer.A.ordinal() == 0
+            assert Outer.Inner.Y.name() == 'Y'
+            assert Outer.Inner.Y.ordinal() == 1
+        ''')
+    }
+}
