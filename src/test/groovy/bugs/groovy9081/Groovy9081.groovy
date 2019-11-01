662e812177efd3182acaae26b6a0f0ad954115cf
Daniel.Sun <sunlan@apache.org>
2019-11-01 17:12:00 +0800

    GROOVY-9298: Eliminate some illegal access warnings when indy is enabled (#1058)
    * Show detailed messages for illegal access warnings
    
    * Fix illegal access warning `java.util.HashMap$Node.toString()`
    
    * Fix illegal access warning `java.math.BigInteger.multiply(long)`
    
    * Fix illegal access warning `java.lang.invoke.MethodHandles$Lookup(java.lang.Class,int)`
    
    * Throw GroovyRuntimeException instead

--- src/test/groovy/bugs/groovy9081/Groovy9081.groovy
+++ src/test/groovy/bugs/groovy9081/Groovy9081.groovy
@@ -19,7 +19,6 @@
 package groovy.bugs.groovy9081
 
 import groovy.bugs.groovy9081.somepkg.ProtectedConstructor
-import org.junit.Ignore
 import org.junit.Test
 
 import java.awt.Font
@@ -68,5 +67,20 @@ final class Groovy9081 {
     @Test
     void testAsType2() {
         [run: {}] as ProtectedConstructor
+    }
+
+    @Test
+    void testAccessPackagePrivateInnerClassMember() {
+        def m = new HashMap()
+        m.a = 69
+        m.entrySet().iterator().next().toString()
+    }
+
+    @Test
+    void testAccessPackagePrivateMethod() {
+        BigInteger a = 333
+        int b = 2
+        BigDecimal c = a * b
+        assert c == 666
     }
 }
