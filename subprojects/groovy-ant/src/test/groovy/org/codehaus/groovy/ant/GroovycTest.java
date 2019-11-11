d1f9977722da5762fee40c3b1730267f476f45ad
Eric Milles <eric.milles@thomsonreuters.com>
2019-11-11 12:46:38 -0600

    minor edits
    (cherry picked from commit 81d8ac64923b9d514b3c49295a7f28241bbb46d4)

--- subprojects/groovy-ant/src/test/groovy/org/codehaus/groovy/ant/GroovycTest.java
+++ subprojects/groovy-ant/src/test/groovy/org/codehaus/groovy/ant/GroovycTest.java
@@ -30,6 +30,8 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 /**
@@ -40,17 +42,16 @@ import java.util.regex.Pattern;
  * matter as the tests remove all class files that should not pre-exist from this directory at each step.
  */
 public class GroovycTest extends GroovyTestCase {
-    private final String classDirectory = "target/classes/groovy/test/org/codehaus/groovy/ant/";
     private final File antFile = new File("src/test-resources/org/codehaus/groovy/ant/GroovycTest.xml");
     private Project project;
     private static boolean warned = false;
 
-    protected void setUp() throws Exception {
-        super.setUp(); //  Potentially throws Exception.
+    protected void setUp() {
         project = new Project();
         project.init();
         ProjectHelper.getProjectHelper().parse(project, antFile);
         project.executeTarget("clean");
+
         String altJavaHome = System.getProperty("java.home");
         if (altJavaHome.lastIndexOf("jre") >= 0) {
             altJavaHome = altJavaHome.substring(0, altJavaHome.lastIndexOf("jre"));
@@ -62,32 +63,40 @@ public class GroovycTest extends GroovyTestCase {
             if (altFile.exists()) {
                 project.setProperty("alt.java.home", altJavaHome);
             }
-        } catch (Exception e) {
-            // could be security, io, etc.  Ignore it.
+        } catch (Exception ignore) {
+            // could be security, io, etc.
             // End result is as if .exists() returned null
         }
     }
 
+    private String getTargetDirectory() {
+        try {
+            return Paths.get(getClass().getResource(".").toURI()).toString() + File.separator;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void ensureNotPresent(final String classname) {
-        if (!(new File(classDirectory + "GroovycTest.class")).exists()) {
+        if (!(new File(getTargetDirectory() + getClass().getSimpleName() + ".class")).exists()) {
             fail("Class file for GroovycTest does not exist and should.");
         }
-        if ((new File(classDirectory + classname + ".class")).exists()) {
+        if ((new File(getTargetDirectory() + classname + ".class")).exists()) {
             fail("Class file for " + classname + " already exists and shouldn't.");
         }
     }
 
     private void ensurePresent(final String classname) {
-        if (!(new File(classDirectory + classname + ".class")).exists()) {
+        if (!(new File(getTargetDirectory() + classname + ".class")).exists()) {
             fail("Class file for " + classname + " does not exist and should.");
         }
     }
 
     private void ensureResultOK(final String classname) {
-        if (!(new File(classDirectory + classname + ".class")).exists()) {
+        if (!(new File(getTargetDirectory() + classname + ".class")).exists()) {
             fail("Class file for " + classname + " does not exist and should.");
         }
-        final File result = new File(classDirectory + classname + "_Result.txt");
+        final File result = new File(getTargetDirectory() + classname + "_Result.txt");
         final char[] buffer = new char[10];
         FileReader fr = null;
         try {
@@ -153,23 +162,23 @@ public class GroovycTest extends GroovyTestCase {
     public void testGroovyc_Joint_NoFork_NestedCompilerArg_WithGroovyClasspath() {
         // capture ant's output so we can verify the effect of passing compilerarg to javac
         ByteArrayOutputStream allOutput = new ByteArrayOutputStream();
-        PrintStream out = new PrintStream(allOutput);
-        PrintStream origOut = System.out;
-        System.setOut(out);
 
-        ensureNotPresent("IncorrectGenericsUsage");
-        project.executeTarget("Groovyc_Joint_NoFork_NestedCompilerArg_WithGroovyClasspath");
-        ensurePresent("IncorrectGenericsUsage");
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(allOutput));
+        try {
+            ensureNotPresent("IncorrectGenericsUsage");
+            project.executeTarget("Groovyc_Joint_NoFork_NestedCompilerArg_WithGroovyClasspath");
+            ensurePresent("IncorrectGenericsUsage");
 
-        String antOutput = allOutput.toString();
-        antOutput = adjustOutputToHandleOpenJDKJavacOutputDifference(antOutput);
-        System.setOut(origOut);
-
-        // verify if passing -Xlint in compilerarg had its effect
-        Pattern p = Pattern.compile(".*?found[ ]*:[ ]*java.util.ArrayList.*", Pattern.DOTALL);
-        assertTrue("Expected line 1 not found in ant output", p.matcher(antOutput).matches());
-        p = Pattern.compile(".*?required[ ]*:[ ]*java.util.ArrayList<java.lang.String>.*", Pattern.DOTALL);
-        assertTrue("Expected line 2 not found in ant output", p.matcher(antOutput).matches());
+            String antOutput = adjustOutputToHandleOpenJDKJavacOutputDifference(allOutput.toString());
+            // verify if passing -Xlint in compilerarg had its effect
+            Pattern p = Pattern.compile(".*?found[ ]*:[ ]*java.util.ArrayList.*", Pattern.DOTALL);
+            assertTrue("Expected line 1 not found in ant output", p.matcher(antOutput).matches());
+            p = Pattern.compile(".*?required[ ]*:[ ]*java.util.ArrayList<java.lang.String>.*", Pattern.DOTALL);
+            assertTrue("Expected line 2 not found in ant output", p.matcher(antOutput).matches());
+        } finally {
+            System.setOut(out);
+        }
     }
 
     /**
@@ -281,5 +290,4 @@ public class GroovycTest extends GroovyTestCase {
             badGroovy.delete();
         }
     }
-
 }
