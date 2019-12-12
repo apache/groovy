635039e03e71b361931e77e7328baff9481d85cd
Daniel.Sun <sunlan@apache.org>
2019-12-13 07:35:32 +0800

    GROOVY-9329: OOME raised while `parseClass(scriptText)` (#1117)

---
+++ src/test/groovy/bugs/Groovy9329.groovy
@@ -0,0 +1,42 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package groovy.bugs
+
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+import static groovy.test.GroovyAssert.assertScript
+
+@CompileStatic
+final class Groovy9329 {
+    @Test
+    void test() {
+        assertScript '''
+            def gcl = new GroovyClassLoader()
+            def scriptText = 'def x = 1'
+            gcl.parseClass(scriptText)
+            
+            def begin = gcl.@classCache.size()
+            3.times { gcl.parseClass(scriptText) }
+            def end = gcl.@classCache.size()
+            
+            assert end == begin
+        '''
+    }
+}
