d3a67f009dc24fc884a410c79fa9819a343141da
Eric Milles <eric.milles@thomsonreuters.com>
2019-12-16 10:55:47 -0600

    minor edits

--- src/main/java/org/codehaus/groovy/control/Phases.java
+++ src/main/java/org/codehaus/groovy/control/Phases.java
@@ -22,37 +22,46 @@ package org.codehaus.groovy.control;
  * Compilation phase identifiers.
  */
 public class Phases {
-    public static final int INITIALIZATION = 1;   // Opening of files and such
-    public static final int PARSING = 2;   // Lexing, parsing, and AST building
-    public static final int CONVERSION = 3;   // CST to AST conversion
-    public static final int SEMANTIC_ANALYSIS = 4;   // AST semantic analysis and elucidation
-    public static final int CANONICALIZATION = 5;   // AST completion
-    public static final int INSTRUCTION_SELECTION = 6;   // Class generation, phase 1
-    public static final int CLASS_GENERATION = 7;   // Class generation, phase 2
-    public static final int OUTPUT = 8;   // Output of class to disk
-    public static final int FINALIZATION = 9;   // Cleanup
-    public static final int ALL = 9;   // Synonym for full compilation
+    /** Opening of files and such */
+    public static final int INITIALIZATION = 1;
+    /** Lexing, parsing, and AST building */
+    public static final int PARSING = 2;
+    /** CST to AST conversion */
+    public static final int CONVERSION = 3;
+    /** AST semantic analysis and elucidation */
+    public static final int SEMANTIC_ANALYSIS = 4;
+    /** AST completion */
+    public static final int CANONICALIZATION = 5;
+    /** Class generation (pt.1) */
+    public static final int INSTRUCTION_SELECTION = 6;
+    /** Class generation (pt.2) */
+    public static final int CLASS_GENERATION = 7;   //
+    /** Output of class to disk */
+    public static final int OUTPUT = 8;
+    /** Cleanup */
+    public static final int FINALIZATION = 9;
 
+    /** Synonym for full compilation */
+    public static final int ALL = FINALIZATION;
+
+    // TODO: If no reference is made to array, convert to switch in getDescription(int).
     public static final String[] descriptions = {
-            "startup"
-            , "initialization"
-            , "parsing"
-            , "conversion"
-            , "semantic analysis"
-            , "canonicalization"
-            , "instruction selection"
-            , "class generation"
-            , "output"
-            , "cleanup"
+        "startup",
+        "initialization",
+        "parsing",
+        "conversion",
+        "semantic analysis",
+        "canonicalization",
+        "instruction selection",
+        "class generation",
+        "output",
+        "cleanup",
     };
-
 
     /**
      * Returns a description of the specified phase.
      */
-
-    public static String getDescription(int phase) {
+    public static String getDescription(final int phase) {
         return descriptions[phase];
     }
-
 }
