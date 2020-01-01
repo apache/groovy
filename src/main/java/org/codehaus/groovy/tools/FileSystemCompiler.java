e9e97c7c87a7e2f6f081ce13f54ee140a8076f36
Paul King <paulk@asert.com.au>
2020-01-01 22:09:30 +1000

    bump copyright year and 'updateNotices'

--- src/main/java/org/codehaus/groovy/tools/FileSystemCompiler.java
+++ src/main/java/org/codehaus/groovy/tools/FileSystemCompiler.java
@@ -324,7 +324,7 @@ public class FileSystemCompiler {
         public String[] getVersion() {
             return new String[]{
                     "Groovy compiler version " + GroovySystem.getVersion(),
-                    "Copyright 2003-2019 The Apache Software Foundation. http://groovy-lang.org/",
+                    "Copyright 2003-2020 The Apache Software Foundation. http://groovy-lang.org/",
                     "",
             };
         }
