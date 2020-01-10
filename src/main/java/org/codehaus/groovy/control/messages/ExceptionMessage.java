137850cbeb0a694fafa42a06637bbd6974bcca8b
Eric Milles <eric.milles@thomsonreuters.com>
2020-01-10 11:01:58 -0600

    save source context in uncaught exception error message

--- src/main/java/org/codehaus/groovy/control/messages/ExceptionMessage.java
+++ src/main/java/org/codehaus/groovy/control/messages/ExceptionMessage.java
@@ -22,36 +22,34 @@ import org.codehaus.groovy.control.Janitor;
 import org.codehaus.groovy.control.ProcessingUnit;
 
 import java.io.PrintWriter;
+import java.util.Objects;
 
 /**
  * A class for error messages produced by the parser system.
  */
 public class ExceptionMessage extends Message {
-    protected boolean verbose = true;
 
-    private Exception cause = null;   // The exception source of the message, if any
-    ProcessingUnit owner = null;
+    private final Exception cause;
+    protected final boolean debug;
+    protected final ProcessingUnit owner;
 
-    public ExceptionMessage(Exception cause, boolean v, ProcessingUnit owner) {
-        this.verbose = v;
-        this.cause = cause;
+    public ExceptionMessage(final Exception cause, final boolean debug, final ProcessingUnit owner) {
+        this.cause = Objects.requireNonNull(cause);
+        this.debug = debug;
         this.owner = owner;
     }
 
     /**
      * Returns the underlying Exception.
      */
-
     public Exception getCause() {
-        return this.cause;
+        return cause;
     }
-
 
     /**
      * Writes out a nicely formatted summary of the exception.
      */
-
-    public void write(PrintWriter output, Janitor janitor) {
+    public void write(final PrintWriter output, final Janitor janitor) {
         String description = "General error during " + owner.getPhaseDescription() + ": ";
 
         String message = cause.getMessage();
@@ -62,12 +60,6 @@ public class ExceptionMessage extends Message {
         }
         output.println();
 
-        //if (verbose) {
-        cause.printStackTrace(output);
-        //}
+        /*if (debug)*/ cause.printStackTrace(output);
     }
-
 }
-
-
-
