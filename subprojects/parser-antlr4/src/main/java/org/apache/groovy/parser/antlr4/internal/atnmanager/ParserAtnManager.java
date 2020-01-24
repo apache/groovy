d1424a8d4f5ea65b474ed9318aeedd44b2e52189
Sam Harwell <sam@tunnelvisionlabs.com>
2020-01-25 02:09:07 +0800

    Tweak `nls` rule
    nls has a "Rule" where the content is a "Greedy Closure"
    
    We need to set the sll field of the StarLoopEntryState (which is derived from DecisionState where the field is defined) to true
    
    I don't think we can do this in the grammar. However, since the nls rule only has one StarLoopEntryState, we should be able to loop over all the states in the entire ATN to find one of type StarLoopEntryState where the containing rule is RULE_nls, and set the field directly in code.

--- subprojects/parser-antlr4/src/main/java/org/apache/groovy/parser/antlr4/internal/atnmanager/ParserAtnManager.java
+++ subprojects/parser-antlr4/src/main/java/org/apache/groovy/parser/antlr4/internal/atnmanager/ParserAtnManager.java
@@ -19,7 +19,10 @@
 package org.apache.groovy.parser.antlr4.internal.atnmanager;
 
 import org.antlr.v4.runtime.atn.ATN;
+import org.antlr.v4.runtime.atn.StarLoopEntryState;
 import org.apache.groovy.parser.antlr4.GroovyLangParser;
+
+import static org.apache.groovy.parser.antlr4.GroovyParser.RULE_nls;
 
 /**
  * Manage ATN for parser to avoid memory leak
@@ -27,6 +30,13 @@ import org.apache.groovy.parser.antlr4.GroovyLangParser;
 public class ParserAtnManager extends AtnManager {
     private final AtnWrapper parserAtnWrapper = new AtnManager.AtnWrapper(GroovyLangParser._ATN);
     public static final ParserAtnManager INSTANCE = new ParserAtnManager();
+
+    static {
+        GroovyLangParser._ATN.states.stream()
+                .filter(e -> (e instanceof StarLoopEntryState) && (RULE_nls == ((StarLoopEntryState) e).ruleIndex))
+                .forEach(e -> ((StarLoopEntryState) e).sll = true);
+
+    }
 
     @Override
     public ATN getATN() {
