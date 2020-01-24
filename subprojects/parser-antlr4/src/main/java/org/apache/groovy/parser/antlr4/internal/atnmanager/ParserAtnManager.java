56e9ff96b9a15cb2512fbe2a5434181ae654d0f4
Daniel Sun <sunlan@apache.org>
2020-01-25 02:38:57 +0800

    Revert "Tweak `nls` rule" for now
    This reverts commit d1424a8d

--- subprojects/parser-antlr4/src/main/java/org/apache/groovy/parser/antlr4/internal/atnmanager/ParserAtnManager.java
+++ subprojects/parser-antlr4/src/main/java/org/apache/groovy/parser/antlr4/internal/atnmanager/ParserAtnManager.java
@@ -19,10 +19,7 @@
 package org.apache.groovy.parser.antlr4.internal.atnmanager;
 
 import org.antlr.v4.runtime.atn.ATN;
-import org.antlr.v4.runtime.atn.StarLoopEntryState;
 import org.apache.groovy.parser.antlr4.GroovyLangParser;
-
-import static org.apache.groovy.parser.antlr4.GroovyParser.RULE_nls;
 
 /**
  * Manage ATN for parser to avoid memory leak
@@ -30,13 +27,6 @@ import static org.apache.groovy.parser.antlr4.GroovyParser.RULE_nls;
 public class ParserAtnManager extends AtnManager {
     private final AtnWrapper parserAtnWrapper = new AtnManager.AtnWrapper(GroovyLangParser._ATN);
     public static final ParserAtnManager INSTANCE = new ParserAtnManager();
-
-    static {
-        GroovyLangParser._ATN.states.stream()
-                .filter(e -> (e instanceof StarLoopEntryState) && (RULE_nls == ((StarLoopEntryState) e).ruleIndex))
-                .forEach(e -> ((StarLoopEntryState) e).sll = true);
-
-    }
 
     @Override
     public ATN getATN() {
