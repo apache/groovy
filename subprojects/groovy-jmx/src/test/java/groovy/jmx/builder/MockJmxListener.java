3150544e10b919781991272f52d6be84988021f8
Paul King <paulk@asert.com.au>
2020-01-25 14:40:31 +1000

    remove legacy comment

--- subprojects/groovy-jmx/src/test/java/groovy/jmx/builder/MockJmxListener.java
+++ subprojects/groovy-jmx/src/test/java/groovy/jmx/builder/MockJmxListener.java
@@ -21,13 +21,6 @@ package groovy.jmx.builder;
 import javax.management.Notification;
 import javax.management.NotificationListener;
 
-/**
- * Created by IntelliJ IDEA.
- * User: paulk
- * Date: 18/07/2010
- * Time: 12:16:46 PM
- * To change this template use File | Settings | File Templates.
- */
 public class MockJmxListener implements MockJmxListenerMBean, NotificationListener {
 
     public void handleNotification(Notification note, Object handback) {
