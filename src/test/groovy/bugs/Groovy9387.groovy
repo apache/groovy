3d8c17dd1b8269f4151fb4785759b60acaf7952c
Eric Milles <eric.milles@thomsonreuters.com>
2020-02-08 12:57:54 -0600

    GROOVY-9387: MissingMethodException: try one more metaClass.invokeMethod (closes #1162)

--- src/test/groovy/bugs/Groovy9387.groovy
+++ src/test/groovy/bugs/Groovy9387.groovy
@@ -18,7 +18,6 @@
  */
 package groovy.bugs
 
-import groovy.test.NotYetImplemented
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.junit.Test
@@ -60,10 +59,8 @@ final class Groovy9387 {
         '''
     }
 
-    @Test @NotYetImplemented
+    @Test
     void testThisSetProperty() {
-        // currently only broken for classic bytecode (TODO: remove assumeFalse line and this comment once issue is fixed - no harm in having an indy testcase at that point)
-        assumeFalse CompilerConfiguration.DEFAULT.optimizationOptions?.indy
         assertScript SUPPORT_ADAPTER + '''
             class C extends BuilderSupportAdapter {
                 String value = 'abc'
