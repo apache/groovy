75409883243c5ccec63595e2904c07b3ac0a73a4
Paul King <paulk@asert.com.au>
2020-02-17 23:34:41 +1000

    GROOVY-9185: groovyc doesn't keep RetentionPolicy in generated bytecode (port to 2_4_X)

--- src/main/org/codehaus/groovy/vmplugin/v5/Java5.java
+++ src/main/org/codehaus/groovy/vmplugin/v5/Java5.java
@@ -243,7 +243,9 @@ public class Java5 implements VMPlugin {
         for (AnnotationNode an : annotations) {
             configureAnnotationFromDefinition(an, node);
         }
-        configureAnnotationFromDefinition(node, node);
+        if (!node.getClassNode().getName().equals("java.lang.annotation.Retention")) {
+            configureAnnotationFromDefinition(node, node);
+        }
     }
 
     private void configureAnnotation(AnnotationNode node, Annotation annotation) {
