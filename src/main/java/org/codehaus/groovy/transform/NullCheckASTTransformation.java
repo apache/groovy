56bc3f1f649ddf5c340d342b30291e04966ed573
Paul King <paulk@asert.com.au>
2020-02-19 14:24:57 +1000

    GROOVY-9406: `VerifyError` when annotating with both @InheritConstructors and @NullCheck and calling the resulting c'tor

--- src/main/java/org/codehaus/groovy/transform/NullCheckASTTransformation.java
+++ src/main/java/org/codehaus/groovy/transform/NullCheckASTTransformation.java
@@ -19,6 +19,7 @@
 package org.codehaus.groovy.transform;
 
 import groovy.transform.NullCheck;
+import org.apache.groovy.ast.tools.ConstructorNodeUtils;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.AnnotatedNode;
 import org.codehaus.groovy.ast.AnnotationNode;
@@ -27,11 +28,14 @@ import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.ConstructorNode;
 import org.codehaus.groovy.ast.MethodNode;
 import org.codehaus.groovy.ast.Parameter;
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression;
 import org.codehaus.groovy.ast.stmt.BlockStatement;
-import org.codehaus.groovy.ast.stmt.Statement;
+import org.codehaus.groovy.classgen.BytecodeSequence;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
 
+import static org.apache.groovy.ast.tools.AnnotatedNodeUtils.isGenerated;
+import static org.apache.groovy.ast.tools.MethodNodeUtils.getCodeAsBlock;
 import static org.codehaus.groovy.ast.ClassHelper.make;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.constX;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.ctorX;
@@ -41,11 +45,11 @@ import static org.codehaus.groovy.ast.tools.GeneralUtils.throwS;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.varX;
 
 /**
- * Handles generation of code for the @AutoImplement annotation.
+ * Handles generation of code for the @NullCheck annotation.
  */
 @GroovyASTTransformation(phase = CompilePhase.CANONICALIZATION)
 public class NullCheckASTTransformation extends AbstractASTTransformation {
-    private static final Class MY_CLASS = NullCheck.class;
+    private static final Class<NullCheck> MY_CLASS = NullCheck.class;
     private static final ClassNode MY_TYPE = make(MY_CLASS);
     private static final String MY_TYPE_NAME = "@" + MY_TYPE.getNameWithoutPackage();
     private static final ClassNode EXCEPTION = ClassHelper.make(IllegalArgumentException.class);
@@ -56,36 +60,45 @@ public class NullCheckASTTransformation extends AbstractASTTransformation {
         AnnotatedNode parent = (AnnotatedNode) nodes[1];
         AnnotationNode anno = (AnnotationNode) nodes[0];
         if (!MY_TYPE.equals(anno.getClassNode())) return;
+        boolean includeGenerated = memberHasValue(anno, "includeGenerated", true);
 
         if (parent instanceof ClassNode) {
             ClassNode cNode = (ClassNode) parent;
             if (!checkNotInterface(cNode, MY_TYPE_NAME)) return;
             for (ConstructorNode cn : cNode.getDeclaredConstructors()) {
-                adjustMethod(cn);
+                adjustMethod(cn, includeGenerated);
             }
             for (MethodNode mn : cNode.getAllDeclaredMethods()) {
-                adjustMethod(mn);
+                adjustMethod(mn, includeGenerated);
             }
         } else if (parent instanceof MethodNode) {
             // handles constructor case too
-            adjustMethod((MethodNode) parent);
+            adjustMethod((MethodNode) parent, includeGenerated);
         }
     }
 
-    private void adjustMethod(MethodNode mn) {
-        Statement origCode = mn.getCode();
-        BlockStatement newCode = new BlockStatement();
+    private void adjustMethod(MethodNode mn, boolean includeGenerated) {
+        BlockStatement newCode = getCodeAsBlock(mn);
         if (mn.getParameters().length == 0) return;
-        for (Parameter p : mn.getParameters()) {
-            newCode.addStatement(ifS(isNullX(varX(p)),
-                    throwS(ctorX(EXCEPTION, constX(p.getName() + " cannot be null")))));
-        }
-        if (origCode instanceof BlockStatement) {
-            for (Statement s : ((BlockStatement) origCode).getStatements()) {
-                newCode.addStatement(s);
+        boolean generated = isGenerated(mn);
+        int startingIndex = 0;
+        if (!includeGenerated && generated) return;
+        if (mn instanceof ConstructorNode) {
+            // some transform has been here already and we assume it knows what it is doing
+            if (mn.getFirstStatement() instanceof BytecodeSequence) return;
+            // ignore any constructors calling this(...) or super(...)
+            ConstructorCallExpression cce = ConstructorNodeUtils.getFirstIfSpecialConstructorCall(mn.getCode());
+            if (cce != null) {
+                if (generated) {
+                    return;
+                } else {
+                    startingIndex = 1; // skip over this/super() call
+                }
             }
-        } else {
-            newCode.addStatement(origCode);
+        }
+        for (Parameter p : mn.getParameters()) {
+            newCode.getStatements().add(startingIndex, ifS(isNullX(varX(p)),
+                    throwS(ctorX(EXCEPTION, constX(p.getName() + " cannot be null")))));
         }
         mn.setCode(newCode);
     }
