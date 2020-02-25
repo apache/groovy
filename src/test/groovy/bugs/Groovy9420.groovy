aa7ea88fdad19eb0ed95b10bae584582e5ad8665
Paul King <paulk@asert.com.au>
2020-02-25 23:20:27 +1000

    GROOVY-9420: Incorrect type inference on map[key]

---
+++ src/test/groovy/bugs/Groovy9420.groovy
@@ -0,0 +1,40 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package groovy.bugs
+
+import gls.CompilableTestSupport
+
+class Groovy9420 extends CompilableTestSupport {
+    void testStaticBooleanIsPropertyInStaticContext() {
+        shouldCompile """
+            @groovy.transform.CompileStatic
+            void blah() {
+                Map<String, String> m = [foo: 'foo']
+                def a = m.get(key)
+                uhuh(a)
+                def b = m[key]
+                uhuh(b)
+            }
+
+            void uhuh(String a) { }
+
+            Object getKey() { 'foo' }
+        """
+    }
+}
