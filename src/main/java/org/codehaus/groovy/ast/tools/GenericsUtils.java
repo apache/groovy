b596499869d2b6502c577fa5f146cbd164bf6387
Paul King <paulk@asert.com.au>
2020-03-01 21:27:50 +1000

    keep old methods as convenience methods

--- src/main/java/org/codehaus/groovy/ast/tools/GenericsUtils.java
+++ src/main/java/org/codehaus/groovy/ast/tools/GenericsUtils.java
@@ -678,7 +678,10 @@ public class GenericsUtils {
 
     private static final boolean PARAMETERIZED_TYPE_CACHE_ENABLED = Boolean.parseBoolean(getSystemPropertySafe("groovy.enable.parameterized.type.cache", "true"));
 
-    @Deprecated
+    /**
+     * Convenience method for {@link #findParameterizedTypeFromCache(ClassNode, ClassNode, boolean)}
+     * when the {@code tryToFindExactType} boolean is {@code false}.
+     */
     public static ClassNode findParameterizedTypeFromCache(final ClassNode genericsClass, final ClassNode actualType) {
         return findParameterizedTypeFromCache(genericsClass, actualType, false);
     }
@@ -697,13 +700,16 @@ public class GenericsUtils {
         return null == sr ? null : sr.get();
     }
 
-    @Deprecated
+    /**
+     * Convenience method for {@link #findParameterizedType(ClassNode, ClassNode, boolean)}
+     * when the {@code tryToFindExactType} boolean is {@code false}.
+     */
     public static ClassNode findParameterizedType(final ClassNode genericsClass, final ClassNode actualType) {
         return findParameterizedType(genericsClass, actualType, false);
     }
 
     /**
-     * Get the parameterized type by search the whole class hierarchy according to generics class and actual receiver.
+     * Get the parameterized type by searching the whole class hierarchy according to generics class and actual receiver.
      * {@link #findParameterizedTypeFromCache(ClassNode, ClassNode, boolean)} is strongly recommended for better performance.
      *
      * @param genericsClass the generics class
