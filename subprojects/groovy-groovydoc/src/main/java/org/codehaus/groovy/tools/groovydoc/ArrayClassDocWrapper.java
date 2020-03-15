047cd800d255bb4674f466bf683d7b3961fa779c
Paul King <paulk@asert.com.au>
2020-03-15 15:39:24 +1000

    remove spotbugs warnings

--- subprojects/groovy-groovydoc/src/main/java/org/codehaus/groovy/tools/groovydoc/ArrayClassDocWrapper.java
+++ subprojects/groovy-groovydoc/src/main/java/org/codehaus/groovy/tools/groovydoc/ArrayClassDocWrapper.java
@@ -294,4 +294,10 @@ public class ArrayClassDocWrapper implements GroovyClassDoc {
     public boolean equals(Object obj) {
         return obj instanceof ArrayClassDocWrapper ? delegate.equals(((ArrayClassDocWrapper) obj).delegate) : delegate.equals(obj);
     }
+
+    @Override
+    public int hashCode() {
+        assert false : "hashCode not designed";
+        return 42; // any arbitrary constant will do
+    }
 }
