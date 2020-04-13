32a306d1ea035ed94fd9836fa220982c1c01a87f
Kamil Jedrzejuk <kamil.jedrzejuk@gmail.com>
2020-04-13 15:58:18 +0200

    GROOVY-9505: Bump ASM_API_VERSION to Opcodes.ASM8 (closes #1222)

--- src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
+++ src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
@@ -122,7 +122,10 @@ public class CompilerConfiguration {
      */
     public static final String[] ALLOWED_JDKS = JDK_TO_BYTECODE_VERSION_MAP.keySet().toArray(new String[JDK_TO_BYTECODE_VERSION_MAP.size()]);
 
-    public static final int ASM_API_VERSION = Opcodes.ASM7;
+    /**
+    * The valid instruction sets.
+    */
+    public static final int ASM_API_VERSION = Opcodes.ASM8;
 
     /**
      * The default source encoding.
