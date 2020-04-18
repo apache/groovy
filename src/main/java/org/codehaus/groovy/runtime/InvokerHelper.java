f3028b394fc9fc35fe239a0bfcdc59cf23f800fc
Daniel Sun <sunlan@apache.org>
2020-04-18 22:44:22 +0800

    Trivial refactoring: make constants final

--- src/main/java/org/codehaus/groovy/runtime/InvokerHelper.java
+++ src/main/java/org/codehaus/groovy/runtime/InvokerHelper.java
@@ -801,8 +801,8 @@ public class InvokerHelper {
         return argBuf.toString();
     }
 
-    private static Set<String> DEFAULT_IMPORT_PKGS = new HashSet<String>();
-    private static Set<String> DEFAULT_IMPORT_CLASSES = new HashSet<String>();
+    private static final Set<String> DEFAULT_IMPORT_PKGS = new HashSet<String>();
+    private static final Set<String> DEFAULT_IMPORT_CLASSES = new HashSet<String>();
     static {
         for (String pkgName : ResolveVisitor.DEFAULT_IMPORTS) {
             DEFAULT_IMPORT_PKGS.add(pkgName.substring(0, pkgName.length() - 1));
