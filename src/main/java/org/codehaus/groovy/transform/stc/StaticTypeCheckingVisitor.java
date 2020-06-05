60aef38f1c738c3562982fad4c3264dd56cc5903
Eric Milles <eric.milles@thomsonreuters.com>
2020-06-05 17:11:54 -0500

    GROOVY-7971: do not save instanceof types under logical or

--- src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -219,6 +219,7 @@ import static org.codehaus.groovy.syntax.Types.INTDIV_EQUAL;
 import static org.codehaus.groovy.syntax.Types.KEYWORD_IN;
 import static org.codehaus.groovy.syntax.Types.KEYWORD_INSTANCEOF;
 import static org.codehaus.groovy.syntax.Types.LEFT_SQUARE_BRACKET;
+import static org.codehaus.groovy.syntax.Types.LOGICAL_OR;
 import static org.codehaus.groovy.syntax.Types.MINUS_MINUS;
 import static org.codehaus.groovy.syntax.Types.MOD;
 import static org.codehaus.groovy.syntax.Types.MOD_EQUAL;
@@ -727,6 +728,9 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
         typeCheckingContext.pushEnclosingBinaryExpression(expression);
         try {
             int op = expression.getOperation().getType();
+            if (op == LOGICAL_OR) {
+                typeCheckingContext.pushTemporaryTypeInfo();
+            }
             Expression leftExpression = expression.getLeftExpression();
             Expression rightExpression = expression.getRightExpression();
 
@@ -735,6 +739,7 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
             ClassNode lType = null;
             if (setterInfo != null) {
                 if (ensureValidSetter(expression, leftExpression, rightExpression, setterInfo)) {
+                    if (op == LOGICAL_OR) typeCheckingContext.popTemporaryTypeInfo();
                     return;
                 }
             } else {
@@ -780,6 +785,8 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
                 elvisOperatorExpression.visit(this);
                 resultType = getType(elvisOperatorExpression);
                 storeType(leftExpression, resultType);
+            } else if (op == LOGICAL_OR) {
+                typeCheckingContext.popTemporaryTypeInfo();
             }
 
             if (resultType == null) {
