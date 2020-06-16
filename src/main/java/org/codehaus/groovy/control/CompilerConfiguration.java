d8d04e693b068b1b2a255c25e14b094c2ad60c57
Paul King <paulk@asert.com.au>
2020-06-16 17:52:32 +1000

    minor refactor: rename local var

--- src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
+++ src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
@@ -687,8 +687,8 @@ public class CompilerConfiguration {
         text = configuration.getProperty("groovy.disabled.global.ast.transformations");
         if (text != null) {
             String[] classNames = text.split(",\\s*}");
-            Set<String> blacklist = new HashSet<>(Arrays.asList(classNames));
-            setDisabledGlobalASTTransformations(blacklist);
+            Set<String> disabledTransforms = new HashSet<>(Arrays.asList(classNames));
+            setDisabledGlobalASTTransformations(disabledTransforms);
         }
     }
 
