74c37bdd7979c4461862e14c2016e74679dbd3d2
Eric Milles <eric.milles@thomsonreuters.com>
2020-08-25 12:14:50 -0500

    GROOVY-9702: source retention for @ASTTest and improved AST modification (closes #1352)

--- src/main/java/groovy/transform/ASTTest.java
+++ src/main/java/groovy/transform/ASTTest.java
@@ -18,6 +18,7 @@
  */
 package groovy.transform;
 
+import groovy.lang.Closure;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.transform.GroovyASTTransformationClass;
 
@@ -52,7 +53,7 @@ import java.lang.annotation.Target;
  * @since 2.0.0
  */
 @Documented
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.PACKAGE, ElementType.TYPE, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 @GroovyASTTransformationClass("org.codehaus.groovy.transform.ASTTestTransformation")
 public @interface ASTTest {
@@ -64,5 +65,5 @@ public @interface ASTTest {
     /**
      * A closure which is executed against the annotated node after the specified phase has completed.
      */
-    Class value();
+    Class<? extends /*@ClosureParams(value=FromString.class,options="")*/ Closure<?>> value();
 }
