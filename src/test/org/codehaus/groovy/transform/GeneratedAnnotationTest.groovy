4e82124a2ae8fc53eba11e057285ff440303c581
Paul King <paulk@asert.com.au>
2020-09-12 15:56:13 +1000

    remove superfluous trailing whitespace

--- src/test/org/codehaus/groovy/transform/GeneratedAnnotationTest.groovy
+++ src/test/org/codehaus/groovy/transform/GeneratedAnnotationTest.groovy
@@ -85,7 +85,7 @@ class GeneratedAnnotationTest extends GroovyShellTestCase {
         def person = evaluate('''
             class Person {
                 String name
-                
+
                 def invokeMethod(String name, args) { }
             }
             new Person()
