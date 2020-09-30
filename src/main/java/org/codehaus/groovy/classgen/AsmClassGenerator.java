71ae8e460998c252cde06bfc8d0e059a2f9b7ff9
Daniel Sun <sunlan@apache.org>
2020-09-30 18:31:58 +0800

    Tweak GROOVY-9126 further: Eliminate more unreachable bytecode

--- src/main/java/org/codehaus/groovy/classgen/AsmClassGenerator.java
+++ src/main/java/org/codehaus/groovy/classgen/AsmClassGenerator.java
@@ -462,7 +462,7 @@ public class AsmClassGenerator extends ClassGenerator {
 
         controller.getCompileStack().clear();
 
-        if (checkIfLastStatementIsReturn(code)) return;
+        if (checkIfLastStatementIsReturnOrThrow(code)) return;
 
         if (node.isVoidMethod()) {
             mv.visitInsn(RETURN);
@@ -481,18 +481,19 @@ public class AsmClassGenerator extends ClassGenerator {
         }
     }
 
-    private boolean checkIfLastStatementIsReturn(Statement code) {
+    private boolean checkIfLastStatementIsReturnOrThrow(Statement code) {
         if (code instanceof BlockStatement) {
             BlockStatement blockStatement = (BlockStatement) code;
             List<Statement> statementList = blockStatement.getStatements();
             int statementCnt = statementList.size();
             if (statementCnt > 0) {
                 Statement lastStatement = statementList.get(statementCnt - 1);
-                if (lastStatement instanceof ReturnStatement) {
+                if (lastStatement instanceof ReturnStatement || lastStatement instanceof ThrowStatement) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 
