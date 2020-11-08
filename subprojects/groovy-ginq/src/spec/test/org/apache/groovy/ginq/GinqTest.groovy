d1a377eb1e8679bacf98c6c9d1a1ada20b9502bb
Daniel Sun <sunlan@apache.org>
2020-11-08 14:07:58 +0800

    tweak doco

--- subprojects/groovy-ginq/src/spec/test/org/apache/groovy/ginq/GinqTest.groovy
+++ subprojects/groovy-ginq/src/spec/test/org/apache/groovy/ginq/GinqTest.groovy
@@ -2290,6 +2290,41 @@ class GinqTest {
     }
 
     @Test
+    void "testGinq - GINQ examples - 1"() {
+        assertScript '''
+            def expected = 
+                [['1 * 1 = 1', '', '', '', '', '', '', '', ''],
+                 ['1 * 2 = 2', '2 * 2 = 4', '', '', '', '', '', '', ''],
+                 ['1 * 3 = 3', '2 * 3 = 6', '3 * 3 = 9', '', '', '', '', '', ''],
+                 ['1 * 4 = 4', '2 * 4 = 8', '3 * 4 = 12', '4 * 4 = 16', '', '', '', '', ''],
+                 ['1 * 5 = 5', '2 * 5 = 10', '3 * 5 = 15', '4 * 5 = 20', '5 * 5 = 25', '', '', '', ''],
+                 ['1 * 6 = 6', '2 * 6 = 12', '3 * 6 = 18', '4 * 6 = 24', '5 * 6 = 30', '6 * 6 = 36', '', '', ''],
+                 ['1 * 7 = 7', '2 * 7 = 14', '3 * 7 = 21', '4 * 7 = 28', '5 * 7 = 35', '6 * 7 = 42', '7 * 7 = 49', '', ''],
+                 ['1 * 8 = 8', '2 * 8 = 16', '3 * 8 = 24', '4 * 8 = 32', '5 * 8 = 40', '6 * 8 = 48', '7 * 8 = 56', '8 * 8 = 64', ''],
+                 ['1 * 9 = 9', '2 * 9 = 18', '3 * 9 = 27', '4 * 9 = 36', '5 * 9 = 45', '6 * 9 = 54', '7 * 9 = 63', '8 * 9 = 72', '9 * 9 = 81']]
+            assert expected == GQ {
+// tag::ginq_examples_01[]
+                from v in (
+                    from a in 1..9
+                    innerjoin b in 1..9 on a <= b
+                    select a as f, b as s, "$a * $b = ${a * b}".toString() as r
+                )
+                groupby v.s
+                select max(v.f == 1 ? v.r : '') as v1,
+                       max(v.f == 2 ? v.r : '') as v2,
+                       max(v.f == 3 ? v.r : '') as v3,
+                       max(v.f == 4 ? v.r : '') as v4,
+                       max(v.f == 5 ? v.r : '') as v5,
+                       max(v.f == 6 ? v.r : '') as v6,
+                       max(v.f == 7 ? v.r : '') as v7,
+                       max(v.f == 8 ? v.r : '') as v8,
+                       max(v.f == 9 ? v.r : '') as v9
+// end::ginq_examples_01[]
+            }.toList()
+        '''
+    }
+
+    @Test
     void "testGinq - GINQ equals - 2"() {
         assertScript '''
             def q1 = GQ {from n in [1, 2, 3] select n}
