97d9c23b8fed4b15651b369e2f2c5f5316345bad
Eric Milles <eric.milles@thomsonreuters.com>
2020-12-07 11:53:43 -0600

    update object expression type

--- src/main/java/org/codehaus/groovy/transform/sc/StaticCompilationVisitor.java
+++ src/main/java/org/codehaus/groovy/transform/sc/StaticCompilationVisitor.java
@@ -546,8 +546,9 @@ public class StaticCompilationVisitor extends StaticTypeCheckingVisitor {
 
         boolean exists = super.existsProperty(pexp, checkForReadOnly, receiverMemoizer);
         if (exists) {
+            objectExpressionType = rType.get();
             if (objectExpression.getNodeMetaData(PROPERTY_OWNER) == null) {
-                objectExpression.putNodeMetaData(PROPERTY_OWNER, rType.get());
+                objectExpression.putNodeMetaData(PROPERTY_OWNER, objectExpressionType);
             }
             if (StaticTypeCheckingSupport.implementsInterfaceOrIsSubclassOf(objectExpressionType, LIST_TYPE)) {
                 objectExpression.putNodeMetaData(COMPONENT_TYPE, inferComponentType(objectExpressionType, int_TYPE));
