0d54bbc5c7f344fb1cef02a10dd848914169970a
Eric Milles <eric.milles@thomsonreuters.com>
2021-01-20 20:46:54 -0600

    GROOVY-9902: STC: allow method checking with partial generic information
    Conflicts:
    	src/main/java/org/codehaus/groovy/ast/GenericsType.java
    	src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingSupport.java

--- src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingSupport.java
+++ src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingSupport.java
@@ -1449,19 +1449,16 @@ public abstract class StaticTypeCheckingSupport {
     }
 
     protected static boolean typeCheckMethodsWithGenerics(ClassNode receiver, ClassNode[] arguments, MethodNode candidateMethod) {
-        if (isUsingUncheckedGenerics(receiver)) {
-            return true;
-        }
-        if (CLASS_Type.equals(receiver)
+        boolean isExtensionMethod = candidateMethod instanceof ExtensionMethodNode;
+        if (!isExtensionMethod
                 && receiver.isUsingGenerics()
-                && !candidateMethod.getDeclaringClass().equals(receiver)
-                && !(candidateMethod instanceof ExtensionMethodNode)) {
+                && receiver.equals(CLASS_Type)
+                && !candidateMethod.getDeclaringClass().equals(CLASS_Type)) {
             return typeCheckMethodsWithGenerics(receiver.getGenericsTypes()[0].getType(), arguments, candidateMethod);
         }
         // both candidate method and receiver have generic information so a check is possible
         GenericsType[] genericsTypes = candidateMethod.getGenericsTypes();
         boolean methodUsesGenerics = (genericsTypes != null && genericsTypes.length > 0);
-        boolean isExtensionMethod = candidateMethod instanceof ExtensionMethodNode;
         if (isExtensionMethod && methodUsesGenerics) {
             ClassNode[] dgmArgs = new ClassNode[arguments.length + 1];
             dgmArgs[0] = receiver;
