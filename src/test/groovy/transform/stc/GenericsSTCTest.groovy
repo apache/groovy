60d966a1aa22b5d20982bca95d8f090e6cadb074
Eric Milles <eric.milles@thomsonreuters.com>
2021-01-27 08:51:39 -0600

    GROOVY-9914: DGM: add return type parameters for collectEntries variant
    Conflicts:
    	src/test/groovy/transform/stc/GenericsSTCTest.groovy

--- src/test/groovy/transform/stc/GenericsSTCTest.groovy
+++ src/test/groovy/transform/stc/GenericsSTCTest.groovy
@@ -242,6 +242,18 @@ class GenericsSTCTest extends StaticTypeCheckingTestCase {
         ''', 'Number'
     }
 
+    // GROOVY-9914
+    void testAssignmentShouldWorkForParameterizedMap() {
+        assertScript '''
+            Map test(Map<String,String> one) {
+              Map<String,Integer> two = one.collectEntries { k,v ->
+                [(k): v.hashCode()]
+              }
+            }
+            assert test(foo:'bar').containsKey('foo')
+        '''
+    }
+
     void testGroovy5154() {
         assertScript '''
             class Foo {
