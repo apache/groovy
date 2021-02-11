c01eef48e299c56e1c9315d0fe3a832f0e595c2f
Paul King <paulk@asert.com.au>
2021-02-11 17:38:52 +1000

    GROOVY-9936: Difference between old/new parser for variable declaration optimisation

--- src/test/org/apache/groovy/parser/antlr4/GroovyParserTest.groovy
+++ src/test/org/apache/groovy/parser/antlr4/GroovyParserTest.groovy
@@ -368,6 +368,7 @@ final class GroovyParserTest extends GroovyTestCase {
         doTest('core/Command_04.groovy', [ExpressionStatement])
         doTest('core/Command_05.groovy')
         doRunAndTestAntlr4('core/Command_06x.groovy')
+        doRunAndTestAntlr4('core/Command_07x.groovy')
     }
 
     /*
