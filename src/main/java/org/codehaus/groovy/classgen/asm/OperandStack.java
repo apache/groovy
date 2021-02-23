ea96d8862770d73ababfc2cc36cc7045edf942a5
Eerik Voimanen <eerik.voimanen@tuni.fi>
2021-02-23 17:28:43 +0200

    GROOVY-9797: Take also negative zero doubles into account by using Double.equals
    Doubles are also IEEE-754 floating point numbers.

--- src/main/java/org/codehaus/groovy/classgen/asm/OperandStack.java
+++ src/main/java/org/codehaus/groovy/classgen/asm/OperandStack.java
@@ -551,7 +551,8 @@ public class OperandStack {
                 mv.visitLdcInsn(value);
             }
         } else if (ClassHelper.double_TYPE.equals(type)) {
-            if ((Double)value==0d) {
+            // GROOVY-9797: Use Double.equals to differentiate between positive and negative zero
+            if (value.equals(0d)) {
                 mv.visitInsn(DCONST_0);
             } else if ((Double)value==1d) {
                 mv.visitInsn(DCONST_1);
