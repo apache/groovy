19224a8ec3d07e319dad8b6bbe49707782f80eaa
Keegan Witt <keeganwitt@gmail.com>
2021-03-09 22:34:39 -0500

    fix GROOVY-9962 Don't replace dollars in strings
    (cherry picked from commit 4e3b3657d5c887437f2f658b71f9f60734c906a4)

--- src/main/java/org/codehaus/groovy/tools/javac/JavaStubGenerator.java
+++ src/main/java/org/codehaus/groovy/tools/javac/JavaStubGenerator.java
@@ -928,13 +928,14 @@ public class JavaStubGenerator {
             String key = entry.getKey();
             if (first) first = false;
             else out.print(", ");
-            out.print(key + "=" + getAnnotationValue(entry.getValue()).replace('$', '.'));
+            out.print(key + "=" + getAnnotationValue(entry.getValue()));
         }
         out.print(") ");
     }
 
     private String getAnnotationValue(Object memberValue) {
         String val = "null";
+        boolean replaceDollars = true;
         if (memberValue instanceof ListExpression) {
             StringBuilder sb = new StringBuilder("{");
             boolean first = true;
@@ -954,10 +955,12 @@ public class JavaStubGenerator {
                 PrintWriter out = new PrintWriter(writer);
                 printAnnotation(out, (AnnotationNode) constValue);
                 val = writer.toString();
-            } else if (constValue instanceof Number || constValue instanceof Boolean)
+            } else if (constValue instanceof Number || constValue instanceof Boolean) {
                 val = constValue.toString();
-            else
+            } else {
                 val = "\"" + escapeSpecialChars(constValue.toString()) + "\"";
+                replaceDollars = false;
+            }
         } else if (memberValue instanceof PropertyExpression) {
             // assume must be static class field or enum value or class that Java can resolve
             val = ((Expression) memberValue).getText();
@@ -974,7 +977,7 @@ public class JavaStubGenerator {
         } else if (memberValue instanceof ClassExpression) {
             val = ((Expression) memberValue).getText() + ".class";
         }
-        return val;
+        return replaceDollars ? val.replace('$', '.') : val;
     }
 
     private static void printModifiers(PrintWriter out, int modifiers) {
---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/Groovy9962.groovy
@@ -0,0 +1,43 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.codehaus.groovy.tools.stubgenerator
+
+/**
+ * Test that annotations with string values containing dollar signs don't change string value in stub.
+ */
+class Groovy9962 extends StringSourcesStubTestCase {
+
+    Map<String, String> provideSources() {
+        [
+            'Foo.java': '''
+                public class Foo {}
+            ''',
+            'Groovy9962.groovy': '''
+                @Deprecated(since = '${name} paid $7')
+                class Groovy9962 {}
+            '''
+        ]
+    }
+
+    void verifyStubs() {
+        def stubSource = stubJavaSourceFor('Groovy9962')
+        assert stubSource.contains('${name} paid $7')
+    }
+}
