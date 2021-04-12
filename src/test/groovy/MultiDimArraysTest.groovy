93639802e3ea118a1129207b5b28acef70e90e01
Eric Milles <eric.milles@thomsonreuters.com>
2021-04-11 20:57:53 -0500

    GROOVY-10031: write NEWARRAY/ANEWARRAY for all single-dimensional arrays

--- src/test/groovy/MultiDimArraysTest.groovy
+++ src/test/groovy/MultiDimArraysTest.groovy
@@ -26,58 +26,73 @@ import groovy.test.GroovyTestCase
 class MultiDimArraysTest extends GroovyTestCase {
 
     void testCallTwoDimStringArray() {
-        def someArrayOfStringArrays = new SomeClass().anArrayOfStringArrays()
-        assert 1 == someArrayOfStringArrays.size()
-        assert someArrayOfStringArrays[0][0] == 'whatever'
+        assertScript '''
+            def someArrayOfStringArrays = new groovy.SomeClass().anArrayOfStringArrays()
+            assert 1 == someArrayOfStringArrays.size()
+            assert someArrayOfStringArrays[0][0] == 'whatever'
+        '''
     }
 
     void testCallTwoDimStringArrayWorkaround() {
-        def someArrayOfStringArrays = new SomeClass().anArrayOfStringArraysWorkaround()
-        assert 1 == someArrayOfStringArrays.size()
-        assert "whatever" == someArrayOfStringArrays[0][0]
-        for (i in 0..<someArrayOfStringArrays.size()) {
-            assert someArrayOfStringArrays[i]
-        }
+        assertScript '''
+            def someArrayOfStringArrays = new groovy.SomeClass().anArrayOfStringArraysWorkaround()
+            assert 1 == someArrayOfStringArrays.size()
+            assert "whatever" == someArrayOfStringArrays[0][0]
+            for (i in 0..<someArrayOfStringArrays.size()) {
+                assert someArrayOfStringArrays[i]
+            }
+        '''
     }
 
     void testCallTwoDimStringArrayWorkaroundWithNull() {
-        def someArrayOfStringArrays = new SomeClass().anArrayOfStringArraysWorkaround()
-        assert 1 == someArrayOfStringArrays.size()
-        assert "whatever" == someArrayOfStringArrays[0][0]
-        someArrayOfStringArrays.each() { assert it }
+        assertScript '''
+            def someArrayOfStringArrays = new groovy.SomeClass().anArrayOfStringArraysWorkaround()
+            assert 1 == someArrayOfStringArrays.size()
+            assert "whatever" == someArrayOfStringArrays[0][0]
+            someArrayOfStringArrays.each() { assert it }
+        '''
     }
 
     void testInsideGroovyMultiDimReplacement() {
-        Object[] someArrayOfStringArrays = [["a", "a", "a"], ["b", "b", "b", null]]
-        assert "a" == someArrayOfStringArrays[0][0]
-        someArrayOfStringArrays.each() { assert it }
+        assertScript '''
+            Object[] someArrayOfStringArrays = [["a", "a", "a"], ["b", "b", "b", null]]
+            assert "a" == someArrayOfStringArrays[0][0]
+            someArrayOfStringArrays.each() { assert it }
+        '''
     }
 
     void testMultiDimCreationWithSizes() {
-        Object[][] objectArray = new Object[2][5]
-        assert objectArray.length == 2
-        objectArray.each {
-            assert it.length == 5
-            it.each { assert it == null }
-        }
+        assertScript '''
+            Object[][] objectArray = new Object[2][5]
+            assert objectArray.length == 2
+            objectArray.each {
+                assert it.length == 5
+                it.each { assert it == null }
+            }
+        '''
     }
 
     void testMultiDimCreationWithoutSizeAtEnd() {
-        def array = new int[5][6][]
-        assert array.class.name == "[[[I"
-        assert array[0].class.name == "[[I"
-        assert array[0][0] == null
+        assertScript '''
+            def array = new int[5][6][]
+            assert array.class.name == "[[[I"
+            assert array[0].class.name == "[[I"
+            assert array[0][0] == null
+        '''
     }
 
     void testMultiDimArrayForCustomClass() {
-        def ff = new MultiDimArraysTest[3][4]
-        assert "[[Lgroovy.MultiDimArraysTest;" == ff.class.name;
+        assertScript '''
+            def arr = new groovy.MultiDimArraysTest[3][4]
+            assert arr.class.name == '[[Lgroovy.MultiDimArraysTest;'
+        '''
     }
 
     void testIntArrayIncrement() {
-        int[][] x = new int[10][10]
-        x[1][1] += 5
-        assert x[1][1] == 5
+        assertScript '''
+            int[][] x = new int[10][10]
+            x[1][1] += 5
+            assert x[1][1] == 5
+        '''
     }
 }
-
