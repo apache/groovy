e7c519e0476271466abe629c77652d3dfed052c7
Eric Milles <eric.milles@thomsonreuters.com>
2021-05-04 16:50:12 -0500

    GROOVY-9064: add test case

--- src/test/groovy/transform/stc/GenericsSTCTest.groovy
+++ src/test/groovy/transform/stc/GenericsSTCTest.groovy
@@ -407,6 +407,18 @@ class GenericsSTCTest extends StaticTypeCheckingTestCase {
         '''
     }
 
+    // GROOVY-9064
+    void testReturnTypeInferenceWithMethodGenerics13() {
+        assertScript '''
+            List getSomeRows() { [new String[]{'x'}] }
+            List<String[]> rows = getSomeRows()
+            rows.each { row ->
+                def col = row[0].toUpperCase()
+                assert col == 'X'
+            }
+        '''
+    }
+
     void testDiamondInferrenceFromConstructor1() {
         assertScript '''
             class Foo<U> {
