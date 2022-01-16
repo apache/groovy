88b768e79c9ceaa2ac46e1cabc926f8f2c7fa820
Paul King <paulk@asert.com.au>
2022-01-16 22:12:57 +1000

    add some javadoc

--- src/main/java/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/java/org/codehaus/groovy/ast/ClassNode.java
@@ -1417,6 +1417,9 @@ public class ClassNode extends AnnotatedNode {
         return (getModifiers() & ACC_ABSTRACT) != 0;
     }
 
+    /**
+     * @return true for native and emulated (annotation based) sealed classes
+     */
     public boolean isSealed() {
         return !getAnnotations(SEALED_TYPE).isEmpty() || !getPermittedSubclasses().isEmpty();
     }
