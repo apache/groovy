40b49a8b100d730844d11471a0c5b2ec6c14d288
Eric Milles <eric.milles@thomsonreuters.com>
2022-02-03 12:59:42 -0600

    GROOVY-10352: class model: load anno enum value as property expression

--- src/main/java/org/codehaus/groovy/vmplugin/v5/Java5.java
+++ src/main/java/org/codehaus/groovy/vmplugin/v5/Java5.java
@@ -301,14 +301,16 @@ public class Java5 implements VMPlugin {
             return new ConstantExpression(value);
 
         if (value instanceof Class)
-            return new ClassExpression(ClassHelper.makeWithoutCaching((Class)value));
+            return new ClassExpression(ClassHelper.makeWithoutCaching((Class<?>)value));
+
+        if (value instanceof Enum )
+            return new PropertyExpression(new ClassExpression(ClassHelper.makeWithoutCaching(value.getClass())), value.toString());
 
         if (value.getClass().isArray()) {
-            ListExpression elementExprs = new ListExpression();
-            int len = Array.getLength(value);
-            for (int i = 0; i != len; ++i)
-                elementExprs.addExpression(annotationValueToExpression(Array.get(value, i)));
-            return elementExprs;
+            ListExpression values = new ListExpression();
+            for (int i = 0, n = Array.getLength(value); i < n; i += 1)
+                values.addExpression(annotationValueToExpression(Array.get(value, i)));
+            return values;
         }
 
         return null;
