c70f04e40121d3ea624184be30a05ac409caf3e0
Paul King <paulk@asert.com.au>
2022-02-15 23:05:50 +1000

    GROOVY-10290: Dynamic Groovy code in Gradle doesn't compile because of $getLookup() method is not static

---
+++ src/test/org/codehaus/groovy/tools/stubgenerator/Groovy10290.groovy
@@ -0,0 +1,46 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.codehaus.groovy.tools.stubgenerator
+
+final class Groovy10290 extends StringSourcesStubTestCase {
+
+    @Override
+    Map<String, String> provideSources() {
+        [
+            'ResettableExpectations.java': '''
+                public interface ResettableExpectations { }
+            ''',
+            'SFTPServer.groovy': '''
+                class SFTPServer implements ResettableExpectations {
+                    static interface SftpExpectation { }
+                    @groovy.transform.CompileStatic
+                    static class SftpExpectOne implements SftpExpectation { }
+                    static class SftpExpectOnePath extends SftpExpectOne { }
+                    class SftpExpectOneOpen extends SftpExpectOnePath { }
+                }
+            '''
+        ]
+    }
+
+    @Override
+    void verifyStubs() {
+        String stub = stubJavaSourceFor('SFTPServer')
+        assert !stub.contains('$getLookup')
+    }
+}
