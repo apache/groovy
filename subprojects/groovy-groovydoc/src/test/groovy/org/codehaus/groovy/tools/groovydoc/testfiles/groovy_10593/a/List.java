128349de9806cec021ee8350758f264c4ac3c5ea
Stephane Talbot <Stephane.Talbot@univ-savoie.fr>
2022-04-23 19:11:58 +0200

    GROOVY-10593: replace '.' by '/' in imports

---
+++ subprojects/groovy-groovydoc/src/test/groovy/org/codehaus/groovy/tools/groovydoc/testfiles/groovy_10593/a/List.java
@@ -0,0 +1,3 @@
+package org.codehaus.groovy.tools.groovydoc.testfiles.groovy_10593.a;
+
+public interface List {}
---
+++ subprojects/groovy-groovydoc/src/test/groovy/org/codehaus/groovy/tools/groovydoc/testfiles/groovy_10593/b/Test.java
@@ -0,0 +1,5 @@
+package org.codehaus.groovy.tools.groovydoc.testfiles.groovy_10593.b;
+
+import org.codehaus.groovy.tools.groovydoc.testfiles.groovy_10593.a.List;
+
+public interface Test extends List {}
