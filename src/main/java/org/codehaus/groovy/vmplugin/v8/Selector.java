91781554a3d9495a421f00f184bc1892dad248d3
Daniel Sun <sunlan@apache.org>
2022-05-14 20:34:44 +0800

    Tweak guards for receiver and parameters further

--- src/main/java/org/codehaus/groovy/vmplugin/v8/Selector.java
+++ src/main/java/org/codehaus/groovy/vmplugin/v8/Selector.java
@@ -63,6 +63,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -85,6 +86,7 @@ import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.MOP
 import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.MOP_INVOKE_CONSTRUCTOR;
 import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.MOP_INVOKE_METHOD;
 import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.NULL_REF;
+import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.SAME_CLASS;
 import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.SAME_CLASSES;
 import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.SAME_MC;
 import static org.codehaus.groovy.vmplugin.v8.IndyGuardsFiltersAndSignatures.SAM_CONVERSION;
@@ -923,10 +925,37 @@ public abstract class Selector {
 
             // guards for receiver and parameter
             Class<?>[] pt = handle.type().parameterArray();
-            MethodHandle test = SAME_CLASSES.bindTo(args)
-                    .asCollector(Object[].class, pt.length)
-                    .asType(MethodType.methodType(boolean.class, pt));
-            handle = MethodHandles.guardWithTest(test, handle, fallback);
+            if (Arrays.stream(args).anyMatch(arg -> null == arg)) {
+                for (int i = 0; i < args.length; i++) {
+                    Object arg = args[i];
+                    Class<?> paramType = pt[i];
+                    MethodHandle test;
+
+                    if (arg == null) {
+                        test = IS_NULL.asType(MethodType.methodType(boolean.class, paramType));
+                        if (LOG_ENABLED) LOG.info("added null argument check at pos " + i);
+                    } else {
+                        if (Modifier.isFinal(paramType.getModifiers())) {
+                            // primitive types are also `final`
+                            continue;
+                        }
+                        test = SAME_CLASS.
+                                bindTo(arg.getClass()).
+                                asType(MethodType.methodType(boolean.class, paramType));
+                        if (LOG_ENABLED) LOG.info("added same class check at pos " + i);
+                    }
+                    Class<?>[] drops = new Class[i];
+                    System.arraycopy(pt, 0, drops, 0, drops.length);
+                    test = MethodHandles.dropArguments(test, 0, drops);
+                    handle = MethodHandles.guardWithTest(test, handle, fallback);
+                }
+            } else {
+                // Avoid guards as possible as we could
+                MethodHandle test = SAME_CLASSES.bindTo(args)
+                        .asCollector(Object[].class, pt.length)
+                        .asType(MethodType.methodType(boolean.class, pt));
+                handle = MethodHandles.guardWithTest(test, handle, fallback);
+            }
         }
 
         /**
