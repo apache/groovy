28c5700a0ef822c19b93612cb248f0951e311b13
Paul King <paulk@asert.com.au>
2022-05-27 17:31:41 +1000

    GROOVY-10627: Bump jackson to 2.13.3 (additional test case)

--- src/test/groovy/bugs/Groovy10461.groovy
+++ src/test/groovy/bugs/Groovy10461.groovy
@@ -30,7 +30,7 @@ final class Groovy10461 {
             @GrabExclude('org.codehaus.groovy:groovy-xml')
             @GrabExclude('org.codehaus.groovy:groovy-json')
 
-            @Grab('com.fasterxml.jackson.core:jackson-databind:2.13.2.2')
+            @Grab('com.fasterxml.jackson.core:jackson-databind:2.13.3')
             import com.fasterxml.jackson.databind.ObjectMapper
 
             @Grab('io.rest-assured:rest-assured:4.4.0')
