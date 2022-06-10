2a641e49509f7becbe081f7c27eb8b74b9377530
Eric Milles <eric.milles@thomsonreuters.com>
2022-06-10 10:25:33 -0500

    GROOVY-10654: `ASTTransformationCustomizer`: enum and array parameters
    3_0_X backport

--- src/test/org/codehaus/groovy/control/customizers/ASTTransformationCustomizerTest.groovy
+++ src/test/org/codehaus/groovy/control/customizers/ASTTransformationCustomizerTest.groovy
@@ -18,138 +18,166 @@
  */
 package org.codehaus.groovy.control.customizers
 
-import groovy.test.GroovyTestCase
 import groovy.transform.TimedInterrupt
 import groovy.util.logging.Log
 import org.codehaus.groovy.ast.ASTNode
 import org.codehaus.groovy.ast.ClassHelper
-import org.codehaus.groovy.ast.expr.ClassExpression
-import org.codehaus.groovy.ast.expr.PropertyExpression
 import org.codehaus.groovy.control.CompilePhase
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.codehaus.groovy.control.SourceUnit
 import org.codehaus.groovy.transform.ASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
+import org.junit.Test
 
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
-import java.util.logging.Logger
+
+import static groovy.test.GroovyAssert.shouldFail
+import static org.codehaus.groovy.ast.tools.GeneralUtils.classX
+import static org.codehaus.groovy.ast.tools.GeneralUtils.propX
 
 /**
  * Tests the {@link ASTTransformationCustomizer}.
  */
-class ASTTransformationCustomizerTest extends GroovyTestCase {
-    CompilerConfiguration configuration
-    ASTTransformationCustomizer customizer
+final class ASTTransformationCustomizerTest {
 
-    void setUp() {
-        configuration = new CompilerConfiguration()
-    }
+    private final CompilerConfiguration config = new CompilerConfiguration()
+    private final GroovyShell shell = new GroovyShell(config)
 
+    @Test
     void testLocalTransformation() {
-        customizer = new ASTTransformationCustomizer(Log)
-        configuration.addCompilationCustomizers(customizer)
-        def shell = new GroovyShell(configuration)
-        def result = shell.evaluate("""
+        def customizer = new ASTTransformationCustomizer(Log)
+        config.addCompilationCustomizers(customizer)
+        def result = shell.evaluate '''
             class MyClass {}
             new MyClass()
-        """)
-        assert result.log.class == Logger
+        '''
+        assert result.log.class == java.util.logging.Logger
     }
 
-    void testLocalTransformationAndCustomClassLoader() {
-        ClassLoader loader = new URLClassLoader([]as URL[]) {
+    @Test
+    void testLocalTransformationClassLoader() {
+        def loader = new URLClassLoader() {
             @Override
             Class<?> loadClass(String name) {
-                null
             }
         }
         shouldFail(ClassNotFoundException) {
-            customizer = new ASTTransformationCustomizer(Log, loader)
+            new ASTTransformationCustomizer(Log, loader)
         }
     }
 
-    void testLocalTransformationWithAnnotationParameter() {
-        customizer = new ASTTransformationCustomizer(Log)
+    @Test
+    void testLocalTransformationStringParameter() {
+        def customizer = new ASTTransformationCustomizer(Log)
         customizer.annotationParameters = [value: 'logger']
-        configuration.addCompilationCustomizers(customizer)
-        def shell = new GroovyShell(configuration)
-        def result = shell.evaluate("""
+        config.addCompilationCustomizers(customizer)
+        def result = shell.evaluate '''
             class MyClass {}
             new MyClass()
-        """)
-        assert result.logger.class == Logger
+        '''
+        assert result.logger.class == java.util.logging.Logger
     }
 
-    void testLocalTransformationWithInvalidAnnotationParameter() {
-        customizer = new ASTTransformationCustomizer(Log)
+    @Test
+    void testLocalTransformationUnknownParameter() {
+        def customizer = new ASTTransformationCustomizer(Log)
         shouldFail(IllegalArgumentException) {
             customizer.annotationParameters = [invalid: 'logger']
         }
     }
 
-    void testGlobalTransformation() {
-        final TestTransformation transformation = new TestTransformation()
-        customizer = new ASTTransformationCustomizer(transformation)
-        configuration.addCompilationCustomizers(customizer)
-        def shell = new GroovyShell(configuration)
-        assert shell.evaluate('true')
-        assert transformation.applied.get()
-    }
-
-    void testGlobalTransformation2() {
-        final TestTransformation transformation = new TestTransformation()
-        customizer = new ASTTransformationCustomizer(transformation)
-        configuration.addCompilationCustomizers(customizer)
-        def shell = new GroovyShell(configuration)
-        assert shell.evaluate("""
-            class A {}
-            class B {}
-            true
-        """)
-        assert transformation.applied.get()
-    }
-
-    void testLocalTransformationWithListOfClassAnnotationParameter() {
-        customizer = new ASTTransformationCustomizer(Newify, value: [Integer, Long])
-        configuration.addCompilationCustomizers(customizer)
-        def shell = new GroovyShell(configuration)
+    @Test
+    void testLocalTransformationListOfClassParameter() {
+        def customizer = new ASTTransformationCustomizer(Newify)
+        customizer.annotationParameters = [value: [Integer, Long]]
+        config.addCompilationCustomizers(customizer)
         def result = shell.evaluate '''
             Integer(11) + Long(31)
         '''
         assert result == 42
     }
 
-    void testAnyExpressionAsParameterValue() {
-        customizer = new ASTTransformationCustomizer(value:100, unit: new PropertyExpression(new ClassExpression(ClassHelper.make(TimeUnit)),'MILLISECONDS'), TimedInterrupt)
-        configuration.addCompilationCustomizers(customizer)
-        def shell = new GroovyShell(configuration)
-        def result = shell.evaluate '''import java.util.concurrent.TimeoutException
-
-boolean interrupted = false
-try {
-    100.times {
-        Thread.sleep(100)
+    @Test
+    void testLocalTransformationArrayOfClassParameter() {
+        def customizer = new ASTTransformationCustomizer(Newify)
+        customizer.annotationParameters = [value: new Class[]{Integer, Long}]
+        config.addCompilationCustomizers(customizer)
+        def result = shell.evaluate '''
+            Integer(11) + Long(31)
+        '''
+        assert result == 42
     }
-} catch (TimeoutException e) {
-    interrupted = true
-}
 
-interrupted'''
-        assert result
+    @Test
+    void testLocalTransformationPropertyExpressionParameter() {
+        def customizer = new ASTTransformationCustomizer(TimedInterrupt)
+        customizer.annotationParameters = [value: 300, unit: propX(classX(ClassHelper.make(TimeUnit)),'MILLISECONDS')]
+        config.addCompilationCustomizers(customizer)
+        assert shell.evaluate('''import java.util.concurrent.TimeoutException
+            boolean interrupted = false
+            try {
+                10.times {
+                    sleep 100
+                }
+            } catch (TimeoutException ignore) {
+                interrupted = true
+            }
+            interrupted
+        ''')
+    }
+
+    @Test // GROOVY-10654
+    void testLocalTransformationEnumerationConstantParameter() {
+        def customizer = new ASTTransformationCustomizer(TimedInterrupt)
+        customizer.annotationParameters = [value: 300, unit: TimeUnit.MILLISECONDS]
+        config.addCompilationCustomizers(customizer)
+        assert shell.evaluate('''import java.util.concurrent.TimeoutException
+            boolean interrupted = false
+            try {
+                10.times {
+                    sleep 100
+                }
+            } catch (TimeoutException ignore) {
+                interrupted = true
+            }
+            interrupted
+        ''')
+    }
+
+    //--------------------------------------------------------------------------
+
+    @Test
+    void testGlobalTransformation() {
+        TestTransformation transformation = new TestTransformation()
+        config.addCompilationCustomizers(new ASTTransformationCustomizer(transformation))
+        assert shell.evaluate('true')
+        assert transformation.applied
+    }
+
+    @Test
+    void testGlobalTransformation2() {
+        TestTransformation transformation = new TestTransformation()
+        config.addCompilationCustomizers(new ASTTransformationCustomizer(transformation))
+        assert shell.evaluate('''
+            class A {}
+            class B {}
+            true
+        ''')
+        assert transformation.applied
     }
 
     @GroovyASTTransformation(phase=CompilePhase.CONVERSION)
     private static class TestTransformation implements ASTTransformation {
 
-        private AtomicBoolean applied = new AtomicBoolean(false)
+        private final applied = new AtomicBoolean()
+
+        boolean isApplied() { return applied.get() }
 
         void visit(ASTNode[] nodes, SourceUnit source) {
             if (applied.getAndSet(true)) {
-                throw new Exception("Global AST transformation should only be applied once")
+                throw new Exception('Global AST transformation should only be applied once')
             }
         }
-        
     }
-
 }
