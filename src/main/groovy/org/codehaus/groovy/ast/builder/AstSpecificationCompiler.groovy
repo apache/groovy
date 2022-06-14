5b8c63f596d3429ed8a60d6aba4b549203ddee15
Eric Milles <eric.milles@thomsonreuters.com>
2022-06-14 10:57:43 -0500

    target class name fix

--- src/main/groovy/org/codehaus/groovy/ast/builder/AstSpecificationCompiler.groovy
+++ src/main/groovy/org/codehaus/groovy/ast/builder/AstSpecificationCompiler.groovy
@@ -192,7 +192,7 @@ class AstSpecificationCompiler implements GroovyInterceptable {
      *       the single closure argument used during invocation
      */
     private void makeNode(Class target, String typeAlias, List<Class<? super ASTNode>> ctorArgs, @DelegatesTo(AstSpecificationCompiler) Closure argBlock) {
-        captureAndCreateNode(target.class.simpleName, argBlock) {
+        captureAndCreateNode(target.simpleName, argBlock) {
             target.newInstance(*enforceConstraints(typeAlias, ctorArgs))
         }
     }
@@ -256,14 +256,14 @@ class AstSpecificationCompiler implements GroovyInterceptable {
      *       a type parameter
      */
     private void makeNodeWithClassParameter(Class target, String alias, List<Class> spec, @DelegatesTo(AstSpecificationCompiler) Closure argBlock, Class type) {
-        captureAndCreateNode(target.class.simpleName, argBlock) {
+        captureAndCreateNode(target.simpleName, argBlock) {
             expression.add(0, ClassHelper.make(type))
             target.newInstance(*enforceConstraints(alias, spec))
         }
     }
 
     private void makeNodeWithStringParameter(Class target, String alias, List<Class> spec, @DelegatesTo(AstSpecificationCompiler) Closure argBlock, String text) {
-        captureAndCreateNode(target.class.simpleName, argBlock) {
+        captureAndCreateNode(target.simpleName, argBlock) {
             expression.add(0, text)
             target.newInstance(*enforceConstraints(alias, spec))
         }
