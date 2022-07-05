61ecf78b608696026a4f9e99f55ceee9a6cb4752
Eric Milles <eric.milles@thomsonreuters.com>
2022-07-05 13:22:33 -0500

    add `@since` tag

--- src/main/java/groovy/transform/Pure.java
+++ src/main/java/groovy/transform/Pure.java
@@ -18,18 +18,20 @@
  */
 package groovy.transform;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Mark the return value of "pure" method only comes from expressions involving constants or other pure methods
+ * Marks the return value of "pure" method only comes from expressions involving
+ * constants or other pure methods.
  *
  * @since 4.0.0
  */
-@java.lang.annotation.Documented
+@Documented
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
 public @interface Pure {
 }
