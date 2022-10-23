5d151068b1e4b13f77b1805f74558d77da4eff12
Eric Milles <eric.milles@thomsonreuters.com>
2022-10-23 05:49:50 -0500

    GROOVY-9089: SC: no cast exception for implicit receiver

--- src/main/java/org/codehaus/groovy/classgen/asm/sc/StaticInvocationWriter.java
+++ src/main/java/org/codehaus/groovy/classgen/asm/sc/StaticInvocationWriter.java
@@ -72,6 +72,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.apache.groovy.ast.tools.ClassNodeUtils.samePackageName;
+import static org.apache.groovy.ast.tools.ExpressionUtils.isSuperExpression;
 import static org.codehaus.groovy.ast.ClassHelper.CLOSURE_TYPE;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.nullX;
 import static org.codehaus.groovy.transform.sc.StaticCompilationMetadataKeys.PRIVATE_BRIDGE_METHODS;
@@ -109,7 +110,11 @@ public class StaticInvocationWriter extends InvocationWriter {
 
     private MethodCallExpression currentCall;
 
-    public StaticInvocationWriter(WriterController wc) {
+    public MethodCallExpression getCurrentCall() {
+        return currentCall;
+    }
+
+    public StaticInvocationWriter(final WriterController wc) {
         super(wc);
         controller = wc;
     }
@@ -353,7 +358,7 @@ public class StaticInvocationWriter extends InvocationWriter {
                 }
                 return true;
             }
-            Expression fixedReceiver = null;
+            Expression  fixedReceiver = receiver;
             boolean fixedImplicitThis = implicitThis;
             if (target.isPrivate()) {
                 if (tryPrivateMethod(target, implicitThis, receiver, args, classNode)) return true;
@@ -392,15 +397,10 @@ public class StaticInvocationWriter extends InvocationWriter {
                     }
                 }
             }
-            if (receiver != null) {
-                boolean callToSuper = receiver instanceof VariableExpression && ((VariableExpression) receiver).isSuperExpression();
-                if (!callToSuper) {
-                    fixedReceiver = fixedReceiver == null ? receiver : fixedReceiver;
-                    // in order to avoid calls to castToType, which is the dynamic behaviour, we make sure that we call CHECKCAST instead
-                    // then replace the top operand type
-                    Expression checkCastReceiver = new CheckcastReceiverExpression(fixedReceiver, target);
-                    return super.writeDirectMethodCall(target, fixedImplicitThis, checkCastReceiver, args);
-                }
+            if (receiver != null && !isSuperExpression(receiver)) {
+                // in order to avoid calls to castToType, which is the dynamic behaviour, make sure that we call CHECKCAST instead then replace the top operand type
+                if (currentCall.getNodeMetaData(StaticTypesMarker.IMPLICIT_RECEIVER) == null) fixedReceiver = new CheckcastReceiverExpression(fixedReceiver, target);
+                return super.writeDirectMethodCall(target, fixedImplicitThis, fixedReceiver, args);
             }
             return super.writeDirectMethodCall(target, implicitThis, receiver, args);
         }
@@ -679,17 +679,15 @@ public class StaticInvocationWriter extends InvocationWriter {
         if (implicitThis && implicitReceiver==null && origin instanceof MethodCallExpression) {
             implicitReceiver = ((MethodCallExpression) origin).getObjectExpression().getNodeMetaData(StaticTypesMarker.IMPLICIT_RECEIVER);
         }
-        if (implicitThis && implicitReceiver != null) {
-            String[] propertyPath = ((String) implicitReceiver).split("\\.");
-            // GROOVY-6021
+        if (implicitThis && implicitReceiver != null) { // GROOVY-6021
+            origin.removeNodeMetaData(StaticTypesMarker.IMPLICIT_RECEIVER);
+            String[] propertyPath = implicitReceiver.toString().split("\\.");
             PropertyExpression pexp = new PropertyExpression(new VariableExpression("this", CLOSURE_TYPE), propertyPath[0]);
             pexp.setImplicitThis(true);
-            for (int i=1; i<propertyPath.length;i++) {
+            for (int i = 1; i < propertyPath.length; i += 1) {
                 pexp.putNodeMetaData(StaticTypesMarker.INFERRED_TYPE, CLOSURE_TYPE);
                 pexp = new PropertyExpression(pexp, propertyPath[i]);
             }
-            pexp.putNodeMetaData(StaticTypesMarker.IMPLICIT_RECEIVER, implicitReceiver);
-            origin.removeNodeMetaData(StaticTypesMarker.IMPLICIT_RECEIVER);
             if (origin instanceof PropertyExpression) {
                 PropertyExpression rewritten = new PropertyExpression(
                         pexp,
@@ -697,9 +695,10 @@ public class StaticInvocationWriter extends InvocationWriter {
                         ((PropertyExpression) origin).isSafe()
                 );
                 rewritten.setSpreadSafe(((PropertyExpression) origin).isSpreadSafe());
-                rewritten.setImplicitThis(false);
+                rewritten.putNodeMetaData(StaticTypesMarker.INFERRED_TYPE,
+                   origin.getNodeMetaData(StaticTypesMarker.INFERRED_TYPE)
+                );
                 rewritten.visit(controller.getAcg());
-                rewritten.putNodeMetaData(StaticTypesMarker.INFERRED_TYPE, origin.getNodeMetaData(StaticTypesMarker.INFERRED_TYPE));
                 return true;
             }
             makeCall(origin, pexp, message, arguments, adapter, safe, spreadSafe, false);
@@ -778,10 +777,6 @@ public class StaticInvocationWriter extends InvocationWriter {
             }
             return type;
         }
-    }
-
-    public MethodCallExpression getCurrentCall() {
-        return currentCall;
     }
 
     @Override
