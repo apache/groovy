d72a1ac5d5e1e052016838385a36506f320e9aa8
Eric Milles <eric.milles@thomsonreuters.com>
2022-11-18 13:30:49 -0600

    GROOVY-10846: STC: use erasure for placeholder with no upper bound

--- src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
+++ src/main/java/org/codehaus/groovy/transform/stc/StaticTypeCheckingVisitor.java
@@ -3865,7 +3865,8 @@ public class StaticTypeCheckingVisitor extends ClassCodeVisitorSupport {
                 addSelfTypes(cn, owners);
             }
         } else {
-            owners.add(Receiver.make(OBJECT_TYPE)); // T or T super Type
+            ClassNode cn = gt.getType().redirect(); // GROOVY-10846
+            owners.add(Receiver.make(cn)); // T or T super Type
         }
     }
 
