40bb7fe8aee10b169e5383359e498581ef1e4c48
Eric Milles <eric.milles@thomsonreuters.com>
2023-04-24 09:35:01 -0500

    GROOVY-11023: SC: `InvocationWriter` cannot handle `ExtensionMethodNode`

--- src/main/java/org/codehaus/groovy/classgen/asm/InvocationWriter.java
+++ src/main/java/org/codehaus/groovy/classgen/asm/InvocationWriter.java
@@ -88,6 +88,7 @@ import org.codehaus.groovy.runtime.ScriptBytecodeAdapter;
 import org.codehaus.groovy.runtime.typehandling.DefaultTypeTransformation;
 import org.codehaus.groovy.runtime.typehandling.ShortTypeHandling;
 import org.codehaus.groovy.syntax.SyntaxException;
+import org.codehaus.groovy.transform.stc.ExtensionMethodNode;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 
@@ -167,7 +168,7 @@ public class InvocationWriter {
     }
 
     protected boolean writeDirectMethodCall(final MethodNode target, final boolean implicitThis, final Expression receiver, final TupleExpression args) {
-        if (target == null) return false;
+        if (target == null || target instanceof ExtensionMethodNode) return false;
 
         ClassNode declaringClass = target.getDeclaringClass();
         ClassNode enclosingClass = controller.getClassNode();
