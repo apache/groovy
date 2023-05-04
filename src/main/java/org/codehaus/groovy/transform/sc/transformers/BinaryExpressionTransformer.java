6177f7cf55b0eaec573e39624d8549ae5c68da55
Eric Milles <eric.milles@thomsonreuters.com>
2023-05-04 13:00:56 -0500

    GROOVY-11044: SC: retain implicit-this of property transformed to setter

--- src/main/java/org/codehaus/groovy/transform/sc/transformers/BinaryExpressionTransformer.java
+++ src/main/java/org/codehaus/groovy/transform/sc/transformers/BinaryExpressionTransformer.java
@@ -185,7 +185,7 @@ public class BinaryExpressionTransformer {
                         pe.getObjectExpression(), // "a"
                         directMCT, // "setX"
                         right, // "val"
-                        false,
+                        pe.isImplicitThis(),
                         pe.isSafe(),
                         pe.getProperty(), // "x"
                         bin);
