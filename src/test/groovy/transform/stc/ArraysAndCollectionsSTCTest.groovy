77024ea1c171ebb6d5b2eb7d3878cf35d6e87120
Eric Milles <eric.milles@thomsonreuters.com>
2023-06-06 08:37:50 -0500

    GROOVY-11080: add test case

--- src/test/groovy/transform/stc/ArraysAndCollectionsSTCTest.groovy
+++ src/test/groovy/transform/stc/ArraysAndCollectionsSTCTest.groovy
@@ -1105,7 +1105,7 @@ class ArraysAndCollectionsSTCTest extends StaticTypeCheckingTestCase {
         'Cannot assign value of type groovy.lang.ListWithDefault<java.lang.Integer> to variable of type java.util.Set<java.lang.Integer>'
     }
 
-    // GROOVY-8001, GROOVY-11028
+    // GROOVY-8001, GROOVY-11028, GROOVY-11080
     void testMapWithTypeArgumentsInitializedByMapLiteral() {
         for (spec in ['CharSequence,Integer', 'String,Number', 'CharSequence,Number']) {
             assertScript """
@@ -1117,6 +1117,13 @@ class ArraysAndCollectionsSTCTest extends StaticTypeCheckingTestCase {
         }
 
         assertScript '''
+            Map<String,Map<String,Object>> map = [:]
+            map.put('foo', [bar:null])
+            map.put('baz', [:])
+            assert 'foo' in map
+        '''
+
+        assertScript '''
             class C {
                 Map<String,Object> map
             }
@@ -1124,6 +1131,8 @@ class ArraysAndCollectionsSTCTest extends StaticTypeCheckingTestCase {
             def c = new C()
             c.map = [key:"$value"]
             assert c.map['key'] == '42'
+            c = new C(map: [key:value])
+            assert c.map.key.toString() == '42'
         '''
 
         assertScript '''
