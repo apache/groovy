e85b955856caca8f9a588672f4efbd5143c37401
Paul King <paulk@asert.com.au>
2023-11-25 09:00:29 +1000

    bump dnsjava version (perf test)

--- build-logic/src/main/groovy/org/apache/groovy/gradle/PerformanceTestsExtension.groovy
+++ build-logic/src/main/groovy/org/apache/groovy/gradle/PerformanceTestsExtension.groovy
@@ -105,7 +105,7 @@ class PerformanceTestsExtension {
                 'com.baulsupp.kolja:jcurses:0.9.5.3',
                 'org.mnode.mstor:mstor:1.0.3',
                 'commons-lang:commons-lang:2.6',
-                'dnsjava:dnsjava:3.5.2',
+                'dnsjava:dnsjava:3.5.3',
                 'net.sourceforge.expectj:expectj:2.0.7',
                 'jline:jline:2.14.6',
                 'prevayler:prevayler:2.02.005',
