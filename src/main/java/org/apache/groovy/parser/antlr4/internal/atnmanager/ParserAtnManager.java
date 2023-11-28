2f411325e797a70f637f795f3ffcbb593bf33163
Daniel Sun <sunlan@apache.org>
2023-11-29 00:25:38 +0800

    GROOVY-11240: Tweak cleanup DFA cache of parser

--- src/main/java/org/apache/groovy/parser/antlr4/internal/atnmanager/ParserAtnManager.java
+++ src/main/java/org/apache/groovy/parser/antlr4/internal/atnmanager/ParserAtnManager.java
@@ -18,19 +18,17 @@
  */
 package org.apache.groovy.parser.antlr4.internal.atnmanager;
 
-import org.antlr.v4.runtime.atn.ATN;
 import org.apache.groovy.parser.antlr4.GroovyLangParser;
 
 /**
  * Manage ATN for parser to avoid memory leak
  */
 public class ParserAtnManager extends AtnManager {
-    private final AtnWrapper parserAtnWrapper = new AtnManager.AtnWrapper(GroovyLangParser._ATN);
     public static final ParserAtnManager INSTANCE = new ParserAtnManager();
 
     @Override
-    public ATN getATN() {
-        return parserAtnWrapper.checkAndClear();
+    protected AtnWrapper createAtnWrapper() {
+        return new AtnWrapper(GroovyLangParser._ATN);
     }
 
     @Override
