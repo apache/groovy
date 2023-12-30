aa7cb8e022708b7d6be26bc2225b9749ebf14aac
Daniel Sun <sunlan@apache.org>
2023-12-30 23:39:00 +0800

    Specify charset to read resources

--- src/main/java/org/codehaus/groovy/control/SourceExtensionHandler.java
+++ src/main/java/org/codehaus/groovy/control/SourceExtensionHandler.java
@@ -26,6 +26,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -44,7 +45,7 @@ public class SourceExtensionHandler {
                 globalServices = loader.getResources("META-INF/services/org.codehaus.groovy.source.Extensions");
             }
             for (URL service : DefaultGroovyMethods.toSet(globalServices)) {
-                try (BufferedReader svcIn = new BufferedReader(new InputStreamReader(URLStreams.openUncachedStream(service)))) {
+                try (BufferedReader svcIn = new BufferedReader(new InputStreamReader(URLStreams.openUncachedStream(service), StandardCharsets.UTF_8))) {
                     String extension = svcIn.readLine();
                     while (extension != null) {
                         extension = extension.trim();
