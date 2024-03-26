8673cd24b446f0e25b3f397ca54fe9af23d0f074
Paul King <paulk@asert.com.au>
2024-03-26 22:12:26 +1000

    GROOVY-11347: Support JDK 23

--- src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
+++ src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
@@ -104,6 +104,8 @@ public class CompilerConfiguration {
     public static final String JDK21 = "21";
     /** This (<code>"22"</code>) is the value for targetBytecode to compile for a JDK 22. */
     public static final String JDK22 = "22";
+    /** This (<code>"22"</code>) is the value for targetBytecode to compile for a JDK 23. */
+    public static final String JDK23 = "23";
 
     /**
      * This constant is for comparing targetBytecode to ensure it is set to JDK 1.5 or later.
@@ -141,7 +143,8 @@ public class CompilerConfiguration {
             JDK19, Opcodes.V19,
             JDK20, Opcodes.V20,
             JDK21, Opcodes.V21,
-            JDK22, Opcodes.V22
+            JDK22, Opcodes.V22,
+            JDK23, Opcodes.V23
     );
 
     public static final String DEFAULT_TARGET_BYTECODE = defaultTargetBytecode();
