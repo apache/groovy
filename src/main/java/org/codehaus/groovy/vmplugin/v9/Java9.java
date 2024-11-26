0c1f2260c440bb9130d7a061236e904957ce4d5c
Daniel Sun <sunlan@apache.org>
2024-11-26 22:59:37 +0800

    GROOVY-11524: Error with circular loading of installed providers when using transitive dependency groovy 4.0.23 in docker

--- src/main/java/org/codehaus/groovy/vmplugin/v9/Java9.java
+++ src/main/java/org/codehaus/groovy/vmplugin/v9/Java9.java
@@ -112,10 +112,10 @@ public class Java9 extends Java8 {
             } finally {
                 result.putAll(doFindClasses(URI.create("jrt:/modules/java.base/"), "java", javaPackages));
             }
-        } catch (Exception e) {
+        } catch (Throwable ignore) {
             Logger logger = Logger.getLogger(getClass().getName());
             if (logger.isLoggable(Level.FINEST)) {
-                logger.finest("[WARNING] Failed to find default imported classes:\n" + DefaultGroovyMethods.asString(e));
+                logger.finest("[WARNING] Failed to find default imported classes:\n" + DefaultGroovyMethods.asString(ignore));
             }
         }
 
