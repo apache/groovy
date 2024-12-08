472228525185592d8d41981ef06ca18f851cf91c
Daniel Sun <sunlan@apache.org>
2024-12-08 13:51:04 +0800

    Add `Pattern` to builtin immutables list
    (cherry picked from commit a96c76e29642a43174dd89573cd26a1381642cab)

--- src/main/java/org/apache/groovy/ast/tools/ImmutablePropertyUtils.java
+++ src/main/java/org/apache/groovy/ast/tools/ImmutablePropertyUtils.java
@@ -88,6 +88,7 @@ public class ImmutablePropertyUtils {
             "java.awt.Color",
             "java.net.URI",
             "java.util.UUID",
+            "java.util.regex.Pattern",
             "java.time.DayOfWeek",
             "java.time.Duration",
             "java.time.Instant",
