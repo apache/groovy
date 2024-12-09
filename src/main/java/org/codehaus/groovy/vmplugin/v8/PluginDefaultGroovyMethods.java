f30c96b7a708824acc34fbc0efae074a50148af8
Eric Milles <eric.milles@thomsonreuters.com>
2024-12-09 10:38:07 -0600

    fix typo

--- src/main/java/org/codehaus/groovy/vmplugin/v8/PluginDefaultGroovyMethods.java
+++ src/main/java/org/codehaus/groovy/vmplugin/v8/PluginDefaultGroovyMethods.java
@@ -533,7 +533,7 @@ public class PluginDefaultGroovyMethods extends DefaultGroovyMethodsSupport {
     }
 
     /**
-     * If a value is present in the {@code OptionalInt}, returns an {@code OptionalInt}
+     * If a value is present in the {@code Optional}, returns an {@code OptionalInt}
      * consisting of the result of applying the given function to the value or else empty.
      * <pre class="groovyTestCase">
      * assert !Optional.empty().mapToInt(x -&gt; 42).isPresent()
@@ -547,7 +547,7 @@ public class PluginDefaultGroovyMethods extends DefaultGroovyMethodsSupport {
     }
 
     /**
-     * If a value is present in the {@code OptionalLong}, returns an {@code OptionalLong}
+     * If a value is present in the {@code Optional}, returns an {@code OptionalLong}
      * consisting of the result of applying the given function to the value or else empty.
      * <pre class="groovyTestCase">
      * assert !Optional.empty().mapToLong(x -&gt; 42L).isPresent()
@@ -561,7 +561,7 @@ public class PluginDefaultGroovyMethods extends DefaultGroovyMethodsSupport {
     }
 
     /**
-     * If a value is present in the {@code OptionalDouble}, returns an {@code OptionalDouble}
+     * If a value is present in the {@code Optional}, returns an {@code OptionalDouble}
      * consisting of the result of applying the given function to the value or else empty.
      * <pre class="groovyTestCase">
      * assert !Optional.empty().mapToDouble(x -&gt; Math.PI).isPresent()
