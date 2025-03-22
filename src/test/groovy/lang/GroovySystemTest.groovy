2d29550e5d641730f68d12985a2f1c309c8626eb
Paul King <paulk@asert.com.au>
2025-03-22 14:07:30 +1000

    formatting

--- src/test/groovy/lang/GroovySystemTest.groovy
+++ src/test/groovy/lang/GroovySystemTest.groovy
@@ -34,7 +34,9 @@ class GroovySystemTest extends GroovyTestCase {
         assert GroovySystem.getVersion()
         // so we can check these values in test reports
         println GroovySystem.getVersion()
-        println System.getProperties()
-        println System.getenv()
+        println '\nSystem properties:'
+        println System.getProperties().entrySet().join('\n')
+        println '\nEnvironment variables:'
+        println System.getenv().entrySet().join('\n')
     }
 }
