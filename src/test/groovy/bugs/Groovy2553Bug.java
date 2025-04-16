709d38e12521747fde53186820e6c680faef3d57
Paul King <paulk@asert.com.au>
2025-04-16 20:17:20 +1000

    move most of src/test/java back to src/test/groovy

--- src/test/groovy/bugs/Groovy2553Bug.java
+++ src/test/groovy/bugs/Groovy2553Bug.java
@@ -21,10 +21,19 @@ package bugs;
 import groovy.lang.GroovyShell;
 import junit.framework.TestCase;
 
+class Autobox {
+
+    public static class Util {
+        public static void printByte(String str, Byte defaultValue) {
+            System.out.println(str + ", " + defaultValue);
+        }
+    }
+}
+
 public class Groovy2553Bug extends TestCase {
 
     public void testMe () {
-        new GroovyShell().evaluate("bugs.Autobox.Util.printByte(\"1\", Byte.valueOf((byte)1))");
-        new GroovyShell().evaluate("bugs.Autobox.Util.printByte(\"1\", (byte)1)");
+        new GroovyShell().evaluate("bugs.Autobox.Util.printByte(\"1\", Byte.valueOf((byte)1));");
+        new GroovyShell().evaluate("bugs.Autobox.Util.printByte(\"1\", (byte)1);");
     }
 }
