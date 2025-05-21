4e3e5f64950faa561c2f724b902a8291a02d15ac
Paul King <paulk@asert.com.au>
2025-05-22 01:09:27 +1000

    add @since version

--- src/main/java/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
+++ src/main/java/org/codehaus/groovy/runtime/DefaultGroovyMethods.java
@@ -13537,6 +13537,7 @@ public class DefaultGroovyMethods extends DefaultGroovyMethodsSupport {
      *
      * @see #sort(List, IntRange, boolean, Closure)
      * @return the sorted list
+     * @since 5.0.0
      */
     public static <T> List<T> sort(List<T> self, IntRange range, @ClosureParams(value=FromString.class,options={"T","T,T"}) Closure<?> closure) {
         return sort(self, range, true, closure);
