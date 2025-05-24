2e2e407253d51161f410d1277bf56b42d8938355
Eric Milles <eric.milles@thomsonreuters.com>
2025-05-23 19:39:05 -0500

    separate class and method within `MissingMethodException` message

--- src/test/groovy/bugs/MethodClosureTest.groovy
+++ src/test/groovy/bugs/MethodClosureTest.groovy
@@ -19,7 +19,7 @@
 package bugs
 
 import org.codehaus.groovy.runtime.MethodClosure
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 import static groovy.test.GroovyAssert.assertScript
 import static groovy.test.GroovyAssert.shouldFail
