fb519abfbdd16bf1c490634130490c60013920ff
ZNT\lcarausu <l.carausu@znt.de>
2025-09-10 16:15:34 +0200

    added tests for record

---
+++ subprojects/groovy-groovydoc/src/test/groovy/org/codehaus/groovy/tools/groovydoc/testfiles/records/Record.java
@@ -0,0 +1,15 @@
+package org.codehaus.groovy.tools.groovydoc.testfiles.records;
+
+/**
+ *  Javadoc for a record.
+ */
+public record Record(String recordName)
+{
+    /**
+     * Constructor of a record.
+     * @param recordName The record name.
+     */
+    public Record
+    {
+    }
+}
