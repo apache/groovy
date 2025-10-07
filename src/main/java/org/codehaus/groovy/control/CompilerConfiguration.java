733846130d8107e1ba37936d98da68db868c9ccd
Paul King <paulk@asert.com.au>
2025-10-07 13:04:12 +1000

    GROOVY-11771: Bump asm to 9.9 and add JDK26 constants

--- src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
+++ src/main/java/org/codehaus/groovy/control/CompilerConfiguration.java
@@ -110,6 +110,8 @@ public class CompilerConfiguration {
     public static final String JDK24 = "24";
     /** This (<code>"25"</code>) is the value for targetBytecode to compile for a JDK 25. */
     public static final String JDK25 = "25";
+    /** This (<code>"26"</code>) is the value for targetBytecode to compile for a JDK 26. */
+    public static final String JDK26 = "26";
 
     /**
      * JDK version to bytecode version mapping.
@@ -129,7 +131,8 @@ public class CompilerConfiguration {
             JDK22, Opcodes.V22,
             JDK23, Opcodes.V23,
             JDK24, Opcodes.V24,
-            JDK25, Opcodes.V25
+            JDK25, Opcodes.V25,
+            JDK26, Opcodes.V26
     );
 
     public static final String DEFAULT_TARGET_BYTECODE = defaultTargetBytecode();
