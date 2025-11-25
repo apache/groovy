af3ee9287436b12a65ea1f3d60e05aaf8295a366
Eric Milles <eric.milles@thomsonreuters.com>
2025-11-25 13:29:54 -0600

    GROOVY-11764: do not visit super class field if it is package-private

--- src/main/java/org/codehaus/groovy/classgen/AsmClassGenerator.java
+++ src/main/java/org/codehaus/groovy/classgen/AsmClassGenerator.java
@@ -155,6 +155,7 @@ import static org.codehaus.groovy.ast.ClassHelper.isStringType;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.callX;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.classX;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.fieldX;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.inSamePackage;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.maybeFallsThrough;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.thisPropX;
 import static org.codehaus.groovy.ast.tools.ParameterUtils.isVargs;
@@ -1252,8 +1253,10 @@ public class AsmClassGenerator extends ClassGenerator {
                     }
                 } else {
                     fieldNode = classNode.getSuperClass().getDeclaredField(name);
-                    // GROOVY-4497: do not visit super class field if it is private
-                    if (fieldNode != null && fieldNode.isPrivate()) fieldNode = null;
+                    // GROOVY-4497, GROOVY-11764: do not visit super class field if it is private or package-private
+                    if (fieldNode != null && (fieldNode.isPrivate() || !(fieldNode.isPublic() || fieldNode.isProtected() || inSamePackage(classNode, classNode.getSuperClass())))) {
+                        fieldNode = null;
+                    }
                 }
 
                 if (fieldNode != null) {
