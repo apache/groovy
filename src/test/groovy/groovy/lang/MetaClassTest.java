0ee7eb7c91d670651f8f1c8791c06105070cb671
Eric Milles <eric.milles@thomsonreuters.com>
2026-01-17 13:55:59 -0600

    GROOVY-11745: check for null sender

--- src/test/groovy/groovy/lang/MetaClassTest.java
+++ src/test/groovy/groovy/lang/MetaClassTest.java
@@ -109,6 +109,8 @@ final class MetaClassTest {
         DymmyClass dymmyClass = new DymmyClass();
         MetaClass metaClass = InvokerHelper.getMetaClass(dymmyClass);
         metaClass.setProperty(dymmyClass, "anInt", Integer.valueOf(10));
+
+        assertEquals(10, dymmyClass.getAnInt());
     }
 
     @Test
@@ -163,6 +165,18 @@ final class MetaClassTest {
 
         // test Integer[]
         metaClass.setProperty(dymmyClass, "integers", list);
+    }
+
+    // GROOVY-11745
+    @Test
+    void testSetPropertyWithoutSender() {
+        MetaClass metaClass = new MetaClassImpl(DymmyClass.class);
+        metaClass.initialize();
+
+        DymmyClass dymmyClass = new DymmyClass();
+        metaClass.setProperty(null, dymmyClass, "anInt", 1234, false, false);
+
+        assertEquals(1234, dymmyClass.getAnInt());
     }
 
     @Test
