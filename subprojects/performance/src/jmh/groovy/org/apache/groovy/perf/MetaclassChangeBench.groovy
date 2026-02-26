0612ace9a88bf823bbe363145c24f70bdadcabe1
James Fredley <jamesfredley@users.noreply.github.com>
2026-02-26 14:41:20 -0500

    reduce fork count to 1 for CI timing budget

--- subprojects/performance/src/jmh/groovy/org/apache/groovy/perf/MetaclassChangeBench.groovy
+++ subprojects/performance/src/jmh/groovy/org/apache/groovy/perf/MetaclassChangeBench.groovy
@@ -44,7 +44,7 @@ import java.util.concurrent.TimeUnit
  */
 @Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
-@Fork(2)
+@Fork(1)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
