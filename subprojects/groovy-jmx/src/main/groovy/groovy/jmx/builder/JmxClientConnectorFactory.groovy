4791af37a228917bc5cb0a9eff0febe2202b69c1
Paul King <paulk@asert.com.au>
2026-04-11 20:38:18 +1000

    GROOVY-11921: groovy-jmx: remove dead IIOP support, refresh docs

--- subprojects/groovy-jmx/src/main/groovy/groovy/jmx/builder/JmxClientConnectorFactory.groovy
+++ subprojects/groovy-jmx/src/main/groovy/groovy/jmx/builder/JmxClientConnectorFactory.groovy
@@ -35,11 +35,11 @@ import javax.management.remote.JMXServiceURL
  * )
  * </pre>
  *
- * @see <a href="http://java.sun.com/j2se/1.5.0/docs/api/javax/management/remote/JMXConnector.html">JMXConnector</a>
+ * @see javax.management.remote.JMXConnector
  */
 class JmxClientConnectorFactory extends AbstractFactory {
 
-    private static final List SUPPORTED_PROTOCOLS = ["rmi", "jrmp", "iiop", "jmxmp"]
+    private static final List SUPPORTED_PROTOCOLS = ["rmi", "jrmp", "jmxmp"]
 
     Object newInstance(FactoryBuilderSupport builder, Object nodeName, Object nodeArgs, Map nodeAttribs) {
         if (nodeArgs) {
