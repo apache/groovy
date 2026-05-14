5f0e39ac3b31ade37cebd2fd2336a10368a7f510
github-action-benchmark <github@users.noreply.github.com>
2026-05-14 22:30:15 +0000

    add Compiler Performance (customSmallerIsBetter) benchmark result for 6b109a62a7987c2a9ab7a6c1456693f0a3f3bf58

--- dev/bench/perf/compiler/data.js
+++ dev/bench/perf/compiler/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778795379129,
+  "lastUpdate": 1778797815067,
   "repoUrl": "https://github.com/apache/groovy",
   "entries": {
     "Compiler Performance": [
@@ -48,6 +48,56 @@ window.BENCHMARK_DATA = {
             "name": "compile@groovy-5",
             "value": 559.27,
             "range": "±51.98",
+            "unit": "ms",
+            "extra": "5.0.6"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Paul King",
+            "username": "paulk-asert",
+            "email": "paulk@asert.com.au"
+          },
+          "committer": {
+            "name": "Paul King",
+            "username": "paulk-asert",
+            "email": "paulk@asert.com.au"
+          },
+          "id": "6b109a62a7987c2a9ab7a6c1456693f0a3f3bf58",
+          "message": "GROOVY-12008: Sealed types: graduate from incubating status",
+          "timestamp": "2026-05-14T12:58:56Z",
+          "url": "https://github.com/apache/groovy/commit/6b109a62a7987c2a9ab7a6c1456693f0a3f3bf58"
+        },
+        "date": 1778797814465,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "compile@current",
+            "value": 539.5699999999999,
+            "range": "±23.05",
+            "unit": "ms",
+            "extra": "current"
+          },
+          {
+            "name": "compile@groovy-3",
+            "value": 618.5433333333333,
+            "range": "±210.98",
+            "unit": "ms",
+            "extra": "3.0.25"
+          },
+          {
+            "name": "compile@groovy-4",
+            "value": 570.23,
+            "range": "±149.77",
+            "unit": "ms",
+            "extra": "4.0.32"
+          },
+          {
+            "name": "compile@groovy-5",
+            "value": 476.57666666666665,
+            "range": "±23.87",
             "unit": "ms",
             "extra": "5.0.6"
           }
