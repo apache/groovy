1f91df683e6dbf1f235a6a19f77d28434d550fdd
Paul King <paulk@asert.com.au>
2026-06-27 14:41:53 +1000

    GROOVY-12107: reject non-hex characters in decodeHex (cont'd)

--- src/main/java/org/codehaus/groovy/runtime/EncodingGroovyMethods.java
+++ src/main/java/org/codehaus/groovy/runtime/EncodingGroovyMethods.java
@@ -382,7 +382,7 @@ public class EncodingGroovyMethods {
         if (c >= '0' && c <= '9') return c - '0';
         if (c >= 'A' && c <= 'F') return c - 'A' + 10;
         if (c >= 'a' && c <= 'f') return c - 'a' + 10;
-        throw new NumberFormatException("illegal hexadecimal character " + c + " in hex string");
+        throw new NumberFormatException("illegal hexadecimal character '" + c + "' in hex string");
     }
 
     /**
