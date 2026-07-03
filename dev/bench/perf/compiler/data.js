ebe666e6a20b50d52e65f0665639ef363e8f0a54
github-action-benchmark <github@users.noreply.github.com>
2026-07-03 07:58:52 +0000

    add Compiler Performance (customSmallerIsBetter) benchmark result for 80d37ed6cc39b365c4f326c028363db39aaa03eb

--- dev/bench/perf/compiler/data.js
+++ dev/bench/perf/compiler/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782979199874,
+  "lastUpdate": 1783065532512,
   "repoUrl": "https://github.com/apache/groovy",
   "entries": {
     "Compiler Performance": [
@@ -2498,6 +2498,56 @@ window.BENCHMARK_DATA = {
             "name": "compile@groovy-5",
             "value": 493.24,
             "range": "±20.92",
+            "unit": "ms",
+            "extra": "5.0.6"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Paul King",
+            "username": "paulk-asert",
+            "email": "paulk@asert.com.au"
+          },
+          "committer": {
+            "name": "Paul King",
+            "username": "paulk-asert",
+            "email": "paulk@asert.com.au"
+          },
+          "id": "80d37ed6cc39b365c4f326c028363db39aaa03eb",
+          "message": "GROOVY-12125: deleteDir() should treat symbolic links as leaves (cleanup and hardening)",
+          "timestamp": "2026-07-02T21:58:37Z",
+          "url": "https://github.com/apache/groovy/commit/80d37ed6cc39b365c4f326c028363db39aaa03eb"
+        },
+        "date": 1783065530540,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "compile@current",
+            "value": 552.1099999999999,
+            "range": "±22.25",
+            "unit": "ms",
+            "extra": "current"
+          },
+          {
+            "name": "compile@groovy-3",
+            "value": 639.2966666666666,
+            "range": "±213.59",
+            "unit": "ms",
+            "extra": "3.0.25"
+          },
+          {
+            "name": "compile@groovy-4",
+            "value": 597.8900000000001,
+            "range": "±146.45",
+            "unit": "ms",
+            "extra": "4.0.32"
+          },
+          {
+            "name": "compile@groovy-5",
+            "value": 482.91,
+            "range": "±25.86",
             "unit": "ms",
             "extra": "5.0.6"
           }
